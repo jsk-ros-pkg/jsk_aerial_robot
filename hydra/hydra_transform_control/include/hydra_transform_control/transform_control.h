e60bc639f52702497ff4397f213cec70bd20c694
chou <bakui@bakui-ThinkPad-T430.(none)>
2015-08-24 14:44:47 +0900

    add lqi and hamilton method

--- hydra/hydra_transform_control/include/hydra_transform_control/transform_control.h
+++ hydra/hydra_transform_control/include/hydra_transform_control/transform_control.h
@@ -15,17 +15,19 @@
 #include <Eigen/LU>
 #include <Eigen/Dense>
 #include <Eigen/Geometry> 
+#include <Eigen/Eigenvalues>
 
 #include <hydra_transform_control/HydraParam.h>
 #include <std_msgs/UInt16.h>
 
 #include <std_msgs/Float32.h>
 #include <boost/thread/mutex.hpp>
+#include <boost/thread.hpp>
 
 class ElementModel{
  public:
   ElementModel(){}
- ElementModel(ros::NodeHandle nh, ros::NodeHandle nh_private, float weight, Eigen::Matrix<double, 3, 3> inertia, Eigen::Matrix<double, 3, 1> origin_offset = Eigen::Vector3d::Zero()) : nh_(nh), nhp_(nh_private)
+ ElementModel(ros::NodeHandle nh, ros::NodeHandle nh_private, float weight, Eigen::Matrix3d inertia, Eigen::Vector3d origin_offset = Eigen::Vector3d::Zero()) : nh_(nh), nhp_(nh_private)
     {
       weight_ = weight;
       inertia_ = inertia;
@@ -34,17 +36,17 @@ class ElementModel{
   ~ElementModel(){}
 
   inline float getWeight(){return weight_;}
-  inline Eigen::Matrix<double, 3, 3> getInertia(){return inertia_;}
-  inline  void setOrigin(Eigen::Matrix<double, 3, 1> origin){origin_ = origin;}
-  inline Eigen::Matrix<double, 3, 1> getOffset(){return origin_offset_;}
-  inline Eigen::Matrix<double, 3, 1> getOrigin(){return origin_;}
+  inline Eigen::Matrix3d getInertia(){return inertia_;}
+  inline  void setOrigin(Eigen::Vector3d origin){origin_ = origin;}
+  inline Eigen::Vector3d getOffset(){return origin_offset_;}
+  inline Eigen::Vector3d getOrigin(){return origin_;}
 
  protected:
   ros::NodeHandle nh_, nhp_;
   float weight_;
-  Eigen::Matrix<double, 3, 3> inertia_;
-  Eigen::Matrix<double, 3, 1> origin_;
-  Eigen::Matrix<double, 3, 1> origin_offset_;
+  Eigen::Matrix3d inertia_;
+  Eigen::Vector3d origin_;
+  Eigen::Vector3d origin_offset_;
 };
 
 
@@ -60,27 +62,48 @@ class TransformController{
   double getPropellerDiameter(); 
   int getLinkNum();
 
-  void getLinksOriginFromCog(std::vector<Eigen::Matrix<double, 3, 1> > &links_origin);
 
-  std::vector<Eigen::Matrix<double, 3, 1> > links_origin_from_cog_; //temporary
+  std::vector<Eigen::Vector3d> links_origin_from_cog_; 
+  std::vector<Eigen::Vector3d> getLinksOriginFromCog()
+    {
+      boost::lock_guard<boost::mutex> lock(origins_mutex_);
+      return links_origin_from_cog_;
+    }
+  void setLinksOriginFromCog(std::vector<Eigen::Vector3d> links_origin_from_cog)
+    {
+      boost::lock_guard<boost::mutex> lock(origins_mutex_);
+      links_origin_from_cog_ = links_origin_from_cog;
+    }
 
-  Eigen::Matrix<double, 3, 3> getRotateMatrix()
+  Eigen::Matrix3d getPrincipalInertia()
+    {
+      boost::lock_guard<boost::mutex> lock(inertia_mutex_);
+      return links_principal_inertia_;
+    }
+  void setPrincipalInertia(Eigen::Matrix3d principal_inertia)
+    {
+      boost::lock_guard<boost::mutex> lock(inertia_mutex_);
+      links_principal_inertia_ = principal_inertia;
+    }
+
+
+  Eigen::Matrix3d getRotateMatrix()
     {
       boost::lock_guard<boost::mutex> lock(rm_mutex_);
       return rotate_matrix_;
     }
-  void setRotateMatrix(Eigen::Matrix<double, 3, 3> rotate_matrix)
+  void setRotateMatrix(Eigen::Matrix3d rotate_matrix)
     {
       boost::lock_guard<boost::mutex> lock(rm_mutex_);
       rotate_matrix_ = rotate_matrix;
     }
 
-  Eigen::Matrix<double, 3, 1> getCog()
+  Eigen::Vector3d getCog()
     {
       boost::lock_guard<boost::mutex> lock(cog_mutex_);
       return cog_;
     }
-  void setCog(Eigen::Matrix<double, 3, 1> cog)
+  void setCog(Eigen::Vector3d cog)
     {
       boost::lock_guard<boost::mutex> lock(cog_mutex_);
       cog_ = cog;
@@ -94,7 +117,7 @@ class TransformController{
   ros::Publisher principal_axis_pub_;
   ros::Publisher cog_rotate_pub_; //for position control => to mocap
 
-  boost::mutex rm_mutex_, cog_mutex_;
+  boost::mutex rm_mutex_, cog_mutex_, origins_mutex_, inertia_mutex_;
 
   tf::TransformListener tf_;
   double control_rate_;
@@ -105,6 +128,7 @@ class TransformController{
   ros::Time system_tf_time_;
 
   bool debug_log_;
+  bool debug2_log_;
 
   //base model config
 
@@ -112,6 +136,10 @@ class TransformController{
   std::vector<int> propeller_direction_;
   std::vector<int> propeller_order_;
 
+  //dynamics config
+  double m_f_rate_; //moment / force rate
+  double f_pwm_rate_; //force / pwm rate
+  double f_pwm_offset_; //force / pwm offset
 
   double link_length_;
   double link_base_rod_length_; //the length of the lik base rod
@@ -137,15 +165,12 @@ class TransformController{
   std::string root_link_name_;
   std::vector<std::string> links_name_;
 
+  Eigen::Vector3d cog_;
 
-  Eigen::Matrix<double, 3, 1> cog_;
+  Eigen::Matrix3d links_inertia_, links_principal_inertia_;
 
-
-  Eigen::Matrix<double, 3, 3> links_inertia_, links_principal_inertia_, init_links_principal_inertia_;
-
-
-  Eigen::Matrix<double, 3, 3> rotate_matrix_;
-  Eigen::Matrix<double, 3, 3> cog_matrix_;
+  Eigen::Matrix3d rotate_matrix_;
+  Eigen::Matrix3d cog_matrix_;
   double rotate_angle_;
 
 
@@ -159,8 +184,37 @@ class TransformController{
   void cogCoordPublish();
   void initParam();
 
+  
+
   void param2contoller();
 
+
+  //lqi
+  boost::thread lqi_thread_;
+  bool lqi_flag_;
+  double lqi_thread_rate_;
+  void lqi();
+  bool hamiltonMatrixSolver();
+  Eigen::MatrixXd A_;
+  Eigen::MatrixXd B_;
+  Eigen::MatrixXd C_;
+  Eigen::MatrixXd A_aug_;
+  Eigen::MatrixXd B_aug_;
+  Eigen::MatrixXd C_aug_;
+
+  Eigen::MatrixXd Q_;
+  Eigen::MatrixXd R_;
+
+  //Q
+  double q_roll_,q_roll_d_,q_pitch_,q_pitch_d_,q_yaw_,q_yaw_d_,q_z_,q_z_d_;
+  double q_roll_i_,q_pitch_i_,q_yaw_i_,q_z_i_;
+  
+  //R
+  std::vector<double> r_;
+
+
+  //shift
+  double alfa_;
 };
 
 
--- hydra/hydra_transform_control/src/transform_control.cpp
+++ hydra/hydra_transform_control/src/transform_control.cpp
@@ -21,11 +21,68 @@ TransformController::TransformController(ros::NodeHandle nh, ros::NodeHandle nh_
   links_origin_from_cog_.resize(link_num_);
   rotate_matrix_ = Eigen::Matrix3d::Identity();
 
-  for(int i = 0; i < link_num_; i++)
-    {
+  //for(int i = 0; i < link_num_; i++)
+  //link_principal_inertia_ << link_i_xx_, 0, 0, 0, link_i_yy_, 0, 0, 0, link_i_zz_;
 
-      //link_principal_inertia_ << link_i_xx_, 0, 0, 0, link_i_yy_, 0, 0, 0, link_i_zz_;
-    }
+
+  //lqi
+  lqi_flag_ = false;
+  //A
+  A_ = Eigen::MatrixXd::Zero(8,8);
+  A_(0,1) = 1;
+  A_(2,3) = 1;
+  A_(4,5) = 1;
+  A_(6,7) = 1;
+
+  //A
+  B_ = Eigen::MatrixXd::Zero(8,4); //4 is the link number, should change!!
+
+
+  //C
+  C_ = Eigen::MatrixXd::Zero(4,8);
+  C_(0,0) = 1;
+  C_(1,2) = 1;
+  C_(2,4) = 1;
+  C_(3,6) = 1;
+
+
+  //A_aug
+  A_aug_ = Eigen::MatrixXd::Zero(12,12);
+  A_aug_.block<8,8>(0,0) = A_;
+  A_aug_.block<4,8>(8,0) = -C_;
+
+
+  //A_aug
+  C_aug_ = Eigen::MatrixXd::Zero(4,12);
+  C_aug_.block<4,8>(0,0) = C_;
+
+
+
+  //Q
+  Eigen::VectorXd  q_diagonal(12);
+  q_diagonal << q_roll_,q_roll_d_,q_pitch_,q_pitch_d_,q_yaw_,q_yaw_d_,q_z_,q_z_d_, q_roll_i_,q_pitch_i_,q_yaw_i_,q_z_i_;
+
+
+
+  Q_ = q_diagonal.asDiagonal();
+
+
+
+  //R
+  R_ = Eigen::Matrix4d(Eigen::Vector4d(r_[0], r_[1],r_[2], r_[3]).asDiagonal()); // bad, 4 should be link num
+
+
+  std::cout << "A:"  << std::endl << A_ << std::endl;
+  std::cout << "C:"  << std::endl << C_ << std::endl;
+
+  std::cout << "A_aug:"  << std::endl << A_aug_ << std::endl;
+  std::cout << "C_aug:"  << std::endl << C_aug_ << std::endl;
+
+
+  std::cout << "Q:"  << std::endl << Q_ << std::endl;
+  std::cout << "R:"  << std::endl << R_ << std::endl;
+
+
 
   if(callback_flag)
     {
@@ -36,12 +93,17 @@ TransformController::TransformController(ros::NodeHandle nh, ros::NodeHandle nh_
       tf_pub_timer_ = nh_private_.createTimer(ros::Duration(1.0 / tf_pub_rate_),
                                               &TransformController::tfPubFunc, this);
       
+      
+      lqi_thread_ = boost::thread(boost::bind(&TransformController::lqi, this));
     }
 
 
 }
 TransformController::~TransformController()
 {
+  lqi_thread_.interrupt();
+  lqi_thread_.join();
+
 }
 
 void TransformController::initParam()
@@ -50,6 +112,7 @@ void TransformController::initParam()
   nh_private_.param("tf_pub_rate", tf_pub_rate_, 60.0);
 
   nh_private_.param("debug_log", debug_log_, false); 
+  nh_private_.param("debug2_log", debug2_log_, false); 
 
   nh_private_.param("link_num", link_num_, 4);
   links_name_.resize(link_num_); 
@@ -60,6 +123,7 @@ void TransformController::initParam()
   link_joint_model_.resize(link_num_ -1);
   controller_model_.resize(2); //controller1 + controller2
   all_mass_ = 0;
+
 
   nh_private_.param("root_link_name", root_link_name_, std::string("/link3"));
   printf(" root_link_name_ is %s\n", root_link_name_.c_str());
@@ -107,6 +171,10 @@ void TransformController::initParam()
     controller2_mass_ = 0.093;
   printf("controller2_mass_ is %.3f\n", controller2_mass_);
 
+  //lqi
+  r_.resize(link_num_);
+
+
 
   Eigen::Matrix3d zero_inertia = Eigen::Matrix3d::Zero(3,3);
   for(int i = 0; i < link_num_; i++)
@@ -125,7 +193,7 @@ void TransformController::initParam()
       /// 1. link base model
       float link_base_model_weight = link_base_rod_mass_[i] + link_base_ring_mass_ + link_base_two_end_mass_ + link_base_center_mass_;
 
-      Eigen::Matrix<double, 3, 3> link_base_model_inertia;
+      Eigen::Matrix3d link_base_model_inertia;
       //ring model + two_end model + rod model
       link_base_model_inertia << 
         link_base_ring_mass_ / 2 * link_base_two_end_offset_ * link_base_two_end_offset_, 0, 0, 
@@ -140,7 +208,7 @@ void TransformController::initParam()
       /// 2. link joint model
       if(i+1 < link_num_)
         {
-          Eigen::Matrix<double, 3, 1> offset_;
+          Eigen::Vector3d offset_;
           offset_ << link_joint_offset_, 0, 0;
           ElementModel link_joint_model(nh_, nh_private_, link_joint_mass_, zero_inertia, offset_);
           link_joint_model_[i] = link_joint_model;
@@ -151,10 +219,17 @@ void TransformController::initParam()
       nh_private_.param(std::string("propeller") + ss.str() + std::string("_direction"), propeller_direction_[i], 1);
       nh_private_.param(std::string("propeller") + ss.str() + std::string("_order"), propeller_order_[i], i);
 
+
+      //lqi
+      if (!nh_private_.getParam (std::string("r") + ss.str(), r_[i]))
+        r_[i] = 1.0;
+      printf("R: r_%d is %.3f\n", i, r_[i]);
     }
 
+
+
   /// 3. controller
-  Eigen::Matrix<double, 3, 1> offset_;
+  Eigen::Vector3d offset_;
   offset_ << controller1_offset_, 0, 0;
   ElementModel controller1_model(nh_, nh_private_, controller1_mass_, zero_inertia, offset_);
   all_mass_ += controller1_model.getWeight();
@@ -167,11 +242,63 @@ void TransformController::initParam()
 
   ROS_INFO("Mass :%f", all_mass_);
 
-  /*
-  nh_private_.param("multilink_i_yy", multilink_i_yy_, 0.0807787);
-  nh_private_.param("multilink_i_xx", multilink_i_xx_, 0.0809074);
-  nh_private_.param("multilink_i_zz", multilink_i_zz_, 0.161686);
-  */
+  //dynamics
+  if (!nh_private_.getParam ("m_f_rate", m_f_rate_))
+    m_f_rate_ = 0.016837; //the sgn is right?, should be nagative
+  printf("m_f_rate_ is %.3f\n",m_f_rate_);
+  if (!nh_private_.getParam ("f_pwm_rate", f_pwm_rate_))
+    f_pwm_rate_ = 0.3029; // with the pwm percentage: x / 1800 * 100
+  printf("f_pwm_rate_ is %.3f\n",f_pwm_rate_);
+  if (!nh_private_.getParam ("f_pwm_offset", f_pwm_offset_))
+    f_pwm_offset_ = -21.196;  // with the pwm percentage: x / 1800 * 100
+  printf("f_pwm_offset_ is %.3f\n",f_pwm_offset_);
+
+  //lqi
+  if (!nh_private_.getParam ("lqi_thread_rate", lqi_thread_rate_))
+    lqi_thread_rate_ = 10.0;
+  printf("lqi_thread_rate_ is %.3f\n", lqi_thread_rate_);
+  if (!nh_private_.getParam ("q_roll", q_roll_))
+    q_roll_ = 1.0;
+  printf("Q: q_roll_ is %.3f\n", q_roll_);
+  if (!nh_private_.getParam ("q_roll_d", q_roll_d_))
+    q_roll_d_ = 1.0;
+  printf("Q: q_roll_d_ is %.3f\n", q_roll_d_);
+  if (!nh_private_.getParam ("q_pitch", q_pitch_))
+    q_pitch_ = 1.0;
+  printf("Q: q_pitch_ is %.3f\n", q_pitch_);
+  if (!nh_private_.getParam ("q_pitch_d", q_pitch_d_))
+    q_pitch_d_ = 1.0;
+  printf("Q: q_pitch_d_ is %.3f\n", q_pitch_d_);
+  if (!nh_private_.getParam ("q_yaw", q_yaw_))
+    q_yaw_ = 1.0;
+  printf("Q: q_yaw_ is %.3f\n", q_yaw_);
+  if (!nh_private_.getParam ("q_yaw_d", q_yaw_d_))
+    q_yaw_d_ = 1.0;
+  printf("Q: q_yaw_d_ is %.3f\n", q_yaw_d_);
+  if (!nh_private_.getParam ("q_z", q_z_))
+    q_z_ = 1.0;
+  printf("Q: q_z_ is %.3f\n", q_z_);
+  if (!nh_private_.getParam ("q_z_d", q_z_d_))
+    q_z_d_ = 1.0;
+  printf("Q: q_z_d_ is %.3f\n", q_z_d_);
+
+  if (!nh_private_.getParam ("q_roll_i", q_roll_i_))
+    q_roll_i_ = 1.0;
+  printf("Q: q_roll_i_ is %.3f\n", q_roll_i_);
+  if (!nh_private_.getParam ("q_pitch_i", q_pitch_i_))
+    q_pitch_i_ = 1.0;
+  printf("Q: q_pitch_i_ is %.3f\n", q_pitch_i_);
+  if (!nh_private_.getParam ("q_yaw_i", q_yaw_i_))
+    q_yaw_i_ = 1.0;
+  printf("Q: q_yaw_i_ is %.3f\n", q_yaw_i_);
+  if (!nh_private_.getParam ("q_z_i", q_z_i_))
+    q_z_i_ = 1.0;
+  printf("Q: q_z_i_ is %.3f\n", q_z_i_);
+
+  if (!nh_private_.getParam ("alfa", alfa_))
+    alfa_ = 0.0;
+  printf("alfa_ is %.3f\n", alfa_);
+
 
 }
 
@@ -207,16 +334,17 @@ void TransformController::controlFunc(const ros::TimerEvent & e)
       principalInertiaComputation(transforms);
       visualization();
 
+      if(!lqi_flag_) lqi_flag_ = true;
     }
 
 }
 
 void TransformController::cogComputation(std::vector<tf::StampedTransform> transforms)
 {
-  Eigen::Matrix<double, 3, 1> cog_n  = Eigen::Vector3d::Zero();
+  Eigen::Vector3d cog_n  = Eigen::Vector3d::Zero();
   for(int i = 0; i < link_num_; i++)
     {
-      Eigen::Matrix<double, 3, 1> link_origin;
+      Eigen::Vector3d link_origin;
       tf::vectorTFToEigen(transforms[i].getOrigin(), link_origin);
 
       Eigen::Quaterniond q;
@@ -230,25 +358,25 @@ void TransformController::cogComputation(std::vector<tf::StampedTransform> trans
         {
           //2. link joint model
           // 座標変換
-          Eigen::Matrix<double, 3, 1> joint_origin = link_rotate * link_joint_model_[i -1].getOffset()  + link_origin;
+          Eigen::Vector3d joint_origin = link_rotate * link_joint_model_[i -1].getOffset()  + link_origin;
           cog_n += joint_origin * link_joint_model_[i - 1].getWeight();
         }
 
       //3. controlers
       if(i == 0)
         {// controller2
-          Eigen::Matrix<double, 3, 1> controller_origin = link_rotate * controller_model_[1].getOffset()  + link_origin;
+          Eigen::Vector3d controller_origin = link_rotate * controller_model_[1].getOffset()  + link_origin;
           cog_n += controller_origin * controller_model_[1].getWeight(); 
         }
       if(i == 1)
         {// controller1
-          Eigen::Matrix<double, 3, 1> controller_origin = link_rotate * controller_model_[0].getOffset()  + link_origin;
+          Eigen::Vector3d controller_origin = link_rotate * controller_model_[0].getOffset()  + link_origin;
           //std::cout << "controller1 :\n" << controller_origin << std::endl;
           cog_n += controller_origin * controller_model_[0].getWeight(); 
         }
     }
 
-  Eigen::Matrix<double, 3, 1> cog = cog_n / all_mass_;
+  Eigen::Vector3d cog = cog_n / all_mass_;
   setCog(cog);
 }
 
@@ -257,7 +385,7 @@ void TransformController::principalInertiaComputation(std::vector<tf::StampedTra
   static bool init_flag = false;
 
   Eigen::Matrix3d links_inertia = Eigen::Matrix3d::Zero(3,3);
-  std::vector<Eigen::Matrix<double, 3, 1> > links_origin_from_cog;
+  std::vector<Eigen::Vector3d > links_origin_from_cog;
   links_origin_from_cog.resize(link_num_);
   for(int i = 0; i < link_num_; i ++)
     {
@@ -367,15 +495,19 @@ void TransformController::principalInertiaComputation(std::vector<tf::StampedTra
 
     }
   links_inertia_ = links_inertia;
-  // if(debug_log_)
-  //   std::cout << " inertia :\n" << links_inertia_ << std::endl;
+   // if(debug_log_)
+   //   std::cout << " inertia :\n" << links_inertia_ << std::endl;
 
 
-
-  
   //eigen solver
+  ros::Time start_time = ros::Time::now();
   Eigen::SelfAdjointEigenSolver<Eigen::Matrix3d> eig(links_inertia_);
+  //printf("inertia eigen time is: %f\n", ros::Time::now().toSec() - start_time.toSec());
+  //std::cout << "The eigenvalues of inertia is:\n"  << eig.eigenvalues() << std::endl;
+  //std::cout << "The diagonal is:\n"  << eig.eigenvalues().asDiagonal() << std::endl;
+
   Eigen::Matrix3d links_principal_inertia = eig.eigenvalues().asDiagonal();
+
   Eigen::Matrix3d rotate_matrix = eig.eigenvectors();
 
   //std::cout << "rotate_matrix :\n" << rotate_matrix << std::endl;
@@ -396,7 +528,7 @@ void TransformController::principalInertiaComputation(std::vector<tf::StampedTra
         {
           ROS_ERROR("start");
           setRotateMatrix(rotate_matrix);
-          links_principal_inertia_ = links_principal_inertia;
+          setPrincipalInertia(links_principal_inertia);
 
           init_flag = true;
         }
@@ -458,7 +590,7 @@ void TransformController::principalInertiaComputation(std::vector<tf::StampedTra
 
 
           setRotateMatrix(rotate_matrix_candidates[no]);
-          links_principal_inertia_ = links_principal_inertia_candidates[no];
+          setPrincipalInertia(links_principal_inertia_candidates[no]);
 
         }
     }
@@ -467,25 +599,27 @@ void TransformController::principalInertiaComputation(std::vector<tf::StampedTra
       setRotateMatrix(rotate_matrix);
     }
 
-  if(debug_log_)
-    std::cout << "pricipal inertia :\n" << links_principal_inertia_ << std::endl;
+
+   if(debug2_log_)
+     std::cout << "pricipal inertia :\n" << getPrincipalInertia() << std::endl;
 
 
   //rotate the link origins from cog
   cog_matrix_ = getRotateMatrix().transpose();
   //std::cout << "cog matrix :\n" << cog_matrix_ << std::endl;
+
+  std::vector<Eigen::Vector3d > links_origin_from_principal_cog;
+  links_origin_from_principal_cog.resize(link_num_);
   for(int i = 0; i < link_num_; i ++)
     {
-      links_origin_from_cog_[i] = cog_matrix_ * links_origin_from_cog[i];
-      if(debug_log_)
-        {
-          
-          std::cout << "link" << i + 1 <<"origin :\n" << links_origin_from_cog_[i] << std::endl;
-        }
+      links_origin_from_principal_cog[i] = cog_matrix_ * links_origin_from_cog[i];
+       if(debug2_log_)
+         std::cout << "link" << i + 1 <<"origin :\n" << links_origin_from_principal_cog[i] << std::endl;
+
     }
+  setLinksOriginFromCog(links_origin_from_principal_cog);
 
-
-  Eigen::Matrix<double, 3, 3> rotate_matrix_tmp = getRotateMatrix();
+  Eigen::Matrix3d rotate_matrix_tmp = getRotateMatrix();
   rotate_angle_ = atan2(rotate_matrix_tmp(1,0), rotate_matrix_tmp(0,0));
   std_msgs::Float32 rotate_msg;
   rotate_msg.data = rotate_angle_;
@@ -501,8 +635,8 @@ void TransformController::cogCoordPublish()
   tf::Transform transform;
 
 
-  Eigen::Matrix<double, 3, 1> cog = getCog();
-  Eigen::Matrix<double, 3, 3> rotate_matrix = getRotateMatrix();
+  Eigen::Vector3d cog = getCog();
+  Eigen::Matrix3d rotate_matrix = getRotateMatrix();
   transform.setOrigin( tf::Vector3(cog(0), cog(1), cog(2)));
   Eigen::Quaterniond q_eigen(rotate_matrix);
 
@@ -515,8 +649,8 @@ void TransformController::cogCoordPublish()
 
 void TransformController::visualization()
 {
-  Eigen::Matrix<double, 3, 1> cog = getCog();
-  Eigen::Matrix<double, 3, 3> rotate_matrix = getRotateMatrix();
+  Eigen::Vector3d cog = getCog();
+  Eigen::Matrix3d rotate_matrix = getRotateMatrix();
 
   visualization_msgs::MarkerArray cog_inertia;
   visualization_msgs::Marker cog_point;
@@ -612,3 +746,169 @@ void TransformController::param2contoller()
 
 }
 
+
+void TransformController::lqi()
+{
+  // x = [roll, roll_d, pitch, pitch_d, yaw, yaw_d, z, z_d]
+  ros::Rate loop_rate(lqi_thread_rate_);
+  static int i = 0;
+  static int cnt = 0;
+
+  while(ros::ok())
+    {
+      if(lqi_flag_)
+        {
+          std::vector<Eigen::Vector3d> links_origin_from_cog = getLinksOriginFromCog();
+          Eigen::Matrix3d links_principal_inertia = getPrincipalInertia();
+          Eigen::Vector4d p_x, p_y, p_c, p_m; 
+
+          //std::cout << "inertia :\n" << links_principal_inertia << std::endl;
+
+          for(; i < link_num_; i++)
+            {
+              int order = propeller_order_[i];
+              p_y(order) = links_origin_from_cog[i](1); 
+              p_x(order) = -links_origin_from_cog[i](0);
+              p_c(order) = propeller_direction_[i] * m_f_rate_ / links_principal_inertia(2,2);
+              p_m(order) = 1 / all_mass_;
+
+              //std::cout << "link" << i + 1 <<"origin :\n" << links_origin_from_cog[i] << std::endl;
+            }
+          i = 0;
+
+          //check the controllability!!
+          if(debug_log_)
+            {
+              std::cout << "x norm :\n" << p_x.norm() << std::endl;
+              std::cout << "y norm :\n" << p_y.norm() << std::endl;
+            }
+
+          if(p_y.norm() < 0.1 || p_x.norm() < 0.1) //[m]
+            {
+              ROS_ERROR("(singular pose, can not resolve the lqi control problem");
+              loop_rate.sleep();
+              continue;
+            }
+
+          B_.row(1) = p_y / links_principal_inertia(0,0);
+          B_.row(3) = p_x / links_principal_inertia(1,1);
+          B_.row(5) = p_c;
+          B_.row(7) = p_m;
+
+          if(debug_log_)
+            std::cout << "B:"  << std::endl << B_ << std::endl;
+
+          B_aug_ = Eigen::MatrixXd::Zero(12, 4);
+          B_aug_.block<8,4>(0,0) = B_;
+          //std::cout << "B_aug:"  << std::endl << B_aug_<< std::endl;
+     
+          if(!hamiltonMatrixSolver()){ continue;}
+        }
+
+      loop_rate.sleep();
+    }
+}
+
+bool TransformController::hamiltonMatrixSolver()
+{
+  //for the R which is  diagonal matrix. should be changed to link_num
+  Eigen::MatrixXd R_inv = Eigen::Matrix4d(Eigen::Vector4d(1 / r_[0], 1 / r_[1], 1 / r_[2], 1 / r_[3]).asDiagonal());
+
+
+  // hamilton matrix
+
+  Eigen::MatrixXcd H = Eigen::MatrixXcd::Zero(24,24); //all right?
+  H.block<12,12>(0,0) = A_aug_.cast<std::complex<double> >();
+
+  H.block<12,12>(12,0) = -(Q_.cast<std::complex<double> >());
+  H.block<12,12>(0,12) = - (B_aug_ * R_inv * B_aug_.transpose()).cast<std::complex<double> >();
+  H.block<12,12>(12,12) = - (A_aug_.transpose()).cast<std::complex<double> >();
+
+  //std::cout << " H  is:" << std::endl << H << std::endl;
+
+  //eigen shift
+  //Eigen::MatrixXcd H_tmp = H; //all right?
+  //H = H_tmp + Eigen::MatrixXcd::Identity(24,24) * alfa_;
+
+
+  //std::cout << " R inv  is:" << std::endl << - B_aug_ * R_inv * B_aug_.transpose() << std::endl;
+
+
+  
+
+  //eigen solving
+  ros::Time start_time = ros::Time::now();
+  Eigen::ComplexEigenSolver<Eigen::MatrixXcd> ces;
+  ces.compute(H);
+
+  if(debug_log_)
+    ROS_INFO("h eigen time is: %f\n", ros::Time::now().toSec() - start_time.toSec());
+  //std::cout << "The eigenvalues of H are:" << std::endl << ces.eigenvalues() << std::endl;
+  //std::cout << "The eigenvalues vector of H are:" << std::endl << es.eigenvectors() << std::endl;
+
+
+
+  Eigen::MatrixXcd phy = Eigen::MatrixXcd::Zero(24,12);
+  int j = 0;
+
+
+  for(int i = 0; i < 24; i++)
+    {
+      if(ces.eigenvalues()[i].real() < 0)
+        {
+          if(j > 11) 
+            {
+              ROS_ERROR("nagativa sigular amount is larger");
+              return false;
+            }
+
+          phy.col(j) = ces.eigenvectors().col(i);
+          j++;
+        }
+
+    }
+
+  if(j != 12)
+    {
+      ROS_ERROR("nagativa sigular value amount is not enough");
+      return false;
+    }
+
+  Eigen::MatrixXcd f = phy.block<12,12>(0,0);
+  Eigen::MatrixXcd g = phy.block<12,12>(12,0);
+
+
+  //std::cout << "f:" << std::endl << f << std::endl;
+  //std::cout << "g:" << std::endl << g << std::endl;
+
+
+  start_time = ros::Time::now();
+  Eigen::MatrixXcd f_inv  = f.inverse();
+  //std::cout << "f inv is:" << std::endl << f_inv << std::endl;
+  if(debug_log_)
+    ROS_INFO("f inverse: %f\n", ros::Time::now().toSec() - start_time.toSec());
+
+
+  Eigen::MatrixXcd P = g * f_inv;
+
+  //std::cout << "P is:" << std::endl << P << std::endl;
+
+  //K
+  Eigen::MatrixXd K = -R_inv * B_aug_.transpose() * P.real();
+
+  if(debug_log_)
+    std::cout << "K is:" << std::endl << K << std::endl;
+
+
+  //check the eigen of new A
+   Eigen::MatrixXd A_dash = Eigen::MatrixXd::Zero(12, 12);
+   A_dash = A_aug_ + B_aug_ * K;
+  // start_time = ros::Time::now();
+   Eigen::EigenSolver<Eigen::MatrixXd> esa(A_dash);
+  if(debug_log_)
+    std::cout << "The eigenvalues of A_hash are:" << std::endl << esa.eigenvalues() << std::endl;
+  // ROS_INFO("A dash: %f\n", ros::Time::now().toSec() - start_time.toSec());
+
+  return true;
+
+}
