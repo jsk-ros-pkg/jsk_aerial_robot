0e0c36d6a7d4c88f4f0ce1f0ab1a1299d502f75c
Tongtybj <chou@jsk.imi.i.u-tokyo.ac.jp>
2016-07-25 10:05:01 +0900

    hoge temp

--- aerial_robot_base/include/aerial_robot_base/sensor/range.h
+++ aerial_robot_base/include/aerial_robot_base/sensor/range.h
@@ -14,13 +14,13 @@ namespace sensor_plugin
   public:
       void initialize(ros::NodeHandle nh, ros::NodeHandle nhp, BasicEstimator* estimator)
       {
-        nh_ = ros::NodeHandle(nh, "leddar_one");
-        nhp_ = ros::NodeHandle(nhp, "leddar_one");
+        nh_ = ros::NodeHandle(nh, "range");
+        nhp_ = ros::NodeHandle(nhp, "range");
         estimator_ = estimator;
         baseRosParamInit();
         rosParamInit();
 
-        leddar_one_sub_ = nh_.subscribe<sensor_msgs::Range>("leddar_one", 1, &LeddarOne::rangeCallback, this, ros::TransportHints().tcpNoDelay());
+        leddar_one_sub_ = nh_.subscribe<sensor_msgs::Range>(range_sensor_sub_name_, 1, &LeddarOne::rangeCallback, this, ros::TransportHints().tcpNoDelay());
 
         raw_pos_z_ = 0;
         pos_z_ = 0;
@@ -36,6 +36,7 @@ namespace sensor_plugin
     ros::Subscriber leddar_one_sub_;
     ros::Time leddar_one_stamp_;
 
+    std::string range_sensor_sub_name_;
     double range_noise_sigma_;
 
     float raw_pos_z_;
@@ -45,41 +46,15 @@ namespace sensor_plugin
 
     bool sensor_fusion_flag_;
 
-void rangeCallback(const px_comm::LeddarOneConstPtr & optical_flow_msg)
+void rangeCallback(const sensor_msgs::RangeConstPtr & range_msg)
 {
-      
+
       static int init_flag = true;
       static float prev_raw_pos_z;
       static bool stop_flag = true;
       static double previous_secs;
-      static double special_increment = 0;
-      double current_secs = optical_flow_msg->header.stamp.toSec();
+      double current_secs = range_msg->header.stamp.toSec();
 
-      //**** Global Sensor Fusion Flag Check
-      if(!estimator_->getSensorFusionFlag())  
-        {
-          //this is for the repeat mode
-          init_flag = true;
-          
-          //this is for the halt mode
-          if(estimate_mode_ & (1 << EGOMOTION_ESTIMATION_MODE))
-            {
-              for(int i = 0; i < estimator_->getFuserEgomotionNo(); i++)
-                {
-                  estimator_->getFuserEgomotion(i)->setMeasureFlag(false);
-                  estimator_->getFuserEgomotion(i)->resetState();
-                }
-            }
-          if(estimate_mode_ & (1 << EXPERIMENT_MODE))
-            {
-              for(int i = 0; i < estimator_->getFuserExperimentNo(); i++)
-                {
-                  estimator_->getFuserExperiment(i)->setMeasureFlag(false);
-                  estimator_->getFuserExperiment(i)->resetState();
-                }
-            }
-          return;
-        }
 
       //**** Optical flow によるmeasuring flag のenable化
       if(init_flag)
@@ -121,7 +96,7 @@ void rangeCallback(const px_comm::LeddarOneConstPtr & optical_flow_msg)
         }
 
       //**** 高さ方向情報の更新
-      raw_pos_z_ = optical_flow_msg->ground_distance;
+      raw_pos_z_ = range_msg->ground_distance;
 
       //  start sensor fusion condition
       if(!sensor_fusion_flag_)
@@ -138,20 +113,6 @@ void rangeCallback(const px_comm::LeddarOneConstPtr & optical_flow_msg)
                   for(int i = 0; i < estimator_->getFuserEgomotionNo(); i++)
                     {
                       Eigen::Matrix<double, 1, 1> temp = Eigen::MatrixXd::Zero(1, 1); 
-                      if((estimator_->getFuserEgomotionId(i) & (1 << BasicEstimator::X_W)) || (estimator_->getFuserEgomotionId(i) & (1 << BasicEstimator::X_B)))
-                        {
-                          temp(0,0) = level_vel_noise_sigma_;
-                          estimator_->getFuserEgomotion(i)->setInitState(x_axis_direction_ * optical_flow_msg->flow_x /1000.0, 1);
-                          estimator_->getFuserEgomotion(i)->setMeasureSigma(temp);
-                          estimator_->getFuserEgomotion(i)->setMeasureFlag();
-                        }
-                      else if((estimator_->getFuserEgomotionId(i) & (1 << BasicEstimator::Y_W)) || (estimator_->getFuserEgomotionId(i) & (1 << BasicEstimator::Y_B)))
-                        {
-                          temp(0,0) = level_vel_noise_sigma_;
-                          estimator_->getFuserEgomotion(i)->setInitState(y_axis_direction_ * optical_flow_msg->flow_y /1000.0 ,1);
-                          estimator_->getFuserEgomotion(i)->setMeasureSigma(temp);
-                          estimator_->getFuserEgomotion(i)->setMeasureFlag();
-                        }
                       else if((estimator_->getFuserEgomotionId(i) & (1 << BasicEstimator::Z_W)))
                         {
                           /*
@@ -160,11 +121,11 @@ void rangeCallback(const px_comm::LeddarOneConstPtr & optical_flow_msg)
                             init_state2 = Eigen::MatrixXd::Zero(2, 1);
                           if(estimator_->getFuserEgomotionPluginName(i) == "kalman_filter/kf_pose_vel_acc_bias")
                             init_state2 = Eigen::MatrixXd::Zero(3, 1);
-                          init_state2(0,0) = optical_flow_msg->ground_distance;
+                          init_state2(0,0) = range_msg->ground_distance;
                           init_state2(1,0) = start_vel_;
                           estimator_->getFuserEgomotion(i)->setInitState(init_state2);
                           */
-                          estimator_->getFuserEgomotion(i)->setInitState(optical_flow_msg->ground_distance, 0);
+                          estimator_->getFuserEgomotion(i)->setInitState(range_msg->ground_distance, 0);
 
                           temp(0,0) = sonar_noise_sigma_;
                           estimator_->getFuserEgomotion(i)->setMeasureSigma(temp);
@@ -182,25 +143,25 @@ void rangeCallback(const px_comm::LeddarOneConstPtr & optical_flow_msg)
                         {
                           temp(0,0) = level_vel_noise_sigma_;
                           estimator_->getFuserExperiment(i)->setMeasureSigma(temp);
-                          estimator_->getFuserExperiment(i)->setInitState(x_axis_direction_ * optical_flow_msg->flow_x /1000.0, 1);
+                          estimator_->getFuserExperiment(i)->setInitState(x_axis_direction_ * range_msg->flow_x /1000.0, 1);
                           estimator_->getFuserExperiment(i)->setMeasureFlag();
                         }
                       else if((estimator_->getFuserExperimentId(i) & (1 << BasicEstimator::Y_W)) || (estimator_->getFuserExperimentId(i) & (1 << BasicEstimator::Y_B)))
                         {
                           temp(0,0) = level_vel_noise_sigma_;
                           estimator_->getFuserExperiment(i)->setMeasureSigma(temp);
-                          estimator_->getFuserExperiment(i)->setInitState(y_axis_direction_ * optical_flow_msg->flow_y /1000.0 ,1);
+                          estimator_->getFuserExperiment(i)->setInitState(y_axis_direction_ * range_msg->flow_y /1000.0 ,1);
                           estimator_->getFuserExperiment(i)->setMeasureFlag();
                         }
                       else if((estimator_->getFuserExperimentId(i) & (1 << BasicEstimator::Z_W)))
                         {
                           /*
                           Eigen::Matrix<double, 2, 1> init_state2 = Eigen::MatrixXd::Zero(2, 1);
-                          init_state2(0,0) = optical_flow_msg->ground_distance;
+                          init_state2(0,0) = range_msg->ground_distance;
                           init_state2(1,0) = start_vel_;
                           estimator_->getFuserExperiment(i)->setInitState(init_state2);
                           */
-                          estimator_->getFuserExperiment(i)->setInitState(optical_flow_msg->ground_distance, 0);
+                          estimator_->getFuserExperiment(i)->setInitState(range_msg->ground_distance, 0);
                           temp(0,0) = sonar_noise_sigma_;
                           estimator_->getFuserExperiment(i)->setMeasureSigma(temp);
                           estimator_->getFuserExperiment(i)->setMeasureFlag();
@@ -226,11 +187,11 @@ void rangeCallback(const px_comm::LeddarOneConstPtr & optical_flow_msg)
           raw_vel_z_ = (raw_pos_z_ - prev_raw_pos_z) / (current_secs - previous_secs);
 
           //**** 速度情報の更新,ボードの向き
-          raw_vel_x_ = x_axis_direction_ * optical_flow_msg->velocity_x; 
-          raw_vel_y_ = y_axis_direction_ * optical_flow_msg->velocity_y; 
+          raw_vel_x_ = x_axis_direction_ * range_msg->velocity_x; 
+          raw_vel_y_ = y_axis_direction_ * range_msg->velocity_y; 
 
-          filtered_vel_x_ = x_axis_direction_ * optical_flow_msg->flow_x /1000.0;
-          filtered_vel_y_ = y_axis_direction_ * optical_flow_msg->flow_y /1000.0;
+          filtered_vel_x_ = x_axis_direction_ * range_msg->flow_x /1000.0;
+          filtered_vel_y_ = y_axis_direction_ * range_msg->flow_y /1000.0;
 
           Eigen::Matrix<double, 1, 1> sigma_temp = Eigen::MatrixXd::Zero(1, 1); 
           Eigen::Matrix<double, 1, 1> temp = Eigen::MatrixXd::Zero(1, 1); 
@@ -245,7 +206,7 @@ void rangeCallback(const px_comm::LeddarOneConstPtr & optical_flow_msg)
                       sigma_temp(0,0) = level_vel_noise_sigma_;
                       estimator_->getFuserEgomotion(i)->setMeasureSigma(sigma_temp);
                       estimator_->getFuserEgomotion(i)->setCorrectMode(1);
-                      if(optical_flow_msg->quality == 0 || raw_vel_x_ == 0 || raw_pos_z_ > 2.5)
+                      if(range_msg->quality == 0 || raw_vel_x_ == 0 || raw_pos_z_ > 2.5)
                         temp(0, 0) = filtered_vel_x_;
                       else
                         temp(0, 0) = raw_vel_x_;
@@ -257,7 +218,7 @@ void rangeCallback(const px_comm::LeddarOneConstPtr & optical_flow_msg)
                       estimator_->getFuserEgomotion(i)->setMeasureSigma(sigma_temp);
                       estimator_->getFuserEgomotion(i)->setCorrectMode(1);
 
-                      if(optical_flow_msg->quality == 0 || raw_vel_y_ == 0 || raw_pos_z_ > 2.5)
+                      if(range_msg->quality == 0 || raw_vel_y_ == 0 || raw_pos_z_ > 2.5)
                         temp(0, 0) = filtered_vel_y_;
                       else
                         temp(0, 0) = raw_vel_y_;
@@ -286,7 +247,7 @@ void rangeCallback(const px_comm::LeddarOneConstPtr & optical_flow_msg)
                       estimator_->getFuserExperiment(i)->setMeasureSigma(sigma_temp);
                       estimator_->getFuserExperiment(i)->setCorrectMode(1);
 
-                      if(optical_flow_msg->quality == 0 || raw_vel_x_ == 0 || raw_pos_z_ > 2.5)
+                      if(range_msg->quality == 0 || raw_vel_x_ == 0 || raw_pos_z_ > 2.5)
                         temp(0, 0) = filtered_vel_x_;
                       else
                         temp(0, 0) = raw_vel_x_;
@@ -298,7 +259,7 @@ void rangeCallback(const px_comm::LeddarOneConstPtr & optical_flow_msg)
                       estimator_->getFuserExperiment(i)->setMeasureSigma(sigma_temp);
                       estimator_->getFuserExperiment(i)->setCorrectMode(1);
 
-                      if(optical_flow_msg->quality == 0 || raw_vel_y_ == 0 || raw_pos_z_ > 2.5)
+                      if(range_msg->quality == 0 || raw_vel_y_ == 0 || raw_pos_z_ > 2.5)
                         temp(0, 0) = filtered_vel_y_;
                       else
                         temp(0, 0) = raw_vel_y_;
@@ -318,7 +279,7 @@ void rangeCallback(const px_comm::LeddarOneConstPtr & optical_flow_msg)
 
           //publish
           aerial_robot_base::States opt_data;
-          opt_data.header.stamp = optical_flow_msg->header.stamp;
+          opt_data.header.stamp = range_msg->header.stamp;
 
           aerial_robot_base::State x_state;
           x_state.id = "x";
@@ -507,29 +468,12 @@ void rangeCallback(const px_comm::LeddarOneConstPtr & optical_flow_msg)
 
     void rosParamInit()
     {
+      nhp_.param("range_sensor_sub_name", range_sensor_sub_name_, 0.005 );
+      printf("range noise sigma  is %f\n", range_sensor_sub_name_);
 
+      nhp_.param("range_noise_sigma", range_noise_sigma_, 0.005 );
+      printf("range noise sigma  is %f\n", range_noise_sigma_);
 
-      nhp_.param("sonar_noise_sigma", sonar_noise_sigma_, 0.01 );
-      printf("sonar noise sigma  is %f\n", sonar_noise_sigma_);
-
-      if (!nhp_.getParam ("start_upper_thre", start_upper_thre_))
-        start_upper_thre_ = 0;
-      printf("%s: start_upper_thre_ is %.3f\n", nhp_.getNamespace().c_str(), start_upper_thre_);
-      if (!nhp_.getParam ("start_lower_thre", start_lower_thre_))
-        start_lower_thre_ = 0;
-      printf("%s: start_lower_thre_ is %.3f\n", nhp_.getNamespace().c_str(), start_lower_thre_);
-      if (!nhp_.getParam ("start_vel", start_vel_))
-        start_vel_ = 0;
-      printf("%s: start_vel_ is %.3f\n", nhp_.getNamespace().c_str(), start_vel_);
-
-      std::string ns = nhp_.getNamespace();
-      if (!nhp_.getParam ("x_axis_direction", x_axis_direction_))
-        x_axis_direction_ = 1.0;
-      printf("%s: x_axis direction_ is %.3f\n", ns.c_str(), x_axis_direction_);
-
-      if (!nhp_.getParam ("y_axis_direction", y_axis_direction_))
-        y_axis_direction_ = 1.0; //-1 is default
-      printf("%s: y_axis direction_ is %.3f\n", ns.c_str(), y_axis_direction_);
     }
   };
 
