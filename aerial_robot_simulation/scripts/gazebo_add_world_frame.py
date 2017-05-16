452e202e5e76769b350dd7b94bc067cc50ae48c5
KahnShi <shifan@jsk.imi.i.u-tokyo.ac.jp>
2017-05-16 22:16:19 +0900

    Fix problems in world frame and /uav/state.

--- aerial_robot_simulation/scripts/gazebo_add_world_frame.py
+++ aerial_robot_simulation/scripts/gazebo_add_world_frame.py
@@ -2,6 +2,7 @@
 import rospy
 import tf
 from nav_msgs.msg import Odometry
+from tf import transformations
 
 class WorldFrame:
     def init(self):
@@ -9,17 +10,18 @@ class WorldFrame:
         self.br = tf.TransformBroadcaster()
         self.sub_hydrus_odom = rospy.Subscriber("/uav/state", Odometry, self.hydrus_odom_callback)
     def hydrus_odom_callback(self, msg):
-        self.br.sendTransform((msg.pose.pose.position.x,
-                               msg.pose.pose.position.y,
-                               msg.pose.pose.position.z),
-                              (msg.pose.pose.orientation.x,
-                               msg.pose.pose.orientation.y,
-                               msg.pose.pose.orientation.z,
-                               msg.pose.pose.orientation.w),
-                              #rospy.Time.now(),
+        origin_t = [msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z]
+        origin_q = [msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w]
+        origin_matrix = transformations.concatenate_matrices(transformations.translation_matrix(origin_t),
+                                                             transformations.quaternion_matrix(origin_q))
+        inv_matrix = transformations.inverse_matrix(origin_matrix)
+        inv_t = transformations.translation_from_matrix(inv_matrix)
+        inv_q = transformations.quaternion_from_matrix(inv_matrix)
+        self.br.sendTransform(inv_t,
+                              inv_q,
                               msg.header.stamp,
-                              "link1",
-                              "world")
+                              "world",
+                              "link2")
 
 if __name__ == '__main__':
     try:
