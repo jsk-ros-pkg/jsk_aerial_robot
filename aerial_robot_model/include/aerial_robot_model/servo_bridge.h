ca89abf27f86b78167801d4610e5ed29cd0392ac
moju zhao <chou@jsk.imi.i.u-tokyo.ac.jp>
2018-11-18 01:16:39 +0900

    Crate the improved and integrated servo bridge for real machine, which can handle the joint, gimbal and pwm servo.

---
+++ aerial_robot_model/include/aerial_robot_model/servo_bridge.h
@@ -0,0 +1,192 @@
+// -*- mode: c++ -*-
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2018, JSK Lab
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/o2r other materials provided
+ *     with the distribution.
+ *   * Neither the name of the JSK Lab nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+
+#ifndef SERVO_BRIDGE_H
+#define SERVO_BRIDGE_H
+
+/* ros */
+#include <ros/ros.h>
+
+/* ros msg */
+#include <std_msgs/UInt8.h>
+#include <std_msgs/Int8.h>
+#include <std_msgs/Empty.h>
+#include <std_msgs/Float64.h>
+#include <std_srvs/SetBool.h>
+#include <sensor_msgs/JointState.h>
+#include <spinal/ServoStates.h>
+#include <spinal/ServoControlCmd.h>
+#include <spinal/ServoTorqueCmd.h>
+
+
+/* util */
+#include <string>
+
+using namespace std;
+
+namespace ValueType
+{
+  enum
+    {BIT, RADIAN};
+};
+
+class SingleServoHandle
+{
+public:
+  SingleServoHandle(string name, int id, double max, double min, int sgn, double offset, double scale, bool receive_real_state):
+    name_(name), id_(id), curr_val_(0), target_val_(0),init_target_val_(false), sgn_(sgn),
+    offset_(offset), scale_(scale), max_(max), min_(min), receive_real_state_(receive_real_state)
+  {
+    /* for simulation */
+    //joint_ctrl_pub_ = nh_.advertise<std_msgs::Float64>(std::string("/j") + std_  + std::string("_controller/command"), 1);
+
+  };
+  ~SingleServoHandle() {};
+
+  boost::shared_ptr<SingleServoHandle> getHandle() { return boost::shared_ptr<SingleServoHandle>(this); }
+
+  inline void setCurrVal(const double& val, int value_type)
+  {
+    if(value_type == ValueType::BIT)
+      curr_val_ = scale_ * sgn_ * (val - offset_);
+    else if(value_type == ValueType::RADIAN)
+      curr_val_ = val;
+    else
+      ROS_ERROR("wrong value type");
+
+    if(!init_target_val_)
+      {
+        target_val_ = val;
+        init_target_val_ = true;
+      }
+  }
+
+  inline void setTargetVal(const double& val, int value_type)
+  {
+    if(value_type == ValueType::BIT)
+      target_val_ = scale_ * sgn_ * (val - offset_);
+    else if(value_type == ValueType::RADIAN)
+      target_val_ = val;
+    else
+      ROS_ERROR("wrong value type");
+
+    if(!receive_real_state_) curr_val_ = target_val_;
+
+  }
+
+  inline void setName(const string& name){ name_ = name; }
+  inline void setId(const int& id){ id_ = id; }
+  inline void setSgn(const int& sgn){ sgn_ = sgn; }
+  inline void setOffset(const int& offset){ offset_ = offset; }
+  inline void setScale(const double& scale){ scale_ = scale; }
+  inline void setMax(const double& max){ max_ = max; }
+  inline void setMin(const double& min){ min_ = min; }
+
+  const double getCurrVal(int value_type) const
+  {
+    if(value_type == ValueType::BIT)
+      return curr_val_ * sgn_ / scale_ + offset_;
+    else if(value_type == ValueType::RADIAN)
+      return curr_val_;
+  }
+
+  const double getTargetVal(int value_type) const
+  {
+    if(value_type == ValueType::BIT)
+      return target_val_ * sgn_ / scale_ + offset_;
+    else if(value_type == ValueType::RADIAN)
+      return target_val_;
+  }
+
+  inline const string& getName(){return name_; }
+  inline const int& getId() const {return id_; }
+  inline const int& getSgn() const {return sgn_; }
+  inline const int& getOffset() const {return offset_; }
+  inline const double& getScale() const {return scale_; }
+  inline const double& getMax() const {return max_; }
+  inline const double& getMin() const {return min_; }
+
+private:
+  int id_;
+  string name_;
+  double curr_val_; // radian
+  double target_val_; // radian
+  int sgn_;
+  int offset_;
+  double scale_;
+  double max_;
+  double min_;
+
+  bool receive_real_state_;
+  bool init_target_val_;
+};
+
+using SingleServoHandlePtr= boost::shared_ptr<SingleServoHandle>;
+using ServoGroupHandler= vector<SingleServoHandlePtr>;
+
+class ServoBridge
+{
+protected:
+  ros::NodeHandle nh_;
+  ros::NodeHandle nhp_;
+
+  ros::Publisher servo_states_pub_;
+  vector<ros::Subscriber> servo_states_subs_;
+  vector<ros::Subscriber> servo_ctrl_subs_;
+  map<string, ros::Publisher> servo_ctrl_pubs_;
+  vector<ros::ServiceServer> servo_torque_ctrl_srvs_;
+  map<string, ros::Publisher> servo_torque_ctrl_pubs_;
+
+  map<string, ServoGroupHandler> servos_handler_;
+  double moving_check_rate_;
+  double moving_angle_thresh_;
+  bool send_init_joint_pose_;
+  int send_init_joint_pose_cnt_;
+
+
+  void servoStatesCallback(const spinal::ServoStatesConstPtr& state_msg, const std::string& servo_group_name);
+  void servoCtrlCallback(const sensor_msgs::JointStateConstPtr& joints_ctrl_msg, const std::string& servo_group_name);
+  bool servoTorqueCtrlCallback(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res, const std::string& servo_group_name);
+
+public:
+  ServoBridge(ros::NodeHandle nh, ros::NodeHandle nhp);
+  ~ServoBridge()  {}
+  void servoStatePublish();
+
+};
+
+
+#endif
---
+++ aerial_robot_model/src/servo_bridge.cpp
@@ -0,0 +1,184 @@
+// -*- mode: c++ -*-
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2018, JSK Lab
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/o2r other materials provided
+ *     with the distribution.
+ *   * Neither the name of the JSK Lab nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+
+#include <aerial_robot_model/servo_bridge.h>
+
+ServoBridge::ServoBridge(ros::NodeHandle nh, ros::NodeHandle nhp): nh_(nh),nhp_(nhp)
+{
+  XmlRpc::XmlRpcValue all_servos_params;
+  nh_.getParam("servo_controller", all_servos_params);
+
+  for(auto servo_group_params: all_servos_params)
+    {
+      ROS_ASSERT(servo_group_params.second.getType() == XmlRpc::XmlRpcValue::TypeStruct);
+
+      ServoGroupHandler servo_group_handler;
+      for(auto servo : servo_group_params.second)
+        {
+          if(servo.first.find("controller") != string::npos)
+            {
+              double max = (servo.second.hasMember("angle_max"))?
+                servo.second["angle_max"]:servo_group_params.second["angle_max"];
+              double min = (servo.second.hasMember("angle_min"))?
+                servo.second["angle_min"]:servo_group_params.second["angle_min"];
+              int sgn = (servo.second.hasMember("angle_sgn"))?
+                servo.second["angle_sgn"]:servo_group_params.second["angle_sgn"];
+              int offset = (servo.second.hasMember("angle_offset"))?
+                servo.second["angle_offset"]:servo_group_params.second["angle_offset"];
+              double scale = (servo.second.hasMember("angle_scale"))?
+                servo.second["angle_scale"]:servo_group_params.second["angle_scale"];
+
+              servo_group_handler.push_back(SingleServoHandlePtr(new SingleServoHandle(servo.second["name"], servo.second["id"], max, min, sgn, offset, scale, servo_group_params.second.hasMember("state_sub_topic"))));
+            }
+        }
+
+      servos_handler_.insert(make_pair(servo_group_params.first, servo_group_handler));
+
+      /* ros pub/sub, service */
+      /* Get servo states (i.e. joint angles) from real machine, if necessary */
+      if(servo_group_params.second.hasMember("state_sub_topic"))
+        servo_states_subs_.push_back(nh_.subscribe<spinal::ServoStates>((string)servo_group_params.second["state_sub_topic"], 10, boost::bind(&ServoBridge::servoStatesCallback, this, _1, servo_group_params.first)));
+
+      /* subscribe target servo state from controller */
+      servo_ctrl_subs_.push_back(nh_.subscribe<sensor_msgs::JointState>(servo_group_params.first + string("_ctrl"), 10, boost::bind(&ServoBridge::servoCtrlCallback, this, _1, servo_group_params.first)));
+
+      /* publish target servo state to real machine */
+      servo_ctrl_pubs_.insert(make_pair(servo_group_params.first, nh_.advertise<spinal::ServoControlCmd>(servo_group_params.second["ctrl_pub_topic"], 1)));
+
+      /* torque on/off */
+      if(servo_group_params.second.hasMember("torque_pub_topic"))
+        {
+          servo_torque_ctrl_srvs_.push_back(nh_.advertiseService<std_srvs::SetBool::Request, std_srvs::SetBool::Response>(servo_group_params.first + string("/torque_enable"), boost::bind(&ServoBridge::servoTorqueCtrlCallback, this, _1, _2, servo_group_params.first)));
+          servo_torque_ctrl_pubs_.insert(make_pair(servo_group_params.first, nh_.advertise<spinal::ServoTorqueCmd>((string)servo_group_params.second["torque_pub_topic"], 1)));
+        }
+    }
+  servo_states_pub_ = nh_.advertise<sensor_msgs::JointState>("joint_states", 1);
+
+}
+
+void ServoBridge::servoStatesCallback(const spinal::ServoStatesConstPtr& state_msg, const string& servo_group_name)
+{
+  if(state_msg->servos.size() != servos_handler_[servo_group_name].size())
+    {
+      ROS_ERROR("[servo bridge, servo state callback]: the joint num from rosparam %d is not equal with ros msgs %d", (int)servos_handler_[servo_group_name].size(), (int)state_msg->servos.size());
+      return;
+    }
+
+  for(auto it: state_msg->servos)
+    {
+      auto servo_handler = find_if(servos_handler_[servo_group_name].begin(),
+                                   servos_handler_[servo_group_name].end(),
+                                   [&](SingleServoHandlePtr s) {return it.index == s->getId();} );
+
+      if(servo_handler == servos_handler_[servo_group_name].end())
+        {
+          ROS_ERROR("[servo bridge, servo state callback]: no matching joint handler for servo index %d", it.index);
+          return;
+        }
+      (*servo_handler)->setCurrVal((double)it.angle, ValueType::BIT);
+    }
+
+  sensor_msgs::JointState servo_states_msg;
+  servo_states_msg.header.stamp = state_msg->stamp;
+
+  for(auto servo_group : servos_handler_)
+    {
+      for(auto servo_handler: servo_group.second)
+        {
+          servo_states_msg.name.push_back(servo_handler->getName());
+          servo_states_msg.position.push_back(servo_handler->getCurrVal(ValueType::RADIAN));
+        }
+    }
+  servo_states_pub_.publish(servo_states_msg);
+}
+
+void ServoBridge::servoCtrlCallback(const sensor_msgs::JointStateConstPtr& servo_ctrl_msg, const string& servo_group_name)
+{
+  if(servo_ctrl_msg->position.size() != servos_handler_[servo_group_name].size())
+    {
+      ROS_ERROR("[servo bridge, servo control control]: the joint num from rosparam %d is not equal with ros msgs %d", (int)servos_handler_[servo_group_name].size(), (int)servo_ctrl_msg->position.size());
+      return;
+    }
+
+  spinal::ServoControlCmd target_angle_msg;
+
+
+  for(int i = 0; i < servo_ctrl_msg->position.size(); i++)
+    {
+      /* consideration: think about the the order of servo (joint, gimbal) */
+
+      /*
+        // use servo_name to search the servo_handler
+        auto servo_handler = find_if(servos_handler_[servo_group_name].begin(),
+        servos_handler_[servo_group_name].end(),
+        [&](SingleServoHandlePtr s) {return servo_ctrl_msg->name.at(i) == s->getName();});
+
+        if(servo_handler == servos_handler_[servo_group_name].end())
+        {
+        ROS_ERROR("[servo bridge, servo control callback]: no matching joint handler for servo %s", servo_ctrl_msg->name.at(i).c_str());
+        return;
+        }
+
+        (*servo_handler)->setTargetVal(servo_ctrl_msg->position[i], ValueType::RADIAN);
+        target_angle_msg.index.push_back((*servo_handler)->getId());
+        target_angle_msg.angles.push_back((*servo_handler)->getTargetVal(ValueType::BIT));
+      */
+
+
+      /*  use the kinematics order (e.g. joint1 ~ joint N, gimbal_roll -> gimbal_pitch) */
+      SingleServoHandlePtr servo_handler = servos_handler_[servo_group_name].at(i);
+      servo_handler->setTargetVal(servo_ctrl_msg->position[i], ValueType::RADIAN);
+      target_angle_msg.index.push_back(servo_handler->getId());
+      target_angle_msg.angles.push_back(servo_handler->getTargetVal(ValueType::BIT));
+    }
+
+  servo_ctrl_pubs_[servo_group_name].publish(target_angle_msg);
+}
+
+bool ServoBridge::servoTorqueCtrlCallback(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res, const std::string& servo_group_name)
+{
+  spinal::ServoTorqueCmd torque_off_msg;
+  for(auto servo_handler: servos_handler_[servo_group_name])
+    {
+      torque_off_msg.index.push_back(servo_handler->getId());
+      torque_off_msg.torque_enable.push_back(req.data);
+    }
+
+  servo_torque_ctrl_pubs_[servo_group_name].publish(torque_off_msg);
+
+  return true;
+}
+
