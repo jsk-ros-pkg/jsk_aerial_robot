3bfacdabc7e9e323725e37ad909fc17906dada83
tongtybj <chou@jsk.imi.i.u-tokyo.ac.jp>
2018-12-20 17:02:42 +0900

    Add torque (effort) publishing process from Spinal

--- aerial_robot_model/include/aerial_robot_model/servo_bridge.h
+++ aerial_robot_model/include/aerial_robot_model/servo_bridge.h
@@ -62,25 +62,22 @@ using namespace std;
 namespace ValueType
 {
   enum
-    {BIT, RADIAN};
+    {BIT = 0, RADIAN = 1};
 };
 
 class SingleServoHandle
 {
 public:
-  SingleServoHandle(string name, int id, int sgn, double offset, double scale, bool receive_real_state):
-    name_(name), id_(id), curr_val_(0), target_val_(0),init_target_val_(false), sgn_(sgn),
-    offset_(offset), scale_(scale), receive_real_state_(receive_real_state)
+  SingleServoHandle(string name, int id, int angle_sgn, double zero_point_offset, double angle_scale, double torque_scale, bool receive_real_state): name_(name), id_(id), curr_angle_val_(0), target_angle_val_(0), init_target_angle_val_(false), curr_torque_val_(0), angle_sgn_(angle_sgn), zero_point_offset_(zero_point_offset), angle_scale_(angle_scale), torque_scale_(torque_scale), receive_real_state_(receive_real_state)
   {
     /* for simulation */
     //joint_ctrl_pub_ = nh_.advertise<std_msgs::Float64>(std::string("/j") + std_  + std::string("_controller/command"), 1);
-
   };
   ~SingleServoHandle() {};
 
   boost::shared_ptr<SingleServoHandle> getHandle() { return boost::shared_ptr<SingleServoHandle>(this); }
 
-  inline void setCurrVal(const double& val, int value_type)
+  inline void setCurrAngleVal(const double& val, int value_type)
   {
     if(value_type == ValueType::BIT)
       {
@@ -90,21 +87,21 @@ public:
             return;
           }
 
-        curr_val_ = scale_ * sgn_ * (val - offset_);
+        curr_angle_val_ = angle_scale_ * angle_sgn_ * (val - zero_point_offset_);
       }
     else if(value_type == ValueType::RADIAN)
-      curr_val_ = val;
+      curr_angle_val_ = val;
     else
       ROS_ERROR("wrong value type");
 
-    if(!init_target_val_)
+    if(!init_target_angle_val_)
       {
-        target_val_ = val;
-        init_target_val_ = true;
+        target_angle_val_ = val;
+        init_target_angle_val_ = true;
       }
   }
 
-  inline void setTargetVal(const double& val, int value_type)
+  inline void setTargetAngleVal(const double& val, int value_type)
   {
     if(value_type == ValueType::BIT)
       {
@@ -113,59 +110,73 @@ public:
             ROS_ERROR("%s: bit target val could not be negative: %f", name_.c_str(), val);
             return;
           }
-        target_val_ = scale_ * sgn_ * (val - offset_);
+        target_angle_val_ = angle_scale_ * angle_sgn_ * (val - zero_point_offset_);
       }
     else if(value_type == ValueType::RADIAN)
-      target_val_ = val;
+      target_angle_val_ = val;
     else
       ROS_ERROR("%s: wrong value type", name_.c_str());
 
-    if(!receive_real_state_) curr_val_ = target_val_;
+    if(!receive_real_state_) curr_angle_val_ = target_angle_val_;
+  }
+
+  inline void setCurrTorqueVal(const double& val)
+  {
+      curr_torque_val_ = torque_scale_ * angle_sgn_ * val;
   }
 
   inline void setName(const string& name){ name_ = name; }
   inline void setId(const int& id){ id_ = id; }
-  inline void setSgn(const int& sgn){ sgn_ = sgn; }
-  inline void setOffset(const int& offset){ offset_ = offset; }
-  inline void setScale(const double& scale){ scale_ = scale; }
+  inline void setAngleSgn(const int& sgn){ angle_sgn_ = sgn; }
+  inline void setZeroPointOffset(const int& offset){ zero_point_offset_ = offset; }
+  inline void setAngleScale(const double& scale){ angle_scale_ = scale; }
+  inline void setTorqueScale(const double& scale){ torque_scale_ = scale; }
 
-  const double getCurrVal(int value_type) const
+  const double getCurrAngleVal(int value_type) const
   {
     if(value_type == ValueType::BIT)
       {
-        return boost::algorithm::clamp(curr_val_ * sgn_ / scale_ + offset_, 0, UINT16_MAX);
+        return boost::algorithm::clamp(curr_angle_val_ * angle_sgn_ / angle_scale_ + zero_point_offset_, 0, UINT16_MAX);
       }
     else if(value_type == ValueType::RADIAN)
-      return curr_val_;
+      return curr_angle_val_;
   }
 
-  const double getTargetVal(int value_type) const
+  const double getTargetAngleVal(int value_type) const
   {
     if(value_type == ValueType::BIT)
       {
-        return boost::algorithm::clamp(target_val_ * sgn_ / scale_ + offset_, 0, UINT16_MAX);
+        return boost::algorithm::clamp(target_angle_val_ * angle_sgn_ / angle_scale_ + zero_point_offset_, 0, UINT16_MAX);
       }
     else if(value_type == ValueType::RADIAN)
-      return target_val_;
+      return target_angle_val_;
+  }
+
+  inline const double getCurrTorqueVal() const
+  {
+    return curr_torque_val_;
   }
 
   inline const string& getName(){return name_; }
   inline const int& getId() const {return id_; }
-  inline const int& getSgn() const {return sgn_; }
-  inline const int& getOffset() const {return offset_; }
-  inline const double& getScale() const {return scale_; }
+  inline const int& getAngleSgn() const {return angle_sgn_; }
+  inline const int& getZeroPointOffset() const {return zero_point_offset_; }
+  inline const double& getAngleScale() const {return angle_scale_; }
+  inline const double& getTorqueScale() const {return torque_scale_; }
 
 private:
   int id_;
   string name_;
-  double curr_val_; // radian
-  double target_val_; // radian
-  int sgn_;
-  int offset_;
-  double scale_;
+  double curr_angle_val_; // radian
+  double target_angle_val_; // radian
+  double curr_torque_val_; // Nm
+  int angle_sgn_;
+  int zero_point_offset_;
+  double angle_scale_;
+  double torque_scale_;
 
   bool receive_real_state_;
-  bool init_target_val_;
+  bool init_target_angle_val_;
 };
 
 using SingleServoHandlePtr= boost::shared_ptr<SingleServoHandle>;
