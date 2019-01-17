6eda7897235bc254c05a28898147f4e1a299584d
Tomoki Anzai <anzai@jsk.imi.i.u-tokyo.ac.jp>
2019-01-17 19:49:04 +0900

    Modify init gimbal angle

--- robots/hydrus_xi/scripts/init_gimbal_angles.py
+++ robots/hydrus_xi/scripts/init_gimbal_angles.py
@@ -10,7 +10,7 @@ class InitGimbalAngle:
         self.pub = rospy.Publisher('/hydrus_xi/gimbals_ctrl', JointState, queue_size=1)
         self.sub = rospy.Subscriber('/hydrus_xi/joint_states', JointState, self.callback)
         self.complete = False
-        self.angle = [math.pi + -0.443246, 1.80942 - math.pi, -0.422855 + math.pi, 1.81924 - math.pi, -0.427232 + math.pi, 1.83216 - math.pi]
+        self.angle = [2.6983, -1.3321, 2.7187, -1.3223, 2.7143, -1.3094]
 
     def callback(self, msg):
         pub_msg = JointState()
