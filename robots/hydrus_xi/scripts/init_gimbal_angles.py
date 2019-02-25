e8fdd9520c3705c58b2858caca15bacaee7e9a4f
Tomoki Anzai <anzai@jsk.imi.i.u-tokyo.ac.jp>
2019-02-25 19:32:28 +0900

    temp

--- robots/hydrus_xi/scripts/init_gimbal_angles.py
+++ robots/hydrus_xi/scripts/init_gimbal_angles.py
@@ -10,7 +10,8 @@ class InitGimbalAngle:
         self.pub = rospy.Publisher('/hydrus_xi/gimbals_ctrl', JointState, queue_size=1)
         self.sub = rospy.Subscriber('/hydrus_xi/joint_states', JointState, self.callback)
         self.complete = False
-        self.angle = [2.6983, -1.3321, 2.7187, -1.3223, 2.7143, -1.3094]
+        #self.angle = [2.6983, -1.3321, 2.7187, -1.3223, 2.7143, -1.3094]
+        self.angle = [0.9491179081867179, -0.35778419036538456, 0.9463033377709107, -0.36272130661470514, 0.950201787642569, -0.3565103549241811]
 
     def callback(self, msg):
         pub_msg = JointState()
