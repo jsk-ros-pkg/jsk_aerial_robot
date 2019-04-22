458e3190f8d6795fd984d93f2ca632aa347eb4f8
moju zhao <chou@jsk.imi.i.u-tokyo.ac.jp>
2019-04-22 12:05:34 +0900

    Refactor the script to calculate the control error RMS

--- aerial_robot_base/scripts/rms.py
+++ aerial_robot_base/scripts/rms.py
@@ -8,108 +8,104 @@ from aerial_robot_msgs.msg import FlatnessPid, States
 
 import sys, select, termios, tty, math
 
+msg = """
+
+s: start to subscribe the topic regarding to control errors, and start to calculate the RMS
+h: stop the calculate the output the RMS results.
+"""
+
 def pos_err_callback(data):
         global start_flag
         if start_flag == True:
-                global msg_cnt
-                global throttle_sum
-                global pitch_sum
-                global roll_sum
-                global yaw_sum
-                msg_cnt = msg_cnt + 1
-                throttle_sum = throttle_sum + data.throttle.pos_err * data.throttle.pos_err
-                pitch_sum = pitch_sum + data.pitch.pos_err * data.pitch.pos_err
-                roll_sum = roll_sum + data.roll.pos_err * data.roll.pos_err
-                yaw_sum = yaw_sum + data.yaw.pos_err * data.yaw.pos_err
+                global four_axis_cnt
+                global four_axis_squared_errors_sum
+                four_axis_cnt = four_axis_cnt + 1
 
-                throttle_rms = math.sqrt(throttle_sum / msg_cnt)
-                pitch_rms = math.sqrt(pitch_sum / msg_cnt)
-                roll_rms = math.sqrt(roll_sum / msg_cnt)
-                yaw_rms = math.sqrt(yaw_sum / msg_cnt)
-                #rospy.loginfo("pos: throttle rms:%f , pitch rms: %f, roll rms: %f, yaw rms: %f \n", throttle_rms, pitch_rms, roll_rms, yaw_rms)
-                #print u"pos: throttle rms:%f , pitch rms: %f, roll rms: %f, yaw rms: %f" % (throttle_rms, pitch_rms, roll_rms, yaw_rms)
+                four_axis_squared_errors_sum[0] = four_axis_squared_errors_sum[0] + data.pitch.pos_err * data.pitch.pos_err
+                four_axis_squared_errors_sum[1] = four_axis_squared_errors_sum[1] + data.roll.pos_err * data.roll.pos_err
+                four_axis_squared_errors_sum[2] = four_axis_squared_errors_sum[2] + data.throttle.pos_err * data.throttle.pos_err
+                four_axis_squared_errors_sum[3] = four_axis_squared_errors_sum[3] + data.yaw.pos_err * data.yaw.pos_err
+
+                temp_rms = [math.sqrt(i / four_axis_cnt) for i in four_axis_squared_errors_sum]
+                rospy.logdebug("RMS errors of position: [%f, %f, %f], RMS error of yaw: %f", temp_rms[0], temp_rms[1], temp_rms[2], temp_rms[3])
 
 def att_err_callback(data):
         global start_flag
         if start_flag == True:
-                global att_msg_cnt
-                global att_pitch_sum
-                global att_roll_sum
+                global att_cnt
+                global roll_pitch_squared_errors_sum
 
-                att_msg_cnt = att_msg_cnt + 1
-                att_pitch_sum = att_pitch_sum + data.states[7].state[2].x * data.states[7].state[2].x
-                att_roll_sum = att_roll_sum + data.states[6].state[2].x * data.states[6].state[2].x
+                att_cnt = att_cnt + 1
 
-                pitch_rms = math.sqrt(att_pitch_sum / att_msg_cnt)
-                roll_rms = math.sqrt(att_roll_sum / att_msg_cnt)
+                roll_pitch_squared_errors_sum = [prev_sum + data.states[6 + i].state[2].x**2 for i, prev_sum in enumerate(roll_pitch_squared_errors_sum)]
+                #att_pitch_sum = att_pitch_sum + data.states[7].state[2].x * data.states[7].state[2].x
 
-                #rospy.loginfo("att: pitch rms: %f, roll rms: %f\n", pitch_rms, roll_rms)
-                #print u"att: pitch rms: %f, roll rms: %f" % (pitch_rms, roll_rms)
+                temp_rms = [math.sqrt(i / att_cnt) for i in roll_pitch_squared_errors_sum ]
+                #pitch_rms = math.sqrt(att_pitch_sum / att_cnt)
+                rospy.logdebug("RMS errors of roll and pitch: [%f, %f] \n", temp_rms[0], temp_rms[1])
 
 def getKey():
-	tty.setraw(sys.stdin.fileno())
-	select.select([sys.stdin], [], [], 0)
-	key = sys.stdin.read(1)
-	termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-	return key
+        tty.setraw(sys.stdin.fileno())
+        select.select([sys.stdin], [], [], 0)
+        key = sys.stdin.read(1)
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+        return key
 
 if __name__=="__main__":
-    	settings = termios.tcgetattr(sys.stdin)
+        settings = termios.tcgetattr(sys.stdin)
 
         start_flag = False
-	msg_cnt = 0
-        yaw_sum = 0
-        pitch_sum = 0
-        roll_sum = 0
-        throttle_sum = 0
+        msg_cnt = 0
 
-        att_msg_cnt = 0
-        att_pitch_sum = 0
-        att_roll_sum = 0
+        four_axis_squared_errors_sum = [0] * 4
+        roll_pitch_squared_errors_sum = [0] * 2
+
+        att_cnt = 0
+        four_axis_cnt = 0
+
         rospy.Subscriber("/controller/debug", FlatnessPid, pos_err_callback)
         rospy.Subscriber("/uav/full_state", States, att_err_callback)
 
-	rospy.init_node('keyboard_command')
+        rospy.init_node('rms_error')
 
+        print msg
+        try:
+                while(True):
+                        key = getKey()
 
-	try:
-		while(True):
-			key = getKey()
-			print "the key value is %d" % ord(key)
-			# takeoff and landing
-			if key == 's':
+                        if key == 's':
+                                rospy.loginfo("start to calculate RMS errors")
                                 start_flag = True
-			if key == 'h':
+                        if key == 'h':
+                                rospy.loginfo("stop calculation")
 
-                                if att_msg_cnt > 0:
-                                        pitch_rms = math.sqrt(att_pitch_sum / att_msg_cnt)
-                                        roll_rms = math.sqrt(att_roll_sum / att_msg_cnt)
-                                        print u"att: pitch rms: %f, roll rms: %f" % (pitch_rms, roll_rms)
+                                temp_rms = [0] * 2
+                                temp2_rms = [0] * 4
+                                if att_cnt > 0:
+                                        temp_rms = [math.sqrt(i / att_cnt) for i in roll_pitch_squared_errors_sum ]
+                                if four_axis_cnt > 0:
+                                        temp2_rms = [math.sqrt(i / four_axis_cnt) for i in four_axis_squared_errors_sum]
 
-                                if msg_cnt > 0:
-                                        throttle_rms = math.sqrt(throttle_sum / msg_cnt)
-                                        pitch_rms = math.sqrt(pitch_sum / msg_cnt)
-                                        roll_rms = math.sqrt(roll_sum / msg_cnt)
-                                        yaw_rms = math.sqrt(yaw_sum / msg_cnt)
-                                        print u"pos: throttle rms:%f , pitch rms: %f, roll rms: %f, yaw rms: %f" % (throttle_rms, pitch_rms, roll_rms, yaw_rms)
+
+                                rospy.loginfo("RMS of att errors: [%f, %f, %f], pos errors: [%f, %f, %f]", temp_rms[0], temp_rms[1], temp2_rms[3], temp2_rms[0], temp2_rms[1], temp2_rms[2])
 
                                 start_flag = False
-                                msg_cnt = 0
-                                yaw_sum = 0
-                                pitch_sum = 0
-                                roll_sum = 0
-                                throttle_sum = 0
-			else:
-				if (key == '\x03'):
-					break
-			rospy.sleep(0.001)
+                                att_cnt = 0
+                                four_axis_cnt = 0
 
-	except Exception as e:
-		print e
-		print repr(e)
+                                four_axis_squared_errors_sum = [0] * 4
+                                roll_pitch_squared_errors_sum = [0] * 2
 
-	finally:
-                
-    		termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+                        else:
+                                if (key == '\x03'):
+                                        break
+                        rospy.sleep(0.001)
+
+        except Exception as e:
+                print e
+                print repr(e)
+
+        finally:
+                termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
 
 
