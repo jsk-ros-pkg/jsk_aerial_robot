9ba2e4e194d9ee4d5f77ceba339892927c679722
tongtybj <chou@jsk.imi.i.u-tokyo.ac.jp>
2019-06-13 00:55:12 +0900

    - Refactor the build configuration for Spinal and Neruon F1 - Add "extern C" for C based function implemented in cpp files

--- aerial_robot_nerve/spinal/mcu_project/Jsk_Lib/it.h
+++ aerial_robot_nerve/spinal/mcu_project/Jsk_Lib/it.h
@@ -1,4 +1,14 @@
+
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
 void Rosserial_RxCpltCallback(UART_HandleTypeDef *huart);
 
 void GPS_RxCpltCallback(UART_HandleTypeDef *huart);
 void GPS_ErrorCpltCallback(UART_HandleTypeDef *huart);
+
+#ifdef __cplusplus
+}
+#endif
