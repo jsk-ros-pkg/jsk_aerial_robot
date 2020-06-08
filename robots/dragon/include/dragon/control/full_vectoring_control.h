7e98357259c7dfc36126c48f1a01a212dd55411a
tongtybj <chou@jsk.imi.i.u-tokyo.ac.jp>
2020-06-09 03:04:04 +0900

    Implement the external wrench estimation inside dragon controller

--- robots/dragon/include/dragon/control/full_vectoring_control.h
+++ robots/dragon/include/dragon/control/full_vectoring_control.h
@@ -37,10 +37,13 @@
 
 #include <aerial_robot_control/control/pose_linear_controller.h>
 #include <dragon/model/full_vectoring_robot_model.h>
+#include <geometry_msgs/WrenchStamped.h>
 #include <spinal/FourAxisCommand.h>
 #include <spinal/RollPitchYawTerm.h>
 #include <spinal/TorqueAllocationMatrixInv.h>
 #include <std_msgs/Float32MultiArray.h>
+#include <tf_conversions/tf_eigen.h>
+
 
 namespace aerial_robot_control
 {
@@ -48,7 +51,11 @@ namespace aerial_robot_control
   {
   public:
     DragonFullVectoringController();
-    ~DragonFullVectoringController(){}
+    ~DragonFullVectoringController()
+    {
+      wrench_estimate_thread_.interrupt();
+      wrench_estimate_thread_.join();
+    }
 
     void initialize(ros::NodeHandle nh, ros::NodeHandle nhp,
                     boost::shared_ptr<aerial_robot_model::RobotModel> robot_model,
@@ -61,6 +68,7 @@ namespace aerial_robot_control
     ros::Publisher flight_cmd_pub_; //for spinal
     ros::Publisher gimbal_control_pub_;
     ros::Publisher target_vectoring_force_pub_;
+    ros::Publisher estimate_external_wrench_pub_;
 
     boost::shared_ptr<Dragon::FullVectoringRobotModel> dragon_robot_model_;
     boost::shared_ptr<aerial_robot_model::RobotModel> robot_model_for_control_;
@@ -71,6 +79,28 @@ namespace aerial_robot_control
     bool gimbal_vectoring_check_flag_;
     double allocation_refine_threshold_;
     int allocation_refine_max_iteration_;
+    Eigen::VectorXd target_wrench_acc_cog_;
+
+    /* external wrench */
+    boost::mutex wrench_mutex_;
+    boost::thread wrench_estimate_thread_;
+    Eigen::VectorXd init_sum_momentum_;
+    Eigen::VectorXd est_external_wrench_;
+    Eigen::MatrixXd momentum_observer_matrix_;
+    Eigen::VectorXd integrate_term_;
+    double prev_est_wrench_timestamp_;
+
+    void externalWrenchEstimate();
+    const Eigen::VectorXd getTargetWrenchAccCog()
+    {
+      boost::lock_guard<boost::mutex> lock(wrench_mutex_);
+      return target_wrench_acc_cog_;
+    }
+    void setTargetWrenchAccCog(const Eigen::VectorXd target_wrench_acc_cog)
+    {
+      boost::lock_guard<boost::mutex> lock(wrench_mutex_);
+      target_wrench_acc_cog_ = target_wrench_acc_cog;
+    }
 
     void controlCore() override;
     void rosParamInit();
