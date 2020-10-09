410dedc45eabe68363bee699b3ce993a1b993a9f
Moju Zhao <chou@jsk.imi.i.u-tokyo.ac.jp>
2020-10-09 21:34:37 +0900

    Modify the CAN sampling point from 55% to 83% which maybe better

--- aerial_robot_nerve/spinal/mcu_project/Src/can.c
+++ aerial_robot_nerve/spinal/mcu_project/Src/can.c
@@ -31,11 +31,11 @@ void MX_CAN1_Init(void)
 {
 
   hcan1.Instance = CAN1;
-  hcan1.Init.Prescaler = 6;
+  hcan1.Init.Prescaler = 3;
   hcan1.Init.Mode = CAN_MODE_NORMAL;
   hcan1.Init.SyncJumpWidth = CAN_SJW_1TQ;
-  hcan1.Init.TimeSeg1 = CAN_BS1_4TQ;
-  hcan1.Init.TimeSeg2 = CAN_BS2_4TQ;
+  hcan1.Init.TimeSeg1 = CAN_BS1_14TQ;
+  hcan1.Init.TimeSeg2 = CAN_BS2_3TQ;
   hcan1.Init.TimeTriggeredMode = DISABLE;
   hcan1.Init.AutoBusOff = DISABLE;
   hcan1.Init.AutoWakeUp = DISABLE;
