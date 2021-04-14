d56be888f495615471839d0278c8573779f555d5
Nobuki Sugito <nobupiiman@gmail.com>
2021-04-14 21:09:31 +0900

    convert spinal into CubeIDE, files under Debug may be unnecessary

---
+++ aerial_robot_nerve/spinal/mcu_project/TrueSTUDIO/spinal/Debug/sources.mk
@@ -0,0 +1,51 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+ELF_SRCS := 
+C_UPPER_SRCS := 
+CXX_SRCS := 
+C++_SRCS := 
+OBJ_SRCS := 
+S_SRCS := 
+CC_SRCS := 
+C_SRCS := 
+CPP_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+OBJDUMP_LIST := 
+C_UPPER_DEPS := 
+S_DEPS := 
+C_DEPS := 
+OBJCOPY_BIN := 
+CC_DEPS := 
+SIZE_OUTPUT := 
+C++_DEPS := 
+EXECUTABLES := 
+OBJS := 
+CXX_DEPS := 
+S_UPPER_DEPS := 
+CPP_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+Application/Hydrus_Lib/CANDevice/imu \
+Application/Hydrus_Lib/CANDevice/initializer \
+Application/Hydrus_Lib/CANDevice/motor \
+Application/Hydrus_Lib/CANDevice/servo \
+Application/Hydrus_Lib/Spine \
+Application/Jsk_Lib/CAN \
+Application/Jsk_Lib/flashmemory \
+Application/Jsk_Lib/flight_control/attitude \
+Application/Jsk_Lib/math \
+Application/Jsk_Lib/sensors/baro \
+Application/Jsk_Lib/sensors/encoder \
+Application/Jsk_Lib/sensors/gps \
+Application/Jsk_Lib/sensors/imu \
+Application/TrueSTUDIO \
+Application/User \
+Application/ros_lib \
+Drivers/CMSIS \
+Drivers/STM32F7xx_HAL_Driver \
+
