bd2db283aade6a259374e0f328da1dcb4104080a
Moju Zhao <chou@jsk.imi.i.u-tokyo.ac.jp>
2021-06-13 18:41:12 +0900

    [Spinal rosserial] refactor the uart process for rosserial. Seperate larget message into several small packets to imrpove the transmit throughput

--- aerial_robot_nerve/spinal/src/ros_lib/STMF7Hardware.cpp
+++ aerial_robot_nerve/spinal/src/ros_lib/STMF7Hardware.cpp
@@ -7,154 +7,136 @@
 
 #include "STMF7Hardware.h"
 #include <string.h>
-#include "it.h"
 
-/* RX */
-namespace
+void STMF7Hardware::init(UART_HandleTypeDef *huart, osMutexId *mutex)
 {
-  uint8_t rx_buf_[RX_BUFFER_SIZE];
-  UART_HandleTypeDef *rx_huart_;
-  uint32_t rd_ptr_ = 0;
+  huart_ = huart;
+
+  /* rx */
+  __HAL_UART_DISABLE_IT(huart, UART_IT_PE);
+  __HAL_UART_DISABLE_IT(huart, UART_IT_ERR);
+  HAL_UART_Receive_DMA(huart, rx_buf_, RX_BUFFER_SIZE);
+  memset(rx_buf_, 0, sizeof(rx_buf_));
+
+
+  /* tx */
+  tx_mutex_ = mutex;
+
+  subscript_in_progress_ = 0;
+  subscript_to_add_ = 0;
+
+  for(int i = 0; i < TX_BUFFER_SIZE; i++)
+    {
+      tx_buffer_unit_[i].tx_len_ = 1;
+      memset(tx_buffer_unit_[i].tx_data_, 0, TX_BUFFER_WIDTH);
+    }
 }
 
-namespace rx
+bool STMF7Hardware::rx_available()
 {
- void init(UART_HandleTypeDef *huart)
- {
-   __HAL_UART_DISABLE_IT(huart, UART_IT_PE);
-   __HAL_UART_DISABLE_IT(huart, UART_IT_ERR);
-   HAL_UART_Receive_DMA(huart, rx_buf_, RX_BUFFER_SIZE);
-
-   memset(rx_buf_, 0, sizeof(rx_buf_));
-   rx_huart_ = huart;
-  }
-
-  bool available()
-  {
-    uint32_t dma_write_ptr =  (RX_BUFFER_SIZE - rx_huart_->hdmarx->Instance->NDTR) % (RX_BUFFER_SIZE);
-    return (rd_ptr_ != dma_write_ptr);
-  }
-
-  int read()
-  {
-    /* handle RX Overrun Error */
-    if ( __HAL_UART_GET_FLAG(rx_huart_, UART_FLAG_ORE) )
-      {
-        __HAL_UART_CLEAR_FLAG(rx_huart_,
-                              UART_CLEAR_NEF | UART_CLEAR_OREF | UART_FLAG_RXNE | UART_FLAG_ORE);
-        HAL_UART_Receive_DMA(rx_huart_, rx_buf_, RX_BUFFER_SIZE);
-      }
-
-
-    uint32_t dma_write_ptr =  (RX_BUFFER_SIZE - rx_huart_->hdmarx->Instance->NDTR) % (RX_BUFFER_SIZE);
-    int c = -1;
-    if(rd_ptr_ != dma_write_ptr)
-      {
-        c = (int)rx_buf_[rd_ptr_++];
-        rd_ptr_ %= RX_BUFFER_SIZE;
-      }
-    return c;
-  }
-};
-
-
-
-/* TX */
-namespace
-{
-  osMutexId *mutex_;
-  UART_HandleTypeDef *tx_huart_;
-  struct TxBufferUnit<TX_BUFFER_WIDTH> tx_buffer_unit_[TX_BUFFER_SIZE];
-  uint8_t subscript_in_progress_;
-  uint8_t subscript_to_add_;
-  bool idle_flag_;
+  uint32_t dma_write_ptr =  (RX_BUFFER_SIZE - huart_->hdmarx->Instance->NDTR) % (RX_BUFFER_SIZE);
+  return (rd_ptr_ != dma_write_ptr);
 }
 
-namespace tx
+int STMF7Hardware::read()
 {
-  void init(UART_HandleTypeDef *huart, osMutexId *mutex)
-  {
-    tx_huart_ = huart;
-    mutex_ = mutex;
-
-    idle_flag_ = true;
-    subscript_in_progress_ = 0;
-    subscript_to_add_ = 0;
-
-    for(int i = 0; i < TX_BUFFER_SIZE; i++)
-      {
-        tx_buffer_unit_[i].tx_len_ = 1;
-        for(int j = 0; j < TX_BUFFER_WIDTH; j++)
-          tx_buffer_unit_[i].tx_data_[j]  = 1;
-      }
-  }
-
-  uint8_t subscriptInProgress(){return subscript_in_progress_;}
-  uint8_t subscriptToAdd(){return subscript_to_add_;}
-  uint8_t  getCurrentTransmitBufferLen()
-  {
-    return tx_buffer_unit_[subscript_in_progress_].tx_len_;
-  }
-
-  uint8_t*  getCurrentTransmitBufferP()
-  {
-    return tx_buffer_unit_[subscript_in_progress_].tx_data_;
-  }
-
-  int publish()
-  {
-    if (subscript_in_progress_ != subscript_to_add_)
-      {
-        /*
-          Timeout value of > 1 ms is necessary for the initial pub/sub/srv topic info transmit (check with 2 ms, with max length of 120 (+8))
-          1 ms will induce "Rejecting message on topicId=x, length=xx with bad checksum.", whcih prevent the propoer send of TWO topic info.
-
-          P.S.: 10 ms might be necessary to handle large service response, e.g., spinal/GetBoardInfo for multilinks with more than eight links
-
-          This occurs for the long topic name and topic types, such as
-          - p_matrix_pseudo_inverse_inertia, type: spinal/PMatrixPseudoInverseWithInertia, length=120
-          - extra_servo_torque_enable, type: spinal/ServoTorqueCmd, length=97
-          - rpy/feedback_state, type: spinal/RollPitchYawTerm, length=92
-
-          This Timeout is not HAL_Delay().
-          Please check HAL_UART_Transmit and UART_WaitOnFlagUntilTimeout(huart, UART_FLAG_TXE, RESET, tickstart, Timeout)
-        */
-        int status = HAL_UART_Transmit(tx_huart_, tx_buffer_unit_[subscript_in_progress_].tx_data_, tx_buffer_unit_[subscript_in_progress_].tx_len_, 10);
+  /* handle RX Overrun Error */
+  if ( __HAL_UART_GET_FLAG(huart_, UART_FLAG_ORE) )
+    {
+      __HAL_UART_CLEAR_FLAG(huart_,
+                            UART_CLEAR_NEF | UART_CLEAR_OREF | UART_FLAG_RXNE | UART_FLAG_ORE);
+      HAL_UART_Receive_DMA(huart_, rx_buf_, RX_BUFFER_SIZE);
+    }
 
 
-        /* mutex to avoid access from task of higher priority running "write" */
-        //if(mutex_ != NULL) osMutexWait(*mutex_, osWaitForever);
+  uint32_t dma_write_ptr =  (RX_BUFFER_SIZE - huart_->hdmarx->Instance->NDTR) % (RX_BUFFER_SIZE);
+  int c = -1;
+  if(rd_ptr_ != dma_write_ptr)
+    {
+      c = (int)rx_buf_[rd_ptr_++];
+      rd_ptr_ %= RX_BUFFER_SIZE;
+    }
+  return c;
+}
 
-        subscript_in_progress_++;
-        if (subscript_in_progress_ == TX_BUFFER_SIZE) subscript_in_progress_ = 0;
 
-        //if(mutex_ != NULL)  osMutexRelease(*mutex_);
+int STMF7Hardware::publish()
+{
+  if (subscript_in_progress_ != subscript_to_add_)
+    {
+      /*
+        Timeout value of > 1 ms is necessary for the initial pub/sub/srv topic info transmit (check with 2 ms, with max length of 120 (+8))
+        1 ms will induce "Rejecting message on topicId=x, length=xx with bad checksum.", whcih prevent the propoer send of TWO topic info.
 
-        return status;
-      }
-    else
-      {
-        return BUFFER_EMPTY;
-      }
-  }
+        P.S.: 10 ms might be necessary to handle large service response, e.g., spinal/GetBoardInfo for multilinks with more than eight links
 
-  void write(uint8_t * new_data, unsigned int new_size)
-  {
-    /* mutex to avoid access from task with higher priority to call "write" or "publish" */
-    if(mutex_ != NULL) osMutexWait(*mutex_, osWaitForever);
+        This occurs for the long topic name and topic types, such as
+        - p_matrix_pseudo_inverse_inertia, type: spinal/PMatrixPseudoInverseWithInertia, length=120
+        - extra_servo_torque_enable, type: spinal/ServoTorqueCmd, length=97
+        - rpy/feedback_state, type: spinal/RollPitchYawTerm, length=92
 
-    /* if subscript comes around and get to one in progress_, skip */
-    if (subscript_in_progress_ != (subscript_to_add_ + 1) % TX_BUFFER_SIZE)
-      {
-        tx_buffer_unit_[subscript_to_add_].tx_len_ = new_size;
-        memcpy(tx_buffer_unit_[subscript_to_add_].tx_data_, new_data, new_size);
+        This Timeout is not HAL_Delay().
+        Please check HAL_UART_Transmit and UART_WaitOnFlagUntilTimeout(huart, UART_FLAG_TXE, RESET, tickstart, Timeout)
+      */
+      int status = HAL_UART_Transmit(huart_, tx_buffer_unit_[subscript_in_progress_].tx_data_, tx_buffer_unit_[subscript_in_progress_].tx_len_, 10);
 
-        subscript_to_add_++;
 
-        if (subscript_to_add_ == TX_BUFFER_SIZE) subscript_to_add_ = 0;
-      }
+      /* mutex to avoid access from task of higher priority running "write" */
+      if(tx_mutex_ != NULL) osMutexWait(*tx_mutex_, osWaitForever);
 
-    if(mutex_ != NULL) osMutexRelease(*mutex_);
-  }
+      subscript_in_progress_++;
+      subscript_in_progress_ %= TX_BUFFER_SIZE;
 
-};
+      if(tx_mutex_ != NULL)  osMutexRelease(*tx_mutex_);
+
+      return status;
+    }
+  else
+    {
+      return BUFFER_EMPTY;
+    }
+}
+
+void STMF7Hardware::write(uint8_t * new_data, unsigned int new_size)
+{
+  /* mutex to avoid access from task with higher priority to call "write" or "publish" */
+  if(tx_mutex_ != NULL) osMutexWait(*tx_mutex_, osWaitForever);
+
+  /* if subscript comes around and get to one in progress_, skip */
+  uint8_t packet_num = new_size / TX_BUFFER_WIDTH + (new_size % TX_BUFFER_WIDTH > 0?1:0);
+  bool overflow = false;
+
+  if(subscript_to_add_ >= subscript_in_progress_ && (subscript_to_add_ + packet_num) >= TX_BUFFER_SIZE)
+    {
+      if ((subscript_to_add_ + packet_num) % TX_BUFFER_SIZE >= subscript_in_progress_)
+        overflow = true;
+    }
+  if(subscript_to_add_ < subscript_in_progress_)
+    {
+      if ((subscript_to_add_ + packet_num)  >= subscript_in_progress_)
+        overflow = true;
+    }
+
+  if(!overflow)
+    {
+      for(unsigned int i = 0; i <  new_size / TX_BUFFER_WIDTH; i++)
+        {
+          tx_buffer_unit_[subscript_to_add_].tx_len_ = TX_BUFFER_WIDTH;
+          memcpy(tx_buffer_unit_[subscript_to_add_].tx_data_, new_data + TX_BUFFER_WIDTH * i, TX_BUFFER_WIDTH);
+
+          subscript_to_add_++;
+          subscript_to_add_ %= TX_BUFFER_SIZE;
+        }
+
+      if(new_size % TX_BUFFER_WIDTH > 0)
+        {
+          tx_buffer_unit_[subscript_to_add_].tx_len_ = new_size % TX_BUFFER_WIDTH;
+          memcpy(tx_buffer_unit_[subscript_to_add_].tx_data_, new_data + TX_BUFFER_WIDTH * (packet_num - 1), new_size % TX_BUFFER_WIDTH);
+
+          subscript_to_add_++;
+          subscript_to_add_ %= TX_BUFFER_SIZE;
+        }
+    }
+
+  if(tx_mutex_ != NULL) osMutexRelease(*tx_mutex_);
+}
--- aerial_robot_nerve/spinal/src/ros_lib/STMF7Hardware.h
+++ aerial_robot_nerve/spinal/src/ros_lib/STMF7Hardware.h
@@ -15,79 +15,9 @@
 #include "cmsis_os.h"
 
 #define BUFFER_EMPTY  -1
-#define TX_BUFFER_SIZE 50 // can not be too small, to handle the first void negotiateTopics() process which send the topic information at the same time.
-#define TX_BUFFER_WIDTH 512 // can not be too small, to handle the large size topic such as board information
+#define TX_BUFFER_SIZE 128
+#define TX_BUFFER_WIDTH 64 // byte
 #define RX_BUFFER_SIZE 512
-
-template <typename T,  int SIZE>
-class RingBuffer
-{
-public:
-  RingBuffer()
-  {
-    byte_in_progress_ = 0;
-    byte_to_add_ = 0;
-    buffer_length_ = (uint16_t)SIZE;
-  }
-  ~RingBuffer(){  }
-
-  bool pop(T& pop_value)
-  {
-    if (byte_in_progress_ != byte_to_add_)
-      {
-        pop_value =  buf_[byte_in_progress_];
-
-        byte_in_progress_++;
-        if (byte_in_progress_ == buffer_length_)
-          byte_in_progress_ = 0;
-
-        return true;
-      }
-    return false;
-  }
-
-  bool push(T new_value)
-  {
-    // the process node should have higher priority than the rx it callback
-#if 0
-    if ((byte_in_progress_ == (byte_to_add_ + 1)) || ( (byte_to_add_ == (buffer_length_ - 1) )&& (byte_in_progress_ == 0)) ) return false;
-#endif
-
-    buf_[byte_to_add_] = new_value;
-
-    byte_to_add_++;
-
-    if (byte_to_add_ == buffer_length_)
-      {
-        byte_to_add_ = 0;
-      }
-    return true;
-  }
-
-  uint16_t length()
-  {
-    if(byte_to_add_ - byte_in_progress_ >= 0)
-      return (byte_to_add_ - byte_in_progress_);
-    else 
-      return (byte_to_add_ - (buffer_length_ - byte_in_progress_));
-  }
-
-private:
-  T buf_[SIZE];
-  int16_t byte_in_progress_, byte_to_add_;
-  uint16_t buffer_length_;
-};
-
-
-
-/* RX */
-namespace rx
-{
-  void init(UART_HandleTypeDef *huart);
-  int read();
-  bool available();
-
-};
 
 /* TX */
 template<int BUFFER_LENGTH>
@@ -95,19 +25,6 @@ struct TxBufferUnit{
   uint8_t tx_data_[BUFFER_LENGTH];//this means each txdata packet should be shorter than 255
   uint16_t tx_len_;
 } ;
-
-namespace tx
-{
-  void init(UART_HandleTypeDef *huart, osMutexId *mutex);
-  int publish();
-  void write(uint8_t * new_data, unsigned int new_size);
-
-  uint8_t subscriptInProgress();
-  uint8_t subscriptToAdd();
-  uint8_t  getCurrentTransmitBufferLen();
-  uint8_t*  getCurrentTransmitBufferP();
-};
-
 
 class STMF7Hardware {
 public:
@@ -126,31 +43,34 @@ public:
     // do nothing
   }
 
-  void init(serial_class* huart, mutex_class* mutex = NULL)
+  void init(serial_class* huart, mutex_class* mutex = NULL);
+
+  uint32_t time()
   {
-    rx::init(huart);
-    tx::init(huart, mutex);
+    return HAL_GetTick();
   }
 
-  bool rx_available()
-  {
-    return rx::available();
-  }
+  /* rx */
+  bool rx_available();
+  int read();
 
-  int read()
-  {
-    return rx::read();
-  }
+  /* tx */
+  void write(uint8_t* data, unsigned int length);
+  int publish();
 
-  void write(uint8_t* data, unsigned int length){
-    tx::write(data, length);
-  }
+private:
+  UART_HandleTypeDef *huart_;
 
-  int publish(){
-    return tx::publish();
-  }
+  /* rx */
+  uint8_t rx_buf_[RX_BUFFER_SIZE];
+  uint32_t rd_ptr_ = 0;
 
-  uint32_t time(){return HAL_GetTick();}
+  /* tx */
+  osMutexId *tx_mutex_;
+  struct TxBufferUnit<TX_BUFFER_WIDTH> tx_buffer_unit_[TX_BUFFER_SIZE];
+  uint8_t subscript_in_progress_;
+  uint8_t subscript_to_add_;
+
 };
 
 
