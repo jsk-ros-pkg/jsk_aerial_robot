80a621236fd698a0d67d94fff1b2b480905b238e
Moju Zhao <chou@jsk.imi.i.u-tokyo.ac.jp>
2021-06-15 22:21:48 +0900

    [Neuron] correct the configuration about Timer for motor PWM. Need -1 for counter period.

--- aerial_robot_nerve/neuron/neuron_f4/Src/tim.c
+++ aerial_robot_nerve/neuron/neuron_f4/Src/tim.c
@@ -35,9 +35,9 @@ void MX_TIM1_Init(void)
   TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
 
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 1;
+  htim1.Init.Prescaler = 2-1;
   htim1.Init.CounterMode = TIM_COUNTERMODE_CENTERALIGNED1;
-  htim1.Init.Period = 36000;
+  htim1.Init.Period = 36000-1;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
