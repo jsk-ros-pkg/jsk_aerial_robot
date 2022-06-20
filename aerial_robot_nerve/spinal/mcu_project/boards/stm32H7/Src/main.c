56313fa000eaa34bcc543c2a3ddec74c936ae888
Moju Zhao <chou@jsk.imi.i.u-tokyo.ac.jp>
2022-06-20 18:35:01 +0900

    Correct the configuration for PWM setting in STM32H7

--- aerial_robot_nerve/spinal/mcu_project/boards/stm32H7/Src/main.c
+++ aerial_robot_nerve/spinal/mcu_project/boards/stm32H7/Src/main.c
@@ -789,7 +789,7 @@ static void MX_TIM4_Init(void)
 
   /* USER CODE END TIM4_Init 1 */
   htim4.Instance = TIM4;
-  htim4.Init.Prescaler = 3;
+  htim4.Init.Prescaler = 7;
   htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim4.Init.Period = 50000;
   htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
