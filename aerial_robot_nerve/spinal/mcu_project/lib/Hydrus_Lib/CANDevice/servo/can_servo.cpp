6df0636c864c40f13bb4340e78816b52f0045156
Moju Zhao <chou@jsk.imi.i.u-tokyo.ac.jp>
2022-10-07 21:17:37 +0900

    [Spinal][Servo][WIP] enable send goal current from rosserial to spinal, and finally to neuron

--- aerial_robot_nerve/spinal/mcu_project/lib/Hydrus_Lib/CANDevice/servo/can_servo.cpp
+++ aerial_robot_nerve/spinal/mcu_project/lib/Hydrus_Lib/CANDevice/servo/can_servo.cpp
@@ -20,6 +20,13 @@ void CANServo::sendData()
 		target_angle[i] = ((servo_[i].torque_enable_ ? 1 : 0) << 15) | (servo_[i].goal_position_ & 0x7FFF);
 	}
 	sendMessage(CAN::MESSAGEID_RECEIVE_SERVO_ANGLE, m_slave_id, servo_.size() * 2, reinterpret_cast<uint8_t*>(target_angle), 0);
+
+	int16_t target_current[4];
+	for (unsigned int i = 0 ; i < servo_.size(); i++) {
+		target_current[i] = servo_[i].goal_current_;
+	}
+	sendMessage(CAN::MESSAGEID_RECEIVE_SERVO_CURRENT, m_slave_id, servo_.size() * 2, reinterpret_cast<uint8_t*>(target_current), 0);
+
 }
 
 void CANServo::receiveDataCallback(uint8_t slave_id, uint8_t message_id, uint32_t DLC, uint8_t* data)
