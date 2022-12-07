fb61c00c20501479226ba92f771e3fb663347742
Moju Zhao <chou@jsk.imi.i.u-tokyo.ac.jp>
2022-12-08 00:04:41 +0900

    [Spider] rename and update the script to replay rosbag

---
+++ robots/tiger/bin/rosbag_play_tiger.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rosbag play $1 --pause --clock /tiger/joints_ctrl:=/tiger/joints_ctrl_orig /tiger/joints_torque_ctrl:=/tiger/joints_torque_ctrl_orig /tiger/debug/pose/pid:=/tiger/debug/pose/pid_orig /tiger/four_axes/command:=/tiger/four_axes/command_orig /tiger/debug/target_vectoring_force:=/tiger/debug/target_vectoring_force_orig /tiger/servo/target_states:=/tiger/servo/target_states_orig ${@:2}
