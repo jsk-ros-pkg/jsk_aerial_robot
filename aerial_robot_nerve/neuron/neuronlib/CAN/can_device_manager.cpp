14747e4ff3077506c913441a6cb7c6f82d94cae3
Moju Zhao <chou@jsk.imi.i.u-tokyo.ac.jp>
2023-02-24 02:59:35 +0900

    [Neuron][CAN] refactor the library for CAN to support STM32G4

--- aerial_robot_nerve/neuron/neuronlib/CAN/can_device_manager.cpp
+++ aerial_robot_nerve/neuron/neuronlib/CAN/can_device_manager.cpp
@@ -10,88 +10,78 @@
 
 namespace CANDeviceManager
 {
-	namespace {
-		std::array<CANDevice*, (1 << CAN::DEVICE_ID_LEN)> can_device_list;
-		int can_timeout_count = 0;
-		int internal_count = 0;
-		constexpr int CAN_MAX_TIMEOUT_COUNT = 100;
-		GPIO_TypeDef* m_GPIOx;
-		uint16_t m_GPIO_Pin;
- 		osMailQId* canMsgMailHandle = NULL;
-	}
+  namespace {
+    std::array<CANDevice*, (1 << CAN::DEVICE_ID_LEN)> can_device_list;
+    int can_timeout_count = 0;
+    int internal_count = 0;
+    constexpr int CAN_MAX_TIMEOUT_COUNT = 100;
+    GPIO_TypeDef* m_GPIOx;
+    uint16_t m_GPIO_Pin;
+    osMailQId* canMsgMailHandle = NULL;
+  }
 
-  	void CAN_START()
-	{
-		CAN::CAN_START();
-	}
+  void CAN_START()
+  {
+    CAN::CAN_START();
+  }
 
-  	void CAN_DEACTIVATE()
-	{
-		CAN::CAN_DEACTIVATE();
-	}
+  void init(CAN_GeranlHandleTypeDef* hcan, uint8_t slave_id, GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
+  {
+    CAN::init(hcan, slave_id);
+    m_GPIOx = GPIOx;
+    m_GPIO_Pin = GPIO_Pin;
+    CAN_START();
+  }
 
-  	void CAN_ACTIVATE()
-	{
-		CAN::CAN_ACTIVATE();
-	}
+  void useRTOS(osMailQId* handle)
+  {
+    canMsgMailHandle = handle;
+  }
 
-	void init(CAN_HandleTypeDef* hcan, uint8_t slave_id, GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
-	{
-		CAN::init(hcan, slave_id);
-		m_GPIOx = GPIOx;
-		m_GPIO_Pin = GPIO_Pin;
-                CAN_START();
-	}
+  int makeCommunicationId(uint8_t device_id, uint8_t slave_id)
+  {
+    return static_cast<int>((device_id << 3) | slave_id);
+  }
 
-	void useRTOS(osMailQId* handle)
-	{
-		canMsgMailHandle = handle;
-	}
+  void addDevice(CANDevice* device)
+  {
+    can_device_list[device->getDeviceId()] = device;
+  }
 
-	int makeCommunicationId(uint8_t device_id, uint8_t slave_id)
-	{
-		return static_cast<int>((device_id << 3) | slave_id);
-	}
+  void tick(int cycle /* ms */)
+  {
+    can_timeout_count++;
+    if (can_timeout_count <= CAN_MAX_TIMEOUT_COUNT) {
+      internal_count++;
+      if (internal_count == (1000 / cycle) - 1) {
+        internal_count = 0;
+      }
+      if (internal_count == 0) {
+        HAL_GPIO_TogglePin(m_GPIOx, m_GPIO_Pin);
+      }
+    }
+  }
 
-	void addDevice(CANDevice* device)
-	{
-		can_device_list[device->getDeviceId()] = device;
-	}
+  void resetTick()
+  {
+    internal_count = 0;
+    HAL_GPIO_WritePin(m_GPIOx, m_GPIO_Pin, GPIO_PIN_RESET);
+  }
 
-	void tick(int cycle /* ms */)
-	{
-		can_timeout_count++;
-		if (can_timeout_count <= CAN_MAX_TIMEOUT_COUNT) {
-			internal_count++;
-			if (internal_count == (1000 / cycle) - 1) {
-				internal_count = 0;
-			}
-			if (internal_count == 0) {
-				HAL_GPIO_TogglePin(m_GPIOx, m_GPIO_Pin);
-			}
-		}
-	}
+  bool isTimeout()
+  {
+    return can_timeout_count > CAN_MAX_TIMEOUT_COUNT;
+  }
 
-	void resetTick()
-	{
-		internal_count = 0;
-		HAL_GPIO_WritePin(m_GPIOx, m_GPIO_Pin, GPIO_PIN_RESET);
-	}
+  __weak void userSendMessages()
+  {
 
-	bool isTimeout()
-	{
-		return can_timeout_count > CAN_MAX_TIMEOUT_COUNT;
-	}
+  }
 
-	__weak void userSendMessages()
-	{
+  __weak void userReceiveMessagesCallback(uint8_t slave_id, uint8_t device_id, uint8_t message_id, uint32_t DLC, uint8_t* data)
+  {
 
-	}
-
-	__weak void userReceiveMessagesCallback(uint8_t slave_id, uint8_t device_id, uint8_t message_id, uint32_t DLC, uint8_t* data)
-	{
-
-	}
+  }
 
   void receiveMessage(can_msg msg)
   {
@@ -106,6 +96,7 @@ namespace CANDeviceManager
   }
 }
 
+#if defined(STM32F1) || defined(STM32F4)
 void HAL_CAN_RxFifo1MsgPendingCallback(CAN_HandleTypeDef* hcan)
 {
   CANDeviceManager::can_timeout_count = 0;
@@ -132,6 +123,37 @@ void HAL_CAN_RxFifo1MsgPendingCallback(CAN_HandleTypeDef* hcan)
         }
     }
 }
+#endif
+
+#ifdef STM32G4
+void HAL_FDCAN_RxFifo1Callback(FDCAN_HandleTypeDef *hfdcan, uint32_t RxFifo0ITs)
+{
+
+  CANDeviceManager::can_timeout_count = 0;
+
+  if(CANDeviceManager::canMsgMailHandle == NULL)
+    {
+      can_msg msg;
+      if (HAL_FDCAN_GetRxMessage(hfdcan, FDCAN_RX_FIFO1, &msg.rx_header, msg.rx_data) == HAL_OK)
+        {
+          /* directly call receivemessage, since RTOS is not initialized */
+          CANDeviceManager::receiveMessage(msg);
+        }
+    }
+  else
+    {
+      /* add data to RTOS queue */
+      can_msg *msg = (can_msg *)osMailAlloc(*CANDeviceManager::canMsgMailHandle, 0);
+
+      if (msg == NULL) return; //allocation is not ready, exit
+
+      if (HAL_FDCAN_GetRxMessage(hfdcan, FDCAN_RX_FIFO1, &(msg->rx_header), msg->rx_data) == HAL_OK)
+        {
+          osMailPut(*CANDeviceManager::canMsgMailHandle, msg);
+        }
+    }
+}
+#endif
 
 extern "C"
 {
