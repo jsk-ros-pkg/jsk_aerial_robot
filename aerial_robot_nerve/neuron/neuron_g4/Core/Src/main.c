35c476cd4cc07ac65ce839741f291f04301fa210
Moju Zhao <chou@jsk.imi.i.u-tokyo.ac.jp>
2023-02-23 16:39:30 +0900

    [Neuron][STM32G4][I2C] modify the setting of I2C for servo external magnetic encoder

--- aerial_robot_nerve/neuron/neuron_g4/Core/Src/main.c
+++ aerial_robot_nerve/neuron/neuron_g4/Core/Src/main.c
@@ -406,7 +406,7 @@ static void MX_I2C1_Init(void)
 
   /* USER CODE END I2C1_Init 1 */
   hi2c1.Instance = I2C1;
-  hi2c1.Init.Timing = 0x30909DEC;
+  hi2c1.Init.Timing = 0x30D2153A;
   hi2c1.Init.OwnAddress1 = 0;
   hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
   hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
