d5aed7662ab17b353c997c1daec64ef2d991c32e
Moju Zhao <chou@jsk.imi.i.u-tokyo.ac.jp>
2023-02-24 22:35:04 +0900

    Revert "[Neuron][STM32G4][PWM][WIP] enable the usage of PWM"
    This reverts commit 5c7cf52b285b9b9460ec36888a44bc7b3a8b53b0.

--- aerial_robot_nerve/neuron/neuronlib/Motor/motor.h
+++ aerial_robot_nerve/neuron/neuronlib/Motor/motor.h
@@ -9,31 +9,24 @@
 #ifndef APPLICATION_MOTOR_TEMP_MOTOR_H_
 #define APPLICATION_MOTOR_TEMP_MOTOR_H_
 
-#if defined(STM32F103xx)
-#include "stm32f1xx_hal.h"
-#elif defined(STM32F413xx)
-#include "stm32f4xx_hal.h"
-#elif defined(STM32G473xx)
-#include "stm32g4xx_hal.h"
-#else
-#error "please specify the STM32 series"
-#endif
-
+#include "can_device_manager.h"
 
 #define IDLE_DUTY 0.5f // 1000[usec] / 2000[usec]
 #define MIN_DUTY_DEFAULT  0.55f   //1100 / 2000
 #define MAX_DUTY_DEFAULT 0.95f // 1900 / 2000
 
-class Motor
+class Motor : public CANDevice
 {
 private:
 	  TIM_HandleTypeDef* pwm_htim_;
 	  void setPwm(uint16_t pwm);
 public:
 	Motor(){}
-	Motor(uint8_t slave_id){}
+	Motor(uint8_t slave_id):CANDevice(CAN::DEVICEID_MOTOR, slave_id){}
 	void init(TIM_HandleTypeDef* htim);
 	void update();
+	void sendData() override;
+	void receiveDataCallback(uint8_t message_id, uint32_t DLC, uint8_t* data) override;
 };
 
 #endif /* APPLICATION_MOTOR_H_ */
