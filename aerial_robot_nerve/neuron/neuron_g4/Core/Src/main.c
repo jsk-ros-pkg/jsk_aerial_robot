2b3b3b91cdeeaa5eac39f482452d6ac1885716a3
Moju Zhao <chou@jsk.imi.i.u-tokyo.ac.jp>
2023-02-24 18:14:10 +0900

    [Neuron][STM32G4][PWM] fix the setting for PWM mode

--- aerial_robot_nerve/neuron/neuron_g4/Core/Src/main.c
+++ aerial_robot_nerve/neuron/neuron_g4/Core/Src/main.c
@@ -509,7 +509,7 @@ static void MX_TIM1_Init(void)
   /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
   htim1.Init.Prescaler = 4-1;
-  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_CENTERALIGNED1;
   htim1.Init.Period = 39999;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
