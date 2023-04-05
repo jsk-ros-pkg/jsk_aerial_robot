e4f884dd94e730afe619aa8c7219da5e99e6c527
sugihara <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2023-04-05 21:55:03 +0900

    [birotor][WIP] update control of birotor

--- robots/birotor/include/birotor/control/birotor_controller.h
+++ robots/birotor/include/birotor/control/birotor_controller.h
@@ -3,12 +3,34 @@
 #pragma once
 
 #include <aerial_robot_control/control/under_actuated_controller.h>
+#include <aerial_robot_estimation/state_estimation.h>
+
 namespace aerial_robot_control
 {
-  class BirotorController: public UnderActuatedController
+  class BirotorController: public PoseLinearController
   {
   public:
-    BirotorController(){}
-    virtual ~BirotorController() = default;
+    BirotorController();
+    ~BirotorController() = default;
+
+    void initialize(ros::NodeHandle nh, ros::NodeHandle nhp,
+                    boost::shared_ptr<aerial_robot_model::RobotModel> robot_model,
+                    boost::shared_ptr<aerial_robot_estimation::StateEstimator> estimator,
+                    boost::shared_ptr<aerial_robot_navigation::BaseNavigator> navigator,
+                    double ctrl_loop_rate
+                    ) override;
+
+  private:
+    ros::Publisher gimbal_control_pub_;
+    boost::shared_ptr<aerial_robot_model::RobotModel> robot_model_;
+
+    std::vector<float> target_base_thrust_;
+    std::vector<double> target_gimbal_angles_;
+    bool hovering_approximate_;
+
+    void controlCore() override;
+    void sendCmd();
+    void rosParamInit();
+
   };
 };
--- robots/birotor/src/control/birotor_controller.cpp
+++ robots/birotor/src/control/birotor_controller.cpp
@@ -1,5 +1,98 @@
 #include <birotor/control/birotor_controller.h>
 
+using namespace aerial_robot_control;
+
+BirotorController::BirotorController():
+  PoseLinearController()
+{
+}
+
+void BirotorController::initialize(ros::NodeHandle nh, ros::NodeHandle nhp,
+                                   boost::shared_ptr<aerial_robot_model::RobotModel> robot_model,
+                                   boost::shared_ptr<aerial_robot_estimation::StateEstimator> estimator,
+                                   boost::shared_ptr<aerial_robot_navigation::BaseNavigator> navigator,
+                                   double ctrl_loop_rate
+                                   )
+{
+  PoseLinearController::initialize(nh, nhp, robot_model, estimator, navigator, ctrl_loop_rate);
+
+  rosParamInit();
+
+  target_base_thrust_.resize(motor_num_);
+  target_gimbal_angles_.resize(motor_num_, 0);
+
+  gimbal_control_pub_ = nh_.advertise<sensor_msgs::JointState>("gimbals_ctrl", 1);
+
+}
+
+void rosParamInit()
+{
+  ros::NodeHandle control_nh(nh_, "controller");
+  getParam<bool>(control_nh, "hovering_approximate", hovering_approximate_, false);
+}
+
+void BirotorController::controlCore()
+{
+  PoseLinearController::controlCore();
+
+  const std::vector<Eigen::Vector3d> rotors_origin = robot_model_->getRotorsOriginFromCog<Eigen::Vector3d>();
+  const std::vector<Eigen::Vector3d> rotors_normal = robot_model_->getRotorsNormalFromCog<Eigen::Vector3d>();
+
+  tf::Matrix3x3 uav_rot = estimator_->getOrientation(Frame::COG, estimate_mode_);
+  tf::Vector3 target_acc_w(pid_controllers_.at(X).result(),
+                           pid_controllers_.at(Y).result(),
+                           pid_controllers_.at(Z).result());
+  tf::Vector3 target_acc_cog = uav_rot.inverse() * target_acc_w;
+
+  double target_ang_acc_x = pid_controllers_.at(ROLL).result();
+  double target_ang_acc_y = pid_controllers_.at(PITCH).result();
+  double target_ang_acc_z = pid_controllers_.at(YAW).result();
+
+  tf::Vector3 target_acc_dash = (tf::Matrix3x3(tf::createQuaternionFromYaw(rpy_.z()))).inverse() * target_acc_w;
+
+  float target_roll;
+  float target_pitch;
+  if(hovering_approximate_)
+    {
+      target_pitch = target_acc_dash.x() / aerial_robot_estimation::G;
+      target_roll = -target_acc_dash.y() / aerial_robot_estimation::G;
+      navigator_->setTargetRoll(target_roll);
+      navigator_->setTargetPitch(target_pitch);
+    }
+  else
+    {
+      target_pitch = atan2(target_acc_dash.x(), target_acc_dash.z());
+      target_roll = atan2(-target_acc_dash.y(), sqrt(target_acc_dash.x() * target_acc_dash.x() + target_acc_dash.z() * target_acc_dash.z()));
+      navigator_->setTargetRoll(target_roll);
+      navigator_->setTargetPitch(target_pitch);
+    }
+
+
+  pid_msg_.roll.total.at(0) = target_ang_acc_x;
+  pid_msg_.roll.p_term.at(0) = pid_controllers_.at(ROLL).getPTerm();
+  pid_msg_.roll.i_term.at(0) = pid_controllers_.at(ROLL).getITerm();
+  pid_msg_.roll.d_term.at(0) = pid_controllers_.at(ROLL).getDTerm();
+  pid_msg_.roll.target_p = target_rpy_.x();
+  pid_msg_.roll.err_p = pid_controllers_.at(ROLL).getErrP();
+  pid_msg_.roll.target_d = target_omega_.x();
+  pid_msg_.roll.err_d = pid_controllers_.at(ROLL).getErrD();
+  pid_msg_.pitch.total.at(0) = target_ang_acc_y;
+  pid_msg_.pitch.p_term.at(0) = pid_controllers_.at(PITCH).getPTerm();
+  pid_msg_.pitch.i_term.at(0) = pid_controllers_.at(PITCH).getITerm();
+  pid_msg_.pitch.d_term.at(0) = pid_controllers_.at(PITCH).getDTerm();
+  pid_msg_.pitch.target_p = target_rpy_.y();
+  pid_msg_.pitch.err_p = pid_controllers_.at(PITCH).getErrP();
+  pid_msg_.pitch.target_d = target_omega_.y();
+  pid_msg_.pitch.err_d = pid_controllers_.at(PITCH).getErrD();
+
+
+}
+
+void BirotorController::sendCmd()
+{
+  PoseLinearController::sendCmd();
+
+}
 
 /* plugin registration */
 #include <pluginlib/class_list_macros.h>
