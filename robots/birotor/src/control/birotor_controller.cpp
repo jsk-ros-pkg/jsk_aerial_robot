e918dcb3f069a27da25c7384484d02eadd286ba5
sugihara <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2023-04-06 15:09:09 +0900

    [birotor][control] implement uni vectoring underactuated control for birotor

--- robots/birotor/src/control/birotor_controller.cpp
+++ robots/birotor/src/control/birotor_controller.cpp
@@ -21,11 +21,13 @@ void BirotorController::initialize(ros::NodeHandle nh, ros::NodeHandle nhp,
   target_base_thrust_.resize(motor_num_);
   target_gimbal_angles_.resize(motor_num_, 0);
 
+  flight_cmd_pub_ = nh_.advertise<spinal::FourAxisCommand>("four_axes/command", 1);
   gimbal_control_pub_ = nh_.advertise<sensor_msgs::JointState>("gimbals_ctrl", 1);
+  target_vectoring_force_pub_ = nh_.advertise<std_msgs::Float32MultiArray>("debug/target_vectoring_force", 1);
 
 }
 
-void rosParamInit()
+void BirotorController::rosParamInit()
 {
   ros::NodeHandle control_nh(nh_, "controller");
   getParam<bool>(control_nh, "hovering_approximate", hovering_approximate_, false);
@@ -35,14 +37,9 @@ void BirotorController::controlCore()
 {
   PoseLinearController::controlCore();
 
-  const std::vector<Eigen::Vector3d> rotors_origin = robot_model_->getRotorsOriginFromCog<Eigen::Vector3d>();
-  const std::vector<Eigen::Vector3d> rotors_normal = robot_model_->getRotorsNormalFromCog<Eigen::Vector3d>();
-
-  tf::Matrix3x3 uav_rot = estimator_->getOrientation(Frame::COG, estimate_mode_);
   tf::Vector3 target_acc_w(pid_controllers_.at(X).result(),
                            pid_controllers_.at(Y).result(),
                            pid_controllers_.at(Z).result());
-  tf::Vector3 target_acc_cog = uav_rot.inverse() * target_acc_w;
 
   double target_ang_acc_x = pid_controllers_.at(ROLL).result();
   double target_ang_acc_y = pid_controllers_.at(PITCH).result();
@@ -67,6 +64,10 @@ void BirotorController::controlCore()
       navigator_->setTargetPitch(target_pitch);
     }
 
+  Eigen::VectorXd target_wrench_acc_cog = Eigen::VectorXd::Zero(4);
+
+  target_wrench_acc_cog(0) = target_acc_dash.z();
+  target_wrench_acc_cog.tail(3) = Eigen::Vector3d(pid_controllers_.at(ROLL).result(), pid_controllers_.at(PITCH).result(), pid_controllers_.at(YAW).result());
 
   pid_msg_.roll.total.at(0) = target_ang_acc_x;
   pid_msg_.roll.p_term.at(0) = pid_controllers_.at(ROLL).getPTerm();
@@ -85,12 +86,68 @@ void BirotorController::controlCore()
   pid_msg_.pitch.target_d = target_omega_.y();
   pid_msg_.pitch.err_d = pid_controllers_.at(PITCH).getErrD();
 
+  Eigen::MatrixXd full_q_mat = Eigen::MatrixXd::Zero(6, 2 * motor_num_);
+
+  Eigen::Matrix3d inertia_inv = robot_model_->getInertia<Eigen::Matrix3d>().inverse();
+  double mass_inv = 1 / robot_model_->getMass();
+
+  double t = ros::Time::now().toSec();
+
+  std::vector<Eigen::Vector3d> rotors_origin_from_cog = robot_model_->getRotorsOriginFromCog<Eigen::Vector3d>();
+  Eigen::MatrixXd wrench_map = Eigen::MatrixXd::Zero(6, 3);
+  wrench_map.block(0, 0, 3, 3) =  Eigen::MatrixXd::Identity(3, 3);
+  Eigen::MatrixXd mask(3, 2);
+  mask << 1, 0, 0, 0, 0, 1;
+  int last_col = 0;
+  for(int i = 0; i < motor_num_; i++){
+    wrench_map.block(3, 0, 3, 3) = aerial_robot_model::skew(rotors_origin_from_cog.at(i));
+    full_q_mat.middleCols(last_col, 2) = wrench_map * mask;
+    last_col += 2;
+  }
+
+  Eigen::MatrixXd allocation_mat = Eigen::MatrixXd::Zero(4, 4);
+  full_q_mat.topRows(3) = mass_inv * full_q_mat.topRows(3);
+  full_q_mat.bottomRows(3) = inertia_inv * full_q_mat.bottomRows(3);
+
+  allocation_mat.topRows(1) = full_q_mat.row(2);
+  allocation_mat.bottomRows(3) = full_q_mat.bottomRows(3);
+
+  Eigen::MatrixXd allocation_mat_inv = allocation_mat.inverse();
+  target_vectoring_f_ = allocation_mat_inv * target_wrench_acc_cog;
+
+  last_col = 0;
+  for(int i = 0; i < motor_num_; i++){
+    Eigen::VectorXd f_i = target_vectoring_f_.segment(last_col, 2);
+    target_base_thrust_.at(i) = f_i.norm();
+
+    target_gimbal_angles_.at(i) = atan2(f_i(0), f_i(1));
+
+    last_col += 2;
+  }
+
 
 }
 
 void BirotorController::sendCmd()
 {
   PoseLinearController::sendCmd();
+
+  spinal::FourAxisCommand flight_command_data;
+  flight_command_data.base_thrust = target_base_thrust_;
+  flight_cmd_pub_.publish(flight_command_data);
+
+  sensor_msgs::JointState gimbal_control_msg;
+  gimbal_control_msg.header.stamp = ros::Time::now();
+  for(int i = 0; i < motor_num_; i++){
+    gimbal_control_msg.position.push_back(target_gimbal_angles_.at(i));
+  }
+  gimbal_control_pub_.publish(gimbal_control_msg);
+
+  std_msgs::Float32MultiArray target_vectoring_force_msg;
+  for(int i = 0; i < target_vectoring_f_.size(); i++){
+    target_vectoring_force_msg.data.push_back(target_vectoring_f_(i));
+  }
+  target_vectoring_force_pub_.publish(target_vectoring_force_msg);
 
 }
 
