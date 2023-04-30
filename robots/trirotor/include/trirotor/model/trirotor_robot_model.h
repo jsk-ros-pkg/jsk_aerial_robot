2057df6876c1abbab2310c98837f4e33aa51b271
sugihara <sugihara-junichiro789@g.ecc.u-tokyo.ac.jp>
2023-04-30 17:40:02 +0900

    [Trirotor][model] override model update function in order to consider baselink rotation.

--- robots/trirotor/include/trirotor/model/trirotor_robot_model.h
+++ robots/trirotor/include/trirotor/model/trirotor_robot_model.h
@@ -6,11 +6,30 @@
 
 using namespace aerial_robot_model;
 
-class TrirotorRobotModel : public aerial_robot_model::RobotModel {
+class TrirotorRobotModel : public aerial_robot_model::RobotModel{
 public:
   TrirotorRobotModel(bool init_with_rosparam = true,
                     bool verbose = false,
                     double fc_t_min_thre = 0,
                     double epsilon = 10);
   virtual ~TrirotorRobotModel() = default;
+
+  std::vector<Eigen::MatrixXd> getRotorMasks() const { return rotor_masks_;};
+  template <class T> std::vector<T> getLinksRotationFromCog();
+
+private:
+  void updateRobotModelImpl(const KDL::JntArray& joint_positions) override;
+  void calcThrustMask();
+
+  std::vector<Eigen::MatrixXd> rotor_masks_;
+  KDL::JntArray gimbal_processed_joint_;
+  std::vector<KDL::Rotation> links_rotation_from_cog_;
+  std::mutex links_rotation_mutex_;
+
 };
+
+template<> inline std::vector<KDL::Rotation> TrirotorRobotModel::getLinksRotationFromCog()
+{
+  std::lock_guard<std::mutex> lock(links_rotation_mutex_);
+  return links_rotation_from_cog_;
+}
