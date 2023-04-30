e70af0dcd9ad6b098e0f63ef2e8e84b934455d10
sugihara <sugihara-junichiro789@g.ecc.u-tokyo.ac.jp>
2023-05-01 00:30:52 +0900

    [Trirotor][Cotroller] add thrust mask calculation function

--- robots/trirotor/src/model/trirotor_robot_model.cpp
+++ robots/trirotor/src/model/trirotor_robot_model.cpp
@@ -7,6 +7,30 @@ TrirotorRobotModel::TrirotorRobotModel(bool init_with_rosparam, bool verbose, do
   const int joint_num = getJointNum();
 
   links_rotation_from_cog_.resize(rotor_num);
+  mask_calc_flag_ = false;
+}
+
+void TrirotorRobotModel::calcThrustMask()
+{
+  if(mask_calc_flag_) return;
+  /* this process is only for untransformable robots */
+  const auto& segment_map = getTree().getSegments();
+  const auto& joint_names = getJointNames();
+  const auto& joint_segment_map = getJointSegmentMap();
+  rotor_masks_.resize(RobotModel::getRotorNum());
+  for (int i = 0; i < joint_names.size(); i++)
+    {
+      Eigen::MatrixXd mask(3, 2);
+      std::vector<std::string> joint_i_child_segments = joint_segment_map.at(joint_names[i]);
+      std::string joint_i_child_segment_name = joint_i_child_segments.at(0);
+      const KDL::Segment& joint_i_child_segment = GetTreeElementSegment(segment_map.at(joint_i_child_segment_name));
+      Eigen::Vector3d gimbal_i_axis = aerial_robot_model::kdlToEigen(joint_i_child_segment.getJoint().JointAxis()); //supposing that the axis of gimbal is along coordinate axis
+      double m_00 = 1.0 - std::abs(gimbal_i_axis[0]);
+      double m_10 = 1.0 - std::abs(gimbal_i_axis[1]);
+      mask << m_00, 0, m_10, 0, 0, 1;
+      rotor_masks_[i] = mask;
+    }
+  mask_calc_flag_ = true;
 }
 
 void TrirotorRobotModel::updateRobotModelImpl(const KDL::JntArray& joint_positions)
@@ -27,6 +51,7 @@ void TrirotorRobotModel::updateRobotModelImpl(const KDL::JntArray& joint_positio
   /* normal robot model update */
   RobotModel::updateRobotModelImpl(gimbal_processed_joint_);
 
+  calcThrustMask();
 }
 
 /* plugin registration */
