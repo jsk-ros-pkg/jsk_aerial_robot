2304a96880689bba0bb2eec5a1dd522733444416
sugihara <sugihara-junichiro789@g.ecc.u-tokyo.ac.jp>
2023-04-30 17:42:59 +0900

    [Trirotor][Controller] add coordination conversion of target wrench.

--- robots/trirotor/include/trirotor/control/trirotor_controller.h
+++ robots/trirotor/include/trirotor/control/trirotor_controller.h
@@ -6,6 +6,7 @@
 #include <aerial_robot_estimation/state_estimation.h>
 #include <std_msgs/Float32MultiArray.h>
 #include <spinal/FourAxisCommand.h>
+#include <trirotor/model/trirotor_robot_model.h>
 
 namespace aerial_robot_control
 {
@@ -27,7 +28,7 @@ namespace aerial_robot_control
     ros::Publisher gimbal_control_pub_;
     ros::Publisher target_vectoring_force_pub_;
 
-    // boost::shared_ptr<aerial_robot_model::RobotModel> robot_model_for_control_;
+    boost::shared_ptr<TrirotorRobotModel> trirotor_robot_model_;
 
     std::vector<float> target_base_thrust_;
     std::vector<double> target_gimbal_angles_;
