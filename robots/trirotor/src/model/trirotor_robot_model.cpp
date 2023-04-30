4ffe722b7bb908b5e16fc6e10ef038e30f97a0e4
sugihara <sugihara-junichiro789@g.ecc.u-tokyo.ac.jp>
2023-04-30 17:40:02 +0900

    [Trirotor][model] override model update function in order to consider baselink rotation.

--- robots/trirotor/src/model/trirotor_robot_model.cpp
+++ robots/trirotor/src/model/trirotor_robot_model.cpp
@@ -3,6 +3,30 @@
 TrirotorRobotModel::TrirotorRobotModel(bool init_with_rosparam, bool verbose, double fc_t_min_thre, double epsilon) :
   RobotModel(init_with_rosparam, verbose, fc_t_min_thre, epsilon)
 {
+  const int rotor_num = getRotorNum();
+  const int joint_num = getJointNum();
+
+  links_rotation_from_cog_.resize(rotor_num);
+}
+
+void TrirotorRobotModel::updateRobotModelImpl(const KDL::JntArray& joint_positions)
+{
+  KDL::TreeFkSolverPos_recursive fk_solver(getTree());
+  /* special process */
+  KDL::Frame f_baselink;
+  fk_solver.JntToCart(joint_positions, f_baselink, getBaselinkName());
+  const KDL::Rotation cog_frame = f_baselink.M * getCogDesireOrientation<KDL::Rotation>().Inverse();
+  const auto joint_index_map = getJointIndexMap();
+  gimbal_processed_joint_ = joint_positions;
+  /* link based on COG */
+  for(int i = 0; i < getRotorNum(); ++i)
+    {
+      KDL::Frame f;
+      links_rotation_from_cog_[i] = cog_frame.Inverse() * f.M;
+    }
+  /* normal robot model update */
+  RobotModel::updateRobotModelImpl(gimbal_processed_joint_);
+
 }
 
 /* plugin registration */
