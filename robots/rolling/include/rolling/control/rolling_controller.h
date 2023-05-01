8dcd0089523d639fe9d3ffac35c6e7a8344cf5d4
sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2023-05-01 19:53:02 +0900

    [WIP][rolling] update controller to fully-actuated control

--- robots/rolling/include/rolling/control/rolling_controller.h
+++ robots/rolling/include/rolling/control/rolling_controller.h
@@ -5,6 +5,7 @@
 #include <aerial_robot_control/control/pose_linear_controller.h>
 #include <spinal/FourAxisCommand.h>
 #include <std_msgs/Float32MultiArray.h>
+#include <rolling/model/rolling_robot_model.h>
 
 namespace aerial_robot_control
 {
@@ -21,11 +22,14 @@ namespace aerial_robot_control
                     double ctrl_loop_rate) override;
 
   private:
-    bool rolling_mode_;
-    bool hovering_approximate_;
     ros::Publisher flight_cmd_pub_;
     ros::Publisher gimbal_control_pub_;
     ros::Publisher target_vectoring_force_pub_;
+
+    boost::shared_ptr<RollingRobotModel> rolling_robot_model_;
+
+    bool rolling_mode_;
+    bool hovering_approximate_;
     std::vector<float> target_base_thrust_;
     std::vector<double> target_gimbal_angles_;
     Eigen::VectorXd target_vectoring_f_;
