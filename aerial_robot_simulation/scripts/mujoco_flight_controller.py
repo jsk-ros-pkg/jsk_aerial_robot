9460d469c0237280d10e5aeb0d3f915a38052a59
sugihara <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2023-05-07 19:48:52 +0900

    [mujoco] update mujoco interface and flight controller

--- aerial_robot_simulation/scripts/mujoco_flight_controller.py
+++ aerial_robot_simulation/scripts/mujoco_flight_controller.py
@@ -5,22 +5,23 @@ import mujoco
 from mujoco import viewer
 import os
 import numpy as np
-from aerial_robot_msgs.msg import ControlInput
+from aerial_robot_msgs.msg import ControlInput, WrenchAllocationMatrix, PoseControlPid
 from geometry_msgs.msg import PoseStamped, TwistStamped
 from sensor_msgs.msg import JointState
 from std_msgs.msg import Empty
-from spinal.msg import FourAxisCommand, TorqueAllocationMatrixInv
+from spinal.msg import FourAxisCommand, TorqueAllocationMatrixInv, FlightConfigCmd
 from pid import PI_D
-from mujoco_navigation import mujocoNavigator
+import tf
 
 class mujocoFlightController:
     def __init__(self):
         self.takeoff_flag = False
         self.flight_config = None
-        self.navigator = mujocoNavigator()
         self.target_pos = np.zeros(3)
         self.target_rpy = np.zeros(3)
+        self.rotor_names = ['rotor1_thrust', 'rotor2_thrust', 'rotor3_thrust', 'rotor4_thrust']
 
+        # controllers
         x_pid_param = rospy.get_param("~x_pid_param")
         y_pid_param = rospy.get_param("~y_pid_param")
         z_pid_param = rospy.get_param("~z_pid_param")
@@ -34,59 +35,116 @@ class mujocoFlightController:
         self.pitch_pid = PI_D(*pitch_pid_param)
         self.yaw_pid = PI_D(*yaw_pid_param)
 
+        self.torque_allocation_mat = np.zeros((4, 4))
+        self.torque_allocation_mat_inv = np.zeros((4, 4))
+
         # ros publisher
         self.control_input = ControlInput()
         self.ctrl_input_pub = rospy.Publisher("ctrl_input", ControlInput, queue_size=1)
-        self.ctrl_input_pus = rospy.Publisher("hoge", ControlInput, queue_size=1)
+        self.pid_pub = rospy.Publisher("pid", PoseControlPid, queue_size=1)
 
         # ros subscriber
-        self.odom = PoseStamped()
+        self.pose = PoseStamped()
         self.twist = TwistStamped()
         self.mocap_sub = rospy.Subscriber("mocap/pose", PoseStamped, self.mocapCallback)
         self.twist_sub = rospy.Subscriber("twist", TwistStamped, self.twistCallback)
+        self.wrench_allocation_mat =  WrenchAllocationMatrix()
+        self.wrench_allocation_mat_sub = rospy.Subscriber("wrench_allocation_mat", WrenchAllocationMatrix, self.wrenchAllocationMatrixCallback)
+        self.takeoff_sub = rospy.Subscriber("teleop_command/takeoff", Empty, self.takeoffCallback)
+        self.halt_sub = rospy.Subscriber("teleop_command/halt", Empty, self.haltCallback)
+
+        # ros param
+        self.takeoff_height = rospy.get_param("~takeoff_height")
 
         # ros timer
         self.prev_time = rospy.get_time()
         timer = rospy.Timer(rospy.Duration(0.01), self.timerCallback)
 
-    def setNavigator(self, navigator):
-        self.navigator = navigator
-
-    def takeoff(self):
+    def takeoffCallback(self, msg):
         self.takeoff_flag = True
+        rpy = tf.transformations.euler_from_quaternion([self.pose.pose.orientation.x, self.pose.pose.orientation.y, self.pose.pose.orientation.z, self.pose.pose.orientation.w])
+        self.target_rpy[2] = rpy[2]
+        self.x_pid.setpoint = self.pose.pose.position.x
+        self.y_pid.setpoint = self.pose.pose.position.y
+        self.z_pid.setpoint = self.takeoff_height
+        self.roll_pid.setpoint = 0.0
+        self.pitch_pid.setpoint = 0.0
+        self.yaw_pid.setpoint = rpy[2]
+        print("xyz=[", self.pose.pose.position.x, self.pose.pose.position.y, self.takeoff_height, "], yaw=", rpy[2])
 
-    def flightConfigCallback(self, msg):
-        pass
+    def haltCallback(self, msg):
+        self.takeoff_flag = False
+        self.control_input.name = self.rotor_names
+        self.control_input.input = [0] * len(self.rotor_names)
+        self.ctrl_input_pub.publish(self.control_input)
+        self.x_pid.reset()
+        self.y_pid.reset()
+        self.z_pid.reset()
+        self.roll_pid.reset()
+        self.pitch_pid.reset()
+        self.yaw_pid.reset()
 
     def mocapCallback(self, msg):
-        self.odom = msg
+        self.pose = msg
 
     def twistCallback(self, msg):
         self.twist = msg
 
+    def wrenchAllocationMatrixCallback(self, msg):
+        self.wrench_allocation_mat = msg
+        self.torque_allocation_mat[0] = msg.f_z
+        self.torque_allocation_mat[1] = msg.t_x
+        self.torque_allocation_mat[2] = msg.t_y
+        self.torque_allocation_mat[3] = msg.t_z
+        self.torque_allocation_mat_inv = np.linalg.pinv(self.torque_allocation_mat)
+
     def timerCallback(self, event):
         now_time = rospy.get_time()
+        self.dt = now_time - self.prev_time
         if not self.takeoff_flag:
+            self.prev_time = now_time
             return
         else:
-            current_rpy = tf.transformations.euler_from_quaternion([self.odom.pose.orientation.x, self.odom.pose.orientation.y, self.odom.pose.orientation.z, self.odom.pose.orientation.w])
+            current_rpy = tf.transformations.euler_from_quaternion([self.pose.pose.orientation.x, self.pose.pose.orientation.y, self.pose.pose.orientation.z, self.pose.pose.orientation.w])
             target_force_w = np.zeros(4)
-            target_force_w[0] = self.x_pid(self.odom.pose.position.x, self.twist.twist.linear.x, dt=now_time-self.prev_time)
-            target_force_w[1] = self.y_pid(self.odom.pose.position.y, self.twist.twist.linear.y, dt=now_time-self.prev_time)
-            target_force_w[2] = self.z_pid(self.odom.pose.position.z, self.twist.twist.linear.z, dt=now_time-self.prev_time)
-            rot_mat = tf.transformations.rotation_matrix(-current_rpy[2], [0, 0, 1])
-            target_force_b = rot_mat.dot(target_force_w)
+            target_force_w[0] = self.x_pid(self.pose.pose.position.x, self.twist.twist.linear.x, dt=self.dt)
+            target_force_w[1] = self.y_pid(self.pose.pose.position.y, self.twist.twist.linear.y, dt=self.dt)
+            target_force_w[2] = self.z_pid(self.pose.pose.position.z, self.twist.twist.linear.z, dt=self.dt)
+
+            cog_rot_mat_from_world = tf.transformations.rotation_matrix(-current_rpy[2], [0, 0, 1])  # (4, 4)
+            target_force_b = np.dot(cog_rot_mat_from_world, target_force_w)
+
             self.roll_pid.setpoint = -target_force_b[1]
             self.pitch_pid.setpoint = target_force_b[0]
 
+            yaw_err = self.yaw_pid.setpoint - current_rpy[2]
+            if yaw_err > np.pi:
+                self.yaw_pid.setpoint = self.yaw_pid.setpoint - 2 * np.pi
+            elif yaw_err < -np.pi:
+                self.yaw_pid.setpoint = self.yaw_pid.setpoint + 2 * np.pi
+
             control_outputs = np.zeros(4)
             control_outputs[0] = target_force_b[2]
-            control_outputs[1] = self.roll_pid(current_rpy[0], self.odom.twist.twist.angular.x, dt=self.dt)
-            control_outputs[2] = self.pitch_pid(current_rpy[1], self.odom.twist.twist.angular.y, dt=self.dt)
-            control_outputs[3] = self.yaw_pid(current_rpy[2], self.odom.twist.twist.angular.z, dt=self.dt)
+            control_outputs[1] = self.roll_pid(current_rpy[0], self.twist.twist.angular.x, dt=self.dt)
+            control_outputs[2] = self.pitch_pid(current_rpy[1], self.twist.twist.angular.y, dt=self.dt)
+            control_outputs[3] = self.yaw_pid(current_rpy[2], self.twist.twist.angular.z, dt=self.dt)
 
-        self.prev_time = now_time
+            rotor_input = np.dot(self.torque_allocation_mat_inv, control_outputs)
 
+            self.control_input.name = self.rotor_names
+            self.control_input.input = rotor_input
+            self.ctrl_input_pub.publish(self.control_input)
+
+            pid = PoseControlPid()
+            pid.x = self.x_pid.getDebugMsg()
+            pid.y = self.y_pid.getDebugMsg()
+            pid.z = self.z_pid.getDebugMsg()
+            pid.roll = self.roll_pid.getDebugMsg()
+            pid.pitch = self.pitch_pid.getDebugMsg()
+            pid.yaw = self.yaw_pid.getDebugMsg()
+            self.pid_pub.publish(pid)
+
+            self.prev_time = now_time
 
 if __name__ == '__main__':
     rospy.init_node("mujoco_flight_controller" , anonymous=True)
--- aerial_robot_simulation/scripts/mujoco_ros_interface.py
+++ aerial_robot_simulation/scripts/mujoco_ros_interface.py
@@ -6,13 +6,14 @@ from mujoco import viewer
 import os
 import numpy as np
 import tf
-from aerial_robot_msgs.msg import ControlInput
+from aerial_robot_msgs.msg import ControlInput, WrenchAllocationMatrix
 from geometry_msgs.msg import PoseStamped, TwistStamped
 from sensor_msgs.msg import JointState
 from spinal.msg import FourAxisCommand, TorqueAllocationMatrixInv
 from pid import PI_D
 from mujoco_flight_controller import mujocoFlightController
 from mujoco_navigation import mujocoNavigator
+import time
 
 class MujocoRosInterface:
     def __init__(self):
@@ -23,16 +24,19 @@ class MujocoRosInterface:
         self.model = mujoco.MjModel.from_xml_path(xml_path)
         self.data = mujoco.MjData(self.model)
 
+        # mujoco model parameter
         self.joint_names = [self.model.joint(i).name for i in range(self.model.njnt)]
         self.joint_names = self.joint_names[1:] # remove root
         self.actuator_names = [self.model.actuator(i).name for i in range(self.model.nu)]
         self.site_names = [self.model.site(i).name for i in range(self.model.nsite)]
+        self.control_input = [0] * self.model.nu
 
         mujoco.mj_step(self.model, self.data)
 
+        # paramter for control
         self.cog = None
-        self.q_mat = np.zeros((4, len(self.site_names)))
         self.mass = None
+        self.wrench_allocation_mat = WrenchAllocationMatrix()
 
         self.pos = np.zeros(3)
         self.rpy = np.zeros(3)
@@ -41,19 +45,22 @@ class MujocoRosInterface:
         self.prev_time = rospy.get_time()
 
         self.calcCog()
-        print(self.mass)
-        print(self.joint_names)
-        print(self.actuator_names)
-        print(self.site_names)
+
+        print("mass=", self.mass)
+        print()
+        print("joint list=", self.joint_names)
+        print()
+        print("actuator list=", self.actuator_names)
+        print()
+        print("rotor list=", self.site_names)
 
         self.cnt = 0
-
-        self.control_input = [0] * self.model.nu
 
         # ros publisher
         self.joint_state_pub = rospy.Publisher("joint_states", JointState, queue_size=1)
         self.mocap_pub = rospy.Publisher("mocap/pose", PoseStamped, queue_size=1)
         self.twist_pub = rospy.Publisher("twist", TwistStamped, queue_size=1)
+        self.wrench_mat_pub = rospy.Publisher("wrench_allocation_mat", WrenchAllocationMatrix, queue_size=1)
 
         # ros subscriber
         ctrl_sub = rospy.Subscriber("ctrl_input", ControlInput, self.ctrlCallback)
@@ -70,37 +77,47 @@ class MujocoRosInterface:
                 self.data.ctrl[i] = self.control_input[i]
             self.viewer.sync()
             mujoco.mj_step(self.model, self.data)
-
+            time.sleep(0.01)
         self.viewer.close()
 
     def calcCog(self):
-        com_pos = np.array(self.data.xipos)
-        mass = np.array(self.data.cinert)[:, -1]
+        com_pos = np.array(self.data.xipos)         # (nbody, 3)
+        mass = np.array(self.data.cinert)[:, -1]    # (nbody, )
         self.mass = np.sum(mass)
         cog = np.dot(com_pos.transpose(), mass) / self.mass
         return cog
 
     def calcWrenchMat(self):
         self.cog = self.calcCog()
-        rotors_origin_from_cog = self.data.site_xpos - self.cog
+        qpos = self.data.qpos
+        rootlink_quat_from_world = np.zeros(4)
+        rootlink_quat_from_world[0] = qpos[4]
+        rootlink_quat_from_world[1] = qpos[5]
+        rootlink_quat_from_world[2] = qpos[6]
+        rootlink_quat_from_world[3] = qpos[3]
+        rootlink_rot_mat_from_world = tf.transformations.quaternion_matrix((rootlink_quat_from_world[0], rootlink_quat_from_world[1], rootlink_quat_from_world[2], rootlink_quat_from_world[3]))[0:3, 0:3] # rotation of rootlink and cog is same
+        rotors_origin_from_cog_in_world = self.data.site_xpos - self.cog  # (4, 3)
+        rotors_origin_from_cog_in_cog = np.dot(rootlink_rot_mat_from_world.T, rotors_origin_from_cog_in_world.T).T
         rotors_rot_mat_from_world = self.data.site_xmat.reshape(len(self.site_names), 3, 3) # 4, 3, 3
-        baselink_rot_mat_from_world = self.data.xmat[mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_BODY, "link2")].reshape(3, 3) # rotation of baselink and cog is same
-        self.baselink_rpy = tf.transformations.euler_from_matrix(baselink_rot_mat_from_world)
         rotors_rot_mat_from_cog = np.ones_like(rotors_rot_mat_from_world)
         for i in range(rotors_rot_mat_from_world.shape[0]):
-            rotors_rot_mat_from_cog[i] = np.dot(np.linalg.inv(baselink_rot_mat_from_world), rotors_rot_mat_from_world[i])
-        rotors_normal_from_cog = rotors_rot_mat_from_cog[:, :, 2]
+            rotors_rot_mat_from_cog[i] = np.dot(np.linalg.inv(rootlink_rot_mat_from_world), rotors_rot_mat_from_world[i])
+        rotors_normal_in_cog = rotors_rot_mat_from_cog[:, :, 2]
 
+        wrench_allocation_mat = WrenchAllocationMatrix()
         for i in range(len(self.site_names)):
-            self.q_mat[0, i] = 1 / self.mass
-            self.q_mat[1:4, i] = np.cross(rotors_origin_from_cog[i], rotors_normal_from_cog[i]) + (-0.01) * (i % 2 * 2 - 1) * rotors_normal_from_cog[i]
+            wrench_allocation_mat.f_z.append(1)
+            rotor_i_torque_allocation_vector = np.cross(rotors_origin_from_cog_in_cog[i], rotors_normal_in_cog[i]) + 0.02 * (i % 2 * 2 - 1) * rotors_normal_in_cog[i]
+            wrench_allocation_mat.t_x.append(rotor_i_torque_allocation_vector[0])
+            wrench_allocation_mat.t_y.append(rotor_i_torque_allocation_vector[1])
+            wrench_allocation_mat.t_z.append(rotor_i_torque_allocation_vector[2])
+            self.wrench_allocation_mat = wrench_allocation_mat
 
     def ctrlCallback(self, msg):
         for actuator_name, actuator_input in zip(msg.name, msg.input):
             if not (actuator_name in self.actuator_names):
                 rospy.logwarn("%s is an invalid actuator name" % (actuator_name))
             else:
-                rospy.logwarn("%s is set" %(actuator_name))
                 actuator_id = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_ACTUATOR, actuator_name)
                 self.control_input[actuator_id] = actuator_input
 
@@ -109,9 +126,11 @@ class MujocoRosInterface:
         self.cnt = (self.cnt + 1) % 20
         now = rospy.Time.now()
         now_time = rospy.get_time()
+
         # print(now_time)
         # print(now)
         # print(self.prev_time)
+
         # joint state
         js = JointState()
         js.header.stamp = now
@@ -133,38 +152,41 @@ class MujocoRosInterface:
         # mocap (pos and quat)
         ps = PoseStamped()
         ps.header.stamp = now
-        baselink_pos  = self.data.xpos[mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_BODY, "link2")]
-        baselink_quat = self.data.xquat[mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_BODY, "link2")]
-        ps.pose.position.x = baselink_pos[0]
-        ps.pose.position.y = baselink_pos[1]
-        ps.pose.position.z = baselink_pos[2]
-        ps.pose.orientation.x = baselink_quat[1]
-        ps.pose.orientation.y = baselink_quat[2]
-        ps.pose.orientation.z = baselink_quat[3]
-        ps.pose.orientation.w = baselink_quat[0]
+        qpos = self.data.qpos
+        quat = [0] * 4
+        quat[0] = qpos[4]
+        quat[1] = qpos[5]
+        quat[2] = qpos[6]
+        quat[3] = qpos[3]
+        ps.pose.position.x = qpos[0]
+        ps.pose.position.y = qpos[1]
+        ps.pose.position.z = qpos[2]
+        ps.pose.orientation.x = qpos[4]
+        ps.pose.orientation.y = qpos[5]
+        ps.pose.orientation.z = qpos[6]
+        ps.pose.orientation.w = qpos[3]
         self.mocap_pub.publish(ps)
-
-        self.prev_pos = self.pos
-        self.pos = baselink_pos
-
-        self.prev_rpy = self.rpy
-        self.rpy = tf.transformations.euler_from_quaternion((baselink_quat[1], baselink_quat[2], baselink_quat[3], baselink_quat[0]))
 
         #twist
         twist = TwistStamped()
+        qvel = self.data.qvel
         twist.header.stamp = now
-        twist.twist.linear.x = (self.pos[0] - self.prev_pos[0])  / (now_time - self.prev_time)
-        twist.twist.linear.y = (self.pos[1] - self.prev_pos[1]) / (now_time - self.prev_time)
-        twist.twist.linear.z = (self.pos[2] - self.prev_pos[2]) / (now_time - self.prev_time)
-        twist.twist.angular.x = (self.rpy[0] - self.prev_rpy[0]) / (now_time - self.prev_time)
-        twist.twist.angular.y = (self.rpy[1] - self.prev_rpy[1]) / (now_time - self.prev_time)
-        twist.twist.angular.z = (self.rpy[2] - self.prev_rpy[2]) / (now_time - self.prev_time)
+        twist.twist.linear.x = qvel[0]
+        twist.twist.linear.y = qvel[1]
+        twist.twist.linear.z = qvel[2]
+        twist.twist.angular.x = qvel[3]
+        twist.twist.angular.y = qvel[4]
+        twist.twist.angular.z = qvel[5]
         self.twist_pub.publish(twist)
 
-        # print(self.pos[0] , self.prev_pos[0], self.pos[0] - self.prev_pos[0])
-        # print(self.rpy[0], self.prev_rpy[0], self.rpy[0] - self.prev_rpy[0])
-        # print()
+        # wrench allocation matrix
+        self.calcWrenchMat()
+        self.wrench_mat_pub.publish(self.wrench_allocation_mat)
+
+        self.prev_pos = self.pos
+        self.prev_rpy = self.rpy
         self.prev_time = now_time
+
 
 if __name__ == '__main__':
     node = MujocoRosInterface()
