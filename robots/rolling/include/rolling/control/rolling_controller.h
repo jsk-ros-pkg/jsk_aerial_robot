d3eb3d8db0652f894e4d0b5639607663fe5d9d0a
sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2023-05-09 21:41:36 +0900

    [rolling][control] update control to use gimbals

--- robots/rolling/include/rolling/control/rolling_controller.h
+++ robots/rolling/include/rolling/control/rolling_controller.h
@@ -3,7 +3,9 @@
 #pragma once
 
 #include <aerial_robot_control/control/pose_linear_controller.h>
+#include <aerial_robot_msgs/WrenchAllocationMatrix.h>
 #include <spinal/FourAxisCommand.h>
+#include <spinal/TorqueAllocationMatrixInv.h>
 #include <std_msgs/Float32MultiArray.h>
 #include <rolling/model/rolling_robot_model.h>
 
@@ -25,17 +27,24 @@ namespace aerial_robot_control
     ros::Publisher flight_cmd_pub_;
     ros::Publisher gimbal_control_pub_;
     ros::Publisher target_vectoring_force_pub_;
+    ros::Publisher wrench_allocation_matrix_pub_;
+    ros::Publisher torque_allocation_matrix_inv_pub_; //for spinal
 
     boost::shared_ptr<RollingRobotModel> rolling_robot_model_;
 
     bool rolling_mode_;
     bool hovering_approximate_;
+    double torque_allocation_matrix_inv_pub_stamp_;
+    double torque_allocation_matrix_inv_pub_interval_;
     std::vector<float> target_base_thrust_;
     std::vector<double> target_gimbal_angles_;
     Eigen::VectorXd target_vectoring_f_;
+    Eigen::MatrixXd q_mat_;
+    Eigen::MatrixXd q_mat_inv_;
     void controlCore() override;
     void sendCmd();
     void rosParamInit();
+    void sendTorqueAllocationMatrixInv();
 
   };
 };
