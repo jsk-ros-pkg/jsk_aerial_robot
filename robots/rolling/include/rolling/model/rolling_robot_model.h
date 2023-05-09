c5e874750549ce737653ae88866fc1d947f8526f
sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2023-05-09 21:42:28 +0900

    [rolling][model] update robot model to get rotation of link and rotor neutral coordinates

--- robots/rolling/include/rolling/model/rolling_robot_model.h
+++ robots/rolling/include/rolling/model/rolling_robot_model.h
@@ -13,4 +13,39 @@ public:
                     double fc_t_min_thre = 0,
                     double epsilon = 10);
   virtual ~RollingRobotModel() = default;
+
+  template <class T> std::vector<T> getRotorsCoordFromCog();
+  template <class T> std::vector<T> getLinksRotationFromCog();
+
+private:
+  std::vector<KDL::Rotation> rotors_coord_rotation_from_cog_;
+  std::vector<KDL::Rotation> links_rotation_from_cog_;
+  std::mutex rotors_coord_rotation_mutex_;
+  std::mutex links_rotation_mutex_;
+
+protected:
+  void updateRobotModelImpl(const KDL::JntArray& joint_positions) override;
 };
+
+
+template<> inline std::vector<KDL::Rotation> RollingRobotModel::getRotorsCoordFromCog()
+{
+  std::lock_guard<std::mutex> lock(rotors_coord_rotation_mutex_);
+  return rotors_coord_rotation_from_cog_;
+}
+
+template<> inline std::vector<Eigen::Matrix3d> RollingRobotModel::getRotorsCoordFromCog()
+{
+  return aerial_robot_model::kdlToEigen(getRotorsCoordFromCog<KDL::Rotation>());
+}
+
+template<> inline std::vector<KDL::Rotation> RollingRobotModel::getLinksRotationFromCog()
+{
+  std::lock_guard<std::mutex> lock(links_rotation_mutex_);
+  return links_rotation_from_cog_;
+}
+
+template<> inline std::vector<Eigen::Matrix3d> RollingRobotModel::getLinksRotationFromCog()
+{
+  return aerial_robot_model::kdlToEigen(getLinksRotationFromCog<KDL::Rotation>());
+}
--- robots/rolling/src/model/rolling_robot_model.cpp
+++ robots/rolling/src/model/rolling_robot_model.cpp
@@ -3,6 +3,38 @@
 RollingRobotModel::RollingRobotModel(bool init_with_rosparam, bool verbose, double fc_t_min_thre, double epsilon) :
   HydrusXiFullyActuatedRobotModel(init_with_rosparam, verbose, fc_t_min_thre, epsilon)
 {
+  const int rotor_num = getRotorNum();
+  rotors_coord_rotation_from_cog_.resize(rotor_num);
+  links_rotation_from_cog_.resize(rotor_num);
+}
+
+void RollingRobotModel::updateRobotModelImpl(const KDL::JntArray& joint_positions)
+{
+  RobotModel::updateRobotModelImpl(joint_positions);
+
+  KDL::TreeFkSolverPos_recursive fk_solver(getTree());
+
+  /* special process */
+  KDL::Frame f_baselink;
+  fk_solver.JntToCart(joint_positions, f_baselink, getBaselinkName());
+  const KDL::Rotation cog_frame = f_baselink.M * getCogDesireOrientation<KDL::Rotation>().Inverse();
+
+  /* link based on COG */
+  for(int i = 0; i < getRotorNum(); ++i)
+    {
+      std::string s = std::to_string(i + 1);
+      KDL::Frame f;
+      fk_solver.JntToCart(joint_positions, f, std::string("link") + s);
+      links_rotation_from_cog_[i] = cog_frame.Inverse() * f.M;
+    }
+
+  for(int i = 0; i < getRotorNum(); ++i)
+    {
+      std::string s = std::to_string(i + 1);
+      KDL::Frame f;
+      fk_solver.JntToCart(joint_positions, f, std::string("rotor_coord") + s);
+      rotors_coord_rotation_from_cog_[i] = cog_frame.Inverse() * f.M;
+    }
 }
 
 /* plugin registration */
