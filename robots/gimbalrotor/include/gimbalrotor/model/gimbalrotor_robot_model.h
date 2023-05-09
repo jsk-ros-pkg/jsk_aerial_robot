47b4b056ee98c8a33cd002dc9ee664c168ff3fe9
sugihara <sugihara-junichiro789@g.ecc.u-tokyo.ac.jp>
2023-05-09 16:59:39 +0900

    [Gimbalrotor][Model] delete unused fuction for mask calculation.

--- robots/gimbalrotor/include/gimbalrotor/model/gimbalrotor_robot_model.h
+++ robots/gimbalrotor/include/gimbalrotor/model/gimbalrotor_robot_model.h
@@ -14,21 +14,17 @@ public:
                     double epsilon = 10);
   virtual ~GimbalrotorRobotModel() = default;
 
-  std::vector<Eigen::MatrixXd> getRotorMasks() { return rotor_masks_;};
   template <class T> std::vector<T> getLinksRotationFromCog();
   template <class T> std::vector<T> getThrustCoordRot();
 
 private:
   void updateRobotModelImpl(const KDL::JntArray& joint_positions) override;
-  void calcThrustMask();
 
-  std::vector<Eigen::MatrixXd> rotor_masks_;
   KDL::JntArray gimbal_processed_joint_;
   std::vector<KDL::Rotation> links_rotation_from_cog_;
   std::vector<KDL::Rotation> thrust_coords_rot_;
   std::mutex links_rotation_mutex_;
   std::mutex thrust_rotation_mutex_;
-  bool mask_calc_flag_;
 
 };
 
