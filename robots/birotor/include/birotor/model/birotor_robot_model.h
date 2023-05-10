f7a948bfb806683e6935bcf7091759d5d3c798e9
sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2023-05-10 21:26:40 +0900

    [birotor][model] add new function to get fixed frame of gimbal

--- robots/birotor/include/birotor/model/birotor_robot_model.h
+++ robots/birotor/include/birotor/model/birotor_robot_model.h
@@ -13,4 +13,24 @@ public:
                     double fc_t_min_thre = 0,
                     double epsilon = 10);
   virtual ~BirotorRobotModel() = default;
+
+  template <class T> std::vector<T> getRotorsCoordRot();
+
+private:
+  void updateRobotModelImpl(const KDL::JntArray& joint_positions) override;
+
+  std::vector<KDL::Rotation> rotors_coord_rot_;
+  std::mutex rotor_rotation_mutex_;
 };
+
+
+template<> inline std::vector<KDL::Rotation> BirotorRobotModel::getRotorsCoordRot()
+{
+  std::lock_guard<std::mutex> lock(rotor_rotation_mutex_);
+  return rotors_coord_rot_;
+}
+
+template<> inline std::vector<Eigen::Matrix3d> BirotorRobotModel::getRotorsCoordRot()
+{
+  return aerial_robot_model::kdlToEigen(getRotorsCoordRot<KDL::Rotation>());
+}
--- robots/birotor/src/model/birotor_robot_model.cpp
+++ robots/birotor/src/model/birotor_robot_model.cpp
@@ -3,6 +3,31 @@
 BirotorRobotModel::BirotorRobotModel(bool init_with_rosparam, bool verbose, double fc_t_min_thre, double epsilon) :
   RobotModel(init_with_rosparam, verbose, fc_t_min_thre, epsilon)
 {
+  const int rotor_num = getRotorNum();
+  rotors_coord_rot_.resize(rotor_num);
+}
+
+void BirotorRobotModel::updateRobotModelImpl(const KDL::JntArray& joint_positions)
+{
+  /* normal robot model update */
+  RobotModel::updateRobotModelImpl(joint_positions);
+
+  KDL::TreeFkSolverPos_recursive fk_solver(getTree());
+
+  /* special process */
+  KDL::Frame f_baselink;
+  fk_solver.JntToCart(joint_positions, f_baselink, getBaselinkName());
+  const KDL::Rotation cog_frame = f_baselink.M * getCogDesireOrientation<KDL::Rotation>().Inverse();
+
+  const auto seg_tf_map = getSegmentsTf();
+
+  /* get local coords of thrust links */
+  for(int i = 0; i < getRotorNum(); ++i)
+    {
+      std::string thrust = "rotor_coord" + std::to_string(i + 1);
+      KDL::Frame f = seg_tf_map.at(thrust);
+      rotors_coord_rot_[i] = cog_frame.Inverse() * f.M;
+    }
 }
 
 /* plugin registration */
