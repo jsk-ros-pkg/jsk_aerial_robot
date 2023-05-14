61ebbe077ed0ffce9f92a4f61b254e4b24722c2c
sugihara <sugihara-junichiro789@g.ecc.u-tokyo.ac.jp>
2023-05-14 13:45:22 +0900

    [Gimbalrotor][Control]add the new mode in which gimbale angles are calculated in spinal

--- robots/gimbalrotor/include/gimbalrotor/control/gimbalrotor_controller.h
+++ robots/gimbalrotor/include/gimbalrotor/control/gimbalrotor_controller.h
@@ -3,9 +3,10 @@
 #pragma once
 
 #include <aerial_robot_control/control/pose_linear_controller.h>
+#include <aerial_robot_control/control/fully_actuated_controller.h>
 #include <aerial_robot_estimation/state_estimation.h>
 #include <std_msgs/Float32MultiArray.h>
-#include <spinal/FourAxisCommand.h>
+#include <std_msgs/UInt32.h>
 #include <gimbalrotor/model/gimbalrotor_robot_model.h>
 
 namespace aerial_robot_control
@@ -27,16 +28,32 @@ namespace aerial_robot_control
     ros::Publisher flight_cmd_pub_;
     ros::Publisher gimbal_control_pub_;
     ros::Publisher target_vectoring_force_pub_;
+    ros::Publisher rpy_gain_pub_; //for spinal
+    ros::Publisher torque_allocation_matrix_inv_pub_; //for spinal
+    ros::Publisher gimbal_dof_pub_; //for spinal
 
     boost::shared_ptr<GimbalrotorRobotModel> gimbalerotor_robot_model_;
-
     std::vector<float> target_base_thrust_;
+    std::vector<float> target_full_thrust_;
     std::vector<double> target_gimbal_angles_;
     bool hovering_approximate_;
     Eigen::VectorXd target_vectoring_f_;
-    void controlCore() override;
-    void sendCmd();
+    Eigen::VectorXd target_vectoring_f_trans_;
+    Eigen::VectorXd target_vectoring_f_rot_;
+    Eigen::MatrixXd integrated_map_inv_trans_;
+    Eigen::MatrixXd integrated_map_inv_rot_;
+    double candidate_yaw_term_;
+    int gimbal_dof_;
+    bool gimbal_calc_in_fc_;
+
     void rosParamInit();
+    bool update() override;
+    virtual void reset() override;
+    void controlCore() override;
+    void sendCmd() override;
+    void sendFourAxisCommand();
+    void sendTorqueAllocationMatrixInv();
+    void setAttitudeGains();
 
   };
 };
