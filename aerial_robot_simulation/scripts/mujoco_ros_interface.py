76d5a4b2d3a65ec0802b7dac808ca5f7849d71c2
sugihara <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2023-06-05 06:44:18 +0900

    [mujoco] update mujoco ros interface

--- aerial_robot_simulation/scripts/mujoco_ros_interface.py
+++ aerial_robot_simulation/scripts/mujoco_ros_interface.py
@@ -5,8 +5,9 @@ import mujoco
 from mujoco import viewer
 import os
 import numpy as np
+import quaternion
 import tf
-from aerial_robot_msgs.msg import ControlInput, WrenchAllocationMatrix
+from aerial_robot_msgs.msg import ControlInput
 from geometry_msgs.msg import PoseStamped, TwistStamped
 from sensor_msgs.msg import JointState
 from spinal.msg import FourAxisCommand, TorqueAllocationMatrixInv, Imu
@@ -27,58 +28,53 @@ class MujocoRosInterface:
         self.joint_names = [self.model.joint(i).name for i in range(self.model.njnt)]
         self.joint_names = self.joint_names[1:] # remove root
         self.actuator_names = [self.model.actuator(i).name for i in range(self.model.nu)]
-        self.site_names = [self.model.site(i).name for i in range(self.model.nsite)]
+        # self.site_names = [self.model.site(i).name for i in range(self.model.nsite)]
         self.control_input = [0] * self.model.nu
+        self.rotor_list = rospy.get_param('rotor_list')
+        print(self.rotor_list)
 
         mujoco.mj_step(self.model, self.data)
 
         # paramter for control
-        self.cog = None
         self.mass = None
-        self.wrench_allocation_mat = WrenchAllocationMatrix()
 
         self.pos = np.zeros(3)
         self.rpy = np.zeros(3)
         self.prev_pos =np.zeros(3)
         self.prev_rpy = np.zeros(3)
-        self.prev_time = rospy.get_time()
 
-        self.calcCog()
-
+        self.mass = np.sum(np.array(self.data.cinert)[:, -1])
         print("mass=", self.mass)
         print()
         print("joint list=", self.joint_names)
         print()
         print("actuator list=", self.actuator_names)
         print()
-        print("rotor list=", self.site_names)
-
-        self.cnt = 0
 
         # ros publisher
         self.joint_state_pub = rospy.Publisher("joint_states", JointState, queue_size=1)
         self.mocap_pub = rospy.Publisher("mocap/pose", PoseStamped, queue_size=1)
         self.twist_pub = rospy.Publisher("twist", TwistStamped, queue_size=1)
-        self.wrench_mat_pub = rospy.Publisher("wrench_allocation_mat", WrenchAllocationMatrix, queue_size=1)
         self.imu_pub = rospy.Publisher("imu", Imu, queue_size=1)
         self.clock_pub = rospy.Publisher('/clock',Clock, queue_size=10)
 
         # ros subscriber
-        ctrl_sub = rospy.Subscriber("ctrl_input", ControlInput, self.ctrlCallback)
+        base_thrust_sub = rospy.Subscriber("four_axes/command", FourAxisCommand, self.baseThrustCallback)
+        ctrl_sub = rospy.Subscriber("mujoco/ctrl_input", ControlInput, self.ctrlCallback)
 
         # ros timer
         timer100 = rospy.Timer(rospy.Duration(0.01), self.timerCallback)
-        clock = rospy.Timer(rospy.Duration(0.001), self.clockCallback)
 
         # ros time
         self.sim_clock = Clock()
         self.zero_time = rospy.get_time()
 
+        clock = rospy.Timer(rospy.Duration(0.001), self.clockCallback)
+
         self.viewer = viewer.launch_passive(self.model, self.data)
         self.viewer.sync()
 
         while not rospy.is_shutdown():
-            self.cog = self.calcCog()
             for i in range(len(self.control_input)):
                 self.data.ctrl[i] = self.control_input[i]
             self.viewer.sync()
@@ -86,40 +82,19 @@ class MujocoRosInterface:
             time.sleep(0.01)
         self.viewer.close()
 
-    def calcCog(self):
-        com_pos = np.array(self.data.xipos)         # (nbody, 3)
-        mass = np.array(self.data.cinert)[:, -1]    # (nbody, )
-        self.mass = np.sum(mass)
-        cog = np.dot(com_pos.transpose(), mass) / self.mass
-        return cog
+    def baseThrustCallback(self, msg):
+        if len(self.rotor_list) != len(msg.base_thrust):
+            rospy.logwarn("size of rotor name list and input list is not same")
+            return
+        for i in range(len(self.rotor_list)):
+            actuator_id = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_ACTUATOR, self.rotor_list[i])
+            self.control_input[actuator_id] = msg.base_thrust[i]
 
-    def calcWrenchMat(self):
-        self.cog = self.calcCog()
-        qpos = self.data.qpos
-        rootlink_quat_from_world = np.zeros(4)
-        rootlink_quat_from_world[0] = qpos[4]
-        rootlink_quat_from_world[1] = qpos[5]
-        rootlink_quat_from_world[2] = qpos[6]
-        rootlink_quat_from_world[3] = qpos[3]
-        rootlink_rot_mat_from_world = tf.transformations.quaternion_matrix((rootlink_quat_from_world[0], rootlink_quat_from_world[1], rootlink_quat_from_world[2], rootlink_quat_from_world[3]))[0:3, 0:3] # rotation of rootlink and cog is same
-        rotors_origin_from_cog_in_world = self.data.site_xpos - self.cog  # (4, 3)
-        rotors_origin_from_cog_in_cog = np.dot(rootlink_rot_mat_from_world.T, rotors_origin_from_cog_in_world.T).T
-        rotors_rot_mat_from_world = self.data.site_xmat.reshape(len(self.site_names), 3, 3) # 4, 3, 3
-        rotors_rot_mat_from_cog = np.ones_like(rotors_rot_mat_from_world)
-        for i in range(rotors_rot_mat_from_world.shape[0]):
-            rotors_rot_mat_from_cog[i] = np.dot(np.linalg.inv(rootlink_rot_mat_from_world), rotors_rot_mat_from_world[i])
-        rotors_normal_in_cog = rotors_rot_mat_from_cog[:, :, 2]
-
-        wrench_allocation_mat = WrenchAllocationMatrix()
-        for i in range(len(self.site_names)):
-            wrench_allocation_mat.f_z.append(1)
-            rotor_i_torque_allocation_vector = np.cross(rotors_origin_from_cog_in_cog[i], rotors_normal_in_cog[i]) + 0.02 * (i % 2 * 2 - 1) * rotors_normal_in_cog[i]
-            wrench_allocation_mat.t_x.append(rotor_i_torque_allocation_vector[0])
-            wrench_allocation_mat.t_y.append(rotor_i_torque_allocation_vector[1])
-            wrench_allocation_mat.t_z.append(rotor_i_torque_allocation_vector[2])
-            self.wrench_allocation_mat = wrench_allocation_mat
 
     def ctrlCallback(self, msg):
+        if len(msg.name) != len(msg.input):
+            rospy.logwarn("size of joint name list and input list is not same")
+            return
         for actuator_name, actuator_input in zip(msg.name, msg.input):
             if not (actuator_name in self.actuator_names):
                 rospy.logwarn("%s is an invalid actuator name" % (actuator_name))
@@ -129,17 +104,11 @@ class MujocoRosInterface:
 
 
     def timerCallback(self, event):
-        self.cnt = (self.cnt + 1) % 20
-        now = rospy.Time.from_sec(rospy.get_time() - self.zero_time)
-        now_time = rospy.get_time()
-
-        # print(now_time)
-        # print(now)
-        # print(self.prev_time)
+        now = self.getNowTime()
 
         # joint state
         js = JointState()
-        js.header.stamp = now
+        js.header.stamp = self.getNowTime()
         js.name = self.joint_names
         joint_pos = self.data.qpos # including root (7 elements in the head of data)
         joint_pos_addr = self.model.jnt_qposadr
@@ -156,56 +125,47 @@ class MujocoRosInterface:
         self.joint_state_pub.publish(js)
 
         # mocap (pos and quat)
+        fc_id = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_SITE ,"fc")
+        fc_pos = self.data.site_xpos[fc_id]
+        fc_rot = self.data.site_xmat[fc_id].reshape(3, 3)
+        fc_quat = quaternion.from_rotation_matrix(fc_rot)
         ps = PoseStamped()
-        ps.header.stamp = now
-        qpos = self.data.qpos
-        quat = [0] * 4
-        quat[0] = qpos[4]
-        quat[1] = qpos[5]
-        quat[2] = qpos[6]
-        quat[3] = qpos[3]
-        ps.pose.position.x = qpos[0]
-        ps.pose.position.y = qpos[1]
-        ps.pose.position.z = qpos[2]
-        ps.pose.orientation.x = qpos[4]
-        ps.pose.orientation.y = qpos[5]
-        ps.pose.orientation.z = qpos[6]
-        ps.pose.orientation.w = qpos[3]
+        ps.header.stamp = self.getNowTime()
+        ps.pose.position.x = fc_pos[0]
+        ps.pose.position.y = fc_pos[1]
+        ps.pose.position.z = fc_pos[2]
+        ps.pose.orientation.x = fc_quat.x
+        ps.pose.orientation.y = fc_quat.y
+        ps.pose.orientation.z = fc_quat.z
+        ps.pose.orientation.w = fc_quat.w
         self.mocap_pub.publish(ps)
 
-        #twist
-        twist = TwistStamped()
-        qvel = self.data.qvel
-        twist.header.stamp = now
-        twist.twist.linear.x = qvel[0]
-        twist.twist.linear.y = qvel[1]
-        twist.twist.linear.z = qvel[2]
-        twist.twist.angular.x = qvel[3]
-        twist.twist.angular.y = qvel[4]
-        twist.twist.angular.z = qvel[5]
-        self.twist_pub.publish(twist)
-
+        # imu
+        rpy = tf.transformations.euler_from_quaternion([fc_quat.x, fc_quat.y, fc_quat.z, fc_quat.w])
         imu = Imu()
-        imu.stamp = now
-        imu.acc_data = np.zeros(3)
-        imu.gyro_data = np.zeros(3)
-        imu.mag_data = np.zeros(3)
-        imu.angles = np.zeros(3)
+        imu.stamp = self.getNowTime()
+        for i in range(self.model.nsensor):
+            # print(self.model.sensor(i))
+            if self.model.sensor(i).name == "acc":
+                imu.acc_data = self.data.sensordata[self.model.sensor(i).adr[0]:self.model.sensor(i).adr[0]+self.model.sensor(i).dim[0]]
+            if self.model.sensor(i).name == "gyro":
+                imu.gyro_data = self.data.sensordata[self.model.sensor(i).adr[0]:self.model.sensor(i).adr[0]+self.model.sensor(i).dim[0]]
+            if self.model.sensor(i).name == "mag":
+                imu.mag_data = self.data.sensordata[self.model.sensor(i).adr[0]:self.model.sensor(i).adr[0]+self.model.sensor(i).dim[0]]
+        imu.angles = rpy
         self.imu_pub.publish(imu)
-
-        # wrench allocation matrix
-        self.calcWrenchMat()
-        self.wrench_mat_pub.publish(self.wrench_allocation_mat)
 
         self.prev_pos = self.pos
         self.prev_rpy = self.rpy
-        self.prev_time = now_time
 
 
     def clockCallback(self, event):
         # print("clock")
         self.sim_clock = rospy.Time.from_sec(rospy.get_time() - self.zero_time)
         self.clock_pub.publish(self.sim_clock)
+
+    def getNowTime(self):
+        return rospy.Time.from_sec(rospy.get_time() - self.zero_time)
 
 if __name__ == '__main__':
     rospy.init_node("mujoco_ros_interface", anonymous=True)
