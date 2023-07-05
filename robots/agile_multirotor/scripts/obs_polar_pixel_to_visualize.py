5c43c63d36bddd1ec936c1bee014f1254c0e7e19
HarukiKozukapenguin <kozukozupenguin@g.ecc.u-tokyo.ac.jp>
2023-07-05 16:49:03 +0900

    [change setting] change theta_list same as RP_227 for visualizer

--- robots/agile_multirotor/scripts/obs_polar_pixel_to_visualize.py
+++ robots/agile_multirotor/scripts/obs_polar_pixel_to_visualize.py
@@ -36,7 +36,7 @@ class PolarPixelVisualizeNode:
         shift_x = rospy.get_param("~shift_x")
         shift_y = rospy.get_param("~shift_y")
 
-        theta_list = np.array([5,15,25,35,45,60,75,90, 105, 120, 134]) #deg
+        theta_list = np.array([5,15,25,35,45,60,75,90, 105, 120]) #deg
         acc_theta_list = np.array([1, 4, 7, 10]) #deg
         max_deg = 135 #deg
         resolution_deg = 0.25 #deg
