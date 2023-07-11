69a18e7217cfcd0403d645c78b1239a108e5d04c
Junichiro Sugihara <j-sugihara@jsk.imi.i.u-tokyo.ac.jp>
2023-07-11 23:40:13 +0900

    [Beetle] add tf from cog to contact point

--- robots/beetle/include/beetle/model/beetle_robot_model.h
+++ robots/beetle/include/beetle/model/beetle_robot_model.h
@@ -15,12 +15,16 @@ public:
   virtual ~BeetleRobotModel() = default;
 
   template<class T> T getContactFrame();
+  template<class T> T getCog2Cp();
 
   void setContactFrame(const KDL::Frame contact_frame){contact_frame_ = contact_frame;}
+  void setCog2Cp(const KDL::Frame Cog2Cp){Cog2Cp_ = Cog2Cp;}
 
 private:
   KDL::Frame contact_frame_;
-  // std::mutex mutex_contact_frame_;
+  KDL::Frame Cog2Cp_;
+  std::mutex mutex_contact_frame_;
+  std::mutex mutex_cog2cp_;
 
 protected:  
   void updateRobotModelImpl(const KDL::JntArray& joint_positions) override;
@@ -28,8 +32,14 @@ protected:
 
 template<> inline KDL::Frame BeetleRobotModel::getContactFrame()
 {
-  // std::lock_guard<std::mutex> lock(mutex_contact_frame_);
+  std::lock_guard<std::mutex> lock(mutex_contact_frame_);
   return contact_frame_;
+}
+
+template<> inline KDL::Frame BeetleRobotModel::getCog2Cp()
+{
+  std::lock_guard<std::mutex> lock(mutex_cog2cp_);
+  return Cog2Cp_;
 }
 
 template<> inline geometry_msgs::TransformStamped BeetleRobotModel::getContactFrame()
