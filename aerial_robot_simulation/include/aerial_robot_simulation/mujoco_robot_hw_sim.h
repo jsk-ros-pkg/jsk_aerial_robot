ab16465f16e3ced8db2765aac733da3d9786fbc9
sugihara <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2023-08-15 22:13:09 +0900

    [mujoco] add timer in mujoco_robot_hw_sim

--- aerial_robot_simulation/include/aerial_robot_simulation/mujoco_robot_hw_sim.h
+++ aerial_robot_simulation/include/aerial_robot_simulation/mujoco_robot_hw_sim.h
@@ -1,20 +1,37 @@
 #include <ros/ros.h>
+#include <ros/callback_queue.h>
+#include <rosgraph_msgs/Clock.h>
+
 #include <aerial_robot_model/model/aerial_robot_model.h>
 #include <aerial_robot_simulation/mujoco_spinal_interface.h>
 #include <aerial_robot_simulation/mujoco_attitude_controller.h>
 #include <aerial_robot_simulation/mujoco_visualization_utils.h>
 #include <mujoco/mujoco.h>
 
+
 class MujocoRobotHWSim
 {
 public:
-  bool initSim(ros::NodeHandle nh
-               );
-  void readSim(ros::Time time, ros::Duration period);
+  MujocoRobotHWSim(ros::NodeHandle nh);
+  virtual ~MujocoRobotHWSim();
+  bool initSim();
+  void readSim(const ros::TimerEvent & e);
   void writeSim(ros::Time time, ros::Duration period);
+  void clockCallback(const ros::TimerEvent & e);
 
 private:
   ros::NodeHandle nh_;
+  ros::Timer clock_timer_;
+  ros::Timer read_timer_;
+  ros::Time zero_time_;
+
+  ros::Publisher clock_pub_;
+
+  ros::AsyncSpinner clock_spinner_;
+  ros::AsyncSpinner read_spinner_;
+  ros::CallbackQueue clock_loop_queue_;
+  ros::CallbackQueue read_loop_queue_;
+
   mjModel* m;
   mjData* d;
   MujocoSpinalInterface spinal_interface_;
--- aerial_robot_simulation/src/mujoco_robot_hw_sim.cpp
+++ aerial_robot_simulation/src/mujoco_robot_hw_sim.cpp
@@ -1,18 +1,50 @@
 #include <aerial_robot_simulation/mujoco_robot_hw_sim.h>
 
-bool MujocoRobotHWSim::initSim(ros::NodeHandle nh
-                               )
+MujocoRobotHWSim::MujocoRobotHWSim(ros::NodeHandle nh):
+  nh_(nh),
+  clock_spinner_(1, &clock_loop_queue_),
+  read_spinner_(1, &read_loop_queue_)
 {
-  nh_ = nh;
-  spinal_interface_.init(nh_);
 
-  
+  clock_pub_ = nh_.advertise<rosgraph_msgs::Clock>("/clock", 10);
+
+  zero_time_ = ros::Time::now();
+  ros::TimerOptions clock_ops(ros::Duration(1.0 / 1000.0),
+                              boost::bind(&MujocoRobotHWSim::clockCallback, this, _1),
+                              &clock_loop_queue_);
+  clock_timer_ = nh_.createTimer(clock_ops);
+  clock_spinner_.start();
+
+  ros::TimerOptions read_ops(ros::Duration(1.0 / 200.0),
+                                boost::bind(&MujocoRobotHWSim::readSim, this, _1),
+                                &read_loop_queue_);
+  read_timer_ = nh_.createTimer(read_ops);
+  read_spinner_.start();
+
+  while(ros::ok())
+    {
+    }
 }
 
-void MujocoRobotHWSim::readSim(ros::Time time, ros::Duration period)
+MujocoRobotHWSim::~MujocoRobotHWSim()
 {
-  
+  clock_timer_.stop();
+  clock_spinner_.stop();
+  read_timer_.stop();
+  read_spinner_.stop();
 
+}
+
+void MujocoRobotHWSim::clockCallback(const ros::TimerEvent & e)
+{
+  rosgraph_msgs::Clock ros_time;
+  ros_time.clock.fromSec((ros::Time::now() - zero_time_).toSec());
+  clock_pub_.publish(ros_time);
+
+}
+
+void MujocoRobotHWSim::readSim(const ros::TimerEvent & e)
+{
 }
 
 
@@ -27,6 +59,6 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "mujoco_robot_hw_sim");
   ros::NodeHandle nh;
 
-  MujocoRobotHWSim mujoco_robot_hw_sim;
-  mujoco_robot_hw_sim.initSim(nh);
+  MujocoRobotHWSim mujoco_robot_hw_sim(nh);
+  // mujoco_robot_hw_sim.initSim(nh);
 }
