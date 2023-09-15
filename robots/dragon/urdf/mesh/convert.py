f4fbd34e0af2ae13ec2e1c5630ed70b8da288194
sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2023-09-15 19:07:27 +0900

    [dragon] modify dae convesion script for blender>2.8.0

--- robots/dragon/urdf/mesh/convert.py
+++ robots/dragon/urdf/mesh/convert.py
@@ -3,17 +3,26 @@ import os.path
 import glob
 meshdir = "./"
 
+# blender -b -P convert.py
+
 for dae_file in glob.glob(os.path.join(meshdir, "*.dae")):
+    for col in bpy.data.collections:
+        for item in col.objects:
+            col.objects.unlink(item)
+            bpy.data.objects.remove(item)
+
+    for item in bpy.context.scene.collection.objects:
+        bpy.context.scene.collection.objects.unlink(item)
+        bpy.data.objects.remove(item)
+
+    for item in bpy.data.meshes:
+        bpy.data.meshes.remove(item)
+
+    # for item in bpy.data.materials:
+    #     bpy.data.materials.remove(item)
+
     bpy.ops.wm.collada_import(filepath=dae_file)
     name = os.path.split(dae_file)[1]
     name = name.split(".")[0] + ".stl"
-    bpy.ops.export_mesh.stl(filepath=os.path.join(meshdir, name), axis_forward='-Z', axis_up='Y')
+    bpy.ops.export_mesh.stl(filepath=os.path.join(meshdir, name))
 
-    for item in bpy.context.scene.objects:
-        bpy.context.scene.objects.unlink(item)
-    for item in bpy.data.objects:
-        bpy.data.objects.remove(item)
-    for item in bpy.data.meshes:
-        bpy.data.meshes.remove(item)
-    for item in bpy.data.materials:
-        bpy.data.materials.remove(item)
