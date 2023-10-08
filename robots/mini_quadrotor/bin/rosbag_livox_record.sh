fee334cc6fe7e63236c5ae9eb638fc99bd98f749
Moju Zhao <chou@jsk.imi.i.u-tokyo.ac.jp>
2023-10-08 15:27:11 +0900

    [Mini Quadrotor][Livox&FastLio] add the launch files about livox mid360 and fast lio into sensors.launch.xml and also update the topic names

--- robots/mini_quadrotor/bin/rosbag_livox_record.sh
+++ robots/mini_quadrotor/bin/rosbag_livox_record.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rosrun mini_quadrotor rosbag_record.sh ${1:-quadrotor} /livox/lidar /livox/imu /Odometry /Odometry_precede ${@:2}
+rosrun mini_quadrotor rosbag_record.sh ${1:-quadrotor} quadrotor/livox/lidar quadrotor/livox/imu quadrotor/Odometry quadrotor/Odometry_precede ${@:2}
