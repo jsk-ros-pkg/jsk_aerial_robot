bd5cf0f0b3b1704a5ac7825439ea5fd6a128a582
Jinjie Li <45286479+Li-Jinjie@users.noreply.github.com>
2023-10-22 12:10:41 +0900

    [Aerial Robot Control] add NMPC Python Interface; add C code generated by Acados Python Interface automatically.

---
+++ aerial_robot_control/include/aerial_robot_control/control/base/nmpc_body_rate/c_generated_code/acados_solver_qd_body_rate_model.h
@@ -0,0 +1,170 @@
+/*
+ * Copyright (c) The acados authors.
+ *
+ * This file is part of acados.
+ *
+ * The 2-Clause BSD License
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.;
+ */
+
+#ifndef ACADOS_SOLVER_qd_body_rate_model_H_
+#define ACADOS_SOLVER_qd_body_rate_model_H_
+
+#include "acados/utils/types.h"
+
+#include "acados_c/ocp_nlp_interface.h"
+#include "acados_c/external_function_interface.h"
+
+#define QD_BODY_RATE_MODEL_NX     10
+#define QD_BODY_RATE_MODEL_NZ     0
+#define QD_BODY_RATE_MODEL_NU     4
+#define QD_BODY_RATE_MODEL_NP     4
+#define QD_BODY_RATE_MODEL_NBX    3
+#define QD_BODY_RATE_MODEL_NBX0   10
+#define QD_BODY_RATE_MODEL_NBU    4
+#define QD_BODY_RATE_MODEL_NSBX   0
+#define QD_BODY_RATE_MODEL_NSBU   0
+#define QD_BODY_RATE_MODEL_NSH    0
+#define QD_BODY_RATE_MODEL_NSG    0
+#define QD_BODY_RATE_MODEL_NSPHI  0
+#define QD_BODY_RATE_MODEL_NSHN   0
+#define QD_BODY_RATE_MODEL_NSGN   0
+#define QD_BODY_RATE_MODEL_NSPHIN 0
+#define QD_BODY_RATE_MODEL_NSBXN  0
+#define QD_BODY_RATE_MODEL_NS     0
+#define QD_BODY_RATE_MODEL_NSN    0
+#define QD_BODY_RATE_MODEL_NG     0
+#define QD_BODY_RATE_MODEL_NBXN   0
+#define QD_BODY_RATE_MODEL_NGN    0
+#define QD_BODY_RATE_MODEL_NY0    14
+#define QD_BODY_RATE_MODEL_NY     14
+#define QD_BODY_RATE_MODEL_NYN    10
+#define QD_BODY_RATE_MODEL_N      20
+#define QD_BODY_RATE_MODEL_NH     0
+#define QD_BODY_RATE_MODEL_NPHI   0
+#define QD_BODY_RATE_MODEL_NHN    0
+#define QD_BODY_RATE_MODEL_NPHIN  0
+#define QD_BODY_RATE_MODEL_NR     0
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+// ** capsule for solver data **
+typedef struct qd_body_rate_model_solver_capsule
+{
+    // acados objects
+    ocp_nlp_in *nlp_in;
+    ocp_nlp_out *nlp_out;
+    ocp_nlp_out *sens_out;
+    ocp_nlp_solver *nlp_solver;
+    void *nlp_opts;
+    ocp_nlp_plan_t *nlp_solver_plan;
+    ocp_nlp_config *nlp_config;
+    ocp_nlp_dims *nlp_dims;
+
+    // number of expected runtime parameters
+    unsigned int nlp_np;
+
+    /* external functions */
+    // dynamics
+
+    external_function_param_casadi *forw_vde_casadi;
+    external_function_param_casadi *expl_ode_fun;
+
+
+
+
+    // cost
+
+    external_function_param_casadi *cost_y_fun;
+    external_function_param_casadi *cost_y_fun_jac_ut_xt;
+    external_function_param_casadi *cost_y_hess;
+
+
+
+    external_function_param_casadi cost_y_0_fun;
+    external_function_param_casadi cost_y_0_fun_jac_ut_xt;
+    external_function_param_casadi cost_y_0_hess;
+
+
+
+    external_function_param_casadi cost_y_e_fun;
+    external_function_param_casadi cost_y_e_fun_jac_ut_xt;
+    external_function_param_casadi cost_y_e_hess;
+
+
+    // constraints
+
+
+
+
+} qd_body_rate_model_solver_capsule;
+
+ACADOS_SYMBOL_EXPORT qd_body_rate_model_solver_capsule * qd_body_rate_model_acados_create_capsule(void);
+ACADOS_SYMBOL_EXPORT int qd_body_rate_model_acados_free_capsule(qd_body_rate_model_solver_capsule *capsule);
+
+ACADOS_SYMBOL_EXPORT int qd_body_rate_model_acados_create(qd_body_rate_model_solver_capsule * capsule);
+
+ACADOS_SYMBOL_EXPORT int qd_body_rate_model_acados_reset(qd_body_rate_model_solver_capsule* capsule, int reset_qp_solver_mem);
+
+/**
+ * Generic version of qd_body_rate_model_acados_create which allows to use a different number of shooting intervals than
+ * the number used for code generation. If new_time_steps=NULL and n_time_steps matches the number used for code
+ * generation, the time-steps from code generation is used.
+ */
+ACADOS_SYMBOL_EXPORT int qd_body_rate_model_acados_create_with_discretization(qd_body_rate_model_solver_capsule * capsule, int n_time_steps, double* new_time_steps);
+/**
+ * Update the time step vector. Number N must be identical to the currently set number of shooting nodes in the
+ * nlp_solver_plan. Returns 0 if no error occurred and a otherwise a value other than 0.
+ */
+ACADOS_SYMBOL_EXPORT int qd_body_rate_model_acados_update_time_steps(qd_body_rate_model_solver_capsule * capsule, int N, double* new_time_steps);
+/**
+ * This function is used for updating an already initialized solver with a different number of qp_cond_N.
+ */
+ACADOS_SYMBOL_EXPORT int qd_body_rate_model_acados_update_qp_solver_cond_N(qd_body_rate_model_solver_capsule * capsule, int qp_solver_cond_N);
+ACADOS_SYMBOL_EXPORT int qd_body_rate_model_acados_update_params(qd_body_rate_model_solver_capsule * capsule, int stage, double *value, int np);
+ACADOS_SYMBOL_EXPORT int qd_body_rate_model_acados_update_params_sparse(qd_body_rate_model_solver_capsule * capsule, int stage, int *idx, double *p, int n_update);
+
+ACADOS_SYMBOL_EXPORT int qd_body_rate_model_acados_solve(qd_body_rate_model_solver_capsule * capsule);
+ACADOS_SYMBOL_EXPORT int qd_body_rate_model_acados_free(qd_body_rate_model_solver_capsule * capsule);
+ACADOS_SYMBOL_EXPORT void qd_body_rate_model_acados_print_stats(qd_body_rate_model_solver_capsule * capsule);
+ACADOS_SYMBOL_EXPORT int qd_body_rate_model_acados_custom_update(qd_body_rate_model_solver_capsule* capsule, double* data, int data_len);
+
+
+ACADOS_SYMBOL_EXPORT ocp_nlp_in *qd_body_rate_model_acados_get_nlp_in(qd_body_rate_model_solver_capsule * capsule);
+ACADOS_SYMBOL_EXPORT ocp_nlp_out *qd_body_rate_model_acados_get_nlp_out(qd_body_rate_model_solver_capsule * capsule);
+ACADOS_SYMBOL_EXPORT ocp_nlp_out *qd_body_rate_model_acados_get_sens_out(qd_body_rate_model_solver_capsule * capsule);
+ACADOS_SYMBOL_EXPORT ocp_nlp_solver *qd_body_rate_model_acados_get_nlp_solver(qd_body_rate_model_solver_capsule * capsule);
+ACADOS_SYMBOL_EXPORT ocp_nlp_config *qd_body_rate_model_acados_get_nlp_config(qd_body_rate_model_solver_capsule * capsule);
+ACADOS_SYMBOL_EXPORT void *qd_body_rate_model_acados_get_nlp_opts(qd_body_rate_model_solver_capsule * capsule);
+ACADOS_SYMBOL_EXPORT ocp_nlp_dims *qd_body_rate_model_acados_get_nlp_dims(qd_body_rate_model_solver_capsule * capsule);
+ACADOS_SYMBOL_EXPORT ocp_nlp_plan_t *qd_body_rate_model_acados_get_nlp_plan(qd_body_rate_model_solver_capsule * capsule);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif  // ACADOS_SOLVER_qd_body_rate_model_H_
---
+++ aerial_robot_control/include/aerial_robot_control/control/base/nmpc_body_rate/nmpc_body_rate_ctl.py
@@ -0,0 +1,223 @@
+#!/usr/bin/env python
+# -*- encoding: ascii -*-
+"""
+Author: LI Jinjie
+File: nmpc_body_rate_ctl.py
+Date: 2023/4/16 9:50 AM
+Description:
+"""
+import os
+import sys
+import shutil
+import errno
+import numpy as np
+from acados_template import AcadosOcp, AcadosOcpSolver, AcadosSimSolver, AcadosModel
+import casadi as ca
+
+from params import nmpc_params as CP
+
+
+class NMPCBodyRateController(object):
+    def __init__(self, is_build_acados=True):
+        opt_model = BodyRateModel().model
+
+        nx = opt_model.x.size()[0]
+        nu = opt_model.u.size()[0]
+        n_params = opt_model.p.size()[0]
+
+        # get file path for acados
+        os.chdir(os.path.dirname(os.path.realpath(__file__)))
+        acados_models_dir = "./acados_models"
+        safe_mkdir_recursive(os.path.join(os.getcwd(), acados_models_dir))
+        acados_source_path = os.environ["ACADOS_SOURCE_DIR"]
+        sys.path.insert(0, acados_source_path)
+
+        # create OCP
+        ocp = AcadosOcp()
+        ocp.acados_include_path = acados_source_path + "/include"
+        ocp.acados_lib_path = acados_source_path + "/lib"
+        ocp.model = opt_model
+        ocp.dims.N = CP.N_node
+
+        # initialize parameters
+        ocp.dims.np = n_params
+        ocp.parameter_values = np.zeros(n_params)
+
+        # cost function
+        # see https://docs.acados.org/python_interface/#acados_template.acados_ocp.AcadosOcpCost for details
+        Q = np.diag([CP.Qp_xy, CP.Qp_xy, CP.Qp_z, CP.Qv_xy, CP.Qv_xy, CP.Qv_z, 0, CP.Qq_xy, CP.Qq_xy, CP.Qq_z])
+        R = np.diag([CP.Rw, CP.Rw, CP.Rw, CP.Rc])
+        ocp.cost.cost_type = "NONLINEAR_LS"
+        ocp.cost.cost_type_e = "NONLINEAR_LS"
+        ocp.cost.W = np.block([[Q, np.zeros((nx, nu))], [np.zeros((nu, nx)), R]])
+        ocp.cost.W_e = Q  # weight matrix at terminal shooting node (N).
+
+        # set constraints
+        ocp.constraints.lbu = np.array([CP.w_min, CP.w_min, CP.w_min, CP.c_min])
+        ocp.constraints.ubu = np.array([CP.w_max, CP.w_max, CP.w_max, CP.c_max])
+        ocp.constraints.idxbu = np.array([0, 1, 2, 3])  # omega_x, omega_y, omega_z, collective_acceleration
+        ocp.constraints.lbx = np.array([CP.v_min, CP.v_min, CP.v_min])
+        ocp.constraints.ubx = np.array([CP.v_max, CP.v_max, CP.v_max])
+        ocp.constraints.idxbx = np.array([3, 4, 5])  # vx, vy, vz
+
+        # initial state
+        x_ref = np.zeros(nx)
+        u_ref = np.zeros(nu)
+        ocp.constraints.x0 = x_ref
+        ocp.cost.yref = np.concatenate((x_ref, u_ref))
+        ocp.cost.yref_e = x_ref
+
+        # solver options
+        ocp.solver_options.qp_solver = "PARTIAL_CONDENSING_HPIPM"
+        ocp.solver_options.hpipm_mode = "BALANCE"  # "BALANCE", "SPEED_ABS", "SPEED", "ROBUST". Default: "BALANCE".
+        # # 0: no warm start; 1: warm start; 2: hot start. Default: 0   Seems only works for FULL_CONDENSING_QPOASES
+        # ocp.solver_options.qp_solver_warm_start = 1
+        ocp.solver_options.hessian_approx = "GAUSS_NEWTON"
+        ocp.solver_options.integrator_type = "ERK"  # explicit Runge-Kutta integrator
+        ocp.solver_options.print_level = 0
+        ocp.solver_options.nlp_solver_type = "SQP_RTI"
+        ocp.solver_options.qp_solver_cond_N = CP.N_node
+        ocp.solver_options.tf = CP.T_horizon
+
+        # compile acados ocp
+        json_file_path = os.path.join("./" + opt_model.name + "_acados_ocp.json")
+        self.solver = AcadosOcpSolver(ocp, json_file=json_file_path, build=is_build_acados)
+
+    def reset(self, xr, ur):
+        # reset x and u of NNPC controller, which prevents warm-starting from the previous solution
+        for i in range(self.solver.N):
+            self.solver.set(i, "x", xr[i, :])
+            self.solver.set(i, "u", ur[i, :])
+        self.solver.set(self.solver.N, "x", xr[self.solver.N, :])
+
+    def update(self, x0, xr, ur):
+        # get x and u, set reference
+        for i in range(self.solver.N):
+            yr = np.concatenate((xr[i, :], ur[i, :]))
+            self.solver.set(i, "yref", yr)
+            quaternion_r = xr[i, 6:10]
+            self.solver.set(i, "p", quaternion_r)  # for nonlinear quaternion error
+        self.solver.set(self.solver.N, "yref", xr[self.solver.N, :])  # final state of x, no u
+
+        # feedback, take the first action
+        u0 = self.solver.solve_for_x0(x0)
+
+        if self.solver.status != 0:
+            raise Exception("acados acados_ocp_solver returned status {}. Exiting.".format(self.solver.status))
+
+        return u0
+
+
+class BodyRateModel(object):
+    def __init__(self):
+        model_name = "qd_body_rate_model"
+
+        # model states
+        x = ca.SX.sym("x")
+        y = ca.SX.sym("y")
+        z = ca.SX.sym("z")
+        vx = ca.SX.sym("vx")
+        vy = ca.SX.sym("vy")
+        vz = ca.SX.sym("vz")
+        qw = ca.SX.sym("qw")
+        qx = ca.SX.sym("qx")
+        qy = ca.SX.sym("qy")
+        qz = ca.SX.sym("qz")
+        states = ca.vertcat(x, y, z, vx, vy, vz, qw, qx, qy, qz)
+
+        # reference for quaternions
+        qwr = ca.SX.sym("qwr")
+        qxr = ca.SX.sym("qxr")
+        qyr = ca.SX.sym("qyr")
+        qzr = ca.SX.sym("qzr")
+        quaternion_r = ca.vertcat(qwr, qxr, qyr, qzr)
+
+        # control inputs
+        wx = ca.SX.sym("wx")
+        wy = ca.SX.sym("wy")
+        wz = ca.SX.sym("wz")
+        c = ca.SX.sym("c")
+        controls = ca.vertcat(wx, wy, wz, c)
+
+        # dynamic model
+        ds = ca.vertcat(
+            vx,
+            vy,
+            vz,
+            2 * (qx * qz + qw * qy) * c,
+            2 * (qy * qz - qw * qx) * c,
+            (1 - 2 * qx**2 - 2 * qy**2) * c - CP.gravity,
+            (-wx * qx - wy * qy - wz * qz) * 0.5,
+            (wx * qw + wz * qy - wy * qz) * 0.5,
+            (wy * qw - wz * qx + wx * qz) * 0.5,
+            (wz * qw + wy * qx - wx * qy) * 0.5,
+        )
+
+        # function
+        f = ca.Function("f", [states, controls], [ds], ["state", "control_input"], ["ds"])
+
+        # NONLINEAR_LS = error^T @ Q @ error; error = y - y_ref
+        qe_x = qwr * qx - qw * qxr + qyr * qz - qy * qzr
+        qe_y = qwr * qy - qw * qyr - qxr * qz + qx * qzr
+        qe_z = qxr * qy - qx * qyr + qwr * qz - qw * qzr
+
+        state_y = ca.vertcat(
+            x,
+            y,
+            z,
+            vx,
+            vy,
+            vz,
+            qwr,
+            qe_x + qxr,
+            qe_y + qyr,
+            qe_z + qzr,
+        )
+        control_y = controls
+
+        # acados model
+        x_dot = ca.SX.sym("x_dot", 10)
+        f_impl = x_dot - f(states, controls)
+
+        model = AcadosModel()
+        model.name = model_name
+        model.f_expl_expr = f(states, controls)  # CasADi expression for the explicit dynamics
+        model.f_impl_expr = f_impl  # CasADi expression for the implicit dynamics
+        model.x = states
+        model.xdot = x_dot
+        model.u = controls
+        model.p = quaternion_r
+        model.cost_y_expr = ca.vertcat(state_y, control_y)  # NONLINEAR_LS
+        model.cost_y_expr_e = state_y
+
+        # constraint
+        constraint = ca.types.SimpleNamespace()
+
+        constraint.w_max = CP.w_max
+        constraint.w_min = CP.w_min
+        constraint.c_max = CP.c_max
+        constraint.c_min = CP.c_min
+
+        constraint.v_max = CP.v_max
+        constraint.v_min = CP.v_min
+        constraint.expr = ca.vcat([wx, wy, wz, c])
+
+        self.model = model
+        self.constraint = constraint
+
+
+def safe_mkdir_recursive(directory, overwrite=False):
+    if not os.path.exists(directory):
+        try:
+            os.makedirs(directory)
+        except OSError as exc:
+            if exc.errno == errno.EEXIST and os.path.isdir(directory):
+                pass
+            else:
+                raise
+    else:
+        if overwrite:
+            try:
+                shutil.rmtree(directory)
+            except:
+                print("Error while removing directory {}".format(directory))
