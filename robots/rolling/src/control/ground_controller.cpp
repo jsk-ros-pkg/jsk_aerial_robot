6e247e3b204fda6c639dce4e84e218324352f538
sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2023-11-01 18:49:35 +0900

    [rolling][control] add full lambda solver with OSQP for steering state

--- robots/rolling/src/control/ground_controller.cpp
+++ robots/rolling/src/control/ground_controller.cpp
@@ -106,3 +106,139 @@ double steeringControlWrenchAllocationConstraint(const std::vector<double> &x, s
 
   return diff;
 }
+
+void RollingController::osqpPractice()
+{
+  int n_variables = 2 * motor_num_;
+  int n_constraints = controlled_q_mat_.rows();
+
+  Eigen::MatrixXd H(n_variables, n_variables);
+  H <<
+    1.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+    0.0, 0.0, 1.0, 0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0, 1.0, 0.0, 0.0,
+    0.0, 0.0, 0.0, 0.0, 1.0, 0.0,
+    0.0, 0.0, 0.0, 0.0, 0.0, 1.0;
+
+  Eigen::SparseMatrix<double> H_s;
+  H_s = H.sparseView();
+
+  Eigen::MatrixXd A(n_constraints, n_variables);
+  A = controlled_q_mat_;
+  // std::cout << A << std::endl;
+  Eigen::SparseMatrix<double> A_s;
+  A_s = A.sparseView();
+
+  Eigen::VectorXd gradient = Eigen::VectorXd::Ones(n_variables);
+
+  double epsilon = 1e-4;
+  Eigen::VectorXd lower_bound = target_wrench_acc_cog_ - Eigen::VectorXd::Ones(n_constraints) * epsilon;
+  Eigen::VectorXd upper_bound = target_wrench_acc_cog_ + Eigen::VectorXd::Ones(n_constraints) * epsilon;
+
+  OsqpEigen::Solver solver;
+
+  solver.settings()->setVerbosity(true);
+  solver.settings()->setWarmStart(true);
+  solver.data()->setNumberOfVariables(n_variables);
+  solver.data()->setNumberOfConstraints(n_constraints);
+  solver.data()->setHessianMatrix(H_s);
+  solver.data()->setLinearConstraintsMatrix(A_s);
+  solver.data()->setGradient(gradient);
+  solver.data()->setLowerBound(lower_bound);
+  solver.data()->setUpperBound(upper_bound);
+
+  if(!solver.initSolver())
+    {
+      std::cout << "init solver error" << std::endl;
+    }
+  solver.solve();
+  auto solution = solver.getSolution();
+
+  for(int i = 0; i < n_variables; i++)
+    {
+      std::cout << solution(i) << " ";
+    }
+  std::cout << std::endl;
+
+}
+
+void RollingController::calcSteeringTargetLambda()
+{
+  int n_variables = 2 * motor_num_;
+  int n_constraints = 3 + 1 + 2 + 2;
+  double mu = 0.5;
+  double epsilon = 0.001;
+
+  Eigen::MatrixXd H(n_variables, n_variables);
+  H <<
+    1.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+    0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+    0.0, 0.0, 1.0, 0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0, 1.0, 0.0, 0.0,
+    0.0, 0.0, 0.0, 0.0, 1.0, 0.0,
+    0.0, 0.0, 0.0, 0.0, 0.0, 1.0;
+
+  Eigen::SparseMatrix<double> H_s;
+  H_s = H.sparseView();
+
+  Eigen::MatrixXd A = Eigen::MatrixXd::Zero(n_constraints, n_variables);
+  A.topRows(3) = full_q_rot_;                                                    //    eq constraint about rpy torque
+  A.block(3, 0, 1, n_variables) = full_q_mat_.row(Z);                            // in eq constraint about z
+  A.block(4, 0, 1, n_variables) = full_q_mat_.row(X) - mu * full_q_mat_.row(Z);  // in eq constraint about x
+  A.block(5, 0, 1, n_variables) = full_q_mat_.row(X) + mu * full_q_mat_.row(Z);  // in eq constraint about x
+  A.block(6, 0, 1, n_variables) = full_q_mat_.row(Y) - mu * full_q_mat_.row(Z);  // in eq constraint about y
+  A.block(7, 0, 1, n_variables) = full_q_mat_.row(Y) + mu * full_q_mat_.row(Z);  // in eq constraint about y
+
+  Eigen::SparseMatrix<double> A_s;
+  A_s = A.sparseView();
+
+  Eigen::VectorXd gradient = Eigen::VectorXd::Ones(n_variables);
+
+  Eigen::VectorXd lower_bound(n_constraints);
+  Eigen::VectorXd upper_bound(n_constraints);
+
+  lower_bound
+    <<
+    target_wrench_acc_cog_(control_dof_ - 6 + ROLL) - epsilon,
+    target_wrench_acc_cog_(control_dof_ - 6 + PITCH) - epsilon,
+    target_wrench_acc_cog_(control_dof_ - 6 + YAW) - epsilon,
+    0.0,
+    -mu * robot_model_->getMass() * robot_model_->getGravity()(Z),
+    -INFINITY,
+    -mu * robot_model_->getMass() * robot_model_->getGravity()(Z),
+    -INFINITY;
+
+  upper_bound <<
+    target_wrench_acc_cog_(control_dof_ - 6 + ROLL) + epsilon,
+    target_wrench_acc_cog_(control_dof_ - 6 + PITCH) + epsilon,
+    target_wrench_acc_cog_(control_dof_ - 6 + YAW) + epsilon,
+    robot_model_->getMass() * robot_model_->getGravity()(Z),
+    INFINITY,
+    mu * robot_model_->getMass() * robot_model_->getGravity()(Z),
+    INFINITY,
+    mu * robot_model_->getMass() * robot_model_->getGravity()(Z);
+
+  OsqpEigen::Solver solver;
+
+  solver.settings()->setVerbosity(true);
+  solver.settings()->setWarmStart(true);
+  solver.data()->setNumberOfVariables(n_variables);
+  solver.data()->setNumberOfConstraints(n_constraints);
+  solver.data()->setHessianMatrix(H_s);
+  solver.data()->setLinearConstraintsMatrix(A_s);
+  solver.data()->setGradient(gradient);
+  solver.data()->setLowerBound(lower_bound);
+  solver.data()->setUpperBound(upper_bound);
+
+  if(!solver.initSolver())
+    {
+      std::cout << "init solver error" << std::endl;
+    }
+  solver.solve();
+  auto solution = solver.getSolution();
+
+  full_lambda_trans_ = solution;
+  full_lambda_all_ = solution;
+
+}
