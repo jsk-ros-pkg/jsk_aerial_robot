60aca437a6b15f2ef5503e694267fdc92acfef9d
sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2023-11-24 12:54:07 +0900

    [rolling] republish desire coord and visualize in rviz

--- robots/rolling/include/rolling/util/pwm_republisher.h
+++ robots/rolling/include/rolling/util/pwm_republisher.h
@@ -1,4 +1,5 @@
 #include <ros/ros.h>
+#include "spinal/DesireCoord.h"
 #include "spinal/Pwms.h"
 #include "std_msgs/Float32.h"
 
@@ -12,10 +13,21 @@ private:
   ros::NodeHandle nh_;
   ros::NodeHandle nhp_;
 
+  /* motor pwms */
   ros::Subscriber motor_pwm_sub_;
   std::vector<ros::Publisher> motor_pwm_pubs_;
-
   int motor_num_;
 
+  /* desire coordinate */
+  ros::Publisher desire_coordinate_roll_pub_, desire_coordinate_pitch_pub_;
+  ros::Subscriber desire_coordinate_sub_;
+  double desire_coordinate_roll_;
+  double desire_coordinate_pitch_;
+
+  /* timer */
+  ros::Timer timer_;
+
   void motorPwmCallback(const spinal::PwmsPtr & pwm_msg);
+  void desireCoordinateCallback(const spinal::DesireCoordPtr & desire_coordinate_msg);
+  void timerCallback(const ros::TimerEvent & e);
 };
