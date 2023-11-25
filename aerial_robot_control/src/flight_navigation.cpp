b2af40bbb687fc5e96b1889dc04335318b042d79
heissereal <miyamichi@jsk.imi.i.u-tokyo.ac.jp>
2023-11-25 18:15:12 +0900

    fix typo

--- aerial_robot_control/src/flight_navigation.cpp
+++ aerial_robot_control/src/flight_navigation.cpp
@@ -182,13 +182,13 @@ void BaseNavigator::naviCallback(const aerial_robot_msgs::FlightNavConstPtr & ms
     {
       ROS_INFO("ok2");
       // setTargetYaw(angles::normalize_angle(msg->target_yaw));
-      setTargetOmageZ(msg->target_omega_z);
+      setTargetOmegaZ(msg->target_omega_z);
     }
   if(msg->yaw_nav_mode == aerial_robot_msgs::FlightNav::VEL_MODE)
     {
       ROS_INFO("ok2");
       // setTargetYaw(angles::normalize_angle(msg->target_yaw));
-      setTargetOmageZ(msg->target_omega_z);
+      setTargetOmegaZ(msg->target_omega_z);
     }
 
   /* xy control */
