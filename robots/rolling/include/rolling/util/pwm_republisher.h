43dcce71eed1df6d440868f6a527e85a4a3d4365
sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2023-12-01 18:29:56 +0900

    [rolling] visualize gimbal angle in rviz

--- robots/rolling/include/rolling/util/pwm_republisher.h
+++ robots/rolling/include/rolling/util/pwm_republisher.h
@@ -1,4 +1,5 @@
 #include <ros/ros.h>
+#include "sensor_msgs/JointState.h"
 #include "spinal/DesireCoord.h"
 #include "spinal/Pwms.h"
 #include "std_msgs/Float32.h"
@@ -24,10 +25,16 @@ private:
   double desire_coordinate_roll_;
   double desire_coordinate_pitch_;
 
+  /* gimbal angles */
+  ros::Subscriber gimbals_control_sub_;
+  std::vector<ros::Publisher> gimbal_control_pubs_;
+
   /* timer */
   ros::Timer timer_;
 
   void motorPwmCallback(const spinal::PwmsPtr & pwm_msg);
   void desireCoordinateCallback(const spinal::DesireCoordPtr & desire_coordinate_msg);
+  void gimbalsControlCallback(const sensor_msgs::JointState & joint_state_msg);
   void timerCallback(const ros::TimerEvent & e);
+
 };
