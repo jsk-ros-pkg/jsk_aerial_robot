c606b33b4071a5f3f6fa43080a3c1e84ef29955f
sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2024-01-12 21:02:53 +0900

    [delta] change the package name from rolling to delta

---
+++ robots/delta/scripts/joint_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import JointState
+import time
+
+rospy.init_node("delta_joint_test")
+
+joint_control_pub = rospy.Publisher("delta/joints_ctrl", JointState, queue_size=10)
+radian = rospy.get_param("~radian", 0)
+joint_state_msg = JointState()
+joint_state_msg.name = ["joint1", "joint2"]
+joint_state_msg.position = [radian, radian]
+
+time.sleep(0.6)
+joint_control_pub.publish(joint_state_msg)
