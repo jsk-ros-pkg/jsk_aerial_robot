20ab38d1ece091a5a403eaa6c537e1e91b39bea3
sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2024-01-19 14:12:32 +0900

    [delta][model] refactor around the control frame

--- robots/delta/include/delta/model/delta_robot_model.h
+++ robots/delta/include/delta/model/delta_robot_model.h
@@ -23,16 +23,13 @@ public:
   template <class T> std::vector<T> getLinksRotationFromCog();
   template <class T> T getCenterPoint();
   void setCircleRadius(double radius) {circle_radius_ = radius;}
-
-  void setTargetFrameName(std::string frame_name);
+  void setTargetFrame(KDL::Frame frame){target_frame_ = frame;};
+  void setTargetFrame(std::string frame_name);
+  KDL::Frame getTargetFrame(){return target_frame_;}
   std::string getTargetFrameName() {return target_frame_name_;}
-  KDL::Frame getTargetFrame();
   Eigen::MatrixXd getFullWrenchAllocationMatrixFromControlFrame();
   Eigen::MatrixXd getFullWrenchAllocationMatrixFromControlFrame(std::string frame_name);
   template <class T> T getInertiaFromTargetFrame();
-
-  Eigen::VectorXd calcFeasibleControlFDists(std::vector<Eigen::Vector3d>&u);
-  Eigen::VectorXd calcFeasibleControlTDists(std::vector<Eigen::Vector3d>&v);
 
 private:
   ros::Publisher feasible_control_force_pub_, feasible_control_torque_pub_;
@@ -47,12 +44,14 @@ private:
   KDL::Frame center_point_;
   KDL::Frame contact_point_;
 
-  std::vector<KDL::Rotation> links_rotation_from_cog_;
   std::string thrust_link_;
   double circle_radius_;
   KDL::RigidBodyInertia link_inertia_; // from root
-  std::map<std::string, KDL::Frame*> additional_frame_;
+  KDL::Frame target_frame_;
   std::string target_frame_name_;
+  std::map<std::string, KDL::Frame*> additional_frame_;
+
+  std::vector<KDL::Rotation> links_rotation_from_cog_;
 
 protected:
   void updateRobotModelImpl(const KDL::JntArray& joint_positions) override;
