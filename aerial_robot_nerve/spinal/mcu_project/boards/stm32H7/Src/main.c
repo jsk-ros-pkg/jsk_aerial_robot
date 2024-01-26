fb45a418a5b7def5800ee861f4ec49b7f0759369
LI, Jinjie <li-jinjie@g.ecc.u-tokyo.ac.jp>
2024-01-26 14:01:58 +0900

    [Spinal][DShot]: change the UART for receiving dshot telemetry to RX only.

--- aerial_robot_nerve/spinal/mcu_project/boards/stm32H7/Src/main.c
+++ aerial_robot_nerve/spinal/mcu_project/boards/stm32H7/Src/main.c
@@ -934,7 +934,7 @@ static void MX_USART2_UART_Init(void)
   huart2.Init.WordLength = UART_WORDLENGTH_8B;
   huart2.Init.StopBits = UART_STOPBITS_1;
   huart2.Init.Parity = UART_PARITY_NONE;
-  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.Mode = UART_MODE_RX;
   huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
   huart2.Init.OverSampling = UART_OVERSAMPLING_16;
   huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
