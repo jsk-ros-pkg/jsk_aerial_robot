bc344eda094734ded5aee830ed050bfcec025625
LI, Jinjie <li-jinjie@g.ecc.u-tokyo.ac.jp>
2024-01-31 13:30:48 +0900

    [Motor Test][script]: now the .txt name can be passed in as argument

--- aerial_robot_nerve/motor_test/scripts/analyze_data.py
+++ aerial_robot_nerve/motor_test/scripts/analyze_data.py
@@ -1,117 +1,131 @@
+import argparse
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Read the data from the .txt file
-folder_path = '/home/jinjie/.ros/'
-file_name = 'u=25.20v_motor_test_1706621604.txt'
-file_path = folder_path + file_name
-data = pd.read_csv(file_path, sep=' ',
-                   names=["PWM", "fx", "fy", "fz", "f_norm", "mx", "my", "mz", "currency", "RPM", "temperature",
-                          "voltage", "State"])
 
-# Filter out the rows where State is 'done'
-data = data[data['PWM'] != 'done']
+def analyze_data(folder_path, file_name):
+    file_path = folder_path + file_name
+    data = pd.read_csv(file_path, sep=' ',
+                       names=["PWM", "fx", "fy", "fz", "f_norm", "mx", "my", "mz", "currency", "RPM", "temperature",
+                              "voltage", "State"])
 
-# convert PWM from string to int
-data['PWM'] = data['PWM'].astype(int)
+    # Filter out the rows where State is 'done'
+    data = data[data['PWM'] != 'done']
 
-# Filter the DataFrame for 'valid' state
-valid_data = data[data['State'] == 'valid']
+    # convert PWM from string to int
+    data['PWM'] = data['PWM'].astype(int)
 
-columns_to_average = ["fx", "fy", "fz", "f_norm", "mx", "my", "mz", "currency", "RPM", "temperature", "voltage"]
+    # Filter the DataFrame for 'valid' state
+    valid_data = data[data['State'] == 'valid']
 
-# Group by 'PWM' and calculate the average for each specified column
-average_values = valid_data.groupby('PWM')[columns_to_average].mean()
+    columns_to_average = ["fx", "fy", "fz", "f_norm", "mx", "my", "mz", "currency", "RPM", "temperature", "voltage"]
 
-# Resetting index to make 'PWM' a column again
-average_values.reset_index(inplace=True)
+    # Group by 'PWM' and calculate the average for each specified column
+    average_values = valid_data.groupby('PWM')[columns_to_average].mean()
 
-# Add a column for power
-average_values['Power'] = average_values['voltage'] * average_values['currency']
+    # Resetting index to make 'PWM' a column again
+    average_values.reset_index(inplace=True)
 
-# Add a column for PWM ratio
-average_values['PWM_Ratio_%'] = average_values['PWM'] / 2000 * 100
+    # Add a column for power
+    average_values['Power'] = average_values['voltage'] * average_values['currency']
 
-# Add a column for kRPM^2 (kilorevolutions per minute squared)
-average_values['kRPM^2'] = (average_values['RPM'] / 1000) ** 2
+    # Add a column for PWM ratio
+    average_values['PWM_Ratio_%'] = average_values['PWM'] / 2000 * 100
 
-# Add a column for Dshot ratio in percentage
-average_values['Dshot_Ratio_%'] = ((average_values['PWM'] - 1000) / 1000) * 100
+    # Add a column for kRPM^2 (kilorevolutions per minute squared)
+    average_values['kRPM^2'] = (average_values['RPM'] / 1000) ** 2
 
-''' plot the data '''
+    # Add a column for Dshot ratio in percentage
+    average_values['Dshot_Ratio_%'] = ((average_values['PWM'] - 1000) / 1000) * 100
 
-# Convert series to numpy arrays
-kRPM2 = average_values['kRPM^2'].to_numpy()
-fz = average_values['fz'].to_numpy()
-mz = average_values['mz'].to_numpy()
-currency = average_values['currency'].to_numpy()
-PWM_ratio = average_values['PWM_Ratio_%'].to_numpy()
+    ''' plot the data '''
 
-# Perform the fittings
-# Linear fit for x:kRPM^2 y:fz
-slope_kRPM2, intercept_kRPM2 = np.polyfit(kRPM2, fz, 1)
-fit_eq_kRPM2 = f"fz = {slope_kRPM2:.4f} * kRPM^2 + {intercept_kRPM2:.4f}"
-print(f"Fitting Equation (x:kRPM^2 y:fz): {fit_eq_kRPM2}")
+    # Convert series to numpy arrays
+    kRPM2 = average_values['kRPM^2'].to_numpy()
+    fz = average_values['fz'].to_numpy()
+    mz = average_values['mz'].to_numpy()
+    currency = average_values['currency'].to_numpy()
+    PWM_ratio = average_values['PWM_Ratio_%'].to_numpy()
 
-# Linear fit for x:fz y:mz, note that the intercept must be 0. Only y=kx form.
-slope_mz_fz, intercept_mz_fz = np.polyfit(fz, mz, 1)
-fit_eq_mz_fz = f"mz = {slope_mz_fz:.4f} * fz"
-print(f"Fitting Equation (x:fz y:mz): {fit_eq_mz_fz}")
+    # Perform the fittings
+    # Linear fit for x:kRPM^2 y:fz
+    slope_kRPM2, intercept_kRPM2 = np.polyfit(kRPM2, fz, 1)
+    fit_eq_kRPM2 = f"fz = {slope_kRPM2:.4f} * kRPM^2 + {intercept_kRPM2:.4f}"
+    print(f"Fitting Equation (x:kRPM^2 y:fz): {fit_eq_kRPM2}")
 
-# 2nd-order polynomial fit for x:fz y:currency
-coeffs_currency_fz = np.polyfit(fz, currency, 2)
-fit_eq_currency_fz = f"currency = {coeffs_currency_fz[0]:.4e} * fz^2 + {coeffs_currency_fz[1]:.4e} * fz + {coeffs_currency_fz[2]:.4f}"
-print(f"Fitting Equation (x:fz y:currency): {fit_eq_currency_fz}")
+    # Linear fit for x:fz y:mz, note that the intercept must be 0. Only y=kx form.
+    slope_mz_fz, intercept_mz_fz = np.polyfit(fz, mz, 1)
+    fit_eq_mz_fz = f"mz = {slope_mz_fz:.4f} * fz"
+    print(f"Fitting Equation (x:fz y:mz): {fit_eq_mz_fz}")
 
-# 2nd-order polynomial fit for x:PWM_ratio y:fz
-coeffs_PWM_ratio_fz = np.polyfit(PWM_ratio, fz, 2)
-fit_eq_PWM_ratio_fz = f"fz = {coeffs_PWM_ratio_fz[0]:.4e} * PWM_Ratio_%^2 + {coeffs_PWM_ratio_fz[1]:.4e} * PWM_Ratio_% + {coeffs_PWM_ratio_fz[2]:.4f}"
-print(f"Fitting Equation (x:PWM_Ratio_% y:fz): {fit_eq_PWM_ratio_fz}")
+    # 2nd-order polynomial fit for x:fz y:currency
+    coeffs_currency_fz = np.polyfit(fz, currency, 2)
+    fit_eq_currency_fz = f"currency = {coeffs_currency_fz[0]:.4e} * fz^2 + {coeffs_currency_fz[1]:.4e} * fz + {coeffs_currency_fz[2]:.4f}"
+    print(f"Fitting Equation (x:fz y:currency): {fit_eq_currency_fz}")
 
-# Create 2x2 subplots
-fig, axs = plt.subplots(2, 2, figsize=(12, 12))
+    # 2nd-order polynomial fit for x:PWM_ratio y:fz
+    coeffs_PWM_ratio_fz = np.polyfit(PWM_ratio, fz, 2)
+    fit_eq_PWM_ratio_fz = f"fz = {coeffs_PWM_ratio_fz[0]:.4e} * PWM_Ratio_%^2 + {coeffs_PWM_ratio_fz[1]:.4e} * PWM_Ratio_% + {coeffs_PWM_ratio_fz[2]:.4f}"
+    print(f"Fitting Equation (x:PWM_Ratio_% y:fz): {fit_eq_PWM_ratio_fz}")
+
+    # Create 2x2 subplots
+    fig, axs = plt.subplots(2, 2, figsize=(12, 12))
+
+    # Plot 1: x:kRPM^2 y:fz
+    axs[0, 0].scatter(kRPM2, fz, color='blue', alpha=0.5, label='Data points')
+    axs[0, 0].plot(kRPM2, slope_kRPM2 * kRPM2 + intercept_kRPM2, color='red', label='Fitted line')
+    axs[0, 0].set_title('fz vs kRPM^2')
+    axs[0, 0].set_xlabel('kRPM^2')
+    axs[0, 0].set_ylabel('fz (N)')
+    axs[0, 0].grid()
+    axs[0, 0].text(0.95, 0.05, fit_eq_kRPM2, horizontalalignment='right', verticalalignment='bottom',
+                   transform=axs[0, 0].transAxes, fontsize=10, color='green')
+
+    # Plot 2: x:fz y:mz
+    axs[0, 1].scatter(fz, mz, color='blue', alpha=0.5, label='Data points')
+    axs[0, 1].plot(fz, slope_mz_fz * fz, color='red', label='Fitted line')
+    axs[0, 1].set_title('mz vs fz')
+    axs[0, 1].set_xlabel('fz (N)')
+    axs[0, 1].set_ylabel('mz (N*m)')
+    axs[0, 1].grid()
+    axs[0, 1].text(0.95, 0.05, fit_eq_mz_fz, horizontalalignment='right', verticalalignment='bottom',
+                   transform=axs[0, 1].transAxes, fontsize=10, color='green')
+
+    # Plot 3: x:fz y:currency
+    axs[1, 0].scatter(fz, currency, color='blue', alpha=0.5, label='Data points')
+    axs[1, 0].plot(np.sort(fz), np.polyval(coeffs_currency_fz, np.sort(fz)), color='red', label='Fitted polynomial')
+    axs[1, 0].set_title('currency vs fz')
+    axs[1, 0].set_xlabel('fz (N)')
+    axs[1, 0].set_ylabel('currency')
+    axs[1, 0].grid()
+    axs[1, 0].text(0.95, 0.05, fit_eq_currency_fz, horizontalalignment='right', verticalalignment='bottom',
+                   transform=axs[1, 0].transAxes, fontsize=10, color='green')
+
+    # Plot 4: fz vs PWM_ratio
+    axs[1, 1].scatter(PWM_ratio, fz, color='blue', alpha=0.5, label='Data points')
+    axs[1, 1].plot(np.sort(PWM_ratio), np.polyval(coeffs_PWM_ratio_fz, np.sort(PWM_ratio)), color='red',
+                   label='Fitted polynomial')
+    axs[1, 1].set_title('fz vs PWM_Ratio_%')
+    axs[1, 1].set_xlabel('PWM_Ratio_%')
+    axs[1, 1].set_ylabel('fz (N)')
+    axs[1, 1].grid()
+    axs[1, 1].text(0.95, 0.05, fit_eq_PWM_ratio_fz, horizontalalignment='right', verticalalignment='bottom',
+                   transform=axs[1, 1].transAxes, fontsize=10, color='green')
+
+    # title with the file name
+    fig.suptitle(file_name)
+
+    # Adjust layout and show plot
+    plt.tight_layout()
+    plt.show()
 
 
-# Plot 1: x:kRPM^2 y:fz
-axs[0, 0].scatter(kRPM2, fz, color='blue', alpha=0.5, label='Data points')
-axs[0, 0].plot(kRPM2, slope_kRPM2 * kRPM2 + intercept_kRPM2, color='red', label='Fitted line')
-axs[0, 0].set_title('fz vs kRPM^2')
-axs[0, 0].set_xlabel('kRPM^2')
-axs[0, 0].set_ylabel('fz (N)')
-axs[0, 0].grid()
-axs[0, 0].text(0.95, 0.05, fit_eq_kRPM2, horizontalalignment='right', verticalalignment='bottom', transform=axs[0, 0].transAxes, fontsize=10, color='green')
+if __name__ == '__main__':
+    # pass text file name as argument
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file_name', help='file name')
+    parser.add_argument('--folder_path', help='path to folder', default='~/.ros/')
+    args = parser.parse_args()
 
-# Plot 2: x:fz y:mz
-axs[0, 1].scatter(fz, mz, color='blue', alpha=0.5, label='Data points')
-axs[0, 1].plot(fz, slope_mz_fz * fz, color='red', label='Fitted line')
-axs[0, 1].set_title('mz vs fz')
-axs[0, 1].set_xlabel('fz (N)')
-axs[0, 1].set_ylabel('mz (N*m)')
-axs[0, 1].grid()
-axs[0, 1].text(0.95, 0.05, fit_eq_mz_fz, horizontalalignment='right', verticalalignment='bottom', transform=axs[0, 1].transAxes, fontsize=10, color='green')
-
-# Plot 3: x:fz y:currency
-axs[1, 0].scatter(fz, currency, color='blue', alpha=0.5, label='Data points')
-axs[1, 0].plot(np.sort(fz), np.polyval(coeffs_currency_fz, np.sort(fz)), color='red', label='Fitted polynomial')
-axs[1, 0].set_title('currency vs fz')
-axs[1, 0].set_xlabel('fz (N)')
-axs[1, 0].set_ylabel('currency')
-axs[1, 0].grid()
-axs[1, 0].text(0.95, 0.05, fit_eq_currency_fz, horizontalalignment='right', verticalalignment='bottom', transform=axs[1, 0].transAxes, fontsize=10, color='green')
-
-# Plot 4: fz vs PWM_ratio
-axs[1, 1].scatter(PWM_ratio, fz, color='blue', alpha=0.5, label='Data points')
-axs[1, 1].plot(np.sort(PWM_ratio), np.polyval(coeffs_PWM_ratio_fz, np.sort(PWM_ratio)), color='red', label='Fitted polynomial')
-axs[1, 1].set_title('fz vs PWM_Ratio_%')
-axs[1, 1].set_xlabel('PWM_Ratio_%')
-axs[1, 1].set_ylabel('fz (N)')
-axs[1, 1].grid()
-axs[1, 1].text(0.95, 0.05, fit_eq_PWM_ratio_fz, horizontalalignment='right', verticalalignment='bottom', transform=axs[1, 1].transAxes, fontsize=10, color='green')
-
-# title with the file name
-fig.suptitle(file_name)
-
-# Adjust layout and show plot
-plt.tight_layout()
-plt.show()
+    analyze_data(args.folder_path, args.file_name)
