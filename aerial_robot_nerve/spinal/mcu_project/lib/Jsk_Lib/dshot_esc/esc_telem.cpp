1fbc859c6db8160512579938df547769a0c621b3
LI, Jinjie <li-jinjie@g.ecc.u-tokyo.ac.jp>
2024-02-01 17:04:23 +0900

    [Spinal][DShot]: refactor rx_buf_ to esc_telem_rx_, in case of unambiguous

--- aerial_robot_nerve/spinal/mcu_project/lib/Jsk_Lib/dshot_esc/esc_telem.cpp
+++ aerial_robot_nerve/spinal/mcu_project/lib/Jsk_Lib/dshot_esc/esc_telem.cpp
@@ -11,9 +11,9 @@ void ESCReader::init(UART_HandleTypeDef* huart)
   // use DMA for UART RX
   __HAL_UART_DISABLE_IT(huart, UART_IT_PE);
   __HAL_UART_DISABLE_IT(huart, UART_IT_ERR);
-  HAL_UART_Receive_DMA(huart, rx_buf_, RX_BUFFER_SIZE);
+  HAL_UART_Receive_DMA(huart, esc_telem_rx_buf_, RX_BUFFER_SIZE);
 
-  memset(rx_buf_, 0, RX_BUFFER_SIZE);
+  memset(esc_telem_rx_buf_, 0, RX_BUFFER_SIZE);
 }
 
 void ESCReader::update(spinal::ESCTelemetry& esc_msg)
@@ -69,7 +69,7 @@ void ESCReader::update(spinal::ESCTelemetry& esc_msg)
 bool ESCReader::available()
 {
   uint32_t dma_write_ptr = (ESC_BUFFER_SIZE - __HAL_DMA_GET_COUNTER(huart_->hdmarx)) % (ESC_BUFFER_SIZE);
-  return (rd_ptr_ != dma_write_ptr);
+  return (esc_telem_rd_ptr_ != dma_write_ptr);
 }
 
 int ESCReader::readOneByte()
@@ -78,15 +78,15 @@ int ESCReader::readOneByte()
   if (__HAL_UART_GET_FLAG(huart_, UART_FLAG_ORE))
   {
     __HAL_UART_CLEAR_FLAG(huart_, UART_CLEAR_NEF | UART_CLEAR_OREF | UART_FLAG_RXNE | UART_FLAG_ORE);
-    HAL_UART_Receive_DMA(huart_, rx_buf_, ESC_BUFFER_SIZE);  // restart
+    HAL_UART_Receive_DMA(huart_, esc_telem_rx_buf_, ESC_BUFFER_SIZE);  // restart
   }
 
   uint32_t dma_write_ptr = (ESC_BUFFER_SIZE - __HAL_DMA_GET_COUNTER(huart_->hdmarx)) % (ESC_BUFFER_SIZE);
   int c = -1;
-  if (rd_ptr_ != dma_write_ptr)
+  if (esc_telem_rd_ptr_ != dma_write_ptr)
   {
-    c = (int)rx_buf_[rd_ptr_++];
-    rd_ptr_ %= ESC_BUFFER_SIZE;
+    c = (int)esc_telem_rx_buf_[esc_telem_rd_ptr_++];
+    esc_telem_rd_ptr_ %= ESC_BUFFER_SIZE;
   }
   return c;
 }
