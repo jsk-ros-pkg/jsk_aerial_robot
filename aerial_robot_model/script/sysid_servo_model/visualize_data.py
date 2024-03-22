351b8a2a3020892e353b3477feb67630e7fb934d
LI, Jinjie <li-jinjie@g.ecc.u-tokyo.ac.jp>
2024-03-22 18:22:17 +0900

    [SYSID][Servo][Py]: modify the style

--- aerial_robot_model/script/sysid_servo_model/visualize_data.py
+++ aerial_robot_model/script/sysid_servo_model/visualize_data.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
+import scienceplots
 from scipy.interpolate import interp1d
 
 # Load the input data from the CSV file
@@ -40,13 +41,19 @@ y0 = 0
 y = odeint(model, y0, t)
 
 # Plotting
-plt.figure(figsize=(10, 5))
-plt.plot(t, y, label='y(t)')
-plt.plot(t, u_interpolated(t), 'r--', label='Interpolated input u(t)')
-plt.plot(time_output, y_output, 'g--', label='Actual output y(t)')
-plt.xlabel('Time')
-plt.ylabel('Values')
-plt.title('First-order system response with varying input')
-plt.legend()
-plt.grid()
+plt.style.use(["science", "grid"])
+plt.rcParams.update({'font.size': 11})  # default is 10
+label_size = 15
+legend_alpha = 0.5
+
+fig = plt.figure(figsize=(3.5, 3))
+plt.plot(t, u_interpolated(t), label='input')
+plt.plot(time_output, y_output, label='real')
+plt.plot(t, y, label='estimated')
+plt.xlabel('Time (s)', fontsize=label_size)
+plt.ylabel('Servo Angle (rad)', fontsize=label_size)
+# plt.title('First-order system response with varying input')
+plt.legend(framealpha=legend_alpha)
+plt.xlim([4, 5])
+plt.tight_layout()
 plt.show()
