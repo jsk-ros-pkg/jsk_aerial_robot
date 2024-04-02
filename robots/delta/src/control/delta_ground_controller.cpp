a81d11055476269d20f52d7707fda44aa5540033
sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2024-04-02 14:16:04 +0900

    [aerial_robot_control] publish pid message about roll and pitch in PoseLinearController

--- robots/delta/src/control/delta_ground_controller.cpp
+++ robots/delta/src/control/delta_ground_controller.cpp
@@ -87,24 +87,6 @@ void RollingController::standingPlanning()
     {
       rolling_navigator_->setGroundNavigationMode(aerial_robot_navigation::STANDING_STATE);
     }
-
-  pid_msg_.roll.total.at(0) = pid_controllers_.at(ROLL).result();
-  pid_msg_.roll.p_term.at(0) = pid_controllers_.at(ROLL).getPTerm();
-  pid_msg_.roll.i_term.at(0) = pid_controllers_.at(ROLL).getITerm();
-  pid_msg_.roll.d_term.at(0) = pid_controllers_.at(ROLL).getDTerm();
-  pid_msg_.roll.target_p = target_rpy_.x();
-  pid_msg_.roll.err_p = pid_controllers_.at(ROLL).getErrP();
-  pid_msg_.roll.target_d = target_omega_.x();
-  pid_msg_.roll.err_d = pid_controllers_.at(ROLL).getErrD();
-  pid_msg_.pitch.total.at(0) = pid_controllers_.at(PITCH).result();
-  pid_msg_.pitch.p_term.at(0) = pid_controllers_.at(PITCH).getPTerm();
-  pid_msg_.pitch.i_term.at(0) = pid_controllers_.at(PITCH).getITerm();
-  pid_msg_.pitch.d_term.at(0) = pid_controllers_.at(PITCH).getDTerm();
-  pid_msg_.pitch.target_p = target_rpy_.y();
-  pid_msg_.pitch.err_p = pid_controllers_.at(PITCH).getErrP();
-  pid_msg_.pitch.target_d = target_omega_.y();
-  pid_msg_.pitch.err_d = pid_controllers_.at(PITCH).getErrD();
-
 }
 
 void RollingController::calcStandingFullLambda()
