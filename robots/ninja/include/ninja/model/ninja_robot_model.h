616f734c138b8456607950773405dd994fd31227
Junichiro Sugihara <j-sugihara@jsk.imi.i.u-tokyo.ac.jp>
2024-07-01 16:20:55 +0900

    [Ninja] create a new feature to generate copy tree of initial module

--- robots/ninja/include/ninja/model/ninja_robot_model.h
+++ robots/ninja/include/ninja/model/ninja_robot_model.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include <beetle/model/beetle_robot_model.h>
+#include <unordered_set>
 
 using namespace aerial_robot_model;
 
@@ -13,9 +14,12 @@ public:
                   double fc_t_min_thre = 0,
                   double epsilon = 10);
   virtual ~NinjaRobotModel() = default;
+  const KDL::Tree& getInitModuleTree(){ return init_module_tree_; }
 
 protected:
   void updateRobotModelImpl(const KDL::JntArray& joint_positions) override;
+  void addSegmentsRecursively(const KDL::Tree& source_tree, KDL::Tree& destination_tree, const std::string& segment_name, std::unordered_set<std::string>& added_segments)
+  void copyTreeStructure(const KDL::Tree& source_tree, KDL::Tree& destination_tree);
 private:
-  KDL::Tree entire_structure_;
+  KDL::Tree init_module_tree_;
 };
--- robots/ninja/src/model/ninja_robot_model.cpp
+++ robots/ninja/src/model/ninja_robot_model.cpp
@@ -3,13 +3,59 @@
 NinjaRobotModel::NinjaRobotModel(bool init_with_rosparam, bool verbose, double fc_t_min_thre, double epsilon) :
   BeetleRobotModel(init_with_rosparam, verbose, fc_t_min_thre, epsilon)
 {
-  
+  /*Create copy tree model of single module*/
+  init_module_tree_ = KDL::Tree("root");
+  copyTreeStructure(getTree(), init_module_tree_);
 }
 
 void NinjaRobotModel::updateRobotModelImpl(const KDL::JntArray& joint_positions)
 {
   GimbalrotorRobotModel::updateRobotModelImpl(joint_positions);
 }
+
+bool NinjaRobotModel::addSegmentsRecursively(const KDL::Tree& source_tree, KDL::Tree& destination_tree, const std::string& segment_name, std::unordered_set<std::string>& added_segments)
+{
+  const KDL::SegmentMap::const_iterator it = source_tree.getSegment(segment_name);
+  if (it == source_tree.getSegments().end()) {
+    std::cerr << "Segment " << segment_name << " not found in source tree!" << std::endl;
+    return false;
+  }
+
+  const std::string parent_name = it->second.parent->second.segment.getName();
+    
+  if (added_segments.find(parent_name) == added_segments.end()) {
+    if (!addSegmentsRecursively(source_tree, destination_tree, parent_name, added_segments)) {
+      return false;
+    }
+  }
+
+  if (!destination_tree.addSegment(it->second.segment, parent_name)) {
+    std::cerr << "Failed to add segment " << segment_name << " to tree!" << std::endl;
+    return false;
+  }
+    
+  added_segments.insert(segment_name);
+  return true;
+}
+
+void NinjaRobotModel::copyTreeStructure(const KDL::Tree& source_tree, KDL::Tree& destination_tree)
+{
+  std::unordered_set<std::string> added_segments;    
+  const std::string& root_name = source_tree.getRootSegment()->first;
+  destination_tree = KDL::Tree(root_name);
+  added_segments.insert(root_name);
+
+  for (const auto& segment_pair : source_tree.getSegments()) {
+    const std::string& segment_name = segment_pair.second.segment.getName();
+    if (added_segments.find(segment_name) == added_segments.end()) {
+      if (!addSegmentsRecursively(source_tree, destination_tree, segment_name, added_segments)) {
+        std::cerr << "Failed to copy segment " << segment_name << " to destination tree!" << std::endl;
+        return;
+      }
+    }
+  }
+}
+
 
 /* plugin registration */
 #include <pluginlib/class_list_macros.h>
