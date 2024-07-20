f20ef4b9fb5a729947ddf5087b2e74f0c41b1450
Jinjie Li <45286479+Li-Jinjie@users.noreply.github.com>
2024-07-20 10:28:20 +0900

    [Control][NMPC][EKF]: finish the first version. tested in simulation.

--- aerial_robot_control/include/aerial_robot_control/nmpc/kalman_filter.h
+++ aerial_robot_control/include/aerial_robot_control/nmpc/kalman_filter.h
@@ -16,14 +16,7 @@ public:
   void init(const Eigen::MatrixXd& A, const Eigen::MatrixXd& B, const Eigen::MatrixXd& G, const Eigen::MatrixXd& C,
             const Eigen::MatrixXd& Q, const Eigen::MatrixXd& R, const Eigen::MatrixXd& P, const Eigen::VectorXd& x)
   {
-    A_ = A;
-    B_ = B;
-    G_ = G;
-    C_ = C;
-    Q_ = Q;
-    R_ = R;
-    P_ = P;
-    x_ = x;
+    A_ = A, B_ = B, G_ = G, C_ = C, Q_ = Q, R_ = R, P_ = P, x_ = x;
   }
 
   void predict(const Eigen::VectorXd& u, const Eigen::MatrixXd& A = Eigen::MatrixXd::Zero(0, 0),
@@ -37,8 +30,26 @@ public:
     if (G.rows() != 0)
       G_ = G;
 
-    // TODO: consider using the nonlinear state transition model
     x_ = A_ * x_ + B_ * u + G_ * Eigen::VectorXd::Random(G_.cols());
+    P_ = A_ * P_ * A_.transpose() + G_ * Q_ * G_.transpose();
+  }
+
+  /**
+   * If use other nonlinear time update function to calculate x, use this method. Otherwise, use void predict().
+   * @param x_new
+   * @param A
+   * @param G
+   */
+  void predictWithX(const Eigen::VectorXd& x_new, const Eigen::MatrixXd& A,
+                    const Eigen::MatrixXd& G = Eigen::MatrixXd::Zero(0, 0))
+  {
+    A_ = A;
+
+    if (G.rows() != 0)
+      G_ = G;
+
+    x_ = x_new;
+
     P_ = A_ * P_ * A_.transpose() + G_ * Q_ * G_.transpose();
   }
 
@@ -52,9 +63,32 @@ public:
     P_ = (Eigen::MatrixXd::Identity(P_.rows(), P_.cols()) - K_ * C_) * P_;
   }
 
+  static Eigen::MatrixXd stdVec2EigenMat(const std::vector<double>& vec, int rows, int cols)
+  {
+    if (vec.size() != rows * cols)
+      throw std::length_error("The size of the vector does not match the size of the matrix");
+
+    // Ensure the matrix uses row-major storage to match the vector's layout
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> mat(rows, cols);
+
+    // Use std::memcpy to copy the data from the vector to the Eigen matrix
+    std::memcpy(mat.data(), vec.data(), vec.size() * sizeof(double));
+
+    return mat;
+  }
+
+  static Eigen::VectorXd stdVec2EigenVec(const std::vector<double>& vec)
+  {
+    Eigen::VectorXd eigen_vec(vec.size());
+    // Use std::memcpy to copy the data directly
+    std::memcpy(eigen_vec.data(), vec.data(), vec.size() * sizeof(double));
+    return eigen_vec;
+  }
+
   /* clang-format off */
   // getter
   Eigen::VectorXd getX() const { return x_; }
+  double getX(int i) const { return x_(i); }
   Eigen::MatrixXd getP() const { return P_; }
   Eigen::MatrixXd getK() const { return K_; }
 
--- aerial_robot_control/src/nmpc/tilt_qd_servo_dist_mdl/nmpc_controller_ekf.cpp
+++ aerial_robot_control/src/nmpc/tilt_qd_servo_dist_mdl/nmpc_controller_ekf.cpp
@@ -13,16 +13,111 @@ void nmpc::TiltQdServoNMPCwEKF::initParams()
   ros::NodeHandle control_nh(nh_, "controller");
   ros::NodeHandle ekf_nh(control_nh, "ekf");
 
+  sim_solver_ptr_->initialize();  // TODO: try to put it together with mpc_solver_ptr_->initialize() in the future
+
   /* EKF */
+  double Qp, Qv, Qq, Qw, Qa, Qd_f_i, Qd_tau_b;
+  getParam<double>(ekf_nh, "Qp", Qp, 0.01);
+  getParam<double>(ekf_nh, "Qv", Qv, 0.01);
+  getParam<double>(ekf_nh, "Qq", Qq, 0.01);
+  getParam<double>(ekf_nh, "Qw", Qw, 0.01);
+  getParam<double>(ekf_nh, "Qa", Qa, 0.01);
+  getParam<double>(ekf_nh, "Qd_f_i", Qd_f_i, 0.5);
+  getParam<double>(ekf_nh, "Qd_tau_b", Qd_tau_b, 0.1);
+
+  double Rp, Rv, Rq, Rw, Ra;
+  getParam<double>(ekf_nh, "Rp", Rp, 0.005);
+  getParam<double>(ekf_nh, "Rv", Rv, 0.05);
+  getParam<double>(ekf_nh, "Rq", Rq, 0.01);
+  getParam<double>(ekf_nh, "Rw", Rw, 0.05);
+  getParam<double>(ekf_nh, "Ra", Ra, 0.01);
+
+  int& NX = sim_solver_ptr_->NX_;
+  int& NU = sim_solver_ptr_->NU_;
+
+  Eigen::MatrixXd A = KalmanFilter::stdVec2EigenMat(sim_solver_ptr_->getMatrixA(), NX, NX);
+
+  Eigen::MatrixXd B = KalmanFilter::stdVec2EigenMat(sim_solver_ptr_->getMatrixB(), NX, NU);
+
+  Eigen::MatrixXd G = Eigen::MatrixXd ::Identity(NX, NX);
+
+  Eigen::MatrixXd C = Eigen::MatrixXd::Identity(17, NX);
+
+  Eigen::MatrixXd Q = Eigen::MatrixXd::Zero(NX, NX);
+  Q(0, 0) = Qp, Q(1, 1) = Qp, Q(2, 2) = Qp;
+  Q(3, 3) = Qv, Q(4, 4) = Qv, Q(5, 5) = Qv;
+  Q(6, 6) = Qq, Q(7, 7) = Qq, Q(8, 8) = Qq, Q(9, 9) = Qq;
+  Q(10, 10) = Qw, Q(11, 11) = Qw, Q(12, 12) = Qw;
+  Q(13, 13) = Qa, Q(14, 14) = Qa, Q(15, 15) = Qa, Q(16, 16) = Qa;
+  Q(17, 17) = Qd_f_i, Q(18, 18) = Qd_f_i, Q(19, 19) = Qd_f_i;
+  Q(20, 20) = Qd_tau_b, Q(21, 21) = Qd_tau_b, Q(22, 22) = Qd_tau_b;
+
+  Eigen::MatrixXd R = Eigen::MatrixXd::Zero(17, NX);
+  R(0, 0) = Rp, R(1, 1) = Rp, R(2, 2) = Rp;
+  R(3, 3) = Rv, R(4, 4) = Rv, R(5, 5) = Rv;
+  R(6, 6) = Rq, R(7, 7) = Rq, R(8, 8) = Rq, R(9, 9) = Rq;
+  R(10, 10) = Rw, R(11, 11) = Rw, R(12, 12) = Rw;
+  R(13, 13) = Ra, R(14, 14) = Ra, R(15, 15) = Ra, R(16, 16) = Ra;
+
+  Eigen::MatrixXd P = Eigen::MatrixXd::Identity(NX, NX);
+
+  Eigen::VectorXd x = Eigen::VectorXd::Zero(NX);
+  x(6) = 1.0; // q.w
+
+  ekf_.init(A, B, G, C, Q, R, P, x);
 }
 
 void nmpc::TiltQdServoNMPCwEKF::calcDisturbWrench()
 {
+  Eigen::VectorXd z = Eigen::VectorXd::Zero(sim_solver_ptr_->NX_);
+  z(0) = odom_.pose.pose.position.x;
+  z(1) = odom_.pose.pose.position.y;
+  z(2) = odom_.pose.pose.position.z;
+  z(3) = odom_.twist.twist.linear.x;
+  z(4) = odom_.twist.twist.linear.y;
+  z(5) = odom_.twist.twist.linear.z;
+  z(6) = odom_.pose.pose.orientation.w;
+  z(7) = odom_.pose.pose.orientation.x;
+  z(8) = odom_.pose.pose.orientation.y;
+  z(9) = odom_.pose.pose.orientation.z;
+  z(10) = odom_.twist.twist.angular.x;
+  z(11) = odom_.twist.twist.angular.y;
+  z(12) = odom_.twist.twist.angular.z;
+  z(13) = joint_angles_.at(0);
+  z(14) = joint_angles_.at(1);
+  z(15) = joint_angles_.at(2);
+  z(16) = joint_angles_.at(3);
+  ekf_.update(z);
+
+  dist_force_w_.x = ekf_.getX(17);
+  dist_force_w_.y = ekf_.getX(18);
+  dist_force_w_.z = ekf_.getX(19);
+  dist_torque_cog_.x = ekf_.getX(20);
+  dist_torque_cog_.y = ekf_.getX(21);
+  dist_torque_cog_.z = ekf_.getX(22);
+
+  // time update  TODO: optimize the for loop
+  Eigen::VectorXd x_eigen = ekf_.getX();
+  std::vector<double> x(sim_solver_ptr_->NX_, 0.0);
+  for (int i = 0; i < sim_solver_ptr_->NX_; i++)
+    x[i] = x_eigen(i);
+
+  std::vector<double> u(sim_solver_ptr_->NU_, 0.0);
+  for (int i = 0; i < sim_solver_ptr_->NU_; i++)
+    u[i] = getCommand(i);
+
+  sim_solver_ptr_->solve(x, u);
+
+  int& NX = sim_solver_ptr_->NX_;
+  Eigen::VectorXd xo = KalmanFilter::stdVec2EigenVec(sim_solver_ptr_->xo_);
+  Eigen::MatrixXd A = KalmanFilter::stdVec2EigenMat(sim_solver_ptr_->getMatrixA(), NX, NX);
+  ekf_.predictWithX(xo, A);
 }
 
 void nmpc::TiltQdServoNMPCwEKF::cfgNMPCCallback(NMPCConfig& config, uint32_t level)
 {
   TiltQdServoNMPC::cfgNMPCCallback(config, level);
+  // TODO： implement the callback function
 }
 
 /* plugin registration */
