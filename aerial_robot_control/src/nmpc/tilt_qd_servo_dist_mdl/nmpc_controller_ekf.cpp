58f2e43ac248e9de150cd757b638294b1df150b1
Jinjie Li <45286479+Li-Jinjie@users.noreply.github.com>
2024-07-23 10:36:35 +0900

    [Control][NMPC][EKF]: update the nmpc_controller_ekf to be align with the newest EKF

--- aerial_robot_control/src/nmpc/tilt_qd_servo_dist_mdl/nmpc_controller_ekf.cpp
+++ aerial_robot_control/src/nmpc/tilt_qd_servo_dist_mdl/nmpc_controller_ekf.cpp
@@ -6,6 +6,18 @@
 
 using namespace aerial_robot_control;
 
+void nmpc::TiltQdServoNMPCwEKF::initialize(ros::NodeHandle nh, ros::NodeHandle nhp,
+                                           boost::shared_ptr<aerial_robot_model::RobotModel> robot_model,
+                                           boost::shared_ptr<aerial_robot_estimation::StateEstimator> estimator,
+                                           boost::shared_ptr<aerial_robot_navigation::BaseNavigator> navigator,
+                                           double ctrl_loop_du)
+{
+  nmpc::TiltQdServoDistNMPC::initialize(nh, nhp, robot_model, estimator, navigator, ctrl_loop_du);
+
+  sub_imu_ = nh_.subscribe("imu", 1, &TiltQdServoNMPCwEKF::callbackImu, this);
+  sub_mocap_ = nh_.subscribe("mocap", 1, &TiltQdServoNMPCwEKF::callbackMoCap, this);
+}
+
 void nmpc::TiltQdServoNMPCwEKF::initParams()
 {
   TiltQdServoNMPC::initParams();
@@ -16,91 +28,64 @@ void nmpc::TiltQdServoNMPCwEKF::initParams()
   sim_solver_ptr_->initialize();  // TODO: try to put it together with mpc_solver_ptr_->initialize() in the future
 
   /* EKF */
-  double Qp, Qv, Qq, Qw, Qa, Qd_f_i, Qd_tau_b;
-  getParam<double>(ekf_nh, "Qp", Qp, 0.01);
-  getParam<double>(ekf_nh, "Qv", Qv, 0.01);
-  getParam<double>(ekf_nh, "Qq", Qq, 0.01);
-  getParam<double>(ekf_nh, "Qw", Qw, 0.01);
-  getParam<double>(ekf_nh, "Qa", Qa, 0.01);
-  getParam<double>(ekf_nh, "Qd_f_i", Qd_f_i, 0.5);
-  getParam<double>(ekf_nh, "Qd_tau_b", Qd_tau_b, 0.1);
+  double ekf_ts;
+  getParam<double>(ekf_nh, "ts", ekf_ts, 0.01);
 
-  double Rp, Rv, Rq, Rw, Ra;
-  getParam<double>(ekf_nh, "Rp", Rp, 0.005);
-  getParam<double>(ekf_nh, "Rv", Rv, 0.05);
-  getParam<double>(ekf_nh, "Rq", Rq, 0.01);
-  getParam<double>(ekf_nh, "Rw", Rw, 0.05);
-  getParam<double>(ekf_nh, "Ra", Ra, 0.01);
+  double Qd_f_ixy, Qd_f_iz, Qd_tau_bxy, Qd_tau_bz;
+  getParam<double>(ekf_nh, "Qd_f_ixy", Qd_f_ixy, 0.5);
+  getParam<double>(ekf_nh, "Qd_f_iz", Qd_f_iz, 0.5);
+  getParam<double>(ekf_nh, "Qd_tau_bxy", Qd_tau_bxy, 0.1);
+  getParam<double>(ekf_nh, "Qd_tau_bz", Qd_tau_bz, 0.1);
+
+  // IMU
+  double R_imu_sf, R_imu_q, R_imu_w;
+  getParam<double>(ekf_nh, "R_imu_sf", R_imu_sf, 0.01);
+  getParam<double>(ekf_nh, "R_imu_q", R_imu_q, 0.1);
+  getParam<double>(ekf_nh, "R_imu_w", R_imu_w, 0.1);
+
+  // MoCap
+  double R_mocap_p, R_mocap_q;
+  getParam<double>(ekf_nh, "R_mocap_p", R_mocap_p, 0.005);
+  getParam<double>(ekf_nh, "R_mocap_q", R_mocap_q, 0.01);
+
+  // Servo
+  double R_servo_a;
+  getParam<double>(ekf_nh, "R_servo_a", R_servo_a, 0.01);
 
   int& NX = sim_solver_ptr_->NX_;
   int& NU = sim_solver_ptr_->NU_;
 
-  Eigen::MatrixXd A = KalmanFilter::stdVec2EigenMat(sim_solver_ptr_->getMatrixA(), NX, NX);
+  Eigen::MatrixXd Q = Eigen::MatrixXd::Zero(6, 6);
+  Q(0, 0) = Qd_f_ixy, Q(1, 1) = Qd_f_ixy, Q(2, 2) = Qd_f_iz;
+  Q(3, 3) = Qd_tau_bxy, Q(4, 4) = Qd_tau_bxy, Q(5, 5) = Qd_tau_bz;
 
-  Eigen::MatrixXd B = KalmanFilter::stdVec2EigenMat(sim_solver_ptr_->getMatrixB(), NX, NU);
+  Eigen::MatrixXd R_imu = Eigen::MatrixXd::Zero(10, 10);
+  R_imu(0, 0) = R_imu_sf, R_imu(1, 1) = R_imu_sf, R_imu(2, 2) = R_imu_sf;
+  R_imu(3, 3) = R_imu_q, R_imu(4, 4) = R_imu_q, R_imu(5, 5) = R_imu_q, R_imu(6, 6) = R_imu_q;
+  R_imu(7, 7) = R_imu_w, R_imu(8, 8) = R_imu_w, R_imu(9, 9) = R_imu_w;
 
-  Eigen::MatrixXd G = Eigen::MatrixXd ::Identity(NX, NX);
+  Eigen::MatrixXd R_mocap = Eigen::MatrixXd::Zero(7, 7);
+  R_mocap(0, 0) = R_mocap_p, R_mocap(1, 1) = R_mocap_p, R_mocap(2, 2) = R_mocap_p;
+  R_mocap(3, 3) = R_mocap_q, R_mocap(4, 4) = R_mocap_q, R_mocap(5, 5) = R_mocap_q, R_mocap(6, 6) = R_mocap_q;
 
-  Eigen::MatrixXd C = Eigen::MatrixXd::Identity(17, NX);
+  Eigen::MatrixXd R_servo = Eigen::MatrixXd::Zero(4, 4);
+  R_servo(0, 0) = R_servo_a, R_servo(1, 1) = R_servo_a, R_servo(2, 2) = R_servo_a, R_servo(3, 3) = R_servo_a;
 
-  Eigen::MatrixXd Q = Eigen::MatrixXd::Zero(NX, NX);
-  Q(0, 0) = Qp, Q(1, 1) = Qp, Q(2, 2) = Qp;
-  Q(3, 3) = Qv, Q(4, 4) = Qv, Q(5, 5) = Qv;
-  Q(6, 6) = Qq, Q(7, 7) = Qq, Q(8, 8) = Qq, Q(9, 9) = Qq;
-  Q(10, 10) = Qw, Q(11, 11) = Qw, Q(12, 12) = Qw;
-  Q(13, 13) = Qa, Q(14, 14) = Qa, Q(15, 15) = Qa, Q(16, 16) = Qa;
-  Q(17, 17) = Qd_f_i, Q(18, 18) = Qd_f_i, Q(19, 19) = Qd_f_i;
-  Q(20, 20) = Qd_tau_b, Q(21, 21) = Qd_tau_b, Q(22, 22) = Qd_tau_b;
+  Eigen::MatrixXd P_init = Eigen::MatrixXd::Identity(NX, NX);
 
-  Eigen::MatrixXd R = Eigen::MatrixXd::Zero(17, 17);
-  R(0, 0) = Rp, R(1, 1) = Rp, R(2, 2) = Rp;
-  R(3, 3) = Rv, R(4, 4) = Rv, R(5, 5) = Rv;
-  R(6, 6) = Rq, R(7, 7) = Rq, R(8, 8) = Rq, R(9, 9) = Rq;
-  R(10, 10) = Rw, R(11, 11) = Rw, R(12, 12) = Rw;
-  R(13, 13) = Ra, R(14, 14) = Ra, R(15, 15) = Ra, R(16, 16) = Ra;
+  Eigen::VectorXd x_init = Eigen::VectorXd::Zero(NX);
+  x_init(6) = 1.0;  // q.w
 
-  Eigen::MatrixXd P = Eigen::MatrixXd::Identity(NX, NX);
-
-  Eigen::VectorXd x = Eigen::VectorXd::Zero(NX);
-  x(6) = 1.0; // q.w
-
-  ekf_.init(A, B, G, C, Q, R, P, x);
+  ekf_.init(ekf_ts, Q, R_imu, R_mocap, R_servo, P_init, x_init);
 }
 
-std::vector<double> nmpc::TiltQdServoNMPCwEKF::meas2VecX()
+void nmpc::TiltQdServoNMPCwEKF::callbackImu(const spinal::ImuConstPtr& msg)
 {
-  Eigen::VectorXd z = Eigen::VectorXd::Zero(17);
-  z(0) = odom_.pose.pose.position.x;
-  z(1) = odom_.pose.pose.position.y;
-  z(2) = odom_.pose.pose.position.z;
-  z(3) = odom_.twist.twist.linear.x;
-  z(4) = odom_.twist.twist.linear.y;
-  z(5) = odom_.twist.twist.linear.z;
-  z(6) = odom_.pose.pose.orientation.w;
-  z(7) = odom_.pose.pose.orientation.x;
-  z(8) = odom_.pose.pose.orientation.y;
-  z(9) = odom_.pose.pose.orientation.z;
-  z(10) = odom_.twist.twist.angular.x;
-  z(11) = odom_.twist.twist.angular.y;
-  z(12) = odom_.twist.twist.angular.z;
-  z(13) = joint_angles_.at(0);
-  z(14) = joint_angles_.at(1);
-  z(15) = joint_angles_.at(2);
-  z(16) = joint_angles_.at(3);
-  ekf_.update(z);
+  // TODO:   ekf_.updateIMU(z);
 
-  calcDisturbWrench();
-
-  // get bx0
-  vector<double> bx0(mpc_solver_ptr_->NBX0_, 0);
-  for (int i = 0; i < mpc_solver_ptr_->NBX0_; i++)
-    bx0[i] = ekf_.getX(i);
-
-  // time update  TODO: optimize the for loop; put this part after mpc_solver_ptr_->solve() in the future
+  // time update  TODO: put this part after mpc_solver_ptr_->solve() in the future
   Eigen::VectorXd x_eigen = ekf_.getX();
-  std::vector<double> x(sim_solver_ptr_->NX_, 0.0);
-  for (int i = 0; i < sim_solver_ptr_->NX_; i++)
-    x[i] = x_eigen(i);
+  std::vector<double> x = EKFEstimator::eigenVec2StdVec(x_eigen);
 
   std::vector<double> u(sim_solver_ptr_->NU_, 0.0);
   for (int i = 0; i < sim_solver_ptr_->NU_; i++)
@@ -109,9 +94,37 @@ std::vector<double> nmpc::TiltQdServoNMPCwEKF::meas2VecX()
   sim_solver_ptr_->solve(x, u);
 
   int& NX = sim_solver_ptr_->NX_;
-  Eigen::VectorXd xo = KalmanFilter::stdVec2EigenVec(sim_solver_ptr_->xo_);
-  Eigen::MatrixXd A = KalmanFilter::stdVec2EigenMat(sim_solver_ptr_->getMatrixA(), NX, NX);
-  ekf_.predictWithX(xo, A);
+  Eigen::VectorXd xo = EKFEstimator::stdVec2EigenVec(sim_solver_ptr_->xo_);
+  Eigen::MatrixXd A = EKFEstimator::stdVec2EigenMat(sim_solver_ptr_->getMatrixA(), NX, NX);
+  ekf_.predict(xo, A);
+}
+
+void nmpc::TiltQdServoNMPCwEKF::callbackMoCap(const geometry_msgs::PoseStampedConstPtr& msg)
+{
+  Eigen::VectorXd z = Eigen::VectorXd::Zero(7);
+  z(0) = msg->pose.position.x;
+  z(1) = msg->pose.position.y;
+  z(2) = msg->pose.position.z;
+  z(3) = msg->pose.orientation.w;
+  z(4) = msg->pose.orientation.x;
+  z(5) = msg->pose.orientation.y;
+  z(6) = msg->pose.orientation.z;
+  ekf_.updateMoCap(z);
+}
+
+void nmpc::TiltQdServoNMPCwEKF::callbackJointStates(const sensor_msgs::JointStateConstPtr& msg)
+{
+  TiltQdServoNMPC::callbackJointStates(msg);
+  ekf_.updateServo(EKFEstimator::stdVec2EigenVec(joint_angles_));
+}
+
+std::vector<double> nmpc::TiltQdServoNMPCwEKF::meas2VecX()
+{
+  calcDisturbWrench();
+
+  // get bx0
+  Eigen::VectorXd x_eigen = ekf_.getX();
+  std::vector<double> bx0 = EKFEstimator::eigenVec2StdVec(x_eigen);
 
   return bx0;
 }
