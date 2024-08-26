819345153a6fadacef26378e6095b4ded8365dae
heissereal <miyamichi-ayano061@g.ecc.u-tokyo.ac.jp>
2024-08-26 20:14:58 +0900

    flag for perching

--- robots/hugmy/include/hugmy/navigation/hugmy_navigation.h
+++ robots/hugmy/include/hugmy/navigation/hugmy_navigation.h
@@ -15,5 +15,12 @@ namespace aerial_robot_navigation
                     boost::shared_ptr<aerial_robot_estimation::StateEstimator> estimator,
                     double loop_du) override;
     void update() override;
+
+  private:
+    ros::Subscriber perching_flag_sub_;
+
+    void perchingFlagCallback(const std_msgs::UInt8 & msg);
+
+    bool perching_flag_;
   };
 };
