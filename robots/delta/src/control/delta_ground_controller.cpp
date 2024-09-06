948f0896a626d3cb3cc98beda73e03d126ed3e99
sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2024-09-06 15:14:11 +0900

    [delta][control] devide primal variable of thrust by absolute value of cos rotor tilt

--- robots/delta/src/control/delta_ground_controller.cpp
+++ robots/delta/src/control/delta_ground_controller.cpp
@@ -472,7 +472,7 @@ void RollingController::nonlinearGroundWrenchAllocation()
   // thrust and gimbal part (use osqp solution)
   for(int i = 0; i < motor_num_; i++)
     {
-      opt_x.at(i) = std::clamp((double)full_lambda_all_.segment(2 * i, 2).norm(),
+      opt_x.at(i) = std::clamp((double)full_lambda_all_.segment(2 * i, 2).norm() / fabs(cos(rotor_tilt_.at(i))),
                                lb.at(i),
                                ub.at(i));
       opt_x.at(i + motor_num_) = std::clamp((double)angles::normalize_angle(atan2(-full_lambda_all_(2 * i + 0), full_lambda_all_(2 * i + 1))),
