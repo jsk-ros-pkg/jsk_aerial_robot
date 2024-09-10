467411b83fccd13aaee82c50c2914a8d316a7a17
sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2024-09-10 14:19:51 +0900

    [delta][util] publish wrench on each thrusters

--- robots/delta/include/delta/util/pwm_republisher.h
+++ robots/delta/include/delta/util/pwm_republisher.h
@@ -1,10 +1,13 @@
 #include <ros/ros.h>
+#include <delta/model/delta_robot_model.h>
 #include "sensor_msgs/JointState.h"
 #include "spinal/DesireCoord.h"
 #include "spinal/Pwms.h"
+#include "spinal/FourAxisCommand.h"
 #include "std_msgs/Float32.h"
 #include "geometry_msgs/Quaternion.h"
 #include <tf/tf.h>
+#include "geometry_msgs/WrenchStamped.h"
 
 class MotorPWMRepublisher
 {
@@ -16,10 +19,15 @@ private:
   ros::NodeHandle nh_;
   ros::NodeHandle nhp_;
 
+  boost::shared_ptr<RollingRobotModel> delta_robot_model_;
+
+  int motor_num_;
+  double m_f_rate_;
+  std::string tf_prefix_;
+
   /* motor pwms */
   ros::Subscriber motor_pwm_sub_;
   std::vector<ros::Publisher> motor_pwm_pubs_;
-  int motor_num_;
 
   /* desire coordinate */
   ros::Publisher desire_coordinate_roll_pub_, desire_coordinate_pitch_pub_;
@@ -31,12 +39,17 @@ private:
   ros::Subscriber gimbals_control_sub_;
   std::vector<ros::Publisher> gimbal_control_pubs_;
 
+  /* four axis command */
+  ros::Subscriber four_axis_command_sub_;
+  std::vector<ros::Publisher> thruster_wrench_pubs_;
+
   /* timer */
   ros::Timer timer_;
 
   void motorPwmCallback(const spinal::PwmsPtr & pwm_msg);
   void desireCoordinateCallback(const geometry_msgs::Quaternion & msg);
   void gimbalsControlCallback(const sensor_msgs::JointState & joint_state_msg);
+  void fourAxisCommandCallback(const spinal::FourAxisCommand & msg);
   void timerCallback(const ros::TimerEvent & e);
 
 };
