76d77aa567838fd46bf403b0814352d73f316aff
LI, Jinjie <li-jinjie@g.ecc.u-tokyo.ac.jp>
2024-09-17 18:21:21 +0900

    [Spinal][Dynamixel]: add default goal_position 2048

--- aerial_robot_nerve/spinal/mcu_project/lib/Jsk_Lib/servo/Dynamixel/dynamixel_serial.h
+++ aerial_robot_nerve/spinal/mcu_project/lib/Jsk_Lib/servo/Dynamixel/dynamixel_serial.h
@@ -282,7 +282,7 @@ private:
 class ServoData {
 public:
 	ServoData(){}
-  ServoData(uint8_t id): id_(id), torque_enable_(false), first_get_pos_flag_(true), internal_offset_(0){}
+  ServoData(uint8_t id): id_(id), torque_enable_(false), first_get_pos_flag_(true), internal_offset_(0), goal_position_(2048){}
 
 	uint8_t id_;
   	int32_t present_position_;
