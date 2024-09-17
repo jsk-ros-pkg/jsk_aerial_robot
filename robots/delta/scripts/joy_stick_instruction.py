b09f4d9ba0e8cde755b09f515fd24c29dca8fe37
sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2024-09-17 20:01:17 +0900

    [delta][script] add keyboard command script and update joy stick instruction script

--- robots/delta/scripts/joy_stick_instruction.py
+++ robots/delta/scripts/joy_stick_instruction.py
@@ -8,14 +8,17 @@ rospy.init_node("delta_joy_stick_instruction")
 
 msg = """\
 
-Standing mode: L1 + up
-Rolling  mode: L1 + down
-Flying   mode: R1 + left
-Down     mode: R1 + right
+Switch to Locomotion mode: L2
+  Standing mode: L1 + up
+  Rolling  mode: L1 + down
+  Flying   mode: L1 + left
+  Down     mode: L1 + right
+  Rolling pitch: L1 + Lstick vertical
+  Rolling yaw  : L1 + Rstick holizontal
 
-Rolling pitch: L1 + Lstick vertical
-Rolling yaw  : L1 + Rstick holizontal
-
+Switch to Manipulation mode: R2
+  end effector x: R1 + Lstick vertical
+  end effector z: R1 + Rstick vertical
 """
 
 print(msg)
