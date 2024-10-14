013e7565827fcf2700824c216c6d6045c441100e
sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2024-10-14 21:01:23 +0900

    [aerial_robot_estimation][imu] calculate baselink rotation from imu and magnetmeter and update it with VO/mocap

--- aerial_robot_estimation/include/aerial_robot_estimation/sensor/imu.h
+++ aerial_robot_estimation/include/aerial_robot_estimation/sensor/imu.h
@@ -77,7 +77,7 @@ namespace sensor_plugin
     double sensor_dt_;
 
     /* imu */
-    tf::Vector3 wz_b_; /* unit z axis of world frame in baselink frame */
+    tf::Vector3 g_b_; /* the *opposite* gravity vector in baselink frame */
     tf::Vector3 omega_; /* the omega both of body frame */
     tf::Vector3 mag_; /* the magnetometer of body frame */
     /* acc */
