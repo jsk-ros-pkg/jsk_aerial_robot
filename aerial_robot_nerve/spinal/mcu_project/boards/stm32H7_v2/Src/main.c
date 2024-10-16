028faee1f6b9824a0f6cd6ba9d11b7f9f56727b0
Junichiro Sugihara <j-sugihara@jsk.imi.i.u-tokyo.ac.jp>
2024-10-16 15:28:53 +0900

    [Spinal4.1][DShot] restore the procedure of reading esc telem as previous version

--- aerial_robot_nerve/spinal/mcu_project/boards/stm32H7_v2/Src/main.c
+++ aerial_robot_nerve/spinal/mcu_project/boards/stm32H7_v2/Src/main.c
@@ -250,7 +250,7 @@ int main(void)
   IMU_ROS_CMD::addImu(&imu_);
   baro_.init(&hi2c1, &nh_, BAROCS_GPIO_Port, BAROCS_Pin);
   gps_.init(&huart3, &nh_, LED2_GPIO_Port, LED2_Pin);
-  battery_status_.init(&hadc1, &nh_, &dshot_);
+  battery_status_.init(&hadc1, &nh_, false);
   estimator_.init(&imu_, &baro_, &gps_, &nh_);  // imu + baro + gps => att + alt + pos(xy)
   dshot_.init(DSHOT600, &htim1,TIM_CHANNEL_1, &htim1,TIM_CHANNEL_2, &htim1,TIM_CHANNEL_3, &htim1, TIM_CHANNEL_4);
   dshot_.initTelemetry(&huart6);
