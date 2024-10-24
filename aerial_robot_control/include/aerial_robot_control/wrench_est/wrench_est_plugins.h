92587c99cc700ae2074ac20ecbc77945dbf9d640
LI, Jinjie <li-jinjie@g.ecc.u-tokyo.ac.jp>
2024-10-24 13:23:23 +0900

    [wrench_est][Plugin]: finish i term plugin.

--- aerial_robot_control/include/aerial_robot_control/wrench_est/wrench_est_plugins.h
+++ aerial_robot_control/include/aerial_robot_control/wrench_est/wrench_est_plugins.h
@@ -6,6 +6,7 @@
 #define AERIAL_ROBOT_CONTROL_WRENCH_EST_PLUGINS_H
 
 #include "aerial_robot_control/wrench_est/wrench_est_base.h"
+#include "aerial_robot_control/nmpc/i_term.h"
 #include <cmath>
 
 namespace aerial_robot_control
@@ -18,17 +19,69 @@ public:
 
   void initialize(ros::NodeHandle nh, double ctrl_loop_du) override
   {
-    double du = getCtrlLoopDu();
-    ROS_INFO("WrenchEstITerm initialize %f", du);
+    setCtrlLoopDu(ctrl_loop_du);
+
+    ros::NodeHandle i_term_nh(nh, "controller/i_term");
+
+    double fx_limit, fy_limit, fz_limit, mx_limit, my_limit, mz_limit;
+    getParam<double>(i_term_nh, "limit_fx", fx_limit, 5.0);
+    getParam<double>(i_term_nh, "limit_fy", fy_limit, 5.0);
+    getParam<double>(i_term_nh, "limit_fz", fz_limit, 5.0);
+    getParam<double>(i_term_nh, "limit_mx", mx_limit, 1.0);
+    getParam<double>(i_term_nh, "limit_my", my_limit, 1.0);
+    getParam<double>(i_term_nh, "limit_mz", mz_limit, 1.0);
+
+    double i_gain_x, i_gain_y, i_gain_z, i_gain_roll, i_gain_pitch, i_gain_yaw;
+    getParam<double>(i_term_nh, "i_gain_x", i_gain_x, 1.0);
+    getParam<double>(i_term_nh, "i_gain_y", i_gain_y, 1.0);
+    getParam<double>(i_term_nh, "i_gain_z", i_gain_z, 1.0);
+    getParam<double>(i_term_nh, "i_gain_roll", i_gain_roll, 0.5);
+    getParam<double>(i_term_nh, "i_gain_pitch", i_gain_pitch, 0.5);
+    getParam<double>(i_term_nh, "i_gain_yaw", i_gain_yaw, 0.5);
+
+    double freq = 1.0 / getCtrlLoopDu();
+    pos_i_term_[0].initialize(i_gain_x, fx_limit, freq);  // x
+    pos_i_term_[1].initialize(i_gain_y, fy_limit, freq);  // y
+    pos_i_term_[2].initialize(i_gain_z, fz_limit, freq);  // z
+
+    pos_i_term_[3].initialize(i_gain_roll, mx_limit, freq);   // roll
+    pos_i_term_[4].initialize(i_gain_pitch, my_limit, freq);  // pitch
+    pos_i_term_[5].initialize(i_gain_yaw, mz_limit, freq);    // yaw
   }
 
   void update(const tf::Vector3& pos_ref, const tf::Quaternion& q_ref, const tf::Vector3& pos,
               const tf::Quaternion& q) override
   {
-    // do nothing
+    double qw = q.w();
+    double qx = q.x();
+    double qy = q.y();
+    double qz = q.z();
+
+    double qwr = q_ref.w();
+    double qxr = q_ref.x();
+    double qyr = q_ref.y();
+    double qzr = q_ref.z();
+
+    double qe_w = qw * qwr + qx * qxr + qy * qyr + qz * qzr;
+    double qe_x = qwr * qx - qw * qxr + qyr * qz - qy * qzr;
+    double qe_y = qwr * qy - qw * qyr - qxr * qz + qx * qzr;
+    double qe_z = qxr * qy - qx * qyr + qwr * qz - qw * qzr;
+
+    double sign_qe_w = qe_w > 0 ? 1 : -1;
+
+    double fx_w_i_term = pos_i_term_[0].update(pos.x() - pos_ref.x());
+    double fy_w_i_term = pos_i_term_[1].update(pos.y() - pos_ref.y());
+    double fz_w_i_term = pos_i_term_[2].update(pos.z() - pos_ref.z());
+    double mx_cog_i_term = pos_i_term_[3].update(sign_qe_w * qe_x);
+    double my_cog_i_term = pos_i_term_[4].update(sign_qe_w * qe_y);
+    double mz_cog_i_term = pos_i_term_[5].update(sign_qe_w * qe_z);
+
+    setDistForceW(fx_w_i_term, fy_w_i_term, fz_w_i_term);
+    setDistTorqueCOG(mx_cog_i_term, my_cog_i_term, mz_cog_i_term);
   }
 
 private:
+  ITerm pos_i_term_[6]; // x, y, z, roll, pitch, yaw
 };
 
 class WrenchEstAcc : public aerial_robot_control::WrenchEstBase
--- aerial_robot_control/src/nmpc/tilt_qd_servo_dist_mdl/nmpc_controller_i_term.cpp
+++ aerial_robot_control/src/nmpc/tilt_qd_servo_dist_mdl/nmpc_controller_i_term.cpp
@@ -9,36 +9,6 @@ using namespace aerial_robot_control;
 void nmpc::TiltQdServoNMPCwITerm::initParams()
 {
   TiltQdServoNMPC::initParams();
-
-  ros::NodeHandle control_nh(nh_, "controller");
-  ros::NodeHandle i_term_nh(control_nh, "i_term");
-
-  /* disturbance rejection using I term */
-  double fx_limit, fy_limit, fz_limit, mx_limit, my_limit, mz_limit;
-  getParam<double>(i_term_nh, "limit_fx", fx_limit, 5.0);
-  getParam<double>(i_term_nh, "limit_fy", fy_limit, 5.0);
-  getParam<double>(i_term_nh, "limit_fz", fz_limit, 5.0);
-  getParam<double>(i_term_nh, "limit_mx", mx_limit, 1.0);
-  getParam<double>(i_term_nh, "limit_my", my_limit, 1.0);
-  getParam<double>(i_term_nh, "limit_mz", mz_limit, 1.0);
-
-  double i_gain_x, i_gain_y, i_gain_z, i_gain_roll, i_gain_pitch, i_gain_yaw;
-  getParam<double>(i_term_nh, "i_gain_x", i_gain_x, 1.0);
-  getParam<double>(i_term_nh, "i_gain_y", i_gain_y, 1.0);
-  getParam<double>(i_term_nh, "i_gain_z", i_gain_z, 1.0);
-  getParam<double>(i_term_nh, "i_gain_roll", i_gain_roll, 0.5);
-  getParam<double>(i_term_nh, "i_gain_pitch", i_gain_pitch, 0.5);
-  getParam<double>(i_term_nh, "i_gain_yaw", i_gain_yaw, 0.5);
-
-  double freq = 1.0 / ctrl_loop_du_;
-  pos_i_term_[0].initialize(i_gain_x, fx_limit, freq);  // x
-  pos_i_term_[1].initialize(i_gain_y, fy_limit, freq);  // y
-  pos_i_term_[2].initialize(i_gain_z, fz_limit, freq);  // z
-
-  pos_i_term_[3].initialize(i_gain_roll, mx_limit, freq);   // roll
-  pos_i_term_[4].initialize(i_gain_pitch, my_limit, freq);  // pitch
-  pos_i_term_[5].initialize(i_gain_yaw, mz_limit, freq);    // yaw
-
   wrench_est_loader_ptr_ = boost::make_shared<pluginlib::ClassLoader<aerial_robot_control::WrenchEstBase>>("aerial_robot_control", "aerial_robot_control::WrenchEstBase");
 
   try
@@ -55,62 +25,38 @@ void nmpc::TiltQdServoNMPCwITerm::initParams()
 
 void nmpc::TiltQdServoNMPCwITerm::calcDisturbWrench()
 {
-  /* update I term */
+  /* get the current state */
   tf::Vector3 pos = estimator_->getPos(Frame::COG, estimate_mode_);
   tf::Quaternion q = estimator_->getQuat(Frame::COG, estimate_mode_);
-  double qw = q.w();
-  double qx = q.x();
-  double qy = q.y();
-  double qz = q.z();
 
+  /* get the target state */
   tf::Vector3 target_pos;
-  double qwr, qxr, qyr, qzr;
-  if (!is_traj_tracking_)
-  {
-    target_pos = navigator_->getTargetPos();
-
-    tf::Vector3 target_rpy = navigator_->getTargetRPY();
-    tf::Quaternion q_ref = tf::Quaternion();
-    q_ref.setRPY(target_rpy.x(), target_rpy.y(), target_rpy.z());
-    qwr = q_ref.w();
-    qxr = q_ref.x();
-    qyr = q_ref.y();
-    qzr = q_ref.z();
-  }
-  else
+  tf::Quaternion target_q;
+  if (is_traj_tracking_)
   {
     target_pos.setX(x_u_ref_.x.data.at(0));
     target_pos.setY(x_u_ref_.x.data.at(1));
     target_pos.setZ(x_u_ref_.x.data.at(2));
 
-    qwr = x_u_ref_.x.data.at(6);
-    qxr = x_u_ref_.x.data.at(7);
-    qyr = x_u_ref_.x.data.at(8);
-    qzr = x_u_ref_.x.data.at(9);
+    target_q.setW(x_u_ref_.x.data.at(6));
+    target_q.setX(x_u_ref_.x.data.at(7));
+    target_q.setY(x_u_ref_.x.data.at(8));
+    target_q.setZ(x_u_ref_.x.data.at(9));
+  }
+  else
+  {
+    target_pos = navigator_->getTargetPos();
+
+    tf::Vector3 target_rpy = navigator_->getTargetRPY();
+    target_q.setRPY(target_rpy.x(), target_rpy.y(), target_rpy.z());
   }
 
-  double qe_w = qw * qwr + qx * qxr + qy * qyr + qz * qzr;
-  double qe_x = qwr * qx - qw * qxr + qyr * qz - qy * qzr;
-  double qe_y = qwr * qy - qw * qyr - qxr * qz + qx * qzr;
-  double qe_z = qxr * qy - qx * qyr + qwr * qz - qw * qzr;
+  /* update I term */
+  wrench_est_ptr_->update(target_pos, target_q, pos, q);
 
-  double sign_qe_w = qe_w > 0 ? 1 : -1;
-
-  double fx_w_i_term = pos_i_term_[0].update(pos.x() - target_pos.x());
-  double fy_w_i_term = pos_i_term_[1].update(pos.y() - target_pos.y());
-  double fz_w_i_term = pos_i_term_[2].update(pos.z() - target_pos.z());
-  double mx_cog_i_term = pos_i_term_[3].update(sign_qe_w * qe_x);
-  double my_cog_i_term = pos_i_term_[4].update(sign_qe_w * qe_y);
-  double mz_cog_i_term = pos_i_term_[5].update(sign_qe_w * qe_z);
-
-  dist_force_w_ = geometry_msgs::Vector3();
-  dist_force_w_.x = fx_w_i_term;
-  dist_force_w_.y = fy_w_i_term;
-  dist_force_w_.z = fz_w_i_term;
-  dist_torque_cog_ = geometry_msgs::Vector3();
-  dist_torque_cog_.x = mx_cog_i_term;
-  dist_torque_cog_.y = my_cog_i_term;
-  dist_torque_cog_.z = mz_cog_i_term;
+  /* get the disturbance wrench */
+  dist_force_w_ = wrench_est_ptr_->getDistForceW();
+  dist_torque_cog_ = wrench_est_ptr_->getDistTorqueCOG();
 }
 
 void nmpc::TiltQdServoNMPCwITerm::cfgNMPCCallback(NMPCConfig& config, uint32_t level)
