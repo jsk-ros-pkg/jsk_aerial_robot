7a4ca7a9d755fc272b4cc81f25d834bd992005aa
sugihara <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2024-10-24 15:09:17 +0900

    [delta][control] refactor controller

--- robots/delta/src/control/delta_flight_controller.cpp
+++ robots/delta/src/control/delta_flight_controller.cpp
@@ -9,18 +9,14 @@ void RollingController::calcAccFromCog()
   tf::quaternionKDLToTF(robot_model_->getCogDesireOrientation<KDL::Rotation>(), cog2baselink_rot);
   tf::Matrix3x3 cog_rot = estimator_->getOrientation(Frame::BASELINK, estimate_mode_) * tf::Matrix3x3(cog2baselink_rot).inverse();
 
-  tf::Vector3 target_acc_w(pid_controllers_.at(X).result(),
-                           pid_controllers_.at(Y).result(),
-                           pid_controllers_.at(Z).result());
-  tf::Vector3 target_acc_cog = cog_rot.inverse() * target_acc_w;
+  tf::Vector3 target_linear_acc_w(pid_controllers_.at(X).result(),
+                                  pid_controllers_.at(Y).result(),
+                                  pid_controllers_.at(Z).result());
+  tf::Vector3 target_linear_acc_cog = cog_rot.inverse() * target_linear_acc_w;
 
-  target_acc_cog_.at(0) = target_acc_cog.x();
-  target_acc_cog_.at(1) = target_acc_cog.y();
-  target_acc_cog_.at(2) = target_acc_cog.z();
+  Eigen::VectorXd target_acc_cog = Eigen::VectorXd::Zero(6);
 
-  Eigen::VectorXd target_wrench_acc_cog = Eigen::VectorXd::Zero(6);
-
-  target_wrench_acc_cog.head(3) = Eigen::Vector3d(target_acc_cog.x(), target_acc_cog.y(), target_acc_cog.z());
+  target_acc_cog.head(3) = Eigen::Vector3d(target_linear_acc_cog.x(), target_linear_acc_cog.y(), target_linear_acc_cog.z());
 
   double target_ang_acc_x = pid_controllers_.at(ROLL).result();
   double target_ang_acc_y = pid_controllers_.at(PITCH).result();
@@ -31,13 +27,13 @@ void RollingController::calcAccFromCog()
   Eigen::Matrix3d inertia = robot_model_->getInertia<Eigen::Matrix3d>();
   Eigen::Vector3d gyro = omega.cross(inertia * omega);
 
-  target_wrench_acc_cog.tail(3)
+  target_acc_cog.tail(3)
     = Eigen::Vector3d(target_ang_acc_x, target_ang_acc_y, target_ang_acc_z)
     + inertia.inverse() * gyro;
 
-  target_wrench_acc_cog_ = target_wrench_acc_cog;
+  setTargetAccCog(target_acc_cog);
 
-  if(navigator_->getForceLandingFlag() && target_acc_w.z() < 5.0) // heuristic measures to avoid to large gimbal angles after force land
+  if(navigator_->getForceLandingFlag() && target_linear_acc_w.z() < 5.0) // heuristic measures to avoid to large gimbal angles after force land
     start_rp_integration_ = false;
 }
 
@@ -46,26 +42,14 @@ void RollingController::calcFlightFullLambda()
   /* calculate integarated allocation */
   Eigen::MatrixXd full_q_mat = rolling_robot_model_->getFullWrenchAllocationMatrixFromControlFrame("cog");
 
-  /* change dimension from wrench to acc */
+  /* change dimension of convertion matrix from wrench to acc */
   full_q_mat.topRows(3) = 1.0 / robot_model_->getMass() * full_q_mat.topRows(3);
   full_q_mat.bottomRows(3) = rolling_robot_model_->getInertiaFromControlFrame<Eigen::Matrix3d>().inverse() * full_q_mat.bottomRows(3);
 
   Eigen::MatrixXd full_q_mat_inv = aerial_robot_model::pseudoinverse(full_q_mat);
 
-  if(use_sr_inv_)
-    {
-      // http://www.thothchildren.com/chapter/5bd8d78751d930518903af34
-      Eigen::MatrixXd sr_inv = full_q_mat.transpose() * (full_q_mat * full_q_mat.transpose() + sr_inv_weight_ * Eigen::MatrixXd::Identity(full_q_mat.rows(), full_q_mat.rows())).inverse();
-      full_q_mat_inv = sr_inv;
-      ROS_WARN_STREAM_ONCE("[control] use SR-Inverse. weight is " << sr_inv_weight_);
-    }
-  else
-    {
-      ROS_WARN_ONCE("[control] use MP-Inverse");
-    }
-
-  full_lambda_trans_ = full_q_mat_inv.leftCols(3) * target_wrench_acc_cog_.head(3);
-  full_lambda_rot_ = full_q_mat_inv.rightCols(3) * target_wrench_acc_cog_.tail(3);
+  full_lambda_trans_ = full_q_mat_inv.leftCols(3) * getTargetAccCog().head(3);
+  full_lambda_rot_ = full_q_mat_inv.rightCols(3) * getTargetAccCog().tail(3);
   full_lambda_all_ = full_lambda_trans_ + full_lambda_rot_;
 }
 
@@ -111,13 +95,13 @@ void nonlinearWrenchAllocationEqConstraints(unsigned m, double *result, unsigned
   std::vector<Eigen::Vector3d> rotors_origin_from_cog = robot_model_for_control->getRotorsOriginFromCog<Eigen::Vector3d>();
   std::vector<double> rotor_tilt = controller->getRotorTilt();
 
-  Eigen::VectorXd target_wrench_acc_cog = controller->getTargetWrenchAccCogForOpt();
+  Eigen::VectorXd target_acc_cog = controller->getTargetAccCog();
   double mass = robot_model_for_control->getMass();
   Eigen::Matrix3d inertia = robot_model_for_control->getInertia<Eigen::Matrix3d>();
   double m_f_rate = robot_model_for_control->getMFRate();
   std::map<int, int> rotor_direction = robot_model_for_control->getRotorDirection();
 
-  Eigen::VectorXd target_wrench_cog = target_wrench_acc_cog;
+  Eigen::VectorXd target_wrench_cog = target_acc_cog;
   target_wrench_cog.head(3) = mass * target_wrench_cog.head(3);
   target_wrench_cog.tail(3) = inertia * target_wrench_cog.tail(3);
 
