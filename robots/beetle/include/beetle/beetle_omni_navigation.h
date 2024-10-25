f66ec5d7748b37b5d60c631ba39572a9b6e95ed9
LI, Jinjie <li-jinjie@g.ecc.u-tokyo.ac.jp>
2024-10-25 13:03:40 +0900

    [beetle-omni][navigation]: delete unused code as much as possible

--- robots/beetle/include/beetle/beetle_omni_navigation.h
+++ robots/beetle/include/beetle/beetle_omni_navigation.h
@@ -8,53 +8,23 @@
 
 namespace aerial_robot_navigation
 {
-  /* target frmae*/
-  enum target_framea
-    {
-     COG,
-     BASE_LINK,
-     CONTACT_POINT,
-     CENTER_OF_MOVING
-    };
-  class BeetleOmniNavigator : public GimbalrotorNavigator
-  {
-  public:
-    BeetleOmniNavigator();
-    ~BeetleOmniNavigator(){}
 
-    void initialize(ros::NodeHandle nh, ros::NodeHandle nhp,
-                    boost::shared_ptr<aerial_robot_model::RobotModel> robot_model,
-                    boost::shared_ptr<aerial_robot_estimation::StateEstimator> estimator) override;
+class BeetleOmniNavigator : public GimbalrotorNavigator
+{
+public:
+  BeetleOmniNavigator() : GimbalrotorNavigator() {};
+  ~BeetleOmniNavigator() override = default;
 
-    void update() override;
+  void initialize(ros::NodeHandle nh, ros::NodeHandle nhp,
+                  boost::shared_ptr<aerial_robot_model::RobotModel> robot_model,
+                  boost::shared_ptr<aerial_robot_estimation::StateEstimator> estimator) override;
 
-    inline void setTargetPosCandX( float value){  target_pos_candidate_.setX(value);}
-    inline void setTargetPosCandY( float value){  target_pos_candidate_.setY(value);}
-    inline void setTargetPosCandZ( float value){  target_pos_candidate_.setZ(value);}
-    inline void setTargetPosCand( tf::Vector3 value){  target_pos_candidate_ = value ;}
-    inline tf::Vector3 getTargetPosCand() {return target_pos_candidate_;}
+  void update() override;
 
-  protected:
-    void rosParamInit() override;
+private:
+  ros::NodeHandle nh_;
+  ros::NodeHandle nhp_;
 
-  private:
-    tf::Vector3 target_pos_candidate_;
-    tf::Vector3 pre_target_pos_;
-    ros::NodeHandle nh_;
-    ros::NodeHandle nhp_;
-
-    double max_target_roll_pitch_rate_;
-
-    bool roll_pitch_control_flag_;
-    bool pre_assembled_ ; 
-
-    void naviCallback(const aerial_robot_msgs::FlightNavConstPtr & msg) override;
-    void joyStickControl(const sensor_msgs::JoyConstPtr & joy_msg) override;
-    void rotateContactPointFrame();
-    tf2_ros::TransformBroadcaster br_;
-    boost::shared_ptr<BeetleRobotModel> beetle_robot_model_;
-    map<string, ros::Subscriber> assembly_flag_subs_;
-    void assemblyFlagCallback(const diagnostic_msgs::KeyValue & msg);
-    void convertTargetPosFromCoG2CoM();
-  };
+  boost::shared_ptr<BeetleRobotModel> beetle_robot_model_;
 };
+};  // namespace aerial_robot_navigation
