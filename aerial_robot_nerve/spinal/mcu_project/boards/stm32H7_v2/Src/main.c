94302b88bab8d449736d04e4b54c075af3784291
Junichiro Sugihara <j-sugihara@jsk.imi.i.u-tokyo.ac.jp>
2024-10-31 23:02:55 +0900

    [Spinal][ACD] Rvert adc channel from 18 to 15 to revive previous voltage sensing way.

--- aerial_robot_nerve/spinal/mcu_project/boards/stm32H7_v2/Src/main.c
+++ aerial_robot_nerve/spinal/mcu_project/boards/stm32H7_v2/Src/main.c
@@ -508,7 +508,7 @@ static void MX_ADC1_Init(void)
   }
   /** Configure Regular Channel
   */
-  sConfig.Channel = ADC_CHANNEL_18;
+  sConfig.Channel = ADC_CHANNEL_15;
   sConfig.Rank = ADC_REGULAR_RANK_1;
   sConfig.SamplingTime = ADC_SAMPLETIME_2CYCLES_5;
   sConfig.SingleDiff = ADC_SINGLE_ENDED;
