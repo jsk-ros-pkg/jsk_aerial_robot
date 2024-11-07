dd0aff3ea225e57b5776b5d5d4a3464fb2452b48
Junichiro Sugihara <j-sugihara@jsk.imi.i.u-tokyo.ac.jp>
2024-11-07 18:29:32 +0900

    [Ninja][teleop]Change the keyboard teleop interface like common keyboard teleop.

--- robots/ninja/script/keyboard_command.py
+++ robots/ninja/script/keyboard_command.py
@@ -1,13 +1,39 @@
 #!/usr/bin/env python
+from __future__ import print_function # for print function in pytho2
 import rospy
 
 from std_msgs.msg import Empty
 from std_msgs.msg import Int8
 from std_msgs.msg import UInt16
 from std_msgs.msg import UInt8
+from aerial_robot_msgs.msg import FlightNav
 import rosgraph
 
 import sys, select, termios, tty
+
+msg = """
+Instruction:
+
+---------------------------
+
+r:  arming motor (please do before takeoff)
+t:  takeoff
+l:  land
+f:  force landing
+h:  halt (force stop motor)
+
+     q           w           e           [
+(turn left)  (forward)  (turn right)  (move up)
+
+     a           s           d           ]
+(move left)  (backward) (move right) (move down)
+
+
+Please don't have caps lock on.
+CTRL+c to quit
+---------------------------
+"""
+
 
 def getKey():
         tty.setraw(sys.stdin.fileno())
@@ -16,9 +42,14 @@ def getKey():
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
         return key
 
+def printMsg(msg, msg_len = 50):
+        print(msg.ljust(msg_len) + "\r", end="")
+
+
 if __name__=="__main__":
         settings = termios.tcgetattr(sys.stdin)
         rospy.init_node("keyboard_command")
+        print(msg)
 
         master = rosgraph.Master('/rostopic')
         try:
@@ -35,8 +66,13 @@ if __name__=="__main__":
         start_pubs = []
         takeoff_pubs = []
         force_landing_pubs = []
-        ctrl_mode_pubs = []
         motion_start_pubs =[]
+        nav_pubs = []
+
+        xy_vel   = rospy.get_param("xy_vel", 0.2)
+        z_vel    = rospy.get_param("z_vel", 0.2)
+        yaw_vel  = rospy.get_param("yaw_vel", 0.2)
+
         for name in robot_names:
                 ns = name + "/teleop_command"
                 land_pubs.append(rospy.Publisher(ns + '/land', Empty, queue_size=1))
@@ -44,48 +80,96 @@ if __name__=="__main__":
                 start_pubs.append(rospy.Publisher(ns + '/start', Empty, queue_size=1))
                 takeoff_pubs.append(rospy.Publisher(ns + '/takeoff', Empty, queue_size=1))
                 force_landing_pubs.append(rospy.Publisher(ns + '/force_landing', Empty, queue_size=1))
-                ctrl_mode_pubs.append(rospy.Publisher(ns + '/ctrl_mode', Int8, queue_size=1))
                 motion_start_pubs.append(rospy.Publisher('task_start', Empty, queue_size=1))
+                nav_pubs.append(rospy.Publisher(name + '/uav/nav', FlightNav, queue_size=1))
 
-        #the way to write publisher in python
-        comm=Int8()
-        gain=UInt16()
         try:
                 while(True):
+                        nav_msg = FlightNav()
+                        nav_msg.control_frame = FlightNav.WORLD_FRAME
+                        nav_msg.target = FlightNav.COG
                         key = getKey()
-                        print("the key value is {}".format(ord(key)))
-                        # takeoff and landing
+                        msg = ""
+
                         if key == 'l':
                                 for land_pub in land_pubs:
                                         land_pub.publish(Empty())
                                         #for hydra joints
+                                msg = "send land command"
                         if key == 'r':
                                 for start_pub in start_pubs:
                                         start_pub.publish(Empty())
                                         #for hydra joints
+                                msg = "send motor-arming command"
                         if key == 'h':
                                 for halt_pub in halt_pubs:
                                         halt_pub.publish(Empty())
                                         #for hydra joints
+                                msg = "send motor-disarming (halt) command"
                         if key == 'f':
                                 for force_landing_pub in force_landing_pubs:
                                         force_landing_pub.publish(Empty())
+                                msg = "send force landing command"
                         if key == 't':
                                 for takeoff_pub in takeoff_pubs:
                                         takeoff_pub.publish(Empty())
+                                msg = "send takeoff command"
                         if key == 'x':
                                 for motion_start_pub in motion_start_pubs:
                                         motion_start_pub.publish()
-                        if key == 'v':
-                                comm.data = 1
-                                for ctrl_mode_pub in ctrl_mode_pubs:
-                                        ctrl_mode_pub.publish(comm)
-                        if key == 'p':
-                                comm.data = 0
-                                for ctrl_mode_pub in ctrl_mode_pubs:
-                                        ctrl_mode_pub.publish(comm)
+                                msg = "send task-start command"
+                        if key == 'w':
+                                nav_msg.pos_xy_nav_mode = FlightNav.VEL_MODE
+                                nav_msg.target_vel_x = xy_vel
+                                for nav_pub in nav_pubs:
+                                        nav_pub.publish(nav_msg)
+                                msg = "send +x vel command"
+                        if key == 's':
+                                nav_msg.pos_xy_nav_mode = FlightNav.VEL_MODE
+                                nav_msg.target_vel_x = -xy_vel
+                                for nav_pub in nav_pubs:
+                                        nav_pub.publish(nav_msg)
+                                msg = "send -x vel command"
+                        if key == 'a':
+                                nav_msg.pos_xy_nav_mode = FlightNav.VEL_MODE
+                                nav_msg.target_vel_y = xy_vel
+                                for nav_pub in nav_pubs:
+                                        nav_pub.publish(nav_msg)
+                                msg = "send +y vel command"
+                        if key == 'd':
+                                nav_msg.pos_xy_nav_mode = FlightNav.VEL_MODE
+                                nav_msg.target_vel_y = -xy_vel
+                                for nav_pub in nav_pubs:
+                                        nav_pub.publish(nav_msg)
+                                msg = "send -y vel command"
+                        if key == 'q':
+                                nav_msg.yaw_nav_mode = FlightNav.VEL_MODE
+                                nav_msg.target_omega_z = yaw_vel
+                                for nav_pub in nav_pubs:
+                                        nav_pub.publish(nav_msg)
+                                msg = "send +yaw vel command"
+                        if key == 'e':
+                                nav_msg.yaw_nav_mode = FlightNav.VEL_MODE
+                                nav_msg.target_omega_z = -yaw_vel
+                                for nav_pub in nav_pubs:
+                                        nav_pub.publish(nav_msg)                                
+                                msg = "send -yaw vel command"
+
+                        if key == '[':
+                                nav_msg.pos_z_nav_mode = FlightNav.VEL_MODE
+                                nav_msg.target_vel_z = z_vel
+                                for nav_pub in nav_pubs:
+                                        nav_pub.publish(nav_msg)
+                                msg = "send +z vel command"
+                        if key == ']':
+                                nav_msg.pos_z_nav_mode = FlightNav.VEL_MODE
+                                nav_msg.target_vel_z = -z_vel
+                                for nav_pub in nav_pubs:
+                                        nav_pub.publish(nav_msg)
+                                msg = "send -z vel command"
                         if key == '\x03':
                                 break
+                        printMsg(msg)
                         rospy.sleep(0.001)
 
         except Exception as e:
