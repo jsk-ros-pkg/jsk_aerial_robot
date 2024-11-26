6d3bfb51693464431707662a7a68e170878dd67d
HarukiKozukapenguin <kozukozupenguin@g.ecc.u-tokyo.ac.jp>
2024-11-26 17:50:58 +0900

    [agile_multirotor][debug] not to set all zero when clash to obstacle

--- robots/agile_multirotor/src/raw_lidar_to_observation_cal.cpp
+++ robots/agile_multirotor/src/raw_lidar_to_observation_cal.cpp
@@ -322,7 +322,7 @@ Scalar ObstacleCalculator::getClosestDistance(
     if (a == 0) continue;
     Scalar b = alpha.dot(beta);
     Scalar c = std::pow(beta.norm(), 2) - std::pow(radius, 2);
-    if (c <= 0) rmin = 0;
+    if (c <= 0) c = 0;
     Scalar D = std::pow(b, 2) - a * c;
     if (0 <= D) {
       Scalar dist = (b - std::sqrt(D)) / a;
