5cf72e971ccef768c0fdf1c669957f34e1f3ebd4
Moju Zhao <chou@jsk.imi.i.u-tokyo.ac.jp>
2024-12-01 01:25:17 +0900

    [Spinal][Neuron][Reboot] recall config reqeust to the reboot neuron to reconect between spinal and neuron

--- aerial_robot_nerve/spinal/mcu_project/lib/Hydrus_Lib/CANDevice/initializer/can_initializer.h
+++ aerial_robot_nerve/spinal/mcu_project/lib/Hydrus_Lib/CANDevice/initializer/can_initializer.h
@@ -18,9 +18,13 @@ class CANInitializer : public CANDevice
 {
 private:
 	std::vector<Neuron>& neuron_;
+	uint8_t reboot_id_;
+	uint32_t reboot_time_;
+
 public:
-	CANInitializer(std::vector<Neuron>& neuron):CANDevice(CAN::DEVICEID_INITIALIZER, CAN::MASTER_ID), neuron_(neuron){}
+	CANInitializer(std::vector<Neuron>& neuron):CANDevice(CAN::DEVICEID_INITIALIZER, CAN::MASTER_ID), neuron_(neuron), reboot_id_(0), reboot_time_(0){}
 	void initDevices();
+	void requestConfig(uint8_t slave_id);
 	void configDevice(const spinal::SetBoardConfig::Request& req);
 	void sendData() override;
 	void receiveDataCallback(uint8_t slave_id, uint8_t message_id, uint32_t DLC, uint8_t* data) override;
