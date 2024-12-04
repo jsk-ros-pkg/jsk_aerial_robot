3f175dc484b3cd2309b824345ab403f5dae571f1
HarukiKozukapenguin <kozukozupenguin@g.ecc.u-tokyo.ac.jp>
2024-12-04 21:05:17 +0900

    [agile_multirotor] add stepladder obstacle

--- robots/agile_multirotor/scripts/obstacle_world.py
+++ robots/agile_multirotor/scripts/obstacle_world.py
@@ -69,8 +69,9 @@ class ObstacleWorld:
         except pd.errors.EmptyDataError as e:
             print("tree data is empty")
             # print("self.obs: ",self.obs)
-        self.spawnWall("wall_body", 0.5, np.array([3.2,0.2,1]) + np.array([shift_x,shift_y,0]), 0.01, 0.6, 2)
+        self.spawnWall("wall_body", 0.5, np.array([3.2,0.1,1]) + np.array([shift_x,shift_y,0]), 0.01, 0.6, 2)
         self.spawnWall("wall_pole", 0.5, np.array([3.2,-0.90,1]) + np.array([shift_x,shift_y,0]), 0.01, 0.1, 2)
+        self.spawnWall("stepladder", 0.5, np.array([1.0,1.3,0.92]) + np.array([shift_x,shift_y,0]), 0.842, 0.92, 1.84)
         # self.spawnWall("right_wall", 0.5, np.array([40,wall_y_position,2]) + np.array([shift_x,shift_y,0]), 90, 0.01, 4)
         # self.spawnWall("left_wall", 0.5, np.array([40,-wall_y_position,2]) + np.array([shift_x,shift_y,0]), 90, 0.01, 4)
         # self.spawnWall("back_wall", 0.5, np.array([-0.7,-0.4,2]) + np.array([shift_x,shift_y,0]), 0.01, 1.5, 4)
