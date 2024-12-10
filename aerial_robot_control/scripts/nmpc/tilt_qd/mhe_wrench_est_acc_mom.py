3b080fd393a283cc88bdfeabce314f46ad411724
LI, Jinjie <li-jinjie@g.ecc.u-tokyo.ac.jp>
2024-12-10 19:46:15 +0900

    [MHE] refactor the name of config file

--- aerial_robot_control/scripts/nmpc/tilt_qd/mhe_wrench_est_acc_mom.py
+++ aerial_robot_control/scripts/nmpc/tilt_qd/mhe_wrench_est_acc_mom.py
@@ -16,7 +16,7 @@ from phys_param_beetle_omni import *
 # read parameters from yaml
 rospack = rospkg.RosPack()
 
-mhe_param_path = os.path.join(rospack.get_path("beetle"), "config", "WrenchEstIMUMHE.yaml")
+mhe_param_path = os.path.join(rospack.get_path("beetle"), "config", "WrenchEstMHEAccMom.yaml")
 with open(mhe_param_path, "r") as f:
     mhe_param_dict = yaml.load(f, Loader=yaml.FullLoader)
 mhe_params = mhe_param_dict["controller"]["mhe"]
--- aerial_robot_control/scripts/nmpc/tilt_qd/mhe_wrench_est_momentum.py
+++ aerial_robot_control/scripts/nmpc/tilt_qd/mhe_wrench_est_momentum.py
@@ -16,7 +16,7 @@ from phys_param_beetle_omni import *
 # read parameters from yaml
 rospack = rospkg.RosPack()
 
-mhe_param_path = os.path.join(rospack.get_path("beetle"), "config", "WrenchEstVelMHE.yaml")
+mhe_param_path = os.path.join(rospack.get_path("beetle"), "config", "WrenchEstMHEMomentum.yaml")
 with open(mhe_param_path, "r") as f:
     mhe_param_dict = yaml.load(f, Loader=yaml.FullLoader)
 mhe_params = mhe_param_dict["controller"]["mhe"]
