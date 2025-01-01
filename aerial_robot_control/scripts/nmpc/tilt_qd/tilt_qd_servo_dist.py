717f7ba654988bfaa2b9c26f28efb147ff1a48e3
Jinjie Li <45286479+Li-Jinjie@users.noreply.github.com>
2025-01-01 22:00:44 +0900

    [NMPC][Py]: separate the ITerm wrench with external wrench for other NMPC methods considering disturbance.

--- aerial_robot_control/scripts/nmpc/tilt_qd/tilt_qd_servo_dist.py
+++ aerial_robot_control/scripts/nmpc/tilt_qd/tilt_qd_servo_dist.py
@@ -76,7 +76,12 @@ class NMPCTiltQdServoDist(NMPCBase):
         qxr = ca.SX.sym("qxr")
         qyr = ca.SX.sym("qyr")
         qzr = ca.SX.sym("qzr")
-        parameters = ca.vertcat(qwr, qxr, qyr, qzr)
+        qr = ca.vertcat(qwr, qxr, qyr, qzr)
+
+        f_d_i_para = ca.SX.sym("fd_para", 3)  # world frame
+        tau_d_b_para = ca.SX.sym("tau_d_para", 3)  # body frame
+
+        parameters = ca.vertcat(qr, f_d_i_para, tau_d_b_para)
 
         # control inputs
         ft1 = ca.SX.sym("ft1")
@@ -164,19 +169,19 @@ class NMPCTiltQdServoDist(NMPCBase):
         # dynamic model
         ds = ca.vertcat(
             v,
-            ca.mtimes(rot_ib, f_u_b) / mass + g_i + f_d_i / mass,
+            (ca.mtimes(rot_ib, f_u_b) + f_d_i + f_d_i_para) / mass + g_i,
             (-wx * qx - wy * qy - wz * qz) / 2,
             (wx * qw + wz * qy - wy * qz) / 2,
             (wy * qw - wz * qx + wx * qz) / 2,
             (wz * qw + wy * qx - wx * qy) / 2,
-            ca.mtimes(inv_iv, (-ca.cross(w, ca.mtimes(iv, w)) + tau_u_b + tau_d_b)),
+            ca.mtimes(inv_iv, (-ca.cross(w, ca.mtimes(iv, w)) + tau_u_b + tau_d_b + tau_d_b_para)),
             (ac - a) / t_servo,
             ca.vertcat(0.0, 0.0, 0.0),
             ca.vertcat(0.0, 0.0, 0.0),
         )
 
         # function
-        func = ca.Function("func", [states, controls], [ds], ["state", "control_input"], ["ds"])
+        func = ca.Function("func", [states, controls], [ds], ["state", "control_input"], ["ds"], {"allow_free": True})
 
         # NONLINEAR_LS = error^T @ Q @ error; error = y - y_ref
         qe_x = qwr * qx - qw * qxr + qyr * qz - qy * qzr
