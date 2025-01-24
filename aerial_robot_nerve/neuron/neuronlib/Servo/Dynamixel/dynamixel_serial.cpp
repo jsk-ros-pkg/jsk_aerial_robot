7be81c8279ecc78bb234cf0e949b5886de975f64
趙　漠居(Zhao, Moju) <chou@jsk.imi.i.u-tokyo.ac.jp>
2025-01-24 12:53:46 +0900

    [Servo][Spinal][Neuron] refactor the rule to handle pulley skip with external encoder (#666)
    * [Servo][Spinal][Neuron] solve the target position sync problem  between spinal and neuron while switch the servo torque on/off. Especially avoid the chattering just after servo on by using the raw position value of servo internal encoder.
    
    * [Spinal][Neuron][Servo] refactor the special process that send goal position from neuron to spinal
    
    * [Neuron][Servo][External Encoder] refactor the behavior of pulley skip. No servo off process. Reset the internal offset and the target angle
    
    * [Neuron][Servo][External Encoder] refactor the rule to skip some error bit to avoid joint servo off (e.g., the pulley skip error and the overload error)
    
    ---------
    
    Co-authored-by: Moju Zhao <tongtybj@gmail.com>

--- aerial_robot_nerve/neuron/neuronlib/Servo/Dynamixel/dynamixel_serial.cpp
+++ aerial_robot_nerve/neuron/neuronlib/Servo/Dynamixel/dynamixel_serial.cpp
@@ -302,13 +302,18 @@ void DynamixelSerial::update()
 
     // check the latest error status with original rule
     for (unsigned int i = 0; i < servo_num_; i++) {
-      if (servo_[i].hardware_error_status_ != 0) {
 
-        // ingnore overload error for current based position control
-        if (servo_[i].hardware_error_status_ == (1 << OVERLOAD_ERROR)) {
+      servo_[i].force_servo_off_ = false;
+
+      uint8_t error_status = servo_[i].hardware_error_status_;
+      error_status &= ~(1 << PULLEY_SKIP_ERROR);  // ignore pulley skip error
+
+      if (error_status != 0) {
+
+        // ingnore overload error for current based position control (only with this error bit)
+        if (error_status == (1 << OVERLOAD_ERROR)) {
           if (servo_[i].operating_mode_ == CURRENT_BASE_POSITION_CONTROL_MODE) {
             if (servo_[i].goal_current_ < servo_[i].current_limit_) {
-              servo_[i].force_servo_off_ = false;
               continue;
             }
           }
@@ -316,9 +321,6 @@ void DynamixelSerial::update()
 
         setTorque(i, false); // servo off
         servo_[i].force_servo_off_ = true;
-      }
-      else {
-        servo_[i].force_servo_off_ = false;
       }
     }
   }
@@ -685,8 +687,24 @@ int8_t DynamixelSerial::readStatusPacket(uint8_t status_packet_instruction)
                         // check pulley skip
                         int32_t diff = internal_offset - s->internal_offset_; // this should be proportional to joint load.
                         // TODO: use diff to estimate the joint torque
+
+
                         if (abs(diff) > pulley_skip_thresh_) {
+                          if (!(s->hardware_error_status_ & (1 << PULLEY_SKIP_ERROR))) {
+                            s->pulley_skip_time_ = HAL_GetTick();
+                          }
+
                           s->hardware_error_status_ |= 1 << PULLEY_SKIP_ERROR;
+                        }
+
+                        if (s->hardware_error_status_ & (1 << PULLEY_SKIP_ERROR)) {
+                          if (HAL_GetTick() - s->pulley_skip_time_ > s->pulley_skip_reset_du_) {
+                            // reset the internal_offset, goal_position, and error flag
+                            s->internal_offset_ = internal_offset;
+                            s->goal_position_ = s->present_position_;
+                            s->send_goal_position_ = true; // send this goal position to spinal
+                            s->hardware_error_status_ &= ~(1 << PULLEY_SKIP_ERROR);
+                          }
                         }
                       }
                     }
