58a367dda9a9cc47e14f5efc52d52fc6128e1fff
Junichiro Sugihara <j-sugihara@jsk.imi.i.u-tokyo.ac.jp>
2025-02-10 14:27:58 +0900

    [Ninja][exp][temp] Modified tmux shell script to connect to correct machine.

--- robots/ninja/shell/four_module_connec.sh
+++ robots/ninja/shell/four_module_connec.sh
@@ -3,8 +3,8 @@ session_name="experiment"
 main_w="main"
 robot1="ninja1"
 robot2="ninja2"
-robot3="ninja3"
-robot4="ninja4"
+robot3="ninja4"
+# robot4="ninja4"
 
 tmux new-session -d -s ${session_name} -n ${main_w}
 
@@ -16,12 +16,12 @@ tmux split-window -v -t ${session_name}
 tmux send-keys -t ${session_name}:${main_w}.0 'ssh ' ${robot1} C-m
 tmux send-keys -t ${session_name}:${main_w}.1 'ssh ' ${robot2} C-m
 tmux send-keys -t ${session_name}:${main_w}.2 'ssh ' ${robot3} C-m
-tmux send-keys -t ${session_name}:${main_w}.3 'ssh ' ${robot4} C-m
+# tmux send-keys -t ${session_name}:${main_w}.3 'ssh ' ${robot4} C-m
 
 tmux send-keys -t ${session_name}:${main_w}.0 'roscd ninja' C-m
 tmux send-keys -t ${session_name}:${main_w}.1 'roscd ninja' C-m
 tmux send-keys -t ${session_name}:${main_w}.2 'roscd ninja' C-m
-tmux send-keys -t ${session_name}:${main_w}.3 'roscd ninja' C-m
+# tmux send-keys -t ${session_name}:${main_w}.3 'roscd ninja' C-m
 
 
 # tmux send-keys -t ${session_name}:${main_w}.1 'roslaunch mecanum_control bringup_remote.launch robot_name:=' ${robot1} C-m
