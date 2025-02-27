800d82ed6b2d3eb958a0f2db72e0cd2a9d49a581
Jinjie Li <45286479+Li-Jinjie@users.noreply.github.com>
2025-02-27 18:00:16 +0900

    [Planning][Drawing] initial version for traj.

---
+++ aerial_robot_planning/scripts/iros25_draw_mapping_mode.py
@@ -0,0 +1,207 @@
+import pandas as pd
+import numpy as np
+import scienceplots
+import matplotlib.pyplot as plt
+import argparse
+
+legend_alpha = 0.5
+
+
+def calculate_rmse(t, x, t_ref, x_ref, is_yaw=False):
+    x_ref_interp = np.interp(t, t_ref, x_ref)
+    if is_yaw:
+        # calculate the RMSE for yaw
+        error = np.minimum(np.abs(x - x_ref_interp), 2 * np.pi - np.abs(x - x_ref_interp))
+    else:
+        error = x - x_ref_interp
+
+    rmse_x = np.sqrt(np.mean(error ** 2))
+    return rmse_x
+
+def quat2euler(qw, qx, qy, qz):
+    roll = np.arctan2(2 * (qw * qx + qy * qz), 1 - 2 * (qx ** 2 + qy ** 2))
+    pitch = np.arcsin(2 * (qw * qy - qz * qx))
+    yaw = np.arctan2(2 * (qw * qz + qx * qy), 1 - 2 * (qy ** 2 + qz ** 2))
+    return roll, pitch, yaw
+
+def main(file_path):
+    # Load the data from csv file
+    data = pd.read_csv(file_path)
+
+    # ======= xyz =========
+    data_xyz = data[
+        ['__time', '/beetle1/uav/cog/odom/pose/pose/position/x', '/beetle1/uav/cog/odom/pose/pose/position/y',
+         '/beetle1/uav/cog/odom/pose/pose/position/z']]
+
+    data_xyz_ref = data[
+        ['__time', '/hand/mocap/pose/pose/position/x', '/hand/mocap/pose/pose/position/y',
+         '/hand/mocap/pose/pose/position/z']]
+
+
+    data_xyz = data_xyz.dropna()
+    data_xyz_ref = data_xyz_ref.dropna()
+
+    # ======= rpy =========
+    data_qwxyz = data[
+        ['__time', '/beetle1/uav/cog/odom/pose/pose/orientation/w', '/beetle1/uav/cog/odom/pose/pose/orientation/x',
+         '/beetle1/uav/cog/odom/pose/pose/orientation/y', '/beetle1/uav/cog/odom/pose/pose/orientation/z']]
+
+    data_qwxyz_ref = data[
+        ['__time', '/hand/mocap/pose/pose/orientation/w', '/hand/mocap/pose/pose/orientation/x',
+         '/hand/mocap/pose/pose/orientation/y', '/hand/mocap/pose/pose/orientation/z']]
+
+    data_qwxyz_ref = data_qwxyz_ref.dropna()
+    data_qwxyz = data_qwxyz.dropna()
+
+    # convert to euler
+    data_euler_ref = pd.DataFrame()
+    data_euler_ref['__time'] = data_qwxyz_ref['__time']
+    data_euler_ref = pd.DataFrame()
+    data_euler_ref['__time'] = data_qwxyz_ref['__time']
+    data_euler_ref['roll'], data_euler_ref['pitch'], data_euler_ref['yaw'] = quat2euler(
+        data_qwxyz_ref['/hand/mocap/pose/pose/orientation/w'], data_qwxyz_ref['/hand/mocap/pose/pose/orientation/x'],
+        data_qwxyz_ref['/hand/mocap/pose/pose/orientation/y'], data_qwxyz_ref['/hand/mocap/pose/pose/orientation/z'])
+
+    data_euler = pd.DataFrame()
+    data_euler['__time'] = data_qwxyz['__time']
+    data_euler['roll'], data_euler['pitch'], data_euler['yaw'] = quat2euler(
+        data_qwxyz['/beetle1/uav/cog/odom/pose/pose/orientation/w'], data_qwxyz['/beetle1/uav/cog/odom/pose/pose/orientation/x'],
+        data_qwxyz['/beetle1/uav/cog/odom/pose/pose/orientation/y'], data_qwxyz['/beetle1/uav/cog/odom/pose/pose/orientation/z'])
+
+
+    # ======= plotting =========
+    plt.style.use(["science", "grid"])
+
+    plt.rcParams.update({'font.size': 11})  # default is 10
+    label_size = 14
+
+    fig = plt.figure(figsize=(7, 7))
+
+    t_bias = max(data_xyz['__time'].iloc[0], data_xyz_ref['__time'].iloc[0])
+    color_ref = '#0072BD' # '#0C5DA5'
+    color_real = '#D95319' # '#FF2C00'
+
+    # --------------------------------
+    plt.subplot(6, 1, 1)
+    t_ref = np.array(data_xyz_ref['__time']) - t_bias
+    x_ref = np.array(data_xyz_ref['/hand/mocap/pose/pose/position/x'])
+    plt.plot(t_ref, x_ref, label='hand', linestyle="--", color=color_ref)
+
+    t = np.array(data_xyz['__time']) - t_bias
+    x = np.array(data_xyz['/beetle1/uav/cog/odom/pose/pose/position/x'])
+    plt.plot(t, x, label='robot', color=color_real)
+
+    plt.legend(framealpha=legend_alpha)
+    plt.ylabel('X (m)', fontsize=label_size)
+
+    # calculate RMSE
+    rmse_x = calculate_rmse(t, x, t_ref, x_ref)
+    print(f'RMSE X (m): {rmse_x}')
+
+    # --------------------------------
+    plt.subplot(6, 1, 4)
+    t_ref = np.array(data_euler_ref['__time']) - t_bias
+    roll_ref = np.array(data_euler_ref['roll'])
+    plt.plot(t_ref, roll_ref * 180 / np.pi, label='ref', linestyle="--", color=color_ref)
+
+    t = np.array(data_euler['__time']) - t_bias
+    roll = np.array(data_euler['roll'])
+    plt.plot(t, roll * 180 / np.pi, label='real', color=color_real)
+
+    plt.ylabel('Roll ($^\\circ$)', fontsize=label_size)
+
+    # calculate RMSE
+    rmse_roll = calculate_rmse(t, roll, t_ref, roll_ref)
+    print(f'RMSE Roll (rad): {rmse_roll}')
+    print(f'RMSE Roll (deg): {rmse_roll * 180 / np.pi}')
+
+    # --------------------------------
+    plt.subplot(6, 1, 2)
+    t_ref = np.array(data_xyz_ref['__time']) - t_bias
+    y_ref = np.array(data_xyz_ref['/hand/mocap/pose/pose/position/y'])
+    plt.plot(t_ref, y_ref, label='ref', linestyle="--", color=color_ref)
+
+    t = np.array(data_xyz['__time']) - t_bias
+    y = np.array(data_xyz['/beetle1/uav/cog/odom/pose/pose/position/y'])
+    plt.plot(t, y, label='Y', color=color_real)
+    plt.ylabel('Y (m)', fontsize=label_size)
+
+    # calculate RMSE
+    rmse_y = calculate_rmse(t, y, t_ref, y_ref)
+    print(f'RMSE Y (m): {rmse_y}')
+
+    # --------------------------------
+    plt.subplot(6, 1, 5)
+    t_ref = np.array(data_euler_ref['__time']) - t_bias
+    pitch_ref = np.array(data_euler_ref['pitch'])
+    plt.plot(t_ref, pitch_ref * 180 / np.pi, label='ref', linestyle="--", color=color_ref)
+
+    t = np.array(data_euler['__time']) - t_bias
+    pitch = np.array(data_euler['pitch'])
+    plt.plot(t, pitch * 180 / np.pi, label='real', color=color_real)
+    plt.ylabel('Pitch ($^\\circ$)', fontsize=label_size)
+
+    # calculate RMSE
+    rmse_pitch = calculate_rmse(t, pitch, t_ref, pitch_ref)
+    print(f'RMSE Pitch (rad): {rmse_pitch}')
+    print(f'RMSE Pitch (deg): {rmse_pitch * 180 / np.pi}')
+
+    # --------------------------------
+    plt.subplot(6, 1, 3)
+    t_ref = np.array(data_xyz_ref['__time']) - t_bias
+    z_ref = np.array(data_xyz_ref['/hand/mocap/pose/pose/position/z'])
+    plt.plot(t_ref, z_ref, label='ref', linestyle="--", color=color_ref)
+
+    t = np.array(data_xyz['__time']) - t_bias
+    z = np.array(data_xyz['/beetle1/uav/cog/odom/pose/pose/position/z'])
+
+    plt.plot(t, z, label='Z', color=color_real)
+    plt.ylabel('Z (m)', fontsize=label_size)
+
+    # calculate RMSE
+    rmse_z = calculate_rmse(t, z, t_ref, z_ref)
+    print(f'RMSE Z (m): {rmse_z}')
+
+    # --------------------------------
+    plt.subplot(6, 1, 6)
+    t_ref = np.array(data_euler_ref['__time']) - t_bias
+    yaw_ref = np.array(data_euler_ref['yaw'])
+    # # if yaw_ref has a jump, we need to fix it
+    # for i in range(1, len(yaw_ref)):
+    #     if yaw_ref[i] - yaw_ref[i - 1] > np.pi:
+    #         yaw_ref[i:] -= 2 * np.pi
+    #     elif yaw_ref[i] - yaw_ref[i - 1] < -np.pi:
+    #         yaw_ref[i:] += 2 * np.pi
+    plt.plot(t_ref, yaw_ref * 180 / np.pi, label='ref', linestyle="--", color=color_ref)
+
+    t = np.array(data_euler['__time']) - t_bias
+    yaw = np.array(data_euler['yaw'])
+    # # if yaw has a jump, we need to fix it
+    # for i in range(1, len(yaw)):
+    #     if yaw[i] - yaw[i - 1] > np.pi:
+    #         yaw[i:] -= 2 * np.pi
+    #     elif yaw[i] - yaw[i - 1] < -np.pi:
+    #         yaw[i:] += 2 * np.pi
+    plt.plot(t, yaw * 180 / np.pi, label='real', color=color_real)
+    plt.ylabel('Yaw ($^\\circ$)', fontsize=label_size)
+
+    # calculate RMSE
+    rmse_yaw = calculate_rmse(t, yaw, t_ref, yaw_ref, is_yaw=True)
+    print(f'RMSE Yaw (rad): {rmse_yaw}')
+    print(f'RMSE Yaw (deg): {rmse_yaw * 180 / np.pi}')
+
+    # --------------------------------
+    plt.tight_layout()
+    # make the subplots very compact
+    fig.subplots_adjust(hspace=0.2)
+    plt.show()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Plot the trajectory. Please use plotjuggler to generate the csv file.')
+    parser.add_argument('file_path', type=str, help='The file name of the trajectory')
+
+    args = parser.parse_args()
+
+    main(args.file_path)
