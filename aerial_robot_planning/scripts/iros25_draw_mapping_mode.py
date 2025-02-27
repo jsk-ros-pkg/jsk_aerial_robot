f6c809124b4e9d1394f68f765392a2a841ca62cf
Jinjie Li <45286479+Li-Jinjie@users.noreply.github.com>
2025-02-27 18:52:17 +0900

    [Planning][Drawing] change the range of x axis; add a yellow area to denote valve rotating

--- aerial_robot_planning/scripts/iros25_draw_mapping_mode.py
+++ aerial_robot_planning/scripts/iros25_draw_mapping_mode.py
@@ -18,11 +18,13 @@ def calculate_rmse(t, x, t_ref, x_ref, is_yaw=False):
     rmse_x = np.sqrt(np.mean(error ** 2))
     return rmse_x
 
+
 def quat2euler(qw, qx, qy, qz):
     roll = np.arctan2(2 * (qw * qx + qy * qz), 1 - 2 * (qx ** 2 + qy ** 2))
     pitch = np.arcsin(2 * (qw * qy - qz * qx))
     yaw = np.arctan2(2 * (qw * qz + qx * qy), 1 - 2 * (qy ** 2 + qz ** 2))
     return roll, pitch, yaw
+
 
 def main(file_path):
     # Load the data from csv file
@@ -36,7 +38,6 @@ def main(file_path):
     data_xyz_ref = data[
         ['__time', '/hand/mocap/pose/pose/position/x', '/hand/mocap/pose/pose/position/y',
          '/hand/mocap/pose/pose/position/z']]
-
 
     data_xyz = data_xyz.dropna()
     data_xyz_ref = data_xyz_ref.dropna()
@@ -65,8 +66,10 @@ def main(file_path):
     data_euler = pd.DataFrame()
     data_euler['__time'] = data_qwxyz['__time']
     data_euler['roll'], data_euler['pitch'], data_euler['yaw'] = quat2euler(
-        data_qwxyz['/beetle1/uav/cog/odom/pose/pose/orientation/w'], data_qwxyz['/beetle1/uav/cog/odom/pose/pose/orientation/x'],
-        data_qwxyz['/beetle1/uav/cog/odom/pose/pose/orientation/y'], data_qwxyz['/beetle1/uav/cog/odom/pose/pose/orientation/z'])
+        data_qwxyz['/beetle1/uav/cog/odom/pose/pose/orientation/w'],
+        data_qwxyz['/beetle1/uav/cog/odom/pose/pose/orientation/x'],
+        data_qwxyz['/beetle1/uav/cog/odom/pose/pose/orientation/y'],
+        data_qwxyz['/beetle1/uav/cog/odom/pose/pose/orientation/z'])
 
     # ======= Plotting =========
     plt.style.use(["science", "grid"])
@@ -75,10 +78,13 @@ def main(file_path):
     legend_alpha = 0.5  # Set transparency for the legend if needed
 
     # Using 7 subplots: first 3 for position, last 4 for quaternion
-    fig = plt.figure(figsize=(7, 10))
+    fig = plt.figure(figsize=(7, 9))
     t_bias = max(data_xyz['__time'].iloc[0], data_xyz_ref['__time'].iloc[0])
     color_ref = '#0072BD'
     color_real = '#D95319'
+
+    time_start_rotate = 28.6993
+    time_stop_rotate = 35.2177
 
     # --- Subplot 1: X position ---
     plt.subplot(7, 1, 1)
@@ -91,6 +97,9 @@ def main(file_path):
     plt.plot(t, x, label='robot', color=color_real)
     plt.legend(framealpha=legend_alpha)
     plt.ylabel('X (m)', fontsize=label_size)
+    plt.xlim(0, 50)
+    plt.axvspan(time_start_rotate, time_stop_rotate, facecolor="#EDB120", alpha=0.3)
+
     rmse_x = calculate_rmse(t, x, t_ref, x_ref)
     print(f'RMSE X (m): {rmse_x}')
 
@@ -103,8 +112,11 @@ def main(file_path):
     t = np.array(data_xyz['__time']) - t_bias
     y = np.array(data_xyz['/beetle1/uav/cog/odom/pose/pose/position/y'])
     plt.plot(t, y, label='robot', color=color_real)
-    plt.legend(framealpha=legend_alpha)
+    # plt.legend(framealpha=legend_alpha)
     plt.ylabel('Y (m)', fontsize=label_size)
+    plt.xlim(0, 50)
+    plt.axvspan(time_start_rotate, time_stop_rotate, facecolor="#EDB120", alpha=0.3)
+
     rmse_y = calculate_rmse(t, y, t_ref, y_ref)
     print(f'RMSE Y (m): {rmse_y}')
 
@@ -117,67 +129,83 @@ def main(file_path):
     t = np.array(data_xyz['__time']) - t_bias
     z = np.array(data_xyz['/beetle1/uav/cog/odom/pose/pose/position/z'])
     plt.plot(t, z, label='robot', color=color_real)
-    plt.legend(framealpha=legend_alpha)
+    # plt.legend(framealpha=legend_alpha)
     plt.ylabel('Z (m)', fontsize=label_size)
+    plt.xlim(0, 50)
+    plt.axvspan(time_start_rotate, time_stop_rotate, facecolor="#EDB120", alpha=0.3)
+
     rmse_z = calculate_rmse(t, z, t_ref, z_ref)
     print(f'RMSE Z (m): {rmse_z}')
 
     # --- Subplot 4: Quaternion w ---
     plt.subplot(7, 1, 4)
     t_ref = np.array(data_qwxyz_ref['__time']) - t_bias
-    qw_ref = np.array(data_qwxyz_ref['/hand/mocap/pose/pose/orientation/w'])
+    qw_ref = -np.array(data_qwxyz_ref['/hand/mocap/pose/pose/orientation/w'])
     plt.plot(t_ref, qw_ref, label='hand', linestyle="--", color=color_ref)
 
     t = np.array(data_qwxyz['__time']) - t_bias
     qw = np.array(data_qwxyz['/beetle1/uav/cog/odom/pose/pose/orientation/w'])
     plt.plot(t, qw, label='robot', color=color_real)
-    plt.legend(framealpha=legend_alpha)
+    plt.legend(framealpha=legend_alpha, loc='lower right')
     plt.ylabel('qw', fontsize=label_size)
+    plt.xlim(0, 50)
+    plt.axvspan(time_start_rotate, time_stop_rotate, facecolor="#EDB120", alpha=0.3)
+
     rmse_qw = calculate_rmse(t, qw, t_ref, qw_ref)
     print(f'RMSE qw: {rmse_qw}')
 
     # --- Subplot 5: Quaternion x ---
     plt.subplot(7, 1, 5)
     t_ref = np.array(data_qwxyz_ref['__time']) - t_bias
-    qx_ref = np.array(data_qwxyz_ref['/hand/mocap/pose/pose/orientation/x'])
+    qx_ref = -np.array(data_qwxyz_ref['/hand/mocap/pose/pose/orientation/x'])
     plt.plot(t_ref, qx_ref, label='hand', linestyle="--", color=color_ref)
 
     t = np.array(data_qwxyz['__time']) - t_bias
     qx = np.array(data_qwxyz['/beetle1/uav/cog/odom/pose/pose/orientation/x'])
     plt.plot(t, qx, label='robot', color=color_real)
-    plt.legend(framealpha=legend_alpha)
+    # plt.legend(framealpha=legend_alpha)
     plt.ylabel('qx', fontsize=label_size)
+    plt.xlim(0, 50)
+    plt.axvspan(time_start_rotate, time_stop_rotate, facecolor="#EDB120", alpha=0.3)
+
     rmse_qx = calculate_rmse(t, qx, t_ref, qx_ref)
     print(f'RMSE qx: {rmse_qx}')
 
     # --- Subplot 6: Quaternion y ---
     plt.subplot(7, 1, 6)
     t_ref = np.array(data_qwxyz_ref['__time']) - t_bias
-    qy_ref = np.array(data_qwxyz_ref['/hand/mocap/pose/pose/orientation/y'])
+    qy_ref = -np.array(data_qwxyz_ref['/hand/mocap/pose/pose/orientation/y'])
     plt.plot(t_ref, qy_ref, label='hand', linestyle="--", color=color_ref)
 
     t = np.array(data_qwxyz['__time']) - t_bias
     qy = np.array(data_qwxyz['/beetle1/uav/cog/odom/pose/pose/orientation/y'])
     plt.plot(t, qy, label='robot', color=color_real)
-    plt.legend(framealpha=legend_alpha)
+    # plt.legend(framealpha=legend_alpha)
     plt.ylabel('qy', fontsize=label_size)
+    plt.xlim(0, 50)
+    plt.axvspan(time_start_rotate, time_stop_rotate, facecolor="#EDB120", alpha=0.3)
+
     rmse_qy = calculate_rmse(t, qy, t_ref, qy_ref)
     print(f'RMSE qy: {rmse_qy}')
 
     # --- Subplot 7: Quaternion z ---
     plt.subplot(7, 1, 7)
     t_ref = np.array(data_qwxyz_ref['__time']) - t_bias
-    qz_ref = np.array(data_qwxyz_ref['/hand/mocap/pose/pose/orientation/z'])
+    qz_ref = -np.array(data_qwxyz_ref['/hand/mocap/pose/pose/orientation/z'])
     plt.plot(t_ref, qz_ref, label='hand', linestyle="--", color=color_ref)
 
     t = np.array(data_qwxyz['__time']) - t_bias
     qz = np.array(data_qwxyz['/beetle1/uav/cog/odom/pose/pose/orientation/z'])
     plt.plot(t, qz, label='robot', color=color_real)
-    plt.legend(framealpha=legend_alpha)
+    # plt.legend(framealpha=legend_alpha)
     plt.ylabel('qz', fontsize=label_size)
+    plt.xlim(0, 50)
+    plt.axvspan(time_start_rotate, time_stop_rotate, facecolor="#EDB120", alpha=0.3)
+
     rmse_qz = calculate_rmse(t, qz, t_ref, qz_ref)
     print(f'RMSE qz: {rmse_qz}')
 
+    # --- Common settings ---
     plt.xlabel('Time (s)', fontsize=label_size)
 
     plt.tight_layout()
