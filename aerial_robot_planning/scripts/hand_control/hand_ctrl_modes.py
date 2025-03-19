5c9ec972abbad6cdefa724f26e6d4ca4e3b57303
Jason.LI <1592890733@qq.com>
2025-03-19 14:34:33 +0800

    [hand-control] Abstracts all state based on hand control into a HandControlBaseMode, which increases readability.

--- aerial_robot_planning/scripts/hand_control/hand_ctrl_modes.py
+++ aerial_robot_planning/scripts/hand_control/hand_ctrl_modes.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Time : 2024/01/14 12:21
-# @Author : JIAXUAN LI
-# @File : object_position_mapping.py
+# @Time : 2025/3/19 12:22
+# @Author : Jason.LI
+# @File : hand_ctrl_modes.py
 # @Software: PyCharm
 
 import math
@@ -16,24 +16,20 @@ from sub_pos_objects import Hand, Arm, Glove
 
 
 ##########################################
-# Derived Class : MappingMode
+# Derived Class : HandControlBaseMode
 ##########################################
-class MappingMode(MPCPubJointTraj):
-    def __init__(
-            self,
-            robot_name: str,
-            hand: Hand,
-            arm: Arm,
-            control_mode: Glove,
-    ):
-        super().__init__(robot_name=robot_name, node_name="mapping_mode_traj_pub")
+class HandControlBaseMode(MPCPubJointTraj):
+    def __init__(self,
+             robot_name: str,
+             hand: Hand,
+             arm: Arm,
+             control_mode: Glove,
+             node_name: str,
+             ):
+        super().__init__(robot_name=robot_name, node_name=node_name)
         self.hand = hand
         self.arm = arm
         self.control_mode = control_mode
-
-        self.initial_hand_position = None
-        self.initial_drone_position = None
-        self.position_change = None
 
         self.is_finished = False
         # initialize vel_twist and acc_twist
@@ -51,14 +47,13 @@ class MappingMode(MPCPubJointTraj):
         self._check_position_tolerance = 0.1
         self._check_orientation_tolerance = 6
 
-        self._check_gesture_time_threshold = 3
-
+        self.to_return_control_mode = None
         self.last_state = None
 
     def _check_finish_auto(self):
         current_time = rospy.Time.now().to_sec()
         if not hasattr(self, "last_check_time"):
-            self.last_check_time = current_time
+            self._last_check_time = current_time
             self._check_last_position = [
                 self.hand.position.pose.position.x,
                 self.hand.position.pose.position.y,
@@ -85,11 +80,7 @@ class MappingMode(MPCPubJointTraj):
         ]
 
         position_change = [abs(current_check_position[i] - self._check_last_position[i]) for i in range(3)]
-
         orientation_change = [abs(current_check_orientation[i] - self._check_last_orientation[i]) for i in range(4)]
-
-        # self._check_last_position[:] = current_check_position
-        # self._check_check_orientation[:] = current_check_orientation
 
         goal_reached = all(change < self._check_position_tolerance for change in position_change) and all(
             change < self._check_orientation_tolerance for change in orientation_change
@@ -101,12 +92,42 @@ class MappingMode(MPCPubJointTraj):
             self.last_state = new_state
 
         if goal_reached:
-            if current_time - self.last_check_time > self._check_time_threshold:
-                rospy.loginfo("Exit mapping mode")
+            if current_time - self._last_check_time > self._check_time_threshold:
+                rospy.loginfo("Exit control")
                 self.is_finished = True
-
         else:
-            self.last_check_time = current_time
+            self._last_check_time = current_time
+
+    def check_finished(self, t_elapsed=None):
+        if self.control_mode:
+            if self.control_mode.control_mode != self.mode_num:
+                self.to_return_control_mode = self.control_mode.control_mode
+                self.is_finished = True
+        else:
+            self._check_finish_auto()
+
+        return self.is_finished
+
+    def get_control_mode(self):
+        return self.to_return_control_mode
+
+
+##########################################
+# Derived Class : MappingMode
+##########################################
+class MappingMode(HandControlBaseMode):
+    def __init__(
+            self,
+            robot_name: str,
+            hand: Hand,
+            arm: Arm,
+            control_mode: Glove,
+    ):
+        super().__init__(robot_name, hand, arm, control_mode, node_name="mapping_mode_traj_pub")
+
+        self.initial_hand_position = None
+        self.initial_drone_position = None
+        self.position_hand_change = None
 
     def fill_trajectory_points(self, t_elapsed: float) -> MultiDOFJointTrajectory:
 
@@ -167,131 +188,24 @@ class MappingMode(MPCPubJointTraj):
 
         return multi_dof_joint_traj
 
-    def check_finished(self, t_elapsed=None):
-        if self.control_mode:
-            if self.control_mode.control_mode != 1:
-                self.to_return_control_mode = self.control_mode.control_mode
-                self.is_finished = True
-        else:
-            self._check_finish_auto()
-
-        return self.is_finished
-
-    def get_control_mode(self):
-        return self.to_return_control_mode
-
 
 ##########################################
 # Derived Class : SphericalMode
 ##########################################
-class SphericalMode(MPCPubJointTraj):
-    def __init__(
-            self,
-            robot_name: str,
-            hand: Hand,
-            arm: Arm,
-            control_mode: Glove,
-    ):
-        super().__init__(robot_name=robot_name, node_name="spherical_mode_traj_pub")
-        self.hand = hand
-        self.arm = arm
-        self.control_mode = control_mode
-
-        self.is_finished = False
-        # initialize vel_twist and acc_twist
-        r_rate, p_rate, y_rate = 0.0, 0.0, 0.0
-        r_acc, p_acc, y_acc = 0.0, 0.0, 0.0
-        vx, vy, vz = 0.0, 0.0, 0.0
-        ax, ay, az = 0.0, 0.0, 0.0
-        self.vel_twist = Twist(linear=Vector3(vx, vy, vz), angular=Vector3(r_rate, p_rate, y_rate))
-        self.acc_twist = Twist(linear=Vector3(ax, ay, az), angular=Vector3(r_acc, p_acc, y_acc))
-
-        self._check_last_time = None
-        self._check_last_position = None
-        self._check_last_orientation = None
-        self._check_time_threshold = 5
-        self._check_position_tolerance = 0.1
-        self._check_orientation_tolerance = 6
-
-        self.to_return_control_mode = None
-
+class SphericalMode(HandControlBaseMode):
+    def __init__(self, robot_name: str, hand: Hand, arm: Arm, control_mode: Glove):
+        super().__init__(robot_name, hand, arm, control_mode, node_name="spherical_mode_traj_pub")
         self.expected_a_d_distance = 2.2
 
-        self.last_state = None
-
-    def _check_finish_auto(self):
-        current_time = rospy.Time.now().to_sec()
-        if not hasattr(self, "last_check_time"):
-            self.last_check_time = current_time
-            self._check_last_position = [
-                self.hand.position.pose.position.x,
-                self.hand.position.pose.position.y,
-                self.hand.position.pose.position.z,
-            ]
-            self._check_last_orientation = [
-                self.hand.position.pose.orientation.x,
-                self.hand.position.pose.orientation.y,
-                self.hand.position.pose.orientation.z,
-                self.hand.position.pose.orientation.w,
-            ]
-            return
-
-        current_check_position = [
-            self.hand.position.pose.position.x,
-            self.hand.position.pose.position.y,
-            self.hand.position.pose.position.z,
-        ]
-        current_check_orientation = [
-            self.hand.position.pose.orientation.x,
-            self.hand.position.pose.orientation.y,
-            self.hand.position.pose.orientation.z,
-            self.hand.position.pose.orientation.w,
-        ]
-
-        position_change = [abs(current_check_position[i] - self._check_last_position[i]) for i in range(3)]
-
-        orientation_change = [abs(current_check_orientation[i] - self._check_last_orientation[i]) for i in range(4)]
-
-        # self._check_last_position[:] = current_check_position
-        # self._check_check_orientation[:] = current_check_orientation
-
-        goal_reached = all(change < self._check_position_tolerance for change in position_change) and all(
-            change < self._check_orientation_tolerance for change in orientation_change
-        )
-
-        new_state = "goal_reached" if goal_reached else "goal_not_reached"
-        if new_state != self.last_state:
-            rospy.loginfo("Now state: reach the goal" if goal_reached else "Now state:not reach the goal")
-            self.last_state = new_state
-
-        if goal_reached:
-            if current_time - self.last_check_time > self._check_time_threshold:
-                rospy.loginfo("Exit mapping mode")
-                self.is_finished = True
-
-        else:
-            self.last_check_time = current_time
+        self.mode_num = 3
 
     def fill_trajectory_points(self, t_elapsed: float) -> MultiDOFJointTrajectory:
-
-        # arm to hand
         a_h_direction = [
             self.hand.position.pose.position.x - self.arm.position.pose.position.x,
             self.hand.position.pose.position.y - self.arm.position.pose.position.y,
         ]
-
         a_h_distance = math.hypot(a_h_direction[0], a_h_direction[1])
-
         a_h_unit_vector = [a_h_direction[0] / a_h_distance, a_h_direction[1] / a_h_distance]
-
-        # hand to drone
-        # h_d_direction = [
-        #     self.uav_odom.pose.pose.position.x - self.hand.position.pose.position.x,
-        #     self.uav_odom.pose.pose.position.y - self.hand.position.pose.position.y,
-        #     self.uav_odom.pose.pose.position.z - self.hand.position.pose.position.z,
-        # ]
-
-        # h_d_distance = math.sqrt(h_d_direction[0] ** 2 + h_d_direction[1] ** 2 + h_d_direction[2] ** 2)
 
         hand_orientation = [
             self.hand.position.pose.orientation.x,
@@ -337,110 +251,20 @@ class SphericalMode(MPCPubJointTraj):
 
         return multi_dof_joint_traj
 
-    def check_finished(self, t_elapsed=None):
-        if self.control_mode:
-            if self.control_mode.control_mode != 3:
-                self.to_return_control_mode = self.control_mode.control_mode
-                self.is_finished = True
-        else:
-            self._check_finish_auto()
-
-        return self.is_finished
-
-    def get_control_mode(self):
-        return self.to_return_control_mode
-
 
 ##########################################
 # Derived Class : CartesianMode
 ##########################################
-class CartesianMode(MPCPubJointTraj):
+class CartesianMode(HandControlBaseMode):
     def __init__(self, robot_name: str, hand: Hand, arm: Arm, control_mode: Glove):
-        super().__init__(robot_name=robot_name, node_name="cartesian_mode_traj_pub")
-        self.hand = hand
-        self.arm = arm
-        self.control_mode = control_mode
-
-        self.is_finished = False
-        # initialize vel_twist and acc_twist
-        r_rate, p_rate, y_rate = 0.0, 0.0, 0.0
-        r_acc, p_acc, y_acc = 0.0, 0.0, 0.0
-        vx, vy, vz = 0.0, 0.0, 0.0
-        ax, ay, az = 0.0, 0.0, 0.0
-        self.vel_twist = Twist(linear=Vector3(vx, vy, vz), angular=Vector3(r_rate, p_rate, y_rate))
-        self.acc_twist = Twist(linear=Vector3(ax, ay, az), angular=Vector3(r_acc, p_acc, y_acc))
-
-        self._check_last_time = None
-        self._check_last_position = None
-        self._check_last_orientation = None
-        self._check_time_threshold = 5
-        self._check_position_tolerance = 0.1
-        self._check_orientation_tolerance = 6
-
-        self.to_return_control_mode = None
-
+        super().__init__(robot_name, hand, arm, control_mode, node_name="cartesian_mode_traj_pub")
         self.expected_d_target_distance = 0.0
         self.last_target_position = None
-
-        self.last_state = None
-
         self.origin_position = None
 
-    def _check_finish_auto(self):
-        current_time = rospy.Time.now().to_sec()
-        if not hasattr(self, "last_check_time"):
-            self.last_check_time = current_time
-            self._check_last_position = [
-                self.hand.position.pose.position.x,
-                self.hand.position.pose.position.y,
-                self.hand.position.pose.position.z,
-            ]
-            self._check_last_orientation = [
-                self.hand.position.pose.orientation.x,
-                self.hand.position.pose.orientation.y,
-                self.hand.position.pose.orientation.z,
-                self.hand.position.pose.orientation.w,
-            ]
-            return
-
-        current_check_position = [
-            self.hand.position.pose.position.x,
-            self.hand.position.pose.position.y,
-            self.hand.position.pose.position.z,
-        ]
-        current_check_orientation = [
-            self.hand.position.pose.orientation.x,
-            self.hand.position.pose.orientation.y,
-            self.hand.position.pose.orientation.z,
-            self.hand.position.pose.orientation.w,
-        ]
-
-        position_change = [abs(current_check_position[i] - self._check_last_position[i]) for i in range(3)]
-
-        orientation_change = [abs(current_check_orientation[i] - self._check_last_orientation[i]) for i in range(4)]
-
-        # self._check_last_position[:] = current_check_position
-        # self._check_check_orientation[:] = current_check_orientation
-
-        goal_reached = all(change < self._check_position_tolerance for change in position_change) and all(
-            change < self._check_orientation_tolerance for change in orientation_change
-        )
-
-        new_state = "goal_reached" if goal_reached else "goal_not_reached"
-        if new_state != self.last_state:
-            rospy.loginfo("Now state: reach the goal" if goal_reached else "Now state:not reach the goal")
-            self.last_state = new_state
-
-        if goal_reached:
-            if current_time - self.last_check_time > self._check_time_threshold:
-                rospy.loginfo("Exit mapping mode")
-                self.is_finished = True
-
-        else:
-            self.last_check_time = current_time
+        self.mode_num = 2
 
     def fill_trajectory_points(self, t_elapsed: float) -> MultiDOFJointTrajectory:
-
         self.origin_position = [
             self.arm.position.pose.position.x + 0.3,
             self.arm.position.pose.position.y,
@@ -458,14 +282,11 @@ class CartesianMode(MPCPubJointTraj):
             self.hand.position.pose.orientation.w,
         ]
 
-        # arm to hand
         o_h_direction = [
             current_hand_position[0] - self.origin_position[0],
             current_hand_position[1] - self.origin_position[1],
         ]
-
         o_h_distance = math.hypot(o_h_direction[0], o_h_direction[1])
-
         o_h_unit_vector = [o_h_direction[0] / o_h_distance, o_h_direction[1] / o_h_distance]
 
         if o_h_distance < 0.15:
@@ -512,114 +333,17 @@ class CartesianMode(MPCPubJointTraj):
 
         return multi_dof_joint_traj
 
-    def check_finished(self, t_elapsed=None):
-        if self.control_mode:
-            if self.control_mode.control_mode != 2:
-                self.to_return_control_mode = self.control_mode.control_mode
-                self.is_finished = True
-        else:
-            self._check_finish_auto()
-
-        return self.is_finished
-
-    def get_control_mode(self):
-        return self.to_return_control_mode
-
 
 ##########################################
 # Derived Class : LockMode
 ##########################################
-class LockMode(MPCPubJointTraj):
-    def __init__(
-            self,
-            robot_name: str,
-            hand: Hand,
-            arm: Arm,
-            control_mode: Glove,
-    ):
-        super().__init__(robot_name=robot_name, node_name="lock_mode_traj_pub")
-        self.hand = hand
-        self.arm = arm
-        self.control_mode = control_mode
-
-        self.is_finished = False
-        # initialize vel_twist and acc_twist
-        r_rate, p_rate, y_rate = 0.0, 0.0, 0.0
-        r_acc, p_acc, y_acc = 0.0, 0.0, 0.0
-        vx, vy, vz = 0.0, 0.0, 0.0
-        ax, ay, az = 0.0, 0.0, 0.0
-        self.vel_twist = Twist(linear=Vector3(vx, vy, vz), angular=Vector3(r_rate, p_rate, y_rate))
-        self.acc_twist = Twist(linear=Vector3(ax, ay, az), angular=Vector3(r_acc, p_acc, y_acc))
-
-        self._check_last_time = None
-        self._check_last_position = None
-        self._check_last_orientation = None
-        self._check_time_threshold = 5
-        self._check_position_tolerance = 0.1
-        self._check_orientation_tolerance = 6
-
-        self.to_return_control_mode = None
-
-        self.expected_d_target_distance = 0.0
-        self.last_target_position = None
-
-        self.last_state = None
-
+class LockMode(HandControlBaseMode):
+    def __init__(self, robot_name: str, hand: Hand, arm: Arm, control_mode: Glove):
+        super().__init__(robot_name, hand, arm, control_mode, node_name="lock_mode_traj_pub")
         self._init_origin_drone_position = None
         self._init_origin_hand_orientation = None
 
-    def _check_finish_auto(self):
-        current_time = rospy.Time.now().to_sec()
-        if not hasattr(self, "last_check_time"):
-            self.last_check_time = current_time
-            self._check_last_position = [
-                self.hand.position.pose.position.x,
-                self.hand.position.pose.position.y,
-                self.hand.position.pose.position.z,
-            ]
-            self._check_last_orientation = [
-                self.hand.position.pose.orientation.x,
-                self.hand.position.pose.orientation.y,
-                self.hand.position.pose.orientation.z,
-                self.hand.position.pose.orientation.w,
-            ]
-            return
-
-        current_check_position = [
-            self.hand.position.pose.position.x,
-            self.hand.position.pose.position.y,
-            self.hand.position.pose.position.z,
-        ]
-        current_check_orientation = [
-            self.hand.position.pose.orientation.x,
-            self.hand.position.pose.orientation.y,
-            self.hand.position.pose.orientation.z,
-            self.hand.position.pose.orientation.w,
-        ]
-
-        position_change = [abs(current_check_position[i] - self._check_last_position[i]) for i in range(3)]
-
-        orientation_change = [abs(current_check_orientation[i] - self._check_last_orientation[i]) for i in range(4)]
-
-        # self._check_last_position[:] = current_check_position
-        # self._check_check_orientation[:] = current_check_orientation
-
-        goal_reached = all(change < self._check_position_tolerance for change in position_change) and all(
-            change < self._check_orientation_tolerance for change in orientation_change
-        )
-
-        new_state = "goal_reached" if goal_reached else "goal_not_reached"
-        if new_state != self.last_state:
-            rospy.loginfo("Now state: reach the goal" if goal_reached else "Now state:not reach the goal")
-            self.last_state = new_state
-
-        if goal_reached:
-            if current_time - self.last_check_time > self._check_time_threshold:
-                rospy.loginfo("Exit mapping mode")
-                self.is_finished = True
-
-        else:
-            self.last_check_time = current_time
+        self.mode_num = 4
 
     def fill_trajectory_points(self, t_elapsed: float) -> MultiDOFJointTrajectory:
         if self._init_origin_drone_position is None:
@@ -656,16 +380,3 @@ class LockMode(MPCPubJointTraj):
             multi_dof_joint_traj.points.append(traj_pt)
 
         return multi_dof_joint_traj
-
-    def check_finished(self, t_elapsed=None):
-        if self.control_mode:
-            if self.control_mode.control_mode != 4:
-                self.to_return_control_mode = self.control_mode.control_mode
-                self.is_finished = True
-        else:
-            self._check_finish_auto()
-
-        return self.is_finished
-
-    def get_control_mode(self):
-        return self.to_return_control_mode
