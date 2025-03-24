8517c73010ca5e171d6b5e40927b1211966d92c2
Jinjie Li <45286479+Li-Jinjie@users.noreply.github.com>
2025-03-24 11:17:22 +0900

    [nmpc][py]: make simulating a function; add an option to save the execution data for testing.

--- aerial_robot_control/scripts/nmpc/tilt_qd/sim_nmpc_tilt_qd.py
+++ aerial_robot_control/scripts/nmpc/tilt_qd/sim_nmpc_tilt_qd.py
@@ -3,9 +3,9 @@
 """
 import copy
 import time
-
 import numpy as np
 import argparse
+import os
 
 from nmpc_viz import Visualizer
 
@@ -29,62 +29,42 @@ from arxiv_tilt_qd_servo_thrust_drag import NMPCTiltQdServoThrustDrag
 # only consider the thrust delay
 from tilt_qd_thrust import NMPCTiltQdThrust
 
-if __name__ == "__main__":
-    # read arguments
-    parser = argparse.ArgumentParser(description="Run the simulation of different NMPC models.")
-    parser.add_argument(
-        "model",
-        type=int,
-        help="The NMPC model to be simulated. Options: 0 (no_servo_delay), 1 (old servo cost), 2 (full).",
-    )
-    parser.add_argument(
-        "-sim",
-        "--sim_model",
-        type=int,
-        default=0,
-        help="The simulation model. " "Options: 0 (default: NMPCTiltQdServoThrust), 1 (NMPCTiltQdServoThrustDrag).",
-    )
-    parser.add_argument("-p", "--plot_type", type=int, default=0, help="The type of plot. Options: 0 (full), 1, 2.")
 
-    args = parser.parse_args()
-
+def simulate(model, sim_model, plot_type, no_viz, save_data, file_path):
     # ========== init ==========
     # ---------- Controller ----------
-    if args.model == 0:
+    if model == 0:
         nmpc = NMPCTiltQdNoServo()
-    elif args.model == 1:
+    elif model == 1:
         nmpc = NMPCTiltQdServo()
-    elif args.model == 2:
+    elif model == 2:
         nmpc = NMPCTiltQdThrust()
-    elif args.model == 3:
+    elif model == 3:
         nmpc = NMPCTiltQdServoThrust()
-
-    elif args.model == 21:
+    elif model == 21:
         nmpc = NMPCTiltQdServoDist()
-    elif args.model == 22:
+    elif model == 22:
         nmpc = NMPCTiltQdServoThrustDist()
 
     # archiving methods
-    elif args.model == 91:
+    elif model == 91:
         nmpc = NMPCTiltQdNoServoNewCost()
-    elif args.model == 92:
+    elif model == 92:
         nmpc = NMPCTiltQdServoOldCost()
-    elif args.model == 93:
+    elif model == 93:
         nmpc = NMPCTiltQdServoVelInput()
         alpha_integ = np.zeros(4)
-    elif args.model == 94:
+    elif model == 94:
         nmpc = NMPCTiltQdServoDragDist()
     else:
-        raise ValueError(f"Invalid control model {args.model}.")
+        raise ValueError(f"Invalid control model {model}.")
 
-    # check if there is t_servo in the controller
     if hasattr(nmpc, "t_servo"):
         t_servo_ctrl = nmpc.t_servo
     else:
         t_servo_ctrl = 0.0
     ts_ctrl = nmpc.ts_ctrl
 
-    # ocp solver
     ocp_solver = nmpc.get_ocp_solver()
     nx = ocp_solver.acados_ocp.dims.nx
     nu = ocp_solver.acados_ocp.dims.nu
@@ -100,12 +80,12 @@ if __name__ == "__main__":
         ocp_solver.set(stage, "u", u_init)
 
     # ---------- Simulator ----------
-    if args.sim_model == 0:
-        sim_nmpc = NMPCTiltQdServoThrust()  # consider both the servo delay and the control delay
-    elif args.sim_model == 1:
+    if sim_model == 0:
+        sim_nmpc = NMPCTiltQdServoThrust()  # consider the dynamics of all actuators, including servo and thrust
+    elif sim_model == 1:
         sim_nmpc = NMPCTiltQdServoThrustDrag()
     else:
-        raise ValueError(f"Invalid sim model {args.sim_model}.")
+        raise ValueError(f"Invalid sim model {sim_model}.")
 
     if hasattr(sim_nmpc, "t_servo"):
         t_servo_sim = sim_nmpc.t_servo
@@ -115,9 +95,9 @@ if __name__ == "__main__":
     ts_sim = 0.005
 
     t_total_sim = 15.0
-    if args.plot_type == 1:
+    if plot_type == 1:
         t_total_sim = 4.0
-    if args.plot_type == 2:
+    if plot_type == 2:
         t_total_sim = 3.0
 
     N_sim = int(t_total_sim / ts_sim)
@@ -132,7 +112,14 @@ if __name__ == "__main__":
 
     # ---------- Others ----------
     xr_ur_converter = nmpc.get_xr_ur_converter()
-    viz = Visualizer(N_sim, nx_sim, nu, x_init_sim)
+
+    # Create visualizer only if visualization is not disabled
+    if not no_viz:
+        viz = Visualizer(N_sim, nx_sim, nu, x_init_sim)
+
+    # Prepare containers to record simulation data (x and u) for future comparison
+    x_history = []
+    u_history = []
 
     is_sqp_change = False
     t_sqp_start = 2.5
@@ -150,9 +137,7 @@ if __name__ == "__main__":
         # --------- update state estimation ---------
         if isinstance(nmpc, NMPCTiltQdServoDist) or isinstance(nmpc, NMPCTiltQdServoThrustDist):
             x_now = np.zeros(nx)
-            x_now[: nx - 6] = copy.deepcopy(
-                x_now_sim[: nx - 6]
-            )  # only remove the last 6 elements, which are the disturbances
+            x_now[: nx - 6] = copy.deepcopy(x_now_sim[: nx - 6]) # only remove the last 6 elements (disturbances)
         else:
             x_now = copy.deepcopy(x_now_sim[:nx])  # the dimension of x_now may be smaller than x_now_sim
 
@@ -163,19 +148,17 @@ if __name__ == "__main__":
         target_xyz = np.array([[0.3, 0.6, 1.0]]).T
         target_rpy = np.array([[0.0, 0.0, 0.0]]).T
 
-        if args.plot_type == 2:
+        if plot_type == 2:
             target_xyz = np.array([[0.0, 0.0, 0.0]]).T
             target_rpy = np.array([[0.5, 0.5, 0.5]]).T
 
         if t_total_sim > 2.0:
             if 2.0 <= t_now < 6:
                 target_xyz = np.array([[0.3, 0.6, 1.0]]).T
-
                 roll = 30.0 / 180.0 * np.pi
                 pitch = 60.0 / 180.0 * np.pi
                 yaw = 90.0 / 180.0 * np.pi
                 target_rpy = np.array([[roll, pitch, yaw]]).T
-
             if t_now >= 6:
                 assert t_sqp_end <= 3.0
                 target_xyz = np.array([[1.0, 1.0, 1.0]]).T
@@ -183,16 +166,15 @@ if __name__ == "__main__":
 
         xr, ur = xr_ur_converter.pose_point_2_xr_ur(target_xyz, target_rpy)
 
-        if args.plot_type == 2:
+        if plot_type == 2:
             if nx > 13:
                 xr[:, 13:] = 0.0
             ur[:, 4:] = 0.0
 
-        # -------- sqp mode --------
+        # -------- SQP mode --------
         if is_sqp_change and t_sqp_start > t_sqp_end:
             if t_now >= t_sqp_start:
                 ocp_solver.solver_options["nlp_solver_type"] = "SQP"
-
             if t_now >= t_sqp_end:
                 ocp_solver.solver_options["nlp_solver_type"] = "SQP_RTI"
 
@@ -201,7 +183,6 @@ if __name__ == "__main__":
 
         if t_ctl >= ts_ctrl:
             t_ctl = 0.0
-
             # 0 ~ N-1
             for j in range(ocp_solver.N):
                 yr = np.concatenate((xr[j, :], ur[j, :]))
@@ -210,7 +191,6 @@ if __name__ == "__main__":
                 params = np.zeros(n_param)
                 params[0:4] = quaternion_r
                 ocp_solver.set(j, "p", params)  # for nonlinear quaternion error
-
             # N
             yr = xr[ocp_solver.N, :]
             ocp_solver.set(ocp_solver.N, "yref", yr)  # final state of x, no u
@@ -227,15 +207,14 @@ if __name__ == "__main__":
                 break
 
         comp_time_end = time.time()
-        viz.comp_time[i] = comp_time_end - comp_time_start
+        if not no_viz:
+            viz.comp_time[i] = comp_time_end - comp_time_start
 
-        # if nmpc is NMPCOverActNoServoNewCost
         if type(nmpc) is NMPCTiltQdNoServoNewCost:
             xr_ur_converter.update_a_prev(u_cmd.item(4), u_cmd.item(5), u_cmd.item(6), u_cmd.item(7))
-
         if type(nmpc) is NMPCTiltQdServoVelInput:
             alpha_integ += u_cmd[4:] * ts_ctrl
-            u_cmd[4:] = alpha_integ  # convert from delta to absolute
+            u_cmd[4:] = alpha_integ  # convert from delta input to real input
 
         # --------- update simulation ----------
         sim_solver.set("x", x_now_sim)
@@ -247,37 +226,80 @@ if __name__ == "__main__":
 
         x_now_sim = sim_solver.get("x")
 
-        # --------- update visualizer ----------
-        viz.update(i, x_now_sim, u_cmd)  # note that the recording frequency of u_cmd is the same as ts_sim
+        # Save current simulation data for later comparison
+        x_history.append(x_now_sim.copy())
+        u_history.append(u_cmd.copy())
+
+        # Update visualization only if enabled
+        if not no_viz:
+            viz.update(i, x_now_sim, u_cmd)
 
     # ========== visualize ==========
-    if args.plot_type == 0:
-        viz.visualize(
-            ocp_solver.acados_ocp.model.name,
-            sim_solver.model_name,
-            ts_ctrl,
-            ts_sim,
-            t_total_sim,
-            t_servo_ctrl=t_servo_ctrl,
-            t_servo_sim=t_servo_sim,
-        )
-    elif args.plot_type == 1:
-        viz.visualize_less(
-            ocp_solver.acados_ocp.model.name,
-            sim_solver.model_name,
-            ts_ctrl,
-            ts_sim,
-            t_total_sim,
-            t_servo_ctrl=t_servo_ctrl,
-            t_servo_sim=t_servo_sim,
-        )
-    elif args.plot_type == 2:
-        viz.visualize_rpy(
-            ocp_solver.acados_ocp.model.name,
-            sim_solver.model_name,
-            ts_ctrl,
-            ts_sim,
-            t_total_sim,
-            t_servo_ctrl=t_servo_ctrl,
-            t_servo_sim=t_servo_sim,
-        )
+    if not no_viz:
+        if plot_type == 0:
+            viz.visualize(
+                ocp_solver.acados_ocp.model.name,
+                sim_solver.model_name,
+                ts_ctrl,
+                ts_sim,
+                t_total_sim,
+                t_servo_ctrl=t_servo_ctrl,
+                t_servo_sim=t_servo_sim,
+            )
+        elif plot_type == 1:
+            viz.visualize_less(
+                ocp_solver.acados_ocp.model.name,
+                sim_solver.model_name,
+                ts_ctrl,
+                ts_sim,
+                t_total_sim,
+                t_servo_ctrl=t_servo_ctrl,
+                t_servo_sim=t_servo_sim,
+            )
+        elif plot_type == 2:
+            viz.visualize_rpy(
+                ocp_solver.acados_ocp.model.name,
+                sim_solver.model_name,
+                ts_ctrl,
+                ts_sim,
+                t_total_sim,
+                t_servo_ctrl=t_servo_ctrl,
+                t_servo_sim=t_servo_sim,
+            )
+    else:
+        print("Visualization has been disabled by the user.")
+
+    # Save the simulation data if requested
+    if save_data:
+        # os.makedirs(file_path, exist_ok=True)
+        data_file = os.path.join(file_path, f"nmpc_{type(nmpc).__name__}_model_{type(sim_nmpc).__name__}.npz")
+        np.savez(data_file, x=np.array(x_history), u=np.array(u_history))
+        print(f"Simulation data saved to {data_file}")
+
+
+if __name__ == "__main__":
+    # read arguments
+    parser = argparse.ArgumentParser(description="Run the simulation of different NMPC models.")
+    parser.add_argument(
+        "model",
+        type=int,
+        help="The NMPC model to be simulated. Options: 0 (no_servo_delay), 1 (old servo cost), 2 (full).",
+    )
+    parser.add_argument(
+        "-sim",
+        "--sim_model",
+        type=int,
+        default=0,
+        help="The simulation model. Options: 0 (default: NMPCTiltQdServoThrust), 1 (NMPCTiltQdServoThrustDrag).",
+    )
+    parser.add_argument("-p", "--plot_type", type=int, default=0, help="The type of plot. Options: 0 (full), 1, 2.")
+
+    # New option 1: disable visualization
+    parser.add_argument("--no_viz", action="store_true", help="Disable visualization after simulation")
+    # New option 2: save simulation data (x and u) to file
+    parser.add_argument("--save_data", action="store_true", help="Save simulation x and u data to file")
+    parser.add_argument("--file_path", type=str, default="../../../../test/tilt_qd/", help="Path to save the data file")
+
+    args = parser.parse_args()
+
+    simulate(args.model, args.sim_model, args.plot_type, args.no_viz, args.save_data, args.file_path)
