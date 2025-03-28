3265fb896ed4ffe22c29a1d82ec3940fc06f5f13
LI, Jinjie <45286479+Li-Jinjie@users.noreply.github.com>
2025-03-28 18:32:53 +0900

    [nmpc][phys] add physical_param_list to summarize all physical parameters, which can be used to initialize the model

--- aerial_robot_control/scripts/nmpc/fix_qd/phys_param_mini_qd.py
+++ aerial_robot_control/scripts/nmpc/fix_qd/phys_param_mini_qd.py
@@ -28,3 +28,12 @@ p2_b = physical_params["p2"]
 p3_b = physical_params["p3"]
 p4_b = physical_params["p4"]
 kq_d_kt = physical_params["kq_d_kt"]
+
+# concatenate the parameters to make a new list
+physical_param_list = [
+    mass, gravity, Ixx, Iyy, Izz,
+    kq_d_kt,
+    dr1, dr2, dr3, dr4,
+    p1_b[0], p1_b[1], p1_b[2], p2_b[0], p2_b[1], p2_b[2], p3_b[0], p3_b[1], p3_b[2], p4_b[0], p4_b[1], p4_b[2],
+    0.0, 0.0,
+]
