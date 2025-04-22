f2d28f61cd2fcf6d115cf24d8f2a4dce42a48870
sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2025-04-22 12:57:57 +0900

    [aerial_robot_dynamics] implement contraction of tensor and vector

--- aerial_robot_dynamics/include/aerial_robot_dynamics/math_utils.h
+++ aerial_robot_dynamics/include/aerial_robot_dynamics/math_utils.h
@@ -1,10 +1,11 @@
 #pragma once
 
 #include <Eigen/Dense>
+#include <vector>
 
 namespace aerial_robot_dynamics
 {
-    void addNoise(Eigen::VectorXd& vec, double sigma)
+    inline void addNoise(Eigen::VectorXd& vec, double sigma)
     {
         std::default_random_engine generator(std::random_device{}());
         std::normal_distribution<double> distribution(0.0, sigma);
@@ -12,6 +13,25 @@ namespace aerial_robot_dynamics
         {
             vec(i) += distribution(generator);
         }
+    }
+
+    inline Eigen::MatrixXd tensorContraction(const std::vector<Eigen::MatrixXd> & tensor1,
+                                      const Eigen::VectorXd & vector1)
+    {
+        for(int i = 0; i < tensor1.size(); i++)
+        {
+            if(tensor1.at(i).cols() != vector1.size())
+            {
+                throw std::invalid_argument("Tensor and vector dimensions do not match.");
+            }
+        }
+
+        Eigen::MatrixXd ret = Eigen::MatrixXd::Zero(tensor1.at(0).rows(), tensor1.size());
+        for(int i = 0; i < tensor1.size(); i++)
+        {
+            ret.col(i) = tensor1.at(i) * vector1;
+        }
+        return ret;
     }
 }
 
