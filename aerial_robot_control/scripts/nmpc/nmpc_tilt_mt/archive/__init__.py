bbf1d43cdadfe4bae9e13c402c07efb8e75f54b7
LI, Jinjie <45286479+Li-Jinjie@users.noreply.github.com>
2025-06-11 21:40:48 +0900

    [nmpc][py]: use one python script to run all models, and use one bash script to generate all nmpc code, which is executed in CMakeLists.txt

---
+++ aerial_robot_control/scripts/nmpc/nmpc_tilt_mt/archive/__init__.py
@@ -0,0 +1,13 @@
+from .tilt_qd_no_servo_ac_cost import NMPCTiltQdNoServoAcCost
+from .tilt_qd_servo_drag_w_dist import NMPCTiltQdServoDragDist
+from .tilt_qd_servo_old_cost import NMPCTiltQdServoOldCost
+from .tilt_qd_servo_thrust_drag import NMPCTiltQdServoThrustDrag
+from .tilt_qd_servo_w_cog_end_dist import NMPCTiltQdServoWCogEndDist
+
+__all__ = [
+    "NMPCTiltQdNoServoAcCost",
+    "NMPCTiltQdServoDragDist",
+    "NMPCTiltQdServoOldCost",
+    "NMPCTiltQdServoThrustDrag",
+    "NMPCTiltQdServoWCogEndDist"
+]
---
+++ aerial_robot_control/scripts/nmpc/nmpc_tilt_mt/fix_qd/__init__.py
@@ -0,0 +1,6 @@
+from .fix_qd_angvel_out import NMPCFixQdAngvelOut
+from .fix_qd_thrust_out import NMPCFixQdThrustOut
+
+__all__ = [
+    "NMPCFixQdAngvelOut", "NMPCFixQdThrustOut"
+]
