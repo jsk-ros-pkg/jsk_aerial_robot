485387e3616da2c752fe350216268e964aa6eef6
johanneskbl <johannes.kubel@gmail.com>
2025-06-15 22:42:23 +0900

    [rtnmpc] Extended vanilla network creation, training and testing framework based on torch

--- aerial_robot_control/scripts/neural_nmpc/model_fitting/train.py
+++ aerial_robot_control/scripts/neural_nmpc/model_fitting/train.py
@@ -2,7 +2,6 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader
 
-# Define model
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
@@ -25,15 +24,15 @@ def get_dataloaders(training_data, test_data):
     """
     Load training and test data into DataLoader objects.
     """
-    batch_size = 32
+    batch_size = 1
 
-    train_dataloader = DataLoader(training_data, batch_size=batch_size)
-    test_dataloader = DataLoader(test_data, batch_size=batch_size)
+    train_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=True)
+    test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
 
-    for X, y in test_dataloader:
-        print(f"Shape of X [N, C, H, W]: {X.shape}")
-        print(f"Shape of y: {y.shape} {y.dtype}")
-        break
+    # for X, y in test_dataloader:
+    #     print(f"Shape of X [N, C, H, W]: {X.shape}")
+    #     print(f"Shape of y: {y.shape} {y.dtype}")
+    #     break
     return train_dataloader, test_dataloader
 
 def get_device():
@@ -56,7 +55,7 @@ def get_loss_function():
 def get_optimizer(model, learning_rate=1e-3):
     return torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-def train(dataloader, model, loss_fn, optimizer):
+def train(dataloader, model, loss_fn, optimizer, device):
     size = len(dataloader.dataset)
     model.train()
     for batch_idx, (X, y) in enumerate(dataloader):
@@ -76,20 +75,51 @@ def train(dataloader, model, loss_fn, optimizer):
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
 
+def test(dataloader, model, loss_fn, device):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+
 if __name__ == "__main__":
-    training_data = []
-    test_data = []
+    training_data = [[1,2], [4,5]]
+    test_data = [[7], [10]]
 
     train_dataloader, test_dataloader = get_dataloaders(training_data, test_data)
     
     device = get_device()
     model = NeuralNetwork().to(device)
 
-    loss_fnc = get_loss_function()
+    loss_fn = get_loss_function()
     
     optimizer = get_optimizer(model)
-    
-    train(train_dataloader, model, loss_fnc, optimizer)
-    
+
+    train(train_dataloader, model, loss_fn, optimizer, device)
+
     print(model)
+
+    epochs = 5
+    for t in range(epochs):
+        print(f"Epoch {t+1}\n-------------------------------")
+        train(train_dataloader, model, loss_fn, optimizer, device)
+        test(test_dataloader, model, loss_fn, device)
+    print("Done!")
+
+    # === Save the model ===
+    torch.save(model.state_dict(), "model.pth")
+    print("Saved PyTorch Model State to model.pth")
+
+    # === Load the model ===
+    model = NeuralNetwork().to(device)
+    model.load_state_dict(torch.load("model.pth", weights_only=True))
 
