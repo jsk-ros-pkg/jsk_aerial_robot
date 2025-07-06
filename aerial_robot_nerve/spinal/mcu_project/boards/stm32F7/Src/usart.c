605a94c36e1a8d6d64085606206ba64f98cff697
J Sugihara <56066868+sugihara-16@users.noreply.github.com>
2025-07-06 11:09:05 +0900

    [Spinal][Servo][WIP] Integrated interface to communicate with multiple types of servo (e.g. Dynamixel, Kondo). (#687)
    * [spinal][kondo] add kondo servo class in jsk_lib
    
    * [spinal][kondo] changed pin assignment to use kondo servo and make kondo servo task using RTOS
    
    * [spinal][kondo] changed gpu flag for kondo servo using usart3
    
    * [Spinal][KondoServo] add converter from Kondoposition to rad and the reverse.
    
    * [Spinal][KondoServo] add new accessor to change the servo target_postions.
    
    * [Spinal][KondoServo][WIP] supressed uart read fanction and modified freertos priority for smooth servo control.
    
    * [Kondo] revert servo callback
    
    * [Spinal][Kondo] add servo state publisher.
    
    * [WIP][Kondo] create kondo servo task in STM32H7
    
    * [Spinal][Kondo] add kondo servo task in stmh7
    
    * [Spinal][UART] Enabling DMA for UART3 RX and securing SRAM region for it with MPU.
    
    * [Spinal][MPU] Assigned part of D1 SRAM region for Kondo servo's dma
    
    * [Spinal][Kondo] change the structure of the interface from normal peripheral use to DMA.
    
    * [Spinal][Kondo][WIP] Enable Kondo servo interface. Test successed.
    
    * [Spinal][Servo][WIP] Create base device driver for servos (not working).
    
    * [Spinal][Servo] Debug around base device driver. It works successfully.
    
    * [Spinal][Servo][Base driver] delted unsed part.
    
    * [Spinal][Kondo] Move KONDO driver file to driver/ directory.
    
    * [Spinal][Kondo] Workaround to apply base device driver to Kondo servo.
    
    * [Spinal][Kondo] Make poling process more efficient (otherwise bordconfig cannot be used).
    
    * [Spinal][Kondo] Impliment torque enable interface.
    
    * [Spinal][Servo] Create new fucntion to set torque in presetn position.
    
    * [Spinal][Kondo] Change board rate from 115200 to 1250000bps.
    
    * [Spinal][Kondo] Workaround to implement ping function for kondo servo.
    
    * [Spinal][Servo] Set dynamixel's pin config as a default config for UART2.
    
    * [Spinal][Servo] Workaround to enable dynamic pin reconfiguration to communicate with multipe types of servo (e.g. dynamixel, kondo).
    
    * [Spinal][Servo] Set dynamixel driver as default one to avoid build error when no servo is used.
    
    * [Spinal][H7] Return the default setting of UART3 in H7 board to support GPS.
    
    * [Spinal][Servo] Change error check method to avoid build error in F7.
    
    * [Spinal][F7] Return the default settings of MCU as master branch.
    
    * [Spinal][Dynamixle]Enable boardless control for dynamixel servo
    
    * [Spinal][Dynamixle] Fix the error to accomodate original best technology board for dynamixel servo XL330-M077-T
    
    * [Spinal][Dynamixle] Fix a spelling error happened in branch migration
    
    * [Spinal][Servo] Setting UART pin to pull up mode to stabilize half-duplex mode.
    
    * [Spinal][Dynamixel] Waiting to switch tx/rx until uart trasmission has finished.
    
    * [Spinal][H7][config] Change default setting of Spinal H7 for activating direct servo features.
    
    ---------
    
    Co-authored-by: sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
    Co-authored-by: fjiwrogno <1948015867@qq.com>

--- aerial_robot_nerve/spinal/mcu_project/boards/stm32F7/Src/usart.c
+++ aerial_robot_nerve/spinal/mcu_project/boards/stm32F7/Src/usart.c
@@ -259,7 +259,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle)
     GPIO_InitStruct.Pin = GPIO_PIN_8|GPIO_PIN_9;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
     GPIO_InitStruct.Alternate = GPIO_AF7_USART3;
     HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
@@ -273,7 +273,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle)
     hdma_usart3_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
     hdma_usart3_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
     hdma_usart3_rx.Init.Mode = DMA_CIRCULAR;
-    hdma_usart3_rx.Init.Priority = DMA_PRIORITY_HIGH;
+    hdma_usart3_rx.Init.Priority = DMA_PRIORITY_VERY_HIGH;
     hdma_usart3_rx.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
     if (HAL_DMA_Init(&hdma_usart3_rx) != HAL_OK)
     {
