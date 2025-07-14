b12bb8d761e24c8542448d9b9d5617954bd80cf5
ZICEN XIONG <zicen-xiong@dragon.t.u-tokyo.ac.jp>
2025-07-14 11:21:53 +0900

    [robot/amoeba]add amoeba

---
+++ robots/amoeba/README.md
@@ -0,0 +1,198 @@
+# Beetle-Art-Omni
+
+## Installation
+
+### 1. Install `acados`
+
+The version of `acados` should be aligned with the version of `3rdparty/acados/CMakeLists.txt` -> `GIT_TAG`.
+
+Specifically, clone the branch for **v0.5.0** from the `acados` git:
+```bash
+git clone https://github.com/acados/acados.git --branch v0.5.0
+```
+Then, follow the instructions below:
+- Install acados itself: Please follow the instructions on the acados website https://docs.acados.org/installation/index.html
+- Install Python interface: Please follow the instructions on the acados website https://docs.acados.org/python_interface/index.html, but don't create virtual env in step 2. The virtual env has compatibility problem with ROS env.
+- **Pay attention** that you must execute the step 5 in https://docs.acados.org/python_interface/index.html to test the installation. This step should automatically install t_renderer. If something wrong, please follow step 6 to manually install t_renderer.
+
+### 2. Install the code base and the necessary ROS related packages ...
+
+Setup the folder architecture and clone the repo **with the specific branch**:
+
+```bash
+mkdir -p ~/[path_to_ws]/src
+cd ~/[path_to_ws]/src
+git clone https://github.com/Li-Jinjie/jsk_aerial_robot_dev.git -b develop/MPC_tilt_mt    # pay attention to the branch flag
+```
+
+### 2.1 ... for Ubuntu 20.04 and ROS Noetic
+Install ROS Noetic for Ubuntu 20.04 from https://wiki.ros.org/ROS/Installation and source the setup file:
+
+```bash
+source /opt/ros/one/setup.bash
+```
+
+We use rosdep to manage the dependencies. So, if you have never done this in your computer before, do the following:
+
+```bash
+sudo rosdep init
+rosdep update
+```
+
+Then, do the following:
+```bash
+cd ~/[path_to_ws]
+wstool init src
+wstool merge -t src src/jsk_aerial_robot/aerial_robot_noetic.rosinstall
+wstool update -t src    # install unofficial packages
+rosdep install -y -r --from-paths src --ignore-src --rosdistro noetic   # install the dependencies/packages stated in package.xml
+```
+
+### 2.2 ... for Ubuntu 22.04 and ROS-O
+Install ROS-O for ubuntu 22.04 from https://ros.packages.techfak.net/ and source the setup file:
+
+```bash
+./jsk_aerial_robot/configure.sh   # for configuration especially for ROS-O in jammy
+source /opt/ros/one/setup.bash
+```
+
+We use rosdep to manage the dependencies. So, if you have never done this in your computer before, do the following:
+
+```bash
+sudo rosdep init
+rosdep update
+```
+
+Then, do the following:
+
+```bash
+cd ~/[path_to_ws]
+wstool init src
+wstool merge -t src src/jsk_aerial_robot/aerial_robot_${ROS_DISTRO}.rosinstall
+wstool update -t src    # install unofficial packages
+rosdep install -y -r --from-paths src --ignore-src --rosdistro $ROS_DISTRO      # install the dependencies/packages stated in package.xml
+```
+
+For convenience, open `~/.bashrc` and add sourcing of the workspace to the end of the file:
+
+```bash
+In ~/.bashrc:
+
+source ~/[path_to_ws]/devel/setup.bash
+```
+
+### 3. Install python packages and link them to acados
+Install required packages:
+```bash
+pip install -r src/jsk_aerial_robot/aerial_robot_control/scripts/requirements.txt
+```
+
+For the first run, **uncomment** these code in `aerial_robot_control/scripts/nmpc/gen_nmpc_code_all.sh`
+```bash
+MODELS=(
+    NMPCFixQdAngvelOut
+    NMPCFixQdThrustOut
+    NMPCTiltQdNoServo
+    NMPCTiltQdServo
+    NMPCTiltQdServoDist
+    NMPCTiltQdServoImpedance
+    NMPCTiltQdServoThrustDist
+    NMPCTiltQdServoThrustImpedance
+    NMPCTiltTriServo
+    NMPCTiltBiServo
+    NMPCTiltBi2OrdServo
+    MHEWrenchEstAccMom
+)
+```
+
+### 4. Build the workspace with `catkin`
+
+```bash
+cd ~/path_to_ws
+catkin build
+```
+
+A frequent problem is the handling of the jobservers in the build process. When occuring during the build process - especially in the aerial_robot_control package - please try simply running the build command again.
+
+### 5. If the build is successful, comment the code in step 3 back.
+
+## Simulation
+
+### 1. Start the simulation
+Run the simulation with the following command:
+```bash
+roslaunch beetle_omni bringup_nmpc_omni.launch real_machine:=false simulation:=True headless:=False nmpc_mode:=0
+```
+### 2. Start the keyboard script
+Run the keyboard with the following command:
+```bash
+rosrun aerial_robot_base keyboard_command.py
+```
+Then input `r` to arm the motors and input `t` to let the drone take off.
+
+### 3. Send the trajectory
+**After the main window printed 'Hovering'**, please run the following command to send a trajectory:
+```bash
+rosrun aerial_robot_planning mpc_smach_node.py beetle1
+```
+Then choose the trajectory you want the drone to perform.
+
+## Experiments
+
+Most commands are the same with simulation.
+
+For using wrench sensor, call the following command in hovering to calibrate the wrench sensor:
+```bash
+rosservice call /cfs_sensor_calib "{}"
+```
+
+# Flying Hand
+
+We need two notebooks, one as ground station and the other for visual feedback.
+
+**MoCap Computer**
+
+1. Run the software for the data glove and calibrate it. Note that the ip should be set to the ros master computer.
+2. Check the MoCap is set correctly.
+
+If there is something wrong with glove connection, please refer
+to https://stretchsense.my.site.com/defaulthelpcenter26Sep/s/article/Studio-Glove-and-Dongle-Setup?language=en_US
+
+**Onboard Computer**
+
+Four terminals are needed.
+
+Before takeoff, launch necessary files
+
+1. `roslaunch beetle_omni bringup_nmpc_omni.launch nmpc_mode:=0 wrench_est_mode:=none  # This is the main launch file for the robot.`
+2. `roslaunch aerial_robot_planning hand_arm_glove.launch`
+3. `rosbag record -a`
+
+After the robot reach the status of hovering - see terminal output of main launch file -,
+
+4. Run `rosrun aerial_robot_planning mpc_smach_node.py beetle1`, and press 'h' to enter the hand control mode. Note that
+   we need to wear the hand part, the shoulder part, and gloves to control the robot.
+5. To modify the `/hand/control_mode` parameter using `rosparam` in the terminal, run the following command:
+    `rosparam set /hand/control_mode <mode_value>`
+    Where `<mode_value>` can be set as one of the following:
+    - **1**: Operation Mode
+    - **2**: Spherical Coordinate Mode
+    - **3**: Cartesian Coordinate Mode
+    - **4**: Lock Mode
+    - **5**: Exit Mode
+
+    To check the current mode, you can run:`rosparam get /operation_mode`
+    
+**Ground Station**
+Three terminals are needed.
+
+Before takeoff:
+
+1. run `sudo ds4drv` to connect the joystick.
+2. `roslaunch aerial_robot_base joy_stick.launch robot_name:=beetle1`
+3. `rviz -d ~/ros1/jsk_ws/src/jsk_aerial_robot_dev/robots/beetle/config/nmpc.rviz`
+
+**Visual Computer**
+
+1. `rosrun aerial_robot_planning visual_fb_mode.py`
+2. Adjust the window to fullfill the screen.
---
+++ robots/amoeba/scripts/iros25_draw_mapping_mode.py
@@ -0,0 +1,368 @@
+import pandas as pd
+import numpy as np
+import scienceplots
+import matplotlib.pyplot as plt
+import argparse
+
+legend_alpha = 0.5
+
+
+def calculate_rmse(t, x, t_ref, x_ref, is_yaw=False):
+    x_ref_interp = np.interp(t, t_ref, x_ref)
+    if is_yaw:
+        # calculate the RMSE for yaw
+        error = np.minimum(np.abs(x - x_ref_interp), 2 * np.pi - np.abs(x - x_ref_interp))
+    else:
+        error = x - x_ref_interp
+
+    rmse_x = np.sqrt(np.mean(error ** 2))
+    return rmse_x
+
+
+def quat2euler(qw, qx, qy, qz):
+    roll = np.arctan2(2 * (qw * qx + qy * qz), 1 - 2 * (qx ** 2 + qy ** 2))
+    pitch = np.arcsin(2 * (qw * qy - qz * qx))
+    yaw = np.arctan2(2 * (qw * qz + qx * qy), 1 - 2 * (qy ** 2 + qz ** 2))
+    return roll, pitch, yaw
+
+
+matlab_blue = '#0072BD'
+matlab_orange = '#D95319'
+matlab_purple = "#7E2F8E"
+
+def main(file_path, plot_type):
+    # Load the data from csv file
+    data = pd.read_csv(file_path)
+
+    if plot_type == 0:
+        # ======= xyz =========
+        data_xyz = data[
+            ['__time', '/beetle1/uav/cog/odom/pose/pose/position/x', '/beetle1/uav/cog/odom/pose/pose/position/y',
+             '/beetle1/uav/cog/odom/pose/pose/position/z']].dropna()
+
+        data_xyz_ref = data[
+            ['__time', '/hand/mocap/pose/pose/position/x', '/hand/mocap/pose/pose/position/y',
+             '/hand/mocap/pose/pose/position/z']].dropna()
+
+        # ======= rpy =========
+        data_qwxyz = data[
+            ['__time', '/beetle1/uav/cog/odom/pose/pose/orientation/w', '/beetle1/uav/cog/odom/pose/pose/orientation/x',
+             '/beetle1/uav/cog/odom/pose/pose/orientation/y', '/beetle1/uav/cog/odom/pose/pose/orientation/z']].dropna()
+
+        data_qwxyz_ref = data[
+            ['__time', '/hand/mocap/pose/pose/orientation/w', '/hand/mocap/pose/pose/orientation/x',
+             '/hand/mocap/pose/pose/orientation/y', '/hand/mocap/pose/pose/orientation/z']].dropna()
+
+        # convert to euler
+        data_euler_ref = pd.DataFrame()
+        data_euler_ref['__time'] = data_qwxyz_ref['__time']
+        data_euler_ref = pd.DataFrame()
+        data_euler_ref['__time'] = data_qwxyz_ref['__time']
+        data_euler_ref['roll'], data_euler_ref['pitch'], data_euler_ref['yaw'] = quat2euler(
+            data_qwxyz_ref['/hand/mocap/pose/pose/orientation/w'],
+            data_qwxyz_ref['/hand/mocap/pose/pose/orientation/x'],
+            data_qwxyz_ref['/hand/mocap/pose/pose/orientation/y'],
+            data_qwxyz_ref['/hand/mocap/pose/pose/orientation/z'])
+
+        data_euler = pd.DataFrame()
+        data_euler['__time'] = data_qwxyz['__time']
+        data_euler['roll'], data_euler['pitch'], data_euler['yaw'] = quat2euler(
+            data_qwxyz['/beetle1/uav/cog/odom/pose/pose/orientation/w'],
+            data_qwxyz['/beetle1/uav/cog/odom/pose/pose/orientation/x'],
+            data_qwxyz['/beetle1/uav/cog/odom/pose/pose/orientation/y'],
+            data_qwxyz['/beetle1/uav/cog/odom/pose/pose/orientation/z'])
+
+        # ======= 绘图设置 =======
+        plt.style.use(["science", "grid"])
+        plt.rcParams.update({'font.size': 11})
+        label_size = 14
+
+        # 以两个数据中较晚的初始时间作为时间偏置
+        t_bias = max(data_xyz['__time'].iloc[0], data_xyz_ref['__time'].iloc[0])
+        # 定义旋转区间（仅用于图中标注）
+        time_start_rotate = 28.6993
+        time_stop_rotate = 35.2177
+
+        # 创建共享 X 轴的 7 个子图
+        fig, axes = plt.subplots(7, 1, sharex=True, figsize=(7, 6))
+
+        # --- Subplot 1: X position ---
+        ax = axes[0]
+        t_ref = np.array(data_xyz_ref['__time']) - t_bias
+        x_ref = np.array(data_xyz_ref['/hand/mocap/pose/pose/position/x'])
+        t = np.array(data_xyz['__time']) - t_bias
+        x = np.array(data_xyz['/beetle1/uav/cog/odom/pose/pose/position/x'])
+        x_ref_offset = x_ref - x_ref[0] + x[0]
+
+        ax.plot(t_ref, x_ref, label='hand', linestyle="--", color=matlab_blue)
+        ax.plot(t_ref, x_ref_offset, label='hand offset', linestyle="-.", color="k")
+        ax.plot(t, x, label='robot', color=matlab_orange)
+        ax.legend(framealpha=legend_alpha, loc="center right")
+        ax.set_ylabel('X (m)', fontsize=label_size)
+        ax.set_xlim(0, 50)
+        ax.axvspan(time_start_rotate, time_stop_rotate, facecolor="#EDB120", alpha=0.3)
+
+        rmse_x = calculate_rmse(t, x, t_ref, x_ref)
+        print(f'RMSE X (m): {rmse_x}')
+
+        # --- Subplot 2: Y position ---
+        ax = axes[1]
+        t_ref = np.array(data_xyz_ref['__time']) - t_bias
+        y_ref = np.array(data_xyz_ref['/hand/mocap/pose/pose/position/y'])
+        t = np.array(data_xyz['__time']) - t_bias
+        y = np.array(data_xyz['/beetle1/uav/cog/odom/pose/pose/position/y'])
+        y_ref_offset = y_ref - y_ref[0] + y[0]
+
+        ax.plot(t_ref, y_ref, label='hand', linestyle="--", color=matlab_blue)
+        ax.plot(t_ref, y_ref_offset, label='hand offset', linestyle="-.", color="k")
+        ax.plot(t, y, label='robot', color=matlab_orange)
+        ax.set_ylabel('Y (m)', fontsize=label_size)
+        ax.set_xlim(0, 50)
+        ax.axvspan(time_start_rotate, time_stop_rotate, facecolor="#EDB120", alpha=0.3)
+
+        rmse_y = calculate_rmse(t, y, t_ref, y_ref)
+        print(f'RMSE Y (m): {rmse_y}')
+
+        # --- Subplot 3: Z position ---
+        ax = axes[2]
+        t_ref = np.array(data_xyz_ref['__time']) - t_bias
+        z_ref = np.array(data_xyz_ref['/hand/mocap/pose/pose/position/z'])
+        ax.plot(t_ref, z_ref, label='hand', linestyle="--", color=matlab_blue)
+        t = np.array(data_xyz['__time']) - t_bias
+        z = np.array(data_xyz['/beetle1/uav/cog/odom/pose/pose/position/z'])
+        ax.plot(t, z, label='robot', color=matlab_orange)
+        ax.set_ylabel('Z (m)', fontsize=label_size)
+        ax.set_xlim(0, 50)
+        ax.axvspan(time_start_rotate, time_stop_rotate, facecolor="#EDB120", alpha=0.3)
+
+        rmse_z = calculate_rmse(t, z, t_ref, z_ref)
+        print(f'RMSE Z (m): {rmse_z}')
+
+        # --- Subplot 4: Quaternion w ---
+        ax = axes[3]
+        t_ref = np.array(data_qwxyz_ref['__time']) - t_bias
+        qw_ref = -np.array(data_qwxyz_ref['/hand/mocap/pose/pose/orientation/w'])
+        ax.plot(t_ref, qw_ref, label='hand', linestyle="--", color=matlab_blue)
+        t = np.array(data_qwxyz['__time']) - t_bias
+        qw = np.array(data_qwxyz['/beetle1/uav/cog/odom/pose/pose/orientation/w'])
+        ax.plot(t, qw, label='robot', color=matlab_orange)
+        ax.legend(framealpha=legend_alpha, loc='lower right')
+        ax.set_ylabel('qw', fontsize=label_size)
+        ax.set_xlim(0, 50)
+        ax.axvspan(time_start_rotate, time_stop_rotate, facecolor="#EDB120", alpha=0.3)
+
+        rmse_qw = calculate_rmse(t, qw, t_ref, qw_ref)
+        print(f'RMSE qw: {rmse_qw}')
+
+        # --- Subplot 5: Quaternion x ---
+        ax = axes[4]
+        t_ref = np.array(data_qwxyz_ref['__time']) - t_bias
+        qx_ref = -np.array(data_qwxyz_ref['/hand/mocap/pose/pose/orientation/x'])
+        ax.plot(t_ref, qx_ref, label='hand', linestyle="--", color=matlab_blue)
+        t = np.array(data_qwxyz['__time']) - t_bias
+        qx = np.array(data_qwxyz['/beetle1/uav/cog/odom/pose/pose/orientation/x'])
+        ax.plot(t, qx, label='robot', color=matlab_orange)
+        ax.set_ylabel('qx', fontsize=label_size)
+        ax.set_xlim(0, 50)
+        ax.axvspan(time_start_rotate, time_stop_rotate, facecolor="#EDB120", alpha=0.3)
+
+        rmse_qx = calculate_rmse(t, qx, t_ref, qx_ref)
+        print(f'RMSE qx: {rmse_qx}')
+
+        # --- Subplot 6: Quaternion y ---
+        ax = axes[5]
+        t_ref = np.array(data_qwxyz_ref['__time']) - t_bias
+        qy_ref = -np.array(data_qwxyz_ref['/hand/mocap/pose/pose/orientation/y'])
+        ax.plot(t_ref, qy_ref, label='hand', linestyle="--", color=matlab_blue)
+        t = np.array(data_qwxyz['__time']) - t_bias
+        qy = np.array(data_qwxyz['/beetle1/uav/cog/odom/pose/pose/orientation/y'])
+        ax.plot(t, qy, label='robot', color=matlab_orange)
+        ax.set_ylabel('qy', fontsize=label_size)
+        ax.set_xlim(0, 50)
+        ax.axvspan(time_start_rotate, time_stop_rotate, facecolor="#EDB120", alpha=0.3)
+
+        rmse_qy = calculate_rmse(t, qy, t_ref, qy_ref)
+        print(f'RMSE qy: {rmse_qy}')
+
+        # --- Subplot 7: Quaternion z ---
+        ax = axes[6]
+        t_ref = np.array(data_qwxyz_ref['__time']) - t_bias
+        qz_ref = -np.array(data_qwxyz_ref['/hand/mocap/pose/pose/orientation/z'])
+        ax.plot(t_ref, qz_ref, label='hand', linestyle="--", color=matlab_blue)
+        t = np.array(data_qwxyz['__time']) - t_bias
+        qz = np.array(data_qwxyz['/beetle1/uav/cog/odom/pose/pose/orientation/z'])
+        ax.plot(t, qz, label='robot', color=matlab_orange)
+        ax.set_ylabel('qz', fontsize=label_size)
+        ax.set_xlim(0, 50)
+        ax.axvspan(time_start_rotate, time_stop_rotate, facecolor="#EDB120", alpha=0.3)
+
+        rmse_qz = calculate_rmse(t, qz, t_ref, qz_ref)
+        print(f'RMSE qz: {rmse_qz}')
+
+        # --- 隐藏除最下方外所有子图的 X 轴刻度，并设置公共 X 轴标签 ---
+        for ax in axes[:-1]:
+            ax.tick_params(labelbottom=False)
+        axes[-1].set_xlabel('Time (s)', fontsize=label_size)
+
+        plt.tight_layout()
+        fig.subplots_adjust(hspace=0.2)
+        plt.show()
+
+    if plot_type == 1:
+        # ======= 提取位置数据 =======
+        data_xyz = data[
+            ['__time', '/beetle1/uav/cog/odom/pose/pose/position/x',
+             '/beetle1/uav/cog/odom/pose/pose/position/y',
+             '/beetle1/uav/cog/odom/pose/pose/position/z']
+        ].dropna()
+
+        data_xyz_ref = data[
+            ['__time', '/hand/mocap/pose/pose/position/x',
+             '/hand/mocap/pose/pose/position/y',
+             '/hand/mocap/pose/pose/position/z']
+        ].dropna()
+
+        # ======= 提取四元数数据 =======
+        data_qwxyz = data[
+            ['__time', '/beetle1/uav/cog/odom/pose/pose/orientation/w',
+             '/beetle1/uav/cog/odom/pose/pose/orientation/x',
+             '/beetle1/uav/cog/odom/pose/pose/orientation/y',
+             '/beetle1/uav/cog/odom/pose/pose/orientation/z']
+        ].dropna()
+
+        data_qwxyz_ref = data[
+            ['__time', '/hand/mocap/pose/pose/orientation/w',
+             '/hand/mocap/pose/pose/orientation/x',
+             '/hand/mocap/pose/pose/orientation/y',
+             '/hand/mocap/pose/pose/orientation/z']
+        ].dropna()
+
+        # ======= 绘图设置 =======
+        plt.style.use(["science", "grid"])
+        plt.rcParams.update({'font.size': 11})
+        label_size = 14
+
+        # 以两个数据中较晚的初始时间作为时间偏置
+        t_bias = max(data_xyz['__time'].iloc[0], data_xyz_ref['__time'].iloc[0])
+        # 定义旋转区间（仅用于图中标注）
+        time_start_rotate = 28.6993
+        time_stop_rotate = 35.2177
+
+        # 创建共享 X 轴的 7 个子图
+        fig, axes = plt.subplots(7, 1, sharex=True, figsize=(7, 5))
+
+        # --- Subplot 1: X 绝对误差 ---
+        ax = axes[0]
+        t_ref = np.array(data_xyz_ref['__time']) - t_bias
+        x_ref = np.array(data_xyz_ref['/hand/mocap/pose/pose/position/x'])
+        t = np.array(data_xyz['__time']) - t_bias
+        x = np.array(data_xyz['/beetle1/uav/cog/odom/pose/pose/position/x'])
+        # 参考值偏移处理后进行插值
+        x_ref_interp = np.interp(t, t_ref, x_ref - x_ref[0] + x[0])
+        abs_error_x = np.abs(x - x_ref_interp)
+        ax.plot(t, abs_error_x, label='X', color=matlab_blue)
+        ax.set_ylabel('X (m)', fontsize=label_size)
+        ax.set_xlim(0, 50)
+        ax.axvspan(time_start_rotate, time_stop_rotate, facecolor="#EDB120", alpha=0.3)
+        ax.tick_params(labelbottom=False)  # 隐藏 x 轴刻度
+
+        # --- Subplot 2: Y 绝对误差 ---
+        ax = axes[1]
+        t_ref = np.array(data_xyz_ref['__time']) - t_bias
+        y_ref = np.array(data_xyz_ref['/hand/mocap/pose/pose/position/y'])
+        t = np.array(data_xyz['__time']) - t_bias
+        y = np.array(data_xyz['/beetle1/uav/cog/odom/pose/pose/position/y'])
+        y_ref_interp = np.interp(t, t_ref, y_ref - y_ref[0] + y[0])
+        abs_error_y = np.abs(y - y_ref_interp)
+        ax.plot(t, abs_error_y, label='Y', color=matlab_blue)
+        ax.set_ylabel('Y (m)', fontsize=label_size)
+        ax.set_xlim(0, 50)
+        ax.axvspan(time_start_rotate, time_stop_rotate, facecolor="#EDB120", alpha=0.3)
+        ax.tick_params(labelbottom=False)
+
+        # --- Subplot 3: Z 绝对误差 ---
+        ax = axes[2]
+        t_ref = np.array(data_xyz_ref['__time']) - t_bias
+        z_ref = np.array(data_xyz_ref['/hand/mocap/pose/pose/position/z'])
+        t = np.array(data_xyz['__time']) - t_bias
+        z = np.array(data_xyz['/beetle1/uav/cog/odom/pose/pose/position/z'])
+        z_ref_interp = np.interp(t, t_ref, z_ref)
+        abs_error_z = np.abs(z - z_ref_interp)
+        ax.plot(t, abs_error_z, label='Z', color=matlab_blue)
+        ax.set_ylabel('Z (m)', fontsize=label_size)
+        ax.set_xlim(0, 50)
+        ax.axvspan(time_start_rotate, time_stop_rotate, facecolor="#EDB120", alpha=0.3)
+        ax.tick_params(labelbottom=False)
+
+        # --- Subplot 4: Quaternion w 绝对误差 ---
+        ax = axes[3]
+        t_ref = np.array(data_qwxyz_ref['__time']) - t_bias
+        qw_ref = -np.array(data_qwxyz_ref['/hand/mocap/pose/pose/orientation/w'])
+        t = np.array(data_qwxyz['__time']) - t_bias
+        qw = np.array(data_qwxyz['/beetle1/uav/cog/odom/pose/pose/orientation/w'])
+        qw_ref_interp = np.interp(t, t_ref, qw_ref)
+        abs_error_qw = np.abs(qw - qw_ref_interp)
+        ax.plot(t, abs_error_qw, label='qw', color=matlab_blue)
+        ax.set_ylabel('qw', fontsize=label_size)
+        ax.set_xlim(0, 50)
+        ax.axvspan(time_start_rotate, time_stop_rotate, facecolor="#EDB120", alpha=0.3)
+        ax.tick_params(labelbottom=False)
+
+        # --- Subplot 5: Quaternion x 绝对误差 ---
+        ax = axes[4]
+        t_ref = np.array(data_qwxyz_ref['__time']) - t_bias
+        qx_ref = -np.array(data_qwxyz_ref['/hand/mocap/pose/pose/orientation/x'])
+        t = np.array(data_qwxyz['__time']) - t_bias
+        qx = np.array(data_qwxyz['/beetle1/uav/cog/odom/pose/pose/orientation/x'])
+        qx_ref_interp = np.interp(t, t_ref, qx_ref)
+        abs_error_qx = np.abs(qx - qx_ref_interp)
+        ax.plot(t, abs_error_qx, label='qx', color=matlab_blue)
+        ax.set_ylabel('qx', fontsize=label_size)
+        ax.set_xlim(0, 50)
+        ax.axvspan(time_start_rotate, time_stop_rotate, facecolor="#EDB120", alpha=0.3)
+        ax.tick_params(labelbottom=False)
+
+        # --- Subplot 6: Quaternion y 绝对误差 ---
+        ax = axes[5]
+        t_ref = np.array(data_qwxyz_ref['__time']) - t_bias
+        qy_ref = -np.array(data_qwxyz_ref['/hand/mocap/pose/pose/orientation/y'])
+        t = np.array(data_qwxyz['__time']) - t_bias
+        qy = np.array(data_qwxyz['/beetle1/uav/cog/odom/pose/pose/orientation/y'])
+        qy_ref_interp = np.interp(t, t_ref, qy_ref)
+        abs_error_qy = np.abs(qy - qy_ref_interp)
+        ax.plot(t, abs_error_qy, label='qy', color=matlab_blue)
+        ax.set_ylabel('qy', fontsize=label_size)
+        ax.set_xlim(0, 50)
+        ax.axvspan(time_start_rotate, time_stop_rotate, facecolor="#EDB120", alpha=0.3)
+        ax.tick_params(labelbottom=False)
+
+        # --- Subplot 7: Quaternion z 绝对误差 ---
+        ax = axes[6]
+        t_ref = np.array(data_qwxyz_ref['__time']) - t_bias
+        qz_ref = -np.array(data_qwxyz_ref['/hand/mocap/pose/pose/orientation/z'])
+        t = np.array(data_qwxyz['__time']) - t_bias
+        qz = np.array(data_qwxyz['/beetle1/uav/cog/odom/pose/pose/orientation/z'])
+        qz_ref_interp = np.interp(t, t_ref, qz_ref)
+        abs_error_qz = np.abs(qz - qz_ref_interp)
+        ax.plot(t, abs_error_qz, label='qz', color=matlab_blue)
+        ax.set_ylabel('qz', fontsize=label_size)
+        ax.set_xlim(0, 50)
+        ax.axvspan(time_start_rotate, time_stop_rotate, facecolor="#EDB120", alpha=0.3)
+        # 最底部子图显示横轴刻度
+        ax.tick_params(labelbottom=True)
+        ax.set_xlabel('Time (s)', fontsize=label_size)
+
+        plt.tight_layout()
+        fig.subplots_adjust(hspace=0.2)
+        plt.show()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Plot the trajectory. Please use plotjuggler to generate the csv file.')
+    parser.add_argument('file_path', type=str, help='The file name of the trajectory')
+    parser.add_argument('--type', type=int, default=0, help='The type of plotting')
+
+    args = parser.parse_args()
+
+    main(args.file_path, args.type)
