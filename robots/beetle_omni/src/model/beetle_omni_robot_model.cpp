f9c7eaf1c0da1b0b54195b42970ad5c4df0d2c19
Jinjie Li <45286479+Li-Jinjie@users.noreply.github.com>
2025-07-16 23:55:30 +0900

    [beetle-omni]: further polish code and remove unused parts.

--- robots/beetle_omni/src/model/beetle_omni_robot_model.cpp
+++ robots/beetle_omni/src/model/beetle_omni_robot_model.cpp
@@ -3,21 +3,17 @@
 BeetleOmniRobotModel::BeetleOmniRobotModel(bool init_with_rosparam, bool verbose, double fc_t_min_thre, double epsilon)
   : RobotModel(init_with_rosparam, verbose, fc_t_min_thre, epsilon)
 {
-  const int rotor_num = getRotorNum();
-  const int joint_num = getJointNum();
 }
 
 void BeetleOmniRobotModel::updateRobotModelImpl(const KDL::JntArray& joint_positions)
 {
-  KDL::TreeFkSolverPos_recursive fk_solver(getTree());
-  // /* special process */
-  KDL::Frame f_baselink;
-  fk_solver.JntToCart(joint_positions, f_baselink, getBaselinkName());
-  const KDL::Rotation cog_frame = f_baselink.M * getCogDesireOrientation<KDL::Rotation>().Inverse();
   RobotModel::updateRobotModelImpl(joint_positions);
-  const auto seg_tf_map = getSegmentsTf();
 
-  updateCoGtoEEContact();  // Note: this function should by manually called.
+  const auto seg_tf_map = getSegmentsTf();
+  if (seg_tf_map.find("ee_contact") != seg_tf_map.end())
+  {
+    updateCoGtoEEContact();
+  }
 }
 
 /* plugin registration */
