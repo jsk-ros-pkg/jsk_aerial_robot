dc70c07c82a8ad2ac0829cf3d12d3f9ca73a92d6
ZICEN XIONG <zicen-xiong@dragon.t.u-tokyo.ac.jp>
2025-08-05 11:15:19 +0900

    [spinal] change USaART3 DMA priority to LOW

--- aerial_robot_nerve/spinal/mcu_project/boards/stm32H7/Src/stm32h7xx_hal_msp.c
+++ aerial_robot_nerve/spinal/mcu_project/boards/stm32H7/Src/stm32h7xx_hal_msp.c
@@ -785,7 +785,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
     hdma_usart3_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
     hdma_usart3_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
     hdma_usart3_rx.Init.Mode = DMA_CIRCULAR;
-    hdma_usart3_rx.Init.Priority = DMA_PRIORITY_HIGH;
+    hdma_usart3_rx.Init.Priority = DMA_PRIORITY_LOW;
     hdma_usart3_rx.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
     if (HAL_DMA_Init(&hdma_usart3_rx) != HAL_OK)
     {
