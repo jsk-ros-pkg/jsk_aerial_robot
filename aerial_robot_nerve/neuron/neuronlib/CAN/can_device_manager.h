746a742167926ccd83abb937ce153f8a9eb7eb75
趙　漠居(Zhao, Moju) <chou@dragon.t.u-tokyo.ac.jp>
2025-09-06 19:14:37 +0900

    [Neuron][STM32G4][WIP] develop new neuron board with core of STM32G4 (#540)
    * [Neuron][STM32G4] add initial cubdeide project with c mode, succeed to perform led toggle
    
    * [Neuron] add a shell script for start cudeide
    
    * [Neuron][STM32G4] convert to c++ mode
    
    * [Neuron][STM32G4] update .gitignore
    
    * [Neuron][STM32G4] convert to RTOS mode, add led tollge test with RTOS task
    
    * [Neuron][STM32G4][IMU] modify setting of SPI GPIO for IMU
    
    * [Neuron][STM32G4][IMU][WIP] enable the data polling from IMU
    
    * [Neuron][FlashMemory] support STM32G4 based board
    
    * [Neuron][STM32G4][FlashMemory] enable the usage of flashmemory in neuron with stm32g4
    
    * [Neuron][STM32G4][FlashMemory] enable to use whole 512KB flash memory by modifying the option byte of DBANK
    
    * [Neuron][STM32G4][UART][DMA] modify setting of UART for the usage of servo with DMA
    
    * [Neuron][STM32G4][UART][DMA][Servo][WIP] enable the usage of Dynamixel Servo
    
    * [Neuron][STM32G4][I2C] modify the setting of I2C for servo external magnetic encoder
    
    * [Neuron][STM32G4][RTOS] change the TimeBase Source from TIM15 to TIM8 for PWM
    
    * [Neuron][STM32G4][Servo] add a delay to wait dynamixel servos initialized
    
    * [Neuron][STM32G4][PWM] modify the setting of PWM for Motor, and also abolish the wrong constants of PWM max duty
    
    * [Neuron][STM32G4][PWM][WIP] enable the usage of PWM
    
    * [Neuron][CAN] refactor the library for CAN to support STM32G4
    
    * [Neuron][STM32G4][FDCAN] modify the setting of FDCAN, and also enable the usage of classic model of FDCAN (i.e., general CAN mode)
    
    * [Neuron][STM32G4][PWM] fix the setting for PWM mode
    
    * Revert "[Neuron][STM32G4][IMU][WIP] enable the data polling from IMU"
    
    This reverts commit 8bcd2b88f758b20699f938ac36427b22ac8d37d8.
    
    * Revert "[Neuron][STM32G4][UART][DMA][Servo][WIP] enable the usage of Dynamixel Servo"
    
    This reverts commit 029616d750fd0bae8df4ffcb0eb2224844112eab.
    
    * Revert "[Neuron][STM32G4][PWM][WIP] enable the usage of PWM"
    
    This reverts commit 5c7cf52b285b9b9460ec36888a44bc7b3a8b53b0.
    
    * [Neuron][STM32G4] enable all functions, and confirm the performance with Spinal (STM32F7) and servo command
    
    * [Neuron] refactor the include path
    
    * [Neuron] correct the initialization order and also remove unnecessary variable
    
    * [Neuron][STM32G4] fix bug: assign slave_id for IMU instance
    
    ---------
    
    Co-authored-by: Moju Zhao <tongtybj@gmail.com>

--- aerial_robot_nerve/neuron/neuronlib/CAN/can_device_manager.h
+++ aerial_robot_nerve/neuron/neuronlib/CAN/can_device_manager.h
@@ -11,26 +11,37 @@
 #include "can_device.h"
 #include "cmsis_os.h"
 
+#if defined(STM32F1) || defined(STM32F4)
 typedef struct
 {
   CAN_RxHeaderTypeDef rx_header;
   uint8_t rx_data[8];
 } can_msg;
+typedef CAN_HandleTypeDef CAN_GeranlHandleTypeDef;
+#endif
+
+#ifdef STM32G4
+typedef struct
+{
+  FDCAN_RxHeaderTypeDef rx_header;
+  uint8_t rx_data[64];
+} can_msg;
+typedef FDCAN_HandleTypeDef CAN_GeranlHandleTypeDef;
+#endif
+
 
 namespace CANDeviceManager
 {
-	void init(CAN_HandleTypeDef* hcan, uint8_t slave_id, GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
-	void addDevice(CANDevice* device);
-	void sendMessages();
-	void useRTOS(osMailQId* handle);
-	void tick(int cycle /* ms */);
-	void resetTick();
-	bool isTimeout();
-	void userSendMessages();
-	void userReceiveMessagesCallback(uint8_t slave_id, uint8_t device_id, uint8_t message_id, uint32_t DLC, uint8_t* data);
-	void CAN_START();
-  	void CAN_ACTIVATE();
-  	void CAN_DEACTIVATE();
+  void init(CAN_GeranlHandleTypeDef* hcan, uint8_t slave_id, GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
+  void addDevice(CANDevice* device);
+  void sendMessages();
+  void useRTOS(osMailQId* handle);
+  void tick(int cycle /* ms */);
+  void resetTick();
+  bool isTimeout();
+  void userSendMessages();
+  void userReceiveMessagesCallback(uint8_t slave_id, uint8_t device_id, uint8_t message_id, uint32_t DLC, uint8_t* data);
+  void CAN_START();
 }
 
 #endif /* APPLICATION_CAN_CAN_DEVICE_MANAGER_H_ */
