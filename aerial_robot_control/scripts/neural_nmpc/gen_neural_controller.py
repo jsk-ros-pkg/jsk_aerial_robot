f9bb86728e311f36ddf8701b625d96557ff0d349
johanneskbl <johannes.kubel@gmail.com>
2025-09-10 16:09:50 +0900

    Automatically generate all necessary neural controllers.

--- aerial_robot_control/scripts/neural_nmpc/gen_neural_controller.py
+++ aerial_robot_control/scripts/neural_nmpc/gen_neural_controller.py
@@ -1,13 +1,41 @@
 from config.configurations import EnvConfig
 from neural_controller import NeuralNMPC
 
+print("Generating nominal controller...")
+model_options = EnvConfig.model_options
+model_options["only_use_nominal"] = True
 rtnmpc = NeuralNMPC(
-    model_options=EnvConfig.model_options,
+    model_options=model_options,
     solver_options=EnvConfig.solver_options,
     sim_options=EnvConfig.sim_options,
     run_options=EnvConfig.run_options,
 )
+print("Successfully generated nominal controller!")
 
-# TODO generate nominal and neural controllers by setting model options here
+
+print("Generating neural plus controller...")
+model_options["only_use_nominal"] = False
+model_options["plus_neural"] = True
+model_options["minus_neural"] = False
+rtnmpc = NeuralNMPC(
+    model_options=model_options,
+    solver_options=EnvConfig.solver_options,
+    sim_options=EnvConfig.sim_options,
+    run_options=EnvConfig.run_options,
+)
+print("Successfully generated neural plus controller!")
+
+print("Generating neural minus controller...")
+model_options["only_use_nominal"] = False
+model_options["plus_neural"] = False
+model_options["minus_neural"] = True
+rtnmpc = NeuralNMPC(
+    model_options=model_options,
+    solver_options=EnvConfig.solver_options,
+    sim_options=EnvConfig.sim_options,
+    run_options=EnvConfig.run_options,
+)
+print("Successfully generated neural minus controller!")
+
 # TODO display log
 # TODO Add this script into CMakeLists.txt to run it automatically when building the package
