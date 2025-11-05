8c4429f86be72439bc7b1411068d7e440fc2c8f4
sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2025-11-05 15:37:56 +0900

    [delta] add rosbag record script

---
+++ robots/delta/bin/rosbag_record.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ $# -ne 1 ];then
+    echo "Usage: rosrun delta rosbag_record.sh file_name_prefix";
+    exit 1
+fi
+
+rosbag record -a -x ".*/cloud_registered.*|.*/compressedDepth.*|.*/theora.*|.*/image" -o $1
