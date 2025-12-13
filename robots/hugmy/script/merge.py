614785a8051a6c75fca921588a008279313200a2
heissereal <miyamichi-ayano061@g.ecc.u-tokyo.ac.jp>
2025-12-13 20:21:16 +0900

    change the topic

--- robots/hugmy/script/merge.py
+++ robots/hugmy/script/merge.py
@@ -9,15 +9,20 @@ pub = rospy.Publisher('human', RectArray, queue_size = 4)
 
 def callback(rects,classes):
     msg = RectArray()
-    if "person" in classes.label_names:
+    # if "person" in classes.label_names:
+    if "human" in classes.label_names:
         for i, label_name in enumerate(classes.label_names):
-            if label_name == "person":
+            # if label_name == "person":
+            if label_name == "human":
                 msg.rects.append(rects.rects[i])
     msg.header = rects.header
     pub.publish(msg)
 
-Rect_sub = message_filters.Subscriber('/edgetpu_object_detector/output/rects', RectArray)
-Class_sub = message_filters.Subscriber('/edgetpu_object_detector/output/class', ClassificationResult)
+# Rect_sub = message_filters.Subscriber('/edgetpu_object_detector/output/rects', RectArray)
+# Class_sub = message_filters.Subscriber('/edgetpu_object_detector/output/class', ClassificationResult)
+
+Rect_sub = message_filters.Subscriber('/edgetpu_human_pose_estimator/output/rects', RectArray)
+Class_sub = message_filters.Subscriber('/edgetpu_human_pose_estimator/output/class', ClassificationResult)
 
 ts = message_filters.TimeSynchronizer([Rect_sub, Class_sub], 10)
 ts.registerCallback(callback)
